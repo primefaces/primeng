d5429ea31bae264b6a9f487ca16433b9b2f09280
Çağatay Çivici <cagatay.civici@github.com>
2016-01-17 15:17:37 +0200

    Use @HostListener

--- app/components/inputtext/inputtext.directive.ts
+++ app/components/inputtext/inputtext.directive.ts
@@ -1,18 +1,12 @@
-import {Directive, ElementRef, Renderer, Input} from 'angular2/core';
+import {Directive, ElementRef, Renderer, Input, HostListener} from 'angular2/core';
 import {UIComponent} from '../../core/uicomponent';
 
 @Directive({
-    selector: '[pInputText]',
-    host: {
-        '(mouseover)': 'onMouseOver()',
-        '(mouseout)': 'onMouseOut()',
-        '(focus)': 'onFocus()',
-        '(blur)': 'onBlur()'
-    }
+    selector: '[pInputText]'
 })
 export class InputTextDirective extends UIComponent {
 
-    constructor(private el: ElementRef) {
+    constructor(private el: ElementRef, private renderer: Renderer) {
         super();
         this.addClass(this.el.nativeElement, 'pui-inputtext');
         this.addClass(this.el.nativeElement, 'ui-widget');
@@ -20,18 +14,22 @@ export class InputTextDirective extends UIComponent {
         this.addClass(this.el.nativeElement, 'ui-corner-all');
     }
 
+    @HostListener('mouseover')
     onMouseOver() {
         this.addClass(this.el.nativeElement, 'ui-state-hover');
     }
 
+    @HostListener('mouseout')
     onMouseOut() {
         this.removeClass(this.el.nativeElement, 'ui-state-hover');
     }
 
+    @HostListener('focus')
     onFocus() {
         this.addClass(this.el.nativeElement, 'ui-state-focus');
     }
 
+    @HostListener('blur')
     onBlur() {
         this.removeClass(this.el.nativeElement, 'ui-state-focus');
     }
