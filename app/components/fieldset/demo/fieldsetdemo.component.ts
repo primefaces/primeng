8062016d643a6521b333831b81d5552f0a64b60b
Mrtcndkn <mrtcndkn@gmail.com>
2016-01-19 11:48:18 +0200

    Fieldset Demo toggleable option

--- app/components/fieldset/demo/fieldsetdemo.component.ts
+++ app/components/fieldset/demo/fieldsetdemo.component.ts
@@ -10,9 +10,18 @@ import {FieldsetComponent} from '../fieldset.component';
             </div>
         </div>
 
-        <div style="margin-bottom:20px" class="ContentSideSections Implementation">
-            <p-fieldset toggleable>
+        <div class="ContentSideSections Implementation">
+            <p-fieldset id="default">
                 <legend>Godfather</legend>
+                The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. 
+                His beloved son Michael has just come home from the war, but does not intend to become part of his father's business. 
+                Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, 
+                kind and benevolent to those who give respect, 
+                but given to ruthless violence whenever anything stands against the good of the family.
+            </p-fieldset>
+            <br>
+            <p-fieldset id="toggle" toggleable="true">
+                <legend>Toggleable</legend>
                 The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. 
                 His beloved son Michael has just come home from the war, but does not intend to become part of his father's business. 
                 Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, 
