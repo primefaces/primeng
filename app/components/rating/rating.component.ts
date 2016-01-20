d718211c5c81973206d33b58267e54fa6c686c17
Mrtcndkn <mrtcndkn@gmail.com>
2016-01-20 17:32:16 +0200

    Cosmetics

--- app/components/rating/rating.component.ts
+++ app/components/rating/rating.component.ts
@@ -6,7 +6,7 @@ import {Component, ElementRef, OnInit, OnDestroy, OnChanges, SimpleChange, Input
     selector: 'p-rating',
     template: '<input type="hidden" />'
 })
-export class RatingComponent implements OnInit, OnDestroy {
+export class RatingComponent implements OnInit, OnDestroy, OnChanges {
 
     @Input() value: number;
 
