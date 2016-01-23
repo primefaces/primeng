06e36c50b0feccb51a4f9bdb1b97994cfb598497
Çağatay Çivici <cagatay.civici@github.com>
2016-01-23 11:30:36 +0200

    Demo update

--- app/components/radiobutton/demo/radiobuttondemo.component.ts
+++ app/components/radiobutton/demo/radiobuttondemo.component.ts
@@ -23,31 +23,54 @@ import {GridDemoComponent} from '../../grid/demo/griddemo.component';
             <h3 class="first">Basic</h3>
             <div class="pui-grid pui-grid-responsive" style="width:250px">
                 <div class="pui-grid-row">
-                    <div class="pui-grid-col-1"><p-radio name="test" value="Option 1" [(model)]="selectedOption"></p-radio></div>
-                    <div class="pui-grid-col-11"><label for="rd1_1" class="ui-widget">Option 1</label></div>
+                    <div class="pui-grid-col-1"><p-radio name="group1" value="Option 1" [(model)]="val1"></p-radio></div>
+                    <div class="pui-grid-col-11"><label class="ui-widget">Option 1</label></div>
                 </div>
                 <div class="pui-grid-row">
-                    <div class="pui-grid-col-1"><p-radio name="test" value="Option 2" [(model)]="selectedOption"></p-radio></div>
-                    <div class="pui-grid-col-11"><label for="rd1_2" class="ui-widget">Option 2</label></div>
+                    <div class="pui-grid-col-1"><p-radio name="group1" value="Option 2" [(model)]="val1"></p-radio></div>
+                    <div class="pui-grid-col-11"><label class="ui-widget">Option 2</label></div>
                 </div>
                 <div class="pui-grid-row">
-                    <div class="pui-grid-col-1"><p-radio name="test" value="Option 3" [(model)]="selectedOption"></p-radio></div>
-                    <div class="pui-grid-col-11"><label for="rd1_3" class="ui-widget">Option 3</label></div>
+                    <div class="pui-grid-col-1"><p-radio name="group1" value="Option 3" [(model)]="val1"></p-radio></div>
+                    <div class="pui-grid-col-11"><label class="ui-widget">Option 3</label></div>
                 </div>
-                <br/>
-                Selected Option = {{selectedOption}}
             </div>
+            Value 1 = {{val1}}
+
+            <h3>Preselection</h3>
+            <div class="pui-grid pui-grid-responsive" style="width:250px">
+                <div class="pui-grid-row">
+                    <div class="pui-grid-col-1"><p-radio name="group1" value="Option 1" [(model)]="val2"></p-radio></div>
+                    <div class="pui-grid-col-11"><label class="ui-widget">Option 1</label></div>
+                </div>
+                <div class="pui-grid-row">
+                    <div class="pui-grid-col-1"><p-radio name="group2" value="Option 2" [(model)]="val2"></p-radio></div>
+                    <div class="pui-grid-col-11"><label class="ui-widget">Option 2</label></div>
+                </div>
+                <div class="pui-grid-row">
+                    <div class="pui-grid-col-1"><p-radio name="group2" value="Option 3" [(model)]="val2"></p-radio></div>
+                    <div class="pui-grid-col-11"><label class="ui-widget">Option 3</label></div>
+                </div>
+            </div>
+            Value 2 = {{val2}}
         </div>
     `,
     styles: [`
+        .pui-grid .pui-grid-col-1,
+        .pui-grid .pui-grid-col-11 {
+            padding: 4px 10px;
+        }
+
         .pui-grid label {
             display: inline-block;
-            margin: 3px 0;
+            margin: 3px 0px 0px 4px;
         }
     `],
     directives: [RadioButtonComponent, ButtonDirective]
 })
 export class RadioButtonDemoComponent {
 
-    selectedOption: string;
+    val1: string;
+
+    val2: string = 'Option 2';
 }
