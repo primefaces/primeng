a0f5cd992ec298c1e3ece041560be04fd487e2a9
Çağatay Çivici <cagatay.civici@github.com>
2016-01-25 17:10:06 +0200

    Docs for galleria

--- app/components/galleria/demo/galleriademo.component.ts
+++ app/components/galleria/demo/galleriademo.component.ts
@@ -1,26 +1,12 @@
 import {Component} from 'angular2/core';
 import {GalleriaComponent} from '../galleria.component';
+import {TabViewComponent} from '../../tabview/tabview.component';
+import {TabPanelComponent} from '../../tabview/tabpanel.component';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
-    template: `
-        <div class="ContentSideSections">
-            <div class="Content100 overHidden TextShadow">
-                <span class="fontSize30 TextShadow orange mediumFont marginBottom20 dispBlock">Galleria</span>
-                <span class="defaultText dispTable">Galleria is a content gallery component.</span>
-            </div>
-        </div>
-
-        <div class="ContentSideSections Implementation">
-            <p-galleria panelWidth="500" panelHeight="313">
-                <ul>
-                    <li *ngFor="#image of images;#i = index;">
-                        <img src="resources/demo/images/galleria/{{image}}" alt="Description for Image {{i}}" title="Image {{i}}"/>
-                    </li>
-                </ul>
-            </p-galleria>
-        </div>
-    `,
-    directives: [GalleriaComponent]
+    templateUrl: '/app/components/galleria/demo/galleriademo.component.html',
+    directives: [GalleriaComponent,TabViewComponent,TabPanelComponent,ROUTER_DIRECTIVES]
 })
 export class GalleriaDemoComponent {
 
