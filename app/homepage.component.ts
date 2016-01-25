744583652d48df63df4dd74f1390f5ede89a2368
Çağatay Çivici <cagatay.civici@github.com>
2016-01-25 23:19:35 +0200

    Add twitter link

--- app/homepage.component.ts
+++ app/homepage.component.ts
@@ -9,6 +9,8 @@ import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
                 <span class="defaultText dispTable">PrimeNG is a collection of rich UI components for AngularJS2. PrimeNG is a sibling of the popular JavaServer Faces Component Suite, <a href="http://www.primefaces.org">PrimeFaces</a>.</span>
                 <br />
                 <span class="defaultText dispTable">All widgets are open source and free to use under Apache License 2.0, a commercial friendly license.</span>
+                <br />
+                <span class="defaultText dispTable">For project news and updates, follow us on <a href="https://twitter.com/prime_ng">twitter</a>.</span>
                 <a href="https://github.com/primefaces/primeng/releases" class="BigButton TextShadowNone YellowBtn"> <span class="floatLeft">Download</span> <img src="resources/images/btnArrow.svg" style="float:right"/></a>
                 <a href="https://github.com/primefaces/primeng" class="BigButton TextShadowNone OrangeBtn"> <span class="floatLeft">View on GitHub</span> <img src="resources/images/btnDocumenticon.svg" style="float:right"/></a>
             </div>
@@ -47,8 +49,8 @@ import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
         </div>
 
         <div class="ContentSideSections overHidden PFLayouts">
-            <span class="dispBlock logoBlueText fontSize30 mediumFont">Premium Layouts and Themes</span>
-            <span class="defaultText">Create awesome web applications in no time, impress your users.</span>
+            <span class="dispBlock logoBlueText fontSize30 mediumFont">Premium Layouts and Themes coming soon...</span>
+            <span class="defaultText">Create <a href="http://www.primefaces.org/themes">awesome web applications</a> in no time, impress your users.</span>
             <div style="padding:30px">
                 <div class="Content33 floatLeft overHidden">
                     <a href="http://www.primefaces.org/layouts/modena-primeui"><img src="resources/images/layouts/modena.png" style="width:100%" /></a>
