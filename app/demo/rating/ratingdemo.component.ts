8c7e0ac2d04089eaa535c932ad7c49cb010140b4
Çağatay Çivici <cagatay.civici@github.com>
2016-01-25 23:08:35 +0200

    Docs for rating

--- app/demo/rating/ratingdemo.component.ts
+++ app/demo/rating/ratingdemo.component.ts
@@ -1,38 +1,12 @@
 import {Component} from 'angular2/core';
 import {RatingComponent} from '../../components/rating/rating.component';
+import {TabViewComponent} from '../../components/tabview/tabview.component';
+import {TabPanelComponent} from '../../components/tabview/tabpanel.component';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
-    template: `
-        <div class="ContentSideSections">
-            <div class="Content100 overHidden TextShadow">
-                <span class="fontSize30 TextShadow orange mediumFont marginBottom20 dispBlock">Rating</span>
-                <span class="defaultText dispTable">Rating components is a star based selection input.</span>
-            </div>
-        </div>
-
-        <div class="ContentSideSections Implementation">
-            <h3 class="first">Basic {{value1}}</h3> 
-            <p-rating [(value)]="value1"></p-rating>
-            <br />
-
-            <h3>Callback {{value2}}</h3>
-            <p-rating [(value)]="value2" (onRate)="handleRate($event)" (onCancel)="handleCancel($event)"></p-rating> <span *ngIf="msg" style="margin-left:10px">{{msg}}</span>
-            <br />
-
-            <h3>No Cancel {{value3}}</h3> 
-            <p-rating [(value)]="value3" [cancel]="false"></p-rating>
-            <br />
-
-            <h3>ReadOnly</h3> 
-            <p-rating value="5" readonly="true" stars="10" [cancel]="false"></p-rating>
-            <br />
-
-            <h3>Disabled</h3> 
-            <p-rating [value]="val4" disabled="true" stars="10"></p-rating>
-            <br />
-        </div>
-    `,
-    directives: [RatingComponent]
+    templateUrl: 'app/demo/rating/ratingdemo.component.html',
+    directives: [RatingComponent,TabViewComponent,TabPanelComponent,ROUTER_DIRECTIVES]
 })
 export class RatingDemoComponent {
 
