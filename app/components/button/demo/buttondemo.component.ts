235687f9e035b97ea769d01e7bf372b208ff37b2
Çağatay Çivici <cagatay.civici@github.com>
2016-01-25 14:43:16 +0200

    Docs for button

--- app/components/button/demo/buttondemo.component.ts
+++ app/components/button/demo/buttondemo.component.ts
@@ -1,5 +1,8 @@
 import {Component} from 'angular2/core';
+import {TabViewComponent} from '../../tabview/tabview.component';
+import {TabPanelComponent} from '../../tabview/tabpanel.component';
 import {ButtonDirective} from '../button.directive';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
     template: `
@@ -16,8 +19,126 @@ import {ButtonDirective} from '../button.directive';
             <br /><br />
             Number of clicks: {{clicks}}
         </div>
+
+        <div class="ContentSideSections Source">
+            <p-tabView effect="fade">
+                <p-tabPanel header="Documentation">
+                    <h3>Getting Started</h3>
+                    <p>Button is applied to a button element with pButton directive.</p>
+                    
+<pre>
+&lt;button type="button" pButton&gt;Click&lt;/button/&gt;
+</pre>
+
+                    <h3>Events</h3>
+                    <p>Events are defined using standard notation.</p>
+                    
+<pre>
+&lt;button type="button" pButton (click)="onclick()"&gt;Click&lt;/button/&gt;
+</pre>
+
+<pre>
+export class Model {
+
+    onclick() {
+
+    }
+
+}
+</pre>
+
+                    <h3>Icons</h3>
+                    <p>Icon on a button is defined with icon attribute and position is customized using iconPos attribute. Default
+                    icon position is left.</p>
+<pre>
+&lt;button type="button" pButton icon="fa-check'" iconPos="left"&gt;Click&lt;/button/&gt;
+</pre>
+
+                    <h3>Attributes</h3>
+                    <div class="doc-tablewrapper">
+                        <table class="doc-table">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Type</th>
+                                    <th>Default</th>
+                                    <th>Description</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>icon</td>
+                                    <td>string</td>
+                                    <td>null</td>
+                                    <td>Name of the icon.</td>
+                                </tr>
+                                <tr>
+                                    <td>iconPos</td>
+                                    <td>string</td>
+                                    <td>left</td>
+                                    <td>Position of the icon, valid values are "left" and "right".</td>
+                                </tr>
+                                <tr>
+                                    <td>disabled</td>
+                                    <td>boolean</td>
+                                    <td>false</td>
+                                    <td>When present, it specifies that the element should be disabled.</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <h3>Styling</h3>
+                    <p>Following is the list of structural style classes, for theming classes visit <a href="#" [routerLink]="['Theming']">theming page</a>.</p>
+                    <div class="doc-tablewrapper">
+                        <table class="doc-table">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Element</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>pui-button</td>
+                                    <td>Button element</td>
+                                </tr>
+                                <tr>
+                                    <td>pui-button-icon</td>
+                                    <td>Icon element</td>
+                                </tr>
+                                <tr>
+                                    <td>pui-button-text</td>
+                                    <td>Label element of the button</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </p-tabPanel>
+                <p-tabPanel header="Source">
+                <h3>Template</h3>
+<pre>
+&lt;button type="text" pButton (click)="count()" [icon]="'fa-check'"&gt;Click&lt;/button&gt;
+
+Number of clicks: \{\{clicks\}\}
+</pre>
+
+                <h3>ButtonDemoComponent</h3>
+<pre>
+export class ButtonDemoComponent {
+
+    clicks: number = 0;
+
+    count() {
+        this.clicks++;
+    }
+}
+</pre>
+                </ p-tabPanel>
+             </p-tabView >
+        </div>
     `,
-    directives: [ButtonDirective]
+    directives: [ButtonDirective,TabPanelComponent,TabViewComponent,ROUTER_DIRECTIVES]
 })
 export class ButtonDemoComponent {
 
