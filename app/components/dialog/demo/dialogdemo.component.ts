2e86108f8fec08d79f93250031c6e35ac5daa18d
Çağatay Çivici <cagatay.civici@github.com>
2016-01-25 16:10:23 +0200

    Docs for Dialog

--- app/components/dialog/demo/dialogdemo.component.ts
+++ app/components/dialog/demo/dialogdemo.component.ts
@@ -1,29 +1,13 @@
 import {Component} from 'angular2/core';
 import {DialogComponent} from '../dialog.component';
 import {ButtonDirective} from '../../button/button.directive';
+import {TabViewComponent} from '../../tabview/tabview.component';
+import {TabPanelComponent} from '../../tabview/tabpanel.component';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
-    template: `
-        <div class="ContentSideSections">
-            <div class="Content100 overHidden TextShadow">
-                <span class="fontSize30 TextShadow orange mediumFont marginBottom20 dispBlock">Dialog</span>
-                <span class="defaultText dispTable">Dialog is a container widget featuring modality, minimize, maximize and animations.</span>
-            </div>
-        </div>
-
-        <div class="ContentSideSections Implementation">
-            <p-dialog id="dlgelement" header="Godfather I" [(visible)]="display" modal="modal" minimizable="minimizable" maximizable="maximizable" showEffect="fade" hideEffect="fade">
-                <p>The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. 
-                   His beloved son Michael has just come home from the war, but does not intend to become part of his father's business. 
-                   Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, 
-                   kind and benevolent to those who give respect, 
-                   but given to ruthless violence whenever anything stands against the good of the family.</p>
-            </p-dialog>
-
-            <button type="text" (click)="showDialog()" pButton icon="fa-external-link-square">Show</button>
-        </div>
-    `,
-    directives: [DialogComponent, ButtonDirective]
+    templateUrl: 'app/components/dialog/demo/dialogdemo.component.html',
+    directives: [DialogComponent, ButtonDirective,TabPanelComponent,TabViewComponent,ROUTER_DIRECTIVES]
 })
 export class DialogDemoComponent {
 
