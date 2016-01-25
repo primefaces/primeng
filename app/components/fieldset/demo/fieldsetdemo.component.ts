6c73d508b707e61e40bbc84e34782c67f1476b6d
Çağatay Çivici <cagatay.civici@github.com>
2016-01-25 16:29:57 +0200

    Docs for fieldset

--- app/components/fieldset/demo/fieldsetdemo.component.ts
+++ app/components/fieldset/demo/fieldsetdemo.component.ts
@@ -1,34 +1,12 @@
 import {Component} from 'angular2/core';
 import {FieldsetComponent} from '../fieldset.component';
+import {TabViewComponent} from '../../tabview/tabview.component';
+import {TabPanelComponent} from '../../tabview/tabpanel.component';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
-    template: `
-        <div class="ContentSideSections">
-            <div class="Content100 overHidden TextShadow">
-                <span class="fontSize30 TextShadow orange mediumFont marginBottom20 dispBlock">Fieldset</span>
-                <span class="defaultText dispTable">Fieldset is a grouping component providing a content toggle feature.</span>
-            </div>
-        </div>
-
-        <div class="ContentSideSections Implementation">
-            <p-fieldset legend="Godfather I">
-                The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. 
-                His beloved son Michael has just come home from the war, but does not intend to become part of his father's business. 
-                Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, 
-                kind and benevolent to those who give respect, 
-                but given to ruthless violence whenever anything stands against the good of the family.
-            </p-fieldset>
-            <br>
-            <p-fieldset legend="Toggleable" toggleable="true">
-                The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. 
-                His beloved son Michael has just come home from the war, but does not intend to become part of his father's business. 
-                Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, 
-                kind and benevolent to those who give respect, 
-                but given to ruthless violence whenever anything stands against the good of the family.
-            </p-fieldset>
-        </div>
-    `,
-    directives: [FieldsetComponent]
+    templateUrl:'app/components/fieldset/demo/fieldsetdemo.component.html',
+    directives: [FieldsetComponent,TabViewComponent,TabPanelComponent,ROUTER_DIRECTIVES]
 })
 export class FieldsetDemoComponent {
 
