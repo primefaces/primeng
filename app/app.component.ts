efae8fdfce5f3293b6454b42c17754fd63462f61
Çağatay Çivici <cagatay.civici@github.com>
2016-01-26 17:27:57 +0200

    Reorg demo imports

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,6 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
-import {HomePageComponent} from './homepage.component';
-import {ThemingComponent} from './theming.component';
-import {InputTextDemoComponent} from './demo/inputtext/inputtextdemo.component';
-import {ButtonDemoComponent} from './demo/button/buttondemo.component';
-import {SpinnerDemoComponent} from './demo/spinner/spinnerdemo.component';
-import {PanelDemoComponent} from './demo/panel/paneldemo.component';
-import {FieldsetDemoComponent} from './demo/fieldset/fieldsetdemo.component';
-import {RatingDemoComponent} from './demo/rating/ratingdemo.component';
-import {PasswordDemoComponent} from './demo/password/passworddemo.component';
-import {DialogDemoComponent} from './demo/dialog/dialogdemo.component';
-import {ToggleButtonDemoComponent} from './demo/togglebutton/togglebuttondemo.component';
-import {GridDemoComponent} from './demo/grid/griddemo.component';
-import {TabViewDemoComponent} from './demo/tabview/tabviewdemo.component';
-import {RadioButtonDemoComponent} from './demo/radiobutton/radiobuttondemo.component';
-import {AccordionDemoComponent} from './demo/accordion/accordiondemo.component';
-import {InputTextareaDemoComponent} from './demo/inputtextarea/inputtextareademo.component';
-import {GalleriaDemoComponent} from './demo/galleria/galleriademo.component';
-import {CheckboxDemoComponent} from './demo/checkbox/checkboxdemo.component';
+import * as DEMOS from './componentdemos';
 
 @Component({
     selector: 'my-app',
@@ -25,24 +8,24 @@ import {CheckboxDemoComponent} from './demo/checkbox/checkboxdemo.component';
     directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-    {path: '/', name: 'HomePage', component: HomePageComponent},
-    {path: '/theming', name: 'Theming', component: ThemingComponent},
-    {path: '/inputtext', name: 'InputTextDemo', component: InputTextDemoComponent},
-    {path: '/button', name: 'ButtonDemo', component: ButtonDemoComponent},
-    {path: '/spinner', name: 'SpinnerDemo', component: SpinnerDemoComponent},
-    {path: '/panel', name: 'PanelDemo', component: PanelDemoComponent},
-    {path: '/fieldset', name: 'FieldsetDemo', component: FieldsetDemoComponent},
-    {path: '/rating', name: 'RatingDemo', component: RatingDemoComponent},
-    {path: '/password', name: 'PasswordDemo', component: PasswordDemoComponent},
-    {path: '/dialog', name: 'DialogDemo', component: DialogDemoComponent},
-    {path: '/togglebutton', name: 'ToggleButtonDemo', component: ToggleButtonDemoComponent},
-    {path: '/grid', name: 'GridDemo', component: GridDemoComponent},
-    {path: '/tabview', name: 'TabViewDemo', component: TabViewDemoComponent},
-    {path: '/radiobutton', name: 'RadioButtonDemo', component: RadioButtonDemoComponent},
-    {path: '/accordion', name: 'AccordionDemo', component: AccordionDemoComponent},
-    {path: '/inputtextarea', name: 'InputTextareaDemo', component: InputTextareaDemoComponent},
-    {path: '/galleria', name: 'GalleriaDemo', component: GalleriaDemoComponent},
-    {path: '/checkbox', name: 'CheckboxDemo', component: CheckboxDemoComponent}
+    { path: '/', name: 'HomePage', component: DEMOS.HomePageComponent},
+    {path: '/theming', name: 'Theming', component: DEMOS.ThemingComponent},
+    {path: '/inputtext', name: 'InputTextDemo', component: DEMOS.InputTextDemoComponent},
+    {path: '/button', name: 'ButtonDemo', component: DEMOS.ButtonDemoComponent},
+    {path: '/spinner', name: 'SpinnerDemo', component: DEMOS.SpinnerDemoComponent},
+    {path: '/panel', name: 'PanelDemo', component: DEMOS.PanelDemoComponent},
+    {path: '/fieldset', name: 'FieldsetDemo', component: DEMOS.FieldsetDemoComponent},
+    {path: '/rating', name: 'RatingDemo', component: DEMOS.RatingDemoComponent},
+    {path: '/password', name: 'PasswordDemo', component: DEMOS.PasswordDemoComponent},
+    {path: '/dialog', name: 'DialogDemo', component: DEMOS.DialogDemoComponent},
+    {path: '/togglebutton', name: 'ToggleButtonDemo', component: DEMOS.ToggleButtonDemoComponent},
+    {path: '/grid', name: 'GridDemo', component: DEMOS.GridDemoComponent},
+    {path: '/tabview', name: 'TabViewDemo', component: DEMOS.TabViewDemoComponent},
+    {path: '/radiobutton', name: 'RadioButtonDemo', component: DEMOS.RadioButtonDemoComponent},
+    {path: '/accordion', name: 'AccordionDemo', component: DEMOS.AccordionDemoComponent},
+    {path: '/inputtextarea', name: 'InputTextareaDemo', component: DEMOS.InputTextareaDemoComponent},
+    {path: '/galleria', name: 'GalleriaDemo', component: DEMOS.GalleriaDemoComponent},
+    {path: '/checkbox', name: 'CheckboxDemo', component: DEMOS.CheckboxDemoComponent}
 ])
 export class AppComponent {
 
