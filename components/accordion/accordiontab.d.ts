bfa75c611a3f26468f21429a75ebe7184b189b05
Çağatay Çivici <cagatay.civici@github.com>
2016-01-26 19:26:21 +0200

    Removed generated files

--- components/accordion/accordiontab.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../../typedefinition/primeui.d.ts" />
-import { Accordion } from './accordion';
-export declare class AccordionTab {
-    header: string;
-    initialized: boolean;
-    constructor(tabview: Accordion);
-}
--- components/tabview/tabpanel.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../../typedefinition/primeui.d.ts" />
-import { TabView } from './tabview';
-export declare class TabPanel {
-    header: string;
-    closable: boolean;
-    initialized: boolean;
-    constructor(tabview: TabView);
-}
