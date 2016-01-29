0be3f06e6dba9d4734417bfd1e38126dbb81a114
Çağatay Çivici <cagatay.civici@github.com>
2016-01-29 13:20:19 +0200

    showcase facelift

--- showcase/resources/js/site.js
+++ showcase/resources/js/site.js
@@ -3,7 +3,7 @@ var DemoApp = {
     changeTheme: function(event, element) {
         var theme = $(element).data("theme"),
         themeLink = $('link[href$="theme.css"]'),
-        newThemeURL =  'resources/primeui/themes/' + theme + '/theme.css';
+        newThemeURL =  'showcase/resources/primeui/themes/' + theme + '/theme.css';
 
         themeLink.attr('href', newThemeURL);
         event.preventDefault();
