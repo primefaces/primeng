c58d17b5111d1672961e63847f45621baab34bc1
Çağatay Çivici <cagatay.civici@github.com>
2016-02-01 01:50:24 +0200

    Use hashlocation strategy

--- showcase/boot.ts
+++ showcase/boot.ts
@@ -1,7 +1,9 @@
-import {bootstrap}    from 'angular2/platform/browser'
+import {bootstrap} from 'angular2/platform/browser';
+import {provide} from 'angular2/core';
 import {AppComponent} from './app.component'
-import {ROUTER_PROVIDERS} from 'angular2/router';
+import {ROUTER_PROVIDERS,LocationStrategy,HashLocationStrategy} from 'angular2/router';
 
 bootstrap(AppComponent, [
-  ROUTER_PROVIDERS
+  ROUTER_PROVIDERS,
+  provide(LocationStrategy, {useClass: HashLocationStrategy})
 ]);
