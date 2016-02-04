6c06e6b438f3fe88adeb91b8558c98f71a133b92
Çağatay Çivici <cagatay.civici@github.com>
2016-02-04 23:04:36 +0200

    Added animated version

--- components/progressbar/progressbar.ts
+++ components/progressbar/progressbar.ts
@@ -4,7 +4,7 @@ import {Component,Input} from 'angular2/core';
     selector: 'p-progressBar',
     template: `
         <div class="pui-progressbar ui-widget ui-widget-content ui-corner-all" role="progressbar" aria-valuemin="0" [attr.aria-valuenow]="value" aria-valuemax="100">
-            <div class="pui-progressbar-value ui-widget-header ui-corner-all" [style.width]="value + '%'" style="display:block"></div>
+            <div class="pui-progressbar-value pui-progressbar-value-animate ui-widget-header ui-corner-all" [style.width]="value + '%'" style="display:block"></div>
             <div class="pui-progressbar-label" [style.display]="value ? 'block' : 'none'">{{value}}%</div>
         </div>
     `
