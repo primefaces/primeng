279b61d1d2023752569ba695e03d521479239e51
Çağatay Çivici <cagatay.civici@github.com>
2016-02-23 19:59:54 +0200

    Use relative path

--- showcase/demo/service/carservice.ts
+++ showcase/demo/service/carservice.ts
@@ -8,21 +8,21 @@ export class CarService {
     constructor(private http: Http) {}
 
     getCarsSmall() {
-        return this.http.get('/showcase/resources/data/cars-small.json')
+        return this.http.get('showcase/resources/data/cars-small.json')
                     .toPromise()
                     .then(res => <Car[]> res.json().data)
                     .then(data => { return data; });
     }
 
     getCarsMedium() {
-        return this.http.get('/showcase/resources/data/cars-medium.json')
+        return this.http.get('showcase/resources/data/cars-medium.json')
                     .toPromise()
                     .then(res => <Car[]> res.json().data)
                     .then(data => { return data; });
     }
 
     getCarsLarge() {
-        return this.http.get('/showcase/resources/data/cars-large.json')
+        return this.http.get('showcase/resources/data/cars-large.json')
                     .toPromise()
                     .then(res => <Car[]> res.json().data)
                     .then(data => { return data; });
