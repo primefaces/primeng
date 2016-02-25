34602615d4980e41feb1dacd67031df1bd0498d2
Mrtcndkn <mrtcndkn@gmail.com>
2016-02-25 13:53:30 +0200

    Setup doc cosmetics

--- showcase/setup.component.ts
+++ showcase/setup.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {pCode} from '../components/codehighlighter/codehighlighter';
 
 @Component({
-    templateUrl: 'showcase/setup.component.html'
+    templateUrl: 'showcase/setup.component.html',
+    directives: [pCode]
 })
 export class SetupComponent {
 
