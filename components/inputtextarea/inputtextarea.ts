9a2b088b11db05aec58b2381b86247fa7da3d4e4
Çağatay Çivici <cagatay.civici@github.com>
2016-03-03 18:52:08 +0200

    Fixed #74

--- components/inputtextarea/inputtextarea.ts
+++ components/inputtextarea/inputtextarea.ts
@@ -1,37 +1,96 @@
-import {Directive, ElementRef, OnInit, OnDestroy, OnChanges, HostBinding, Input, SimpleChange} from 'angular2/core';
+import {Directive,ElementRef,HostListener,Input,OnInit} from 'angular2/core';
+import {NgModel} from 'angular2/common';
 
 @Directive({
-    selector: '[pInputTextarea]'
+    selector: '[pInputTextarea]',
+    host: {
+        '[class.ui-inputtext]': 'true',
+        '[class.ui-corner-all]': 'true',
+        '[class.ui-state-default]': 'true',
+        '[class.ui-widget]': 'true',
+        '[class.ui-state-hover]': 'hover',
+        '[class.ui-state-focus]': 'focus',
+        '[class.ui-state-disabled]': 'isDisabled()',
+        '[class.ui-state-error]': 'isInvalid()',
+        '[attr.rows]': 'rows',
+        '[attr.cols]': 'cols'
+    },
+    providers: [NgModel]
 })
-export class InputTextarea implements OnInit, OnDestroy, OnChanges {
-
+export class InputTextarea implements OnInit {
+    
     @Input() autoResize: boolean;
+    
+    @Input() rows: number;
+    
+    @Input() cols: number;
 
-    @Input() disabled: boolean;
-
-    initialized: boolean;
-
-    constructor(private el: ElementRef) {
-        this.initialized = false;
-    }
-
+    hover: boolean;
+    
+    focus: boolean;
+    
+    rowsDefault: number;
+    
+    colsDefault: number;
+        
+    constructor(private el: ElementRef, private control: NgModel) {}
+    
     ngOnInit() {
-        jQuery(this.el.nativeElement).puiinputtextarea({
-            autoResize: this.autoResize
-        });
-        this.initialized = true;
+        this.rowsDefault = this.rows;
+        this.colsDefault = this.cols;
     }
-
-    ngOnChanges(changes: { [key: string]: SimpleChange }) {
-        if (this.initialized) {
-            for (var key in changes) {
-                jQuery(this.el.nativeElement).puiinputtextarea('option', key, changes[key].currentValue);
-            }
+    
+    @HostListener('mouseover', ['$event']) 
+    onMouseover(e) {
+        this.hover = true;
+    }
+    
+    @HostListener('mouseout', ['$event']) 
+    onMouseout(e) {
+        this.hover = false;
+    }
+    
+    @HostListener('focus', ['$event']) 
+    onFocus(e) {
+        this.focus = true;
+        
+        if(this.autoResize) {
+            this.resize();
         }
     }
+    
+    @HostListener('blur', ['$event']) 
+    onBlur(e) {
+        this.focus = false;
+        
+        if(this.autoResize) {
+            this.resize();
+        }
+    }
+    
+    isDisabled() {
+        return this.el.nativeElement.disabled;
+    }
+    
+    isInvalid() {
+        return !this.control.valid;
+    }
+    
+    @HostListener('keyup', ['$event']) 
+    onKeyup(e) {
+        if(this.autoResize) {
+            this.resize();
+        }
+    }
+    
+    resize () {
+        let linesCount = 0,
+        lines = this.el.nativeElement.value.split('\n');
 
-    ngOnDestroy() {
-        jQuery(this.el.nativeElement).puiinputtextarea('destroy');
-        this.initialized = false;
+        for(let i = lines.length-1; i >= 0 ; --i) {
+            linesCount += Math.floor((lines[i].length / this.colsDefault) + 1);
+        }
+
+        this.rows = (linesCount >= this.rowsDefault) ? (linesCount + 1) : this.rowsDefault;
     }
 }
