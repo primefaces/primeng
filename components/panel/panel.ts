efa80a2bbf728eb126d4d1e98000b6435db4278f
Çağatay Çivici <cagatay.civici@github.com>
2016-03-07 17:08:17 +0200

    Add style and stylelcass

--- components/panel/panel.ts
+++ components/panel/panel.ts
@@ -3,7 +3,7 @@ import {Component,Input,Output,EventEmitter} from 'angular2/core';
 @Component({
     selector: 'p-panel',
     template: `
-        <div class="ui-panel ui-widget ui-widget-content ui-corner-all">
+        <div [ngClass]="'ui-panel ui-widget ui-widget-content ui-corner-all'" [attr.style]="style" [attr.class]="styleClass">
             <div class="ui-panel-titlebar ui-widget-header ui-helper-clearfix ui-corner-all">
                 <span class="ui-panel-title">{{header}}</span>
                 <a *ngIf="toggleable" class="ui-panel-titlebar-icon ui-panel-titlebar-toggler ui-corner-all ui-state-default" href="#"
@@ -24,6 +24,10 @@ export class Panel {
     @Input() header: string;
 
     @Input() collapsed: boolean = false;
+    
+    @Input() style: string
+        
+    @Input() styleClass: string
 
     @Output() onBeforeToggle: EventEmitter<any> = new EventEmitter();
 
