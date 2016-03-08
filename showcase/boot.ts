deefb12277613a15f17ca3e555938db3aa424efa
Çağatay Çivici <cagatay.civici@github.com>
2016-03-08 19:44:21 +0200

    Prod mode

--- showcase/boot.ts
+++ showcase/boot.ts
@@ -2,8 +2,10 @@ import {bootstrap} from 'angular2/platform/browser';
 import {provide} from 'angular2/core';
 import {AppComponent} from './app.component'
 import {ROUTER_PROVIDERS,LocationStrategy,HashLocationStrategy} from 'angular2/router';
+import {enableProdMode} from 'angular2/core';
 import 'rxjs/Rx';
 
+enableProdMode();
 bootstrap(AppComponent, [
   ROUTER_PROVIDERS,
   provide(LocationStrategy, {useClass: HashLocationStrategy})
