a71d18f4e86bfc67321d18b2df22b5a083243bd9
Çağatay Çivici <cagatay.civici@github.com>
2016-03-10 17:37:36 +0200

    demo update of buttons

--- showcase/demo/inputtext/inputtextdemo.ts
+++ showcase/demo/inputtext/inputtextdemo.ts
@@ -22,7 +22,7 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
             <h3>Disabled</h3>
             <input id="in" type="text" size="30" pInputText [disabled]="disabled" />
 
-            <button type="button" (click)="toggleDisabled()" pButton>Toggle</button>
+            <button type="button" (click)="toggleDisabled()" pButton label="Toggle"></button>
         </div>
 
         <div class="ContentSideSections Source">
@@ -105,7 +105,7 @@ import {InputText} from 'primeng/primeng';
 &lt;h3&gt;Disabled&lt;/h3&gt;
 &lt;input id="in" type="text" size="30" pInputText [disabled]="disabled" /&gt;
 
-&lt;button type="button" (click)="toggleDisabled()" pButton&gt;Toggle&lt;/button&gt;
+&lt;button type="button" (click)="toggleDisabled()" pButton label="Toggle"&gt;&lt;/button&gt;
 </code>
 </pre>
 
