ee488d6299c0b7104f7e9775836eacaf094e8211
Çağatay Çivici <cagatay.civici@github.com>
2016-03-14 12:22:26 +0200

    Add dynamic columns sample to demo

--- showcase/demo/datatable/datatabledemo.ts
+++ showcase/demo/datatable/datatabledemo.ts
@@ -19,9 +19,18 @@ export class DataTableDemo implements OnInit {
 
     cars: Car[];
     
+    cols: any[];
+    
     constructor(private carService: CarService) { }
 
     ngOnInit() {
         this.carService.getCarsSmall().then(cars => this.cars = cars);
+        
+        this.cols = [
+            {field: 'vin', header: 'Vin'},
+            {field: 'year', header: 'Year'},
+            {field: 'brand', header: 'Brand'},
+            {field: 'color', header: 'Color'}
+        ];
     }
 }
