63421c70ecc376b95ee260a287f2108f0cad370c
Çağatay Çivici <cagatay.civici@github.com>
2016-03-14 18:18:19 +0200

    Events for tree node selection

--- showcase/demo/tree/treedemo.ts
+++ showcase/demo/tree/treedemo.ts
@@ -7,13 +7,17 @@ import {TabPanel} from '../../../components/tabview/tabpanel';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import {HTTP_PROVIDERS}    from 'angular2/http';
 import {NodeService} from '../service/nodeservice';
+import {Growl} from '../../../components/growl/growl';
+import {Message} from '../../../components/api/message';
 
 @Component({
     templateUrl: 'showcase/demo/tree/treedemo.html',
-    directives: [Tree,TabView,TabPanel,CodeHighlighter,ROUTER_DIRECTIVES],
+    directives: [Tree,TabView,Growl,TabPanel,CodeHighlighter,ROUTER_DIRECTIVES],
     providers: [HTTP_PROVIDERS,NodeService]
 })
 export class TreeDemo implements OnInit {
+    
+    msgs: Message[];
     
     files: TreeNode[];
     
@@ -25,5 +29,15 @@ export class TreeDemo implements OnInit {
 
     ngOnInit() {
         this.nodeService.getFiles().then(files => this.files = files);
+    }
+    
+    nodeSelect(event) {
+        this.msgs = [];
+        this.msgs.push({severity: 'info', summary: 'Node Selected', detail: event.node.label});
+    }
+    
+    nodeUnselect(event) {
+        this.msgs = [];
+        this.msgs.push({severity: 'info', summary: 'Node Unselected', detail: event.node.label});
     }
 }
