e9da86b686cb5acf93a35a7fdc27558e59ce72c5
Mrtcndkn <mrtcndkn@gmail.com>
2016-03-29 16:07:47 +0300

    Responsive page updated

--- showcase/demo/responsive/responsivedemo.ts
+++ showcase/demo/responsive/responsivedemo.ts
@@ -13,22 +13,50 @@ import {Dialog} from '../../../components/dialog/dialog';
 import {Panel} from '../../../components/panel/panel';
 import {DataTable} from '../../../components/datatable/datatable';
 import {DataGrid} from '../../../components/datagrid/datagrid';
+import {AutoComplete} from '../../../components/autocomplete/autocomplete';
+import {Calendar} from '../../../components/calendar/calendar';
+import {SplitButton} from '../../../components/splitbutton/splitbutton';
+import {SplitButtonItem} from '../../../components/splitbutton/splitbuttonitem';
+import {Password} from '../../../components/password/password';
+import {RadioButton} from '../../../components/radiobutton/radiobutton';
+import {LineChart} from '../../../components/chart/linechart/linechart';
+import {Tree} from '../../../components/tree/tree';
+import {Menu} from '../../../components/menu/menu';
+import {PanelMenu} from '../../../components/panelmenu/panelmenu';
+import {PickList} from '../../../components/picklist/picklist';
+import {Carousel} from '../../../components/carousel/carousel';
+import {OrderList} from '../../../components/orderlist/orderlist';
+import {TreeNode} from '../../../components/api/treenode';
+import {UITreeNode} from '../../../components/tree/uitreenode';
+import {TreeNodeTemplateLoader} from '../../../components/tree/treenodetemplateloader';
 import {Header} from '../../../components/common/header';
 import {Column} from '../../../components/column/column';
 import {Car} from '../domain/car';
 import {CarService} from '../service/carservice';
+import {NodeService} from '../service/nodeservice';
+import {CountryService} from '../service/countryservice';
 import {HTTP_PROVIDERS}    from 'angular2/http';
 
 @Component({
     templateUrl: 'showcase/demo/responsive/responsivedemo.html',
-    directives: [Header,DataGrid,TabPanel,TabView,CodeHighlighter,ROUTER_DIRECTIVES,InputText,InputTextarea,Button,Dropdown,Listbox,Dialog,Panel,DataTable,Column],
-    providers: [HTTP_PROVIDERS,CarService]
+    directives: [PanelMenu,Menu,PickList,Carousel,OrderList,Tree,TreeNodeTemplateLoader,UITreeNode,LineChart,RadioButton,Password,SplitButton,SplitButtonItem,AutoComplete,Header,DataGrid,TabPanel,TabView,CodeHighlighter,ROUTER_DIRECTIVES,InputText,InputTextarea,Button,Dropdown,Listbox,Dialog,Panel,DataTable,Column,Calendar],
+    providers: [HTTP_PROVIDERS,CarService,CountryService,NodeService]
 })
 export class ResponsiveDemo implements OnInit {
 
     cities: SelectItem[];
 
+    files: TreeNode[];
+
+    sourceCars: Car[];
+
+    targetCars: Car[];
+
+    data: any;
+
     selectedCity: string;
+
+    val: string;
 
     options: SelectItem[];
 
@@ -38,11 +66,35 @@ export class ResponsiveDemo implements OnInit {
 
     cars: Car[];
 
+    cars1: Car[];
+
+    cars2: Car[];
+
+    cars3: Car[];
+
+    date: string;
+
+    text: string;
+
+    filteredCountriesSingle: any[];
+
     showDialog() {
         this.display = true;
     }
 
-    constructor(private carService: CarService) {
+    constructor(private carService: CarService, private countryService: CountryService, private nodeService: NodeService) {
+        this.cars2 = [
+            {vin: 'r3278r2', year: 2010, brand: 'Audi', color: 'Black'},
+            {vin: 'jhto2g2', year: 2015, brand: 'BMW', color: 'White'},
+            {vin: 'h453w54', year: 2012, brand: 'Honda', color: 'Blue'},
+            {vin: 'g43gwwg', year: 1998, brand: 'Renault', color: 'White'},
+            {vin: 'gf45wg5', year: 2011, brand: 'VW', color: 'Red'},
+            {vin: 'bhv5y5w', year: 2015, brand: 'Jaguar', color: 'Blue'},
+            {vin: 'ybw5fsd', year: 2012, brand: 'Ford', color: 'Yellow'},
+            {vin: '45665e5', year: 2011, brand: 'Mercedes', color: 'Brown'},
+            {vin: 'he6sb5v', year: 2015, brand: 'Ford', color: 'Black'}
+        ];
+
         this.cities = [];
         this.cities.push({label:'Select Cities', value:'Select Cities'});
         this.cities.push({label:'New York', value:'New York'});
@@ -57,9 +109,59 @@ export class ResponsiveDemo implements OnInit {
         this.options.push({label:'Option 3', value:'Option 3'});
         this.options.push({label:'Option 4', value:'Option 4'});
         this.options.push({label:'Option 5', value:'Option 5'});
+
+        this.data = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'My First dataset',
+                    fillColor: 'rgba(220,220,220,0.2)',
+                    strokeColor: 'rgba(220,220,220,1)',
+                    pointColor: 'rgba(220,220,220,1)',
+                    pointStrokeColor: '#fff',
+                    pointHighlightFill: '#fff',
+                    pointHighlightStroke: 'rgba(220,220,220,1)',
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: 'My Second dataset',
+                    fillColor: 'rgba(151,187,205,0.2)',
+                    strokeColor: 'rgba(151,187,205,1)',
+                    pointColor: 'rgba(151,187,205,1)',
+                    pointStrokeColor: '#fff',
+                    pointHighlightFill: '#fff',
+                    pointHighlightStroke: 'rgba(151,187,205,1)',
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        }
     }
 
     ngOnInit() {
         this.carService.getCarsMedium().then(cars => this.cars = cars);
+        this.nodeService.getFiles().then(files => this.files = files);
+        this.carService.getCarsSmall().then(cars1 => this.cars1 = cars1);
+        this.carService.getCarsSmall().then(cars3 => this.sourceCars = cars3);
+        this.targetCars = [];
     }
+
+    filterCountrySingle(event) {
+        let query = event.query;
+        this.countryService.getCountries().then(countries => {
+            this.filteredCountriesSingle = this.filterCountry(query, countries);
+        });
+    }
+
+    filterCountry(query, countries: any[]):any[] {
+        //in a real application, make a request to a remote url with the query and return filtered results, for demo we filter at client side
+        let filtered : any[] = [];
+        for(let i = 0; i < countries.length; i++) {
+            let country = countries[i];
+            if(country.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+                filtered.push(country);
+            }
+        }
+        return filtered;
+    }
+
 }
