1683c5d683d7b5c352ec99209ae6fa6242ffd521
Mrtcndkn <mrtcndkn@gmail.com>
2016-03-30 19:08:45 +0300

    If panel visible button click closes the panel

--- components/overlaypanel/overlaypanel.ts
+++ components/overlaypanel/overlaypanel.ts
@@ -18,31 +18,31 @@ declare var PUI: any;
     providers: [DomHandler]
 })
 export class OverlayPanel implements OnInit, OnDestroy {
-    
+
     @Input() dismissable: boolean = true;
-    
+
     @Input() showCloseIcon: boolean;
-    
+
     @Input() style: string;
-        
+
     @Input() styleClass: string;
 
     @Output() onBeforeShow: EventEmitter<any> = new EventEmitter();
 
     @Output() onAfterShow: EventEmitter<any> = new EventEmitter();
-    
+
     @Output() onBeforeHide: EventEmitter<any> = new EventEmitter();
 
     @Output() onAfterHide: EventEmitter<any> = new EventEmitter();
-    
+
     visible: boolean = false;
-    
+
     hoverCloseIcon: boolean;
-    
+
     documentClickListener: any;
-    
+
     constructor(private el: ElementRef, private domHandler: DomHandler, private renderer: Renderer) {}
-    
+
     ngOnInit() {
         if(this.dismissable) {
             this.documentClickListener = this.renderer.listenGlobal('body', 'click', () => {
@@ -50,15 +50,18 @@ export class OverlayPanel implements OnInit, OnDestroy {
             });
         }
     }
-    
+
     show(event,target?) {
         this.onBeforeShow.emit(null);
         let elementTarget = target||event.currentTarget||event.target;
         let container = this.el.nativeElement.children[0];
         container.style.zIndex = ++PUI.zindex;
-        
+
         if(this.visible) {
             this.domHandler.absolutePosition(container, elementTarget);
+            if(this.dismissable) {
+                this.hide();
+            }
         }
         else {
             this.visible = true;
@@ -66,10 +69,10 @@ export class OverlayPanel implements OnInit, OnDestroy {
             this.domHandler.fadeIn(container, 250);
         }
         this.onAfterShow.emit(null);
-        
+
         event.stopPropagation();
     }
-    
+
     hide() {
         if(this.visible) {
             this.onBeforeHide.emit(null);
@@ -77,12 +80,12 @@ export class OverlayPanel implements OnInit, OnDestroy {
             this.onAfterHide.emit(null);
         }
     }
-    
+
     onCloseClick(event) {
         this.hide();
         event.preventDefault();
     }
-    
+
     ngOnDestroy() {
         if(this.documentClickListener) {
             this.documentClickListener();
