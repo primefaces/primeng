a9d268eaae06fd72ceffd61d3ad8eb22f990ea5b
Çağatay Çivici <cagatay.civici@github.com>
2016-04-05 19:06:33 +0300

    NgModel and Model driven forms support for RadioButton

--- components/radiobutton/radiobutton.ts
+++ components/radiobutton/radiobutton.ts
@@ -1,20 +1,30 @@
-import {Component, ElementRef, OnInit, OnDestroy, OnChanges, SimpleChange, Input, Output, EventEmitter} from 'angular2/core';
+import {Component,Input,Output,EventEmitter,forwardRef,Provider} from 'angular2/core';
+import {NG_VALUE_ACCESSOR, ControlValueAccessor} from 'angular2/common';
+import {CONST_EXPR} from 'angular2/src/facade/lang';
+
+const RADIO_VALUE_ACCESSOR: Provider = CONST_EXPR(
+    new Provider(NG_VALUE_ACCESSOR, {
+        useExisting: forwardRef(() => RadioButton),
+        multi: true
+    })
+);
 
 @Component({
     selector: 'p-radio',
     template: `
         <div class="ui-radiobutton ui-widget">
             <div class="ui-helper-hidden-accessible">
-                <input type="radio" [attr.name]="name" [attr.value]="value" [checked]="isChecked()"/>
+                <input type="radio" [attr.name]="name" [attr.value]="value" [checked]="checked" (blur)="onModelTouched()">
             </div>
             <div class="ui-radiobutton-box ui-widget ui-radiobutton-relative ui-state-default" (click)="onclick()"
-                        (mouseover)="hover=true" (mouseout)="hover=false" [ngClass]="{'ui-state-hover':hover,'ui-state-active':isChecked(),'ui-state-disabled':disabled}">
-                <span class="ui-radiobutton-icon" [ngClass]="{'fa fa-fw fa-circle':isChecked()}"></span>
+                        (mouseover)="hover=true" (mouseout)="hover=false" [ngClass]="{'ui-state-hover':hover,'ui-state-active':checked,'ui-state-disabled':disabled}">
+                <span class="ui-radiobutton-icon" [ngClass]="{'fa fa-fw fa-circle':checked}"></span>
             </div>
         </div>
-    `
+    `,
+    providers: [RADIO_VALUE_ACCESSOR]
 })
-export class RadioButton {
+export class RadioButton implements ControlValueAccessor {
 
     @Input() value: any;
 
@@ -22,20 +32,34 @@ export class RadioButton {
 
     @Input() disabled: boolean;
 
-    @Input() model: any;
-
     @Output() click: EventEmitter<any> = new EventEmitter();
-
-    @Output() modelChange: EventEmitter<any> = new EventEmitter();
-
-    hover: boolean;
+    
+    model: any;
+    
+    onModelChange: Function = () => {};
+    
+    onModelTouched: Function = () => {};
+    
+    checked: boolean;
+    
+    private hover: boolean;
 
     onclick() {
         this.click.emit(null);
-        this.modelChange.emit(this.value);
+        this.checked = true;
+        this.onModelChange(this.value);
+    }
+        
+    writeValue(model: any) : void {
+        this.model = model;
+        this.checked = (this.model == this.value);
     }
     
-    isChecked() {
-        return this.value == this.model;
+    registerOnChange(fn: Function): void {
+        this.onModelChange = fn;
+    }
+
+    registerOnTouched(fn: Function): void {
+        this.onModelTouched = fn;
     }
 }
