caef943432379b8cd0f87a36ea0fcf36f4d1d81c
Çağatay Çivici <cagatay.civici@github.com>
2016-04-05 14:59:29 +0300

    NgModel and Model driven forms support for InputSwitc

--- components/inputswitch/inputswitch.ts
+++ components/inputswitch/inputswitch.ts
@@ -1,4 +1,13 @@
-import {Component, ElementRef, AfterContentInit, OnDestroy, OnChanges, Input, Output, SimpleChange, EventEmitter} from 'angular2/core';
+import {Component,ElementRef,AfterViewInit,OnDestroy,OnChanges,Input,Output,SimpleChange,EventEmitter,forwardRef,Provider} from 'angular2/core';
+import {NG_VALUE_ACCESSOR,ControlValueAccessor} from 'angular2/common';
+import {CONST_EXPR} from 'angular2/src/facade/lang';
+
+const INPUTSWITCH_VALUE_ACCESSOR: Provider = CONST_EXPR(
+    new Provider(NG_VALUE_ACCESSOR, {
+        useExisting: forwardRef(() => InputSwitch),
+        multi: true
+    })
+);
 
 @Component({
     selector: 'p-inputSwitch',
@@ -12,30 +21,31 @@ import {Component, ElementRef, AfterContentInit, OnDestroy, OnChanges, Input, Ou
             </div>
             <div class="ui-inputswitch-handle ui-state-default"></div>
             <div class="ui-helper-hidden-accessible">
-                <input type="checkbox" />
+                <input type="checkbox" (blur)="onModelTouched()"/>
             </div>
         </div>
-    `
+    `,
+    providers: [INPUTSWITCH_VALUE_ACCESSOR]
 })
-export class InputSwitch implements AfterContentInit, OnDestroy, OnChanges {
+export class InputSwitch implements AfterViewInit, OnDestroy, OnChanges {
 
     @Input() onLabel: string = 'On';
 
     @Input() offLabel: string = 'Off';
-
-    @Input() checked: boolean;
     
     @Input() style: string;
         
     @Input() styleClass: string;
 
     @Output() onChange: EventEmitter<any> = new EventEmitter();
-
-    @Output() checkedChange: EventEmitter<any> = new EventEmitter();
+    
+    value: boolean;
+    
+    onModelChange: Function = () => {};
+    
+    onModelTouched: Function = () => {};
 
     initialized: boolean;
-
-    stopNgOnChangesPropagation: boolean;
 
     inputSwitchElement: any;
 
@@ -43,32 +53,41 @@ export class InputSwitch implements AfterContentInit, OnDestroy, OnChanges {
         this.initialized = false;
     }
 
-    ngAfterContentInit() {
-        setTimeout(() => {
-            this.inputSwitchElement = jQuery(this.el.nativeElement.children[0]).find('> .ui-helper-hidden-accessible > input');
-            this.inputSwitchElement.puiswitch({
-                checked: this.checked,
-                enhanced: true,
-                change: (event: Event, ui: PrimeUI.InputSwitchEventParams) => {
-                    this.stopNgOnChangesPropagation = true;
-                    this.checkedChange.emit(ui.checked);
-                    if (this.onChange) {
-                        this.onChange.emit({originalEvent: event, checked: ui.checked});
-                    }
+    ngAfterViewInit() {
+        this.inputSwitchElement = jQuery(this.el.nativeElement.children[0]).find('> .ui-helper-hidden-accessible > input');
+        this.inputSwitchElement.puiswitch({
+            checked: this.value,
+            enhanced: true,
+            change: (event: Event, ui: any) => {
+                this.value = ui.checked;
+                this.onModelChange(this.value);
+                if (this.onChange) {
+                    this.onChange.emit({originalEvent: event, checked: this.value});
                 }
-            });
-            this.initialized = true;
-        }, 10);
+            }
+        });
+        this.initialized = true;
+    }
+    
+    writeValue(value: any) : void {
+        this.value = value;
+        
+        if(this.initialized) {
+            this.inputSwitchElement.puiswitch('option', 'checked', this.value);
+        }
+    }
+    
+    registerOnChange(fn: Function): void {
+        this.onModelChange = fn;
+    }
+
+    registerOnTouched(fn: Function): void {
+        this.onModelTouched = fn;
     }
 
     ngOnChanges(changes: { [key: string]: SimpleChange }) {
         if (this.initialized) {
             for (var key in changes) {
-                if (key == 'checked' && this.stopNgOnChangesPropagation) {
-                    this.stopNgOnChangesPropagation = false;
-                    continue;
-                }
-
                 this.inputSwitchElement.puiswitch('option', key, changes[key].currentValue);
             }
         }
