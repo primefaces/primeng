e82a6765f024f2bfd00c00b38edfeeddad0ded53
Çağatay Çivici <cagatay.civici@github.com>
2016-04-05 16:49:40 +0300

    NgModel and Model driven forms support for Calendar

--- components/calendar/calendar.ts
+++ components/calendar/calendar.ts
@@ -1,25 +1,31 @@
-import {Component,ElementRef,AfterViewInit,OnDestroy,OnChanges,Input,Output,SimpleChange,EventEmitter} from 'angular2/core';
+import {Component,ElementRef,AfterViewInit,OnDestroy,OnChanges,Input,Output,SimpleChange,EventEmitter,forwardRef,Provider} from 'angular2/core';
 import {Button} from '../button/button';
+import {NG_VALUE_ACCESSOR, ControlValueAccessor} from 'angular2/common';
+import {CONST_EXPR} from 'angular2/src/facade/lang';
+
+const CALENDAR_VALUE_ACCESSOR: Provider = CONST_EXPR(
+    new Provider(NG_VALUE_ACCESSOR, {
+        useExisting: forwardRef(() => Calendar),
+        multi: true
+    })
+);
 
 @Component({
     selector: 'p-calendar',
     template:  `
         <span [attr.style]="style" [attr.class]="styleClass" [ngClass]="'ui-calendar'" *ngIf="!inline">
         <input #in type="text" [attr.placeholder]="placeholder" [attr.style]="inputStyle" [attr.class]="inputStyleClass"
-                [value]="value||''" (input)="valueChange.emit($event.target.value)" [readonly]="readonlyInput"
-                [disabled]="disabled" (mouseenter)="hovered=true" (mouseleave)="hovered=false" (focus)="focused=true" (blur)="focused=false"
+                [value]="value||''" (input)="onInput($event)" [readonly]="readonlyInput"
+                [disabled]="disabled" (mouseenter)="hovered=true" (mouseleave)="hovered=false" (focus)="focused=true" (blur)="onBlur($event)"
                 [ngClass]="{'ui-inputfield ui-inputtext ui-widget ui-state-default': true, 'ui-corner-all': !showIcon, 'ui-corner-left': showIcon,
                     'ui-state-hover':hovered,'ui-state-focus':focused,'ui-state-disabled':disabled}"
         ><button type="button" icon="fa-calendar" pButton *ngIf="showIcon" (click)="onButtonClick($event,in)" class="ui-datepicker-trigger"></button></span>
         <div *ngIf="inline"></div>
     `,
-    directives: [Button]
+    directives: [Button],
+    providers: [CALENDAR_VALUE_ACCESSOR]
 })
-export class Calendar implements AfterViewInit,OnChanges,OnDestroy {
-
-    @Input() value: string;
-
-    @Output() valueChange: EventEmitter<any> = new EventEmitter();
+export class Calendar implements AfterViewInit,OnChanges,OnDestroy,ControlValueAccessor {
 
     @Input() readonlyInput: boolean;
 
@@ -64,14 +70,18 @@ export class Calendar implements AfterViewInit,OnChanges,OnDestroy {
     @Input() showIcon: boolean;
 
     @Output() onSelect: EventEmitter<any> = new EventEmitter();
+    
+    value: string;
+    
+    onModelChange: Function = () => {};
+    
+    onModelTouched: Function = () => {};
 
     hovered: boolean;
 
     focused: boolean;
 
     initialized: boolean;
-
-    stopNgOnChangesPropagation: boolean;
     
     calendarElement: any;
 
@@ -95,21 +105,39 @@ export class Calendar implements AfterViewInit,OnChanges,OnDestroy {
             minDate: this.minDate,
             maxDate: this.maxDate,
             onSelect: (dateText: string) => {
-                this.stopNgOnChangesPropagation = true;
-                this.onSelect.emit(dateText);
-                this.valueChange.emit(dateText);
+                this.value = dateText;
+                this.onModelChange(this.value);
+                this.onSelect.emit(this.value);
             }
         });
         this.initialized = true;
+    }
+    
+    onInput(event) {
+        this.onModelChange(event.target.value);
+    }
+    
+    onBlur(event) {
+        this.value = event.target.value;
+        this.onModelTouched();
+        this.focused=false;
+    }
+    
+    writeValue(value: any) : void {
+        this.value = value;
+    }
+    
+    registerOnChange(fn: Function): void {
+        this.onModelChange = fn;
+    }
+
+    registerOnTouched(fn: Function): void {
+        this.onModelTouched = fn;
     }
 
     ngOnChanges(changes: {[key: string]: SimpleChange}) {
         if (this.initialized) {
             for (var key in changes) {
-                if(key == 'value' && this.stopNgOnChangesPropagation) {
-                    continue;
-                }
-
                 this.calendarElement.datepicker('option', key, changes[key].currentValue);
             }
         }
