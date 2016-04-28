b6cb6ecfccef0db184808a48d1fcc9300a9a9dff
Çağatay Çivici <cagatay.civici@github.com>
2016-04-28 16:35:51 +0300

    Refactored galleria

--- components/galleria/galleria.ts
+++ components/galleria/galleria.ts
@@ -1,34 +1,39 @@
-import {Component,ElementRef,AfterViewChecked,AfterViewInit,OnDestroy,Input,SimpleChange,Output,Renderer,IterableDiffers,EventEmitter} from 'angular2/core';
+import {Component,ElementRef,AfterViewChecked,AfterViewInit,OnDestroy,Input,Output,IterableDiffers,EventEmitter} from 'angular2/core';
 import {DomHandler} from '../dom/domhandler';
 
 @Component({
     selector: 'p-galleria',
     template: `
-    <div [ngClass]="{'ui-galleria ui-widget ui-widget-content ui-corner-all' : true}" [attr.style]="style" [class]="styleClass">
-        <ul class="ui-galleria-panel-wrapper">
-            <li class="ui-galleria-panel ui-helper-hidden" *ngFor="#image of images;">
-                <img class="ui-panel-images" src="{{image.source}}" alt="{{image.alt}}" title="{{image.title}}"/>
-            </li>
-        </ul>
-        <div [ngClass]="{'ui-galleria-filmstrip-wrapper' : true}">
-            <ul class="ui-galleria-filmstrip">
-                <li #frame [ngClass]="{'ui-galleria-frame-active':i==activeIndex}" class="ui-galleria-frame" (click)="frameClick(frame)" *ngFor="#image of images;#i=index">
-                    <div class="ui-galleria-frame-content">
-                        <img src="{{image.source}}" alt="{{image.alt}}" title="{{image.title}}" class="ui-galleria-frame-image">
-                    </div>
+        <div [ngClass]="{'ui-galleria ui-widget ui-widget-content ui-corner-all':true}" [attr.style]="style" [class]="styleClass" [style.width.px]="panelWidth">
+            <ul class="ui-galleria-panel-wrapper" [style.width.px]="panelWidth" [style.height.px]="panelHeight">
+                <li *ngFor="#image of images;#i=index" class="ui-galleria-panel" [ngClass]="{'ui-helper-hidden':i!=activeIndex}"
+                    [style.width.px]="panelWidth" [style.height.px]="panelHeight">
+                    <img class="ui-panel-images" [src]="image.source" [alt]="image.alt" [title]="image.title"/>
                 </li>
             </ul>
+            <div [ngClass]="{'ui-galleria-filmstrip-wrapper':true}">
+                <ul class="ui-galleria-filmstrip" style="transition:left 1s" [style.left.px]="stripLeft">
+                    <li #frame *ngFor="#image of images;#i=index" [ngClass]="{'ui-galleria-frame-active':i==activeIndex}" class="ui-galleria-frame" (click)="frameClick(frame)"
+                        [style.width.px]="frameWidth" [style.height.px]="frameHeight" [style.transition]="'opacity 0.75s ease'">
+                        <div class="ui-galleria-frame-content">
+                            <img [src]="image.source" [alt]="image.alt" [title]="image.title" class="ui-galleria-frame-image"
+                                [style.width.px]="frameWidth" [style.height.px]="frameHeight">
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="ui-galleria-nav-prev fa fa-fw fa-chevron-circle-left" (click)="clickNavLeft()" [style.bottom.px]="frameHeight/2"></div>
+            <div class="ui-galleria-nav-next fa fa-fw fa-chevron-circle-right" (click)="clickNavRight()" [style.bottom.px]="frameHeight/2"></div>
+            <div class="ui-galleria-caption" *ngIf="showCaption&&images" style="display:block">
+                <h4>{{images[activeIndex].title}}</h4><p>{{images[activeIndex].alt}}</p>
+            </div>
         </div>
-        <div class="ui-galleria-nav-prev fa fa-fw fa-chevron-circle-left" (click)="onPrev()"></div>
-        <div class="ui-galleria-nav-next fa fa-fw fa-chevron-circle-right" (click)="onNext()"></div>
-        <div class="ui-galleria-caption" *ngIf="showCaption"></div>
-    </div>
     `,
     providers: [DomHandler]
 })
 export class Galleria implements AfterViewChecked,AfterViewInit,OnDestroy {
     
-    @Input() images: GalleriaImages[];
+    @Input() images: GalleriaImage[];
     
     @Input() style: string;
 
@@ -72,13 +77,13 @@ export class Galleria implements AfterViewChecked,AfterViewInit,OnDestroy {
     
     private interval: any;
     
-    private stepFactorHelper: number = 0;
+    private stripLeft: number = 0;
     
     private imagesChanged: boolean;
     
     private initialized: boolean;
 
-    constructor(private el: ElementRef, private domHandler: DomHandler,private renderer: Renderer, differs: IterableDiffers) {
+    constructor(private el: ElementRef, private domHandler: DomHandler, differs: IterableDiffers) {
         this.differ = differs.find([]).create(null);
     }
     
@@ -103,68 +108,33 @@ export class Galleria implements AfterViewChecked,AfterViewInit,OnDestroy {
         this.container = this.el.nativeElement.children[0];
         this.panelWrapper = this.domHandler.findSingle(this.el.nativeElement, 'ul.ui-galleria-panel-wrapper');
         this.initialized = true;
+        
+        if(this.showFilmstrip) {
+            this.stripWrapper = this.domHandler.findSingle(this.container,'div.ui-galleria-filmstrip-wrapper');
+            this.strip = this.domHandler.findSingle(this.stripWrapper,'ul.ui-galleria-filmstrip');
+        }
     }
     
     render() {
-        this.panels = this.domHandler.find(this.panelWrapper, 'li.ui-galleria-panel');
-        this.panelWrapper.style.width = this.panelWidth + 'px';
-        this.panelWrapper.style.height = this.panelHeight + 'px';
-        for (let i = 0; i < this.panels.length; i++) {
-            this.panels[i].style.width = this.panelWidth + 'px';
-            this.panels[i].style.height = this.panelHeight + 'px';
-        }
-        this.container.style.width = this.panelWidth + 'px';
+        this.panels = this.domHandler.find(this.panelWrapper, 'li.ui-galleria-panel'); 
+        this.frames = this.domHandler.find(this.strip,'li.ui-galleria-frame');
         
         if(this.showFilmstrip) {
-            this.renderStrip();
+            this.stripWrapper.style.width = this.domHandler.width(this.panelWrapper) - 50 + 'px';
+            this.stripWrapper.style.height = this.frameHeight + 'px';
         }
-
-        var activePanel = this.panels[this.activeIndex];
-        this.domHandler.removeClass(activePanel,'ui-helper-hidden');
-
+        
         if(this.showCaption) {
             this.caption = this.domHandler.findSingle(this.container,'div.ui-galleria-caption');
             this.caption.style.bottom = this.showFilmstrip ? this.domHandler.getOuterHeight(this.stripWrapper,true) + 'px' : 0 + 'px';
             this.caption.style.width = this.domHandler.width(this.panelWrapper) + 'px';
-            this.caption.style.display = 'block';
-            
-            this.showPanelCaption(activePanel);
         }
-
-        this.container.style.visibility = 'visible';
-
+   
         if(this.autoPlay) {
             this.startSlideshow();
         }
-    }
-    
-    renderStrip() {
-        //strip
-        var frameStyle = 'width:' + this.frameWidth + "px;height:" + this.frameHeight + 'px;';
-                    
-        this.stripWrapper = this.domHandler.findSingle(this.container,'div.ui-galleria-filmstrip-wrapper');
-        this.stripWrapper.style.width = this.domHandler.width(this.panelWrapper) - 50 + 'px';
-        this.stripWrapper.style.height = this.frameHeight + 'px';
         
-        this.strip = this.domHandler.findSingle(this.stripWrapper,'ul.ui-galleria-filmstrip');
-                    
-        for(let i = 0; i < this.panels.length; i++) {
-            let frameImages = [],
-            image  = this.domHandler.find(this.panels[i], 'img.ui-panel-images');
-            this.frames = this.domHandler.find(this.strip,'li.ui-galleria-frame');
-            
-            //if(i == this.activeIndex)
-            //    this.domHandler.addClass(this.frames[i],'ui-galleria-frame-active');
-                
-            this.frames[i].style = frameStyle;
-            
-            frameImages[i] = this.domHandler.findSingle(this.frames[i],'img.ui-galleria-frame-image');
-            frameImages[i].style = frameStyle;
-        }
-                    
-        //navigators
-        this.domHandler.findSingle(this.container,'div.ui-galleria-nav-prev').style.bottom = this.frameHeight/2 + 'px';
-        this.domHandler.findSingle(this.container,'div.ui-galleria-nav-next').style.bottom = this.frameHeight/2 + 'px';
+        this.container.style.visibility = 'visible';
     }
     
     startSlideshow() {
@@ -183,14 +153,14 @@ export class Galleria implements AfterViewChecked,AfterViewInit,OnDestroy {
         this.slideshowActive = false;
     }
     
-    onNext() {
+    clickNavRight() {
         if(this.slideshowActive) {
             this.stopSlideshow();
         }
         this.next();
     } 
     
-    onPrev() {
+    clickNavLeft() {
         if(this.slideshowActive) {
             this.stopSlideshow();
         }
@@ -212,86 +182,50 @@ export class Galleria implements AfterViewChecked,AfterViewInit,OnDestroy {
     }
     
     next() {
-        if(this.activeIndex  !== (this.panels.length-1)) {
+        if(this.activeIndex  !== (this.panels.length-1))
             this.select(this.activeIndex + 1, true);
-        } 
-        else {
+        else
             this.select(0, true);
-        }
-    }
-    
-    hidePanelCaption() {
-        this.domHandler.removeClass(this.caption, 'ui-helper-hidden');
     }
         
-    showPanelCaption(panel) {
-        setTimeout(() => {
-            this.domHandler.addClass(this.caption, 'ui-helper-hidden');
-        }, 750);
-        this.domHandler.fadeIn(this.caption, 1000);
-        var image = panel.children;
-        this.caption.innerHTML= '<h4>' + image[0].title + '</h4><p>' + image[0].alt + '</p>';
-    }
-    
     select(index, reposition) {
-        if(index !== this.activeIndex) {
-            if(this.showCaption) {
-                this.hidePanelCaption();
-            }
-            
-            var oldPanel = this.panels[this.activeIndex],
+        if(index !== this.activeIndex) {            
+            let oldPanel = this.panels[this.activeIndex],
             newPanel = this.panels[index];
             
-            this.domHandler.addClass(oldPanel, 'ui-helper-hidden');
-            this.domHandler.removeClass(newPanel, 'ui-helper-hidden');
             this.domHandler.fadeIn(newPanel, 500);
             
             if(this.showFilmstrip) {
-                var oldFrame = this.frames[this.activeIndex],
+                let oldFrame = this.frames[this.activeIndex],
                 newFrame = this.frames[index];
-                //this.domHandler.removeClass(oldFrame,'ui-galleria-frame-active');
-                oldFrame.style.opacity = '';
-                newFrame.style.opacity = '1.0';
-                //this.domHandler.addClass(newFrame,'ui-galleria-frame-active');
-                newFrame.style.transition = "0.75s ease";
                 
-                //viewport
                 if(reposition === undefined || reposition === true) {
-                    var frameLeft = newFrame.offsetLeft,
+                    let frameLeft = newFrame.offsetLeft,
                     stepFactor = this.frameWidth + 10,
                     stripLeft = this.strip.offsetLeft,
                     frameViewportLeft = frameLeft + stripLeft,
                     frameViewportRight = frameViewportLeft + this.frameWidth;
                     
                     if(frameViewportRight > this.domHandler.width(this.stripWrapper)) {
-                        this.stepFactorHelper -= stepFactor;
-                        this.strip.style.left = this.stepFactorHelper + 'px';
-                        this.strip.style.transition = "1s ease";
-                    } else if(frameViewportLeft < 0) {
-                        this.stepFactorHelper = this.stepFactorHelper/8;
-                        this.stepFactorHelper += stepFactor;
-                        this.strip.style.left = this.stepFactorHelper + 'px';
-                        this.strip.style.transition = "1s ease";
+                        this.stripLeft -= stepFactor;
+                    } 
+                    else if(frameViewportLeft < 0) {
+                        this.stripLeft = this.stripLeft/8 + stepFactor;
                     }
                 }
-            }
-            
-            //caption
-            if(this.showCaption) {
-                this.showPanelCaption(newPanel);
             }
             
             this.activeIndex = index;
         }
     }
-    
+        
     ngOnDestroy() {
         this.stopSlideshow();
     }
 
 }
 
-export interface GalleriaImages {
+export interface GalleriaImage {
     source: string;
     alt?: any;
     title?: any;
--- showcase/demo/galleria/galleriademo.ts
+++ showcase/demo/galleria/galleriademo.ts
@@ -1,6 +1,6 @@
 import {Component} from 'angular2/core';
 import {Galleria} from '../../../components/galleria/galleria';
-import {GalleriaImages} from '../../../components/galleria/galleria';
+import {GalleriaImage} from '../../../components/galleria/galleria';
 import {CodeHighlighter} from '../../../components/codehighlighter/codehighlighter';
 import {TabView} from '../../../components/tabview/tabview';
 import {TabPanel} from '../../../components/tabview/tabpanel';
@@ -11,23 +11,24 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
     directives: [Galleria,TabView,TabPanel,CodeHighlighter,ROUTER_DIRECTIVES]
 })
 export class GalleriaDemo {
-    images: GalleriaImages[];
+    
+    images: GalleriaImage[];
     
     constructor() {
         setTimeout(() => {
             this.images = [];
-            this.images.push({source:'showcase/resources/demo/images/galleria/galleria1.jpg', alt:'1', title:'Image 1'});
-            this.images.push({source:'showcase/resources/demo/images/galleria/galleria2.jpg', alt:'2', title:'Image 2'});
-            this.images.push({source:'showcase/resources/demo/images/galleria/galleria3.jpg', alt:'3', title:'Image 3'});
-            this.images.push({source:'showcase/resources/demo/images/galleria/galleria4.jpg', alt:'4', title:'Image 4'});
-            this.images.push({source:'showcase/resources/demo/images/galleria/galleria5.jpg', alt:'5', title:'Image 5'});
-            this.images.push({source:'showcase/resources/demo/images/galleria/galleria6.jpg', alt:'6', title:'Image 6'});
-            this.images.push({source:'showcase/resources/demo/images/galleria/galleria7.jpg', alt:'7', title:'Image 7'});
-            this.images.push({source:'showcase/resources/demo/images/galleria/galleria8.jpg', alt:'8', title:'Image 8'});
-            this.images.push({source:'showcase/resources/demo/images/galleria/galleria9.jpg', alt:'9', title:'Image 9'});
-            this.images.push({source:'showcase/resources/demo/images/galleria/galleria10.jpg', alt:'10', title:'Image 10'});
-            this.images.push({source:'showcase/resources/demo/images/galleria/galleria11.jpg', alt:'11', title:'Image 11'});
-            this.images.push({source:'showcase/resources/demo/images/galleria/galleria12.jpg', alt:'12', title:'Image 12'});
+            this.images.push({source:'showcase/resources/demo/images/galleria/galleria1.jpg', alt:'Description for Image 1', title:'Title 1'});
+            this.images.push({source:'showcase/resources/demo/images/galleria/galleria2.jpg', alt:'Description for Image 2', title:'Title 2'});
+            this.images.push({source:'showcase/resources/demo/images/galleria/galleria3.jpg', alt:'Description for Image 3', title:'Title 3'});
+            this.images.push({source:'showcase/resources/demo/images/galleria/galleria4.jpg', alt:'Description for Image 4', title:'Title 4'});
+            this.images.push({source:'showcase/resources/demo/images/galleria/galleria5.jpg', alt:'Description for Image 5', title:'Title 5'});
+            this.images.push({source:'showcase/resources/demo/images/galleria/galleria6.jpg', alt:'Description for Image 6', title:'Title 6'});
+            this.images.push({source:'showcase/resources/demo/images/galleria/galleria7.jpg', alt:'Description for Image 7', title:'Title 7'});
+            this.images.push({source:'showcase/resources/demo/images/galleria/galleria8.jpg', alt:'Description for Image 8', title:'Title 8'});
+            this.images.push({source:'showcase/resources/demo/images/galleria/galleria9.jpg', alt:'Description for Image 9', title:'Title 9'});
+            this.images.push({source:'showcase/resources/demo/images/galleria/galleria10.jpg', alt:'Description for Image 10', title:'Title 10'});
+            this.images.push({source:'showcase/resources/demo/images/galleria/galleria11.jpg', alt:'Description for Image 11', title:'Title 11'});
+            this.images.push({source:'showcase/resources/demo/images/galleria/galleria12.jpg', alt:'Description for Image 12', title:'Title 12'});
         }, 2050);
     }
     
