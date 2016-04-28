d9cbe2ddee5c2b94942574712f58e19b5f9bf53d
Çağatay Çivici <cagatay.civici@github.com>
2016-04-28 12:26:26 +0300

    Refactor

--- components/growl/growl.ts
+++ components/growl/growl.ts
@@ -7,7 +7,7 @@ import {DomHandler} from '../dom/domhandler';
     template: `
         <div class="ui-growl ui-widget" [style.zIndex]="zIndex">
             <div *ngFor="#msg of value" class="ui-growl-item-container ui-state-highlight ui-corner-all ui-shadow" aria-live="polite"
-                [ngClass]="{'ui-growl-message-info ':msg.severity == 'info','ui-growl-message-warning':msg.severity == 'warn','ui-growl-message-error':msg.severity == 'error'}">
+                [ngClass]="{'ui-growl-message-info ':msg.severity == 'info','ui-growl-message-warn':msg.severity == 'warn','ui-growl-message-error':msg.severity == 'error'}">
                 <div class="ui-growl-item">
                      <div class="ui-growl-icon-close fa fa-close" (click)="remove(msg)"></div>
                      <span class="ui-growl-image fa fa-2x ui-growl-image-info"
