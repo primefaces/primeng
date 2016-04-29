bf50daacfc90d695b7e58e77cd97c8772da7c2db
Riccardo Merolla <riccardo.merolla@gmail.com>
2016-04-29 12:28:39 +0200

    Button import fix path case
    The Button import path was wrong

--- showcase/demo/toolbar/toolbardemo.ts
+++ showcase/demo/toolbar/toolbardemo.ts
@@ -3,7 +3,7 @@ import {Toolbar} from '../../../components/toolbar/toolbar';
 import {CodeHighlighter} from '../../../components/codehighlighter/codehighlighter';
 import {TabView} from '../../../components/tabview/tabview';
 import {TabPanel} from '../../../components/tabview/tabpanel';
-import {Button} from '../../../components/button/Button';
+import {Button} from '../../../components/button/button';
 import {SplitButton} from '../../../components/splitbutton/splitbutton';
 import {SplitButtonItem} from '../../../components/splitbutton/splitbuttonitem';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
