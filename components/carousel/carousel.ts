f8d77ac65eb393cffc44ae2bca30e0a62812cdaa
Mrtcndkn <mrtcndkn@gmail.com>
2016-05-03 15:49:10 +0300

    Carousel Implemented Natively

--- components/carousel/carousel.ts
+++ components/carousel/carousel.ts
@@ -1,87 +1,238 @@
-import {Component,ElementRef,AfterViewInit,OnDestroy,OnChanges,Input,Output,SimpleChange,EventEmitter,TemplateRef,ContentChild} from 'angular2/core';
+import {Component,ElementRef,AfterViewInit,AfterViewChecked,DoCheck,OnDestroy,OnChanges,Input,Output,SimpleChange,IterableDiffers,EventEmitter,TemplateRef,ContentChild,NgZone} from 'angular2/core';
+import {DomHandler} from '../dom/domhandler';
 
 @Component({
     selector: 'p-carousel',
     template: `
-        <div class="ui-carousel ui-widget-content ui-corner-all">
-            <div class="ui-carousel-viewport">
-                <ul>
-                    <template ngFor [ngForOf]="value" [ngForTemplate]="itemTemplate"></template>
+        <div [ngClass]="{'ui-carousel ui-widget ui-widget-content ui-corner-all':true}" [attr.style]="style" [class]="styleClass">
+            <div [ngClass]="{'ui-carousel-header ui-widget-header':true}">
+                <div class="ui-carousel-header-title">{{headerText}}</div>
+                <span class="ui-carousel-button ui-carousel-next-button fa fa-arrow-circle-right" (click)="onNextNav()" [ngClass]="{'ui-state-disabled':page === totalPages-1 && !circular}"></span>
+                <span class="ui-carousel-button ui-carousel-prev-button fa fa-arrow-circle-left" (click)="onPrevNav()" [ngClass]="{'ui-state-disabled':page === 0 && !circular}"></span>
+                <div class="ui-carousel-page-links">
+                    <a href="#" class="ui-carousel-page-link fa fa-circle-o" *ngFor="let links of anchorPageLinks;let i=index" [ngClass]="{'fa-dot-circle-o':page===i}"></a>
+                </div>
+                <select class="ui-carousel-mobiledropdown ui-widget ui-state-default ui-corner-left" (change)="setPage($event.target.value)">
+                    <option *ngFor="let option of mobileDropdownOptions" [value]="option" [selected]="value == option">{{option+1}}</option>
+                </select>
+            </div>
+            <div [ngClass]="{'ui-carousel-viewport':true}">
+                <ul class="ui-carousel-items" [style.transition]="easing">
+                    <template ngFor [ngForOf]="dataToRender" [ngForTemplate]="itemTemplate"></template>
                 </ul>
             </div>
         </div>
-    `
+    `,
+    providers: [DomHandler]
 })
-export class Carousel {
+export class Carousel implements AfterViewChecked,AfterViewInit,DoCheck{
     
     @Input() value: any[];
 
-    @Input() numVisible: number;
+    @Input() numVisible: number = 3;
 
-    @Input() firstVisible: number;
+    @Input() firstVisible: number = 0;
 
     @Input() headerText: string;
 
-    @Input() effectDuration: any;
+    @Input() effectDuration: any = 500;
 
-    @Input() circular: boolean;
+    @Input() circular: boolean = false;
 
-    @Input() breakpoint: number;
+    @Input() breakpoint: number = 560;
 
-    @Input() responsive: boolean;
+    @Input() responsive: boolean = true;
 
-    @Input() autoplayInterval: number;
+    @Input() autoplayInterval: number = 0;
 
-    @Input() easing: string;
+    @Input() easing: string = 'left 1s';
 
-    @Input() pageLinks: number;
+    @Input() pageLinks: number = 3;
 
     @Input() style: string;
 
     @Input() styleClass: string;
     
     @ContentChild(TemplateRef) itemTemplate: TemplateRef;
+    
+    private dataToRender: any[];
+    
+    private container: any;
+    
+    private viewport: any;
+    
+    private itemsContainer: any;
+    
+    private itemsCount: any;
+    
+    private items: any;
+    
+    private columns: any;
+    
+    private first: any;
+    
+    private page: any;
+    
+    private totalPages: any;
+    
+    private mobileDropdown: any;
+    
+    private pageLinksEl: any;
+    
+    private valuesChanged: any;
+    
+    private interval: any;
+    
+    private anchorPageLinks: any[];
+    
+    private mobileDropdownOptions: any[];
+    
+    differ: any;
 
-    initialized: boolean;
-
-    carouselElement: any;
-
-    constructor(private el: ElementRef) {
-        this.initialized = false;
+    constructor(private el: ElementRef, private domHandler: DomHandler, differs: IterableDiffers,private zone:NgZone) {
+        this.differ = differs.find([]).create(null);
     }
-
-    ngAfterViewInit() {
-        this.carouselElement = jQuery(this.el.nativeElement).find('> .ui-carousel > .ui-carousel-viewport > ul');
-        this.carouselElement.puicarousel({
-            numVisible: this.numVisible,
-            firstVisible: this.firstVisible,
-            headerText: this.headerText,
-            effectDuration: this.effectDuration,
-            circular: this.circular,
-            breakpoint: this.breakpoint,
-            responsive: this.responsive,
-            autoplayInterval: this.autoplayInterval,
-            easing: this.easing,
-            pageLinks: this.pageLinks,
-            style: this.style,
-            styleClass: this.styleClass,
-            enhanced: true
-        });
-        this.initialized = true;
+    
+    ngDoCheck() {
+        let changes = this.differ.diff(this.value);
+        
+        if(changes) {
+            this.dataToRender = this.value;
+            this.valuesChanged = true;
+        }
+    
+        this.generateLinks();
+        this.generateMobileDropdown();
     }
-
-    ngOnChanges(changes: {[key: string]: SimpleChange}) {
-        if (this.initialized) {
-            for (var key in changes) {
-                this.carouselElement.puicarousel('option', key, changes[key].currentValue);
-            }
+    
+    ngAfterViewChecked() {
+        if(this.valuesChanged) {
+            this.stopAutoplay();
+            this.render();
+            this.valuesChanged = false;
         }
     }
 
-    ngOnDestroy() {
-        this.carouselElement.puicarousel('destroy');
-        this.initialized = false;
-        this.carouselElement = null;
+    ngAfterViewInit() {
+        this.container = this.el.nativeElement.children[0];
+        this.viewport = this.domHandler.findSingle(this.el.nativeElement,'div.ui-carousel-viewport');
+        this.itemsContainer = this.domHandler.findSingle(this.el.nativeElement,'ul.ui-carousel-items');
+        
+        if(this.value && this.value.length) {
+            this.render();
+        } 
+    }
+    
+    generateLinks() {
+        this.anchorPageLinks = [];
+        for (let i = 0; i < this.pageLinks; i++) {
+            this.anchorPageLinks.push(i);
+        }
+    }
+    
+    generateMobileDropdown() {
+        this.mobileDropdownOptions = [];
+        for (let i = 0; i < this.numVisible * this.pageLinks; i++) {
+            this.mobileDropdownOptions.push(i);
+        }
+    }
+    
+    render() {
+        this.items = this.domHandler.find(this.itemsContainer,'li');
+        this.itemsCount = this.itemsContainer.children.length;
+        this.columns = this.numVisible;
+        this.first = this.firstVisible;
+        this.page = this.first/this.columns;
+        this.totalPages = Math.ceil(this.itemsCount/this.columns);
+        
+        this.mobileDropdown = this.domHandler.findSingle(this.el.nativeElement,'select.ui-carousel-mobiledropdown');
+        this.pageLinksEl = this.domHandler.findSingle(this.el.nativeElement,'div.ui-carousel-page-links');
+        
+        if(this.responsive) {
+            this.refreshDimensions();
+            this.container.style.width = (this.domHandler.width(this.container)) + 'px';
+        }
+        else {
+            this.calculateItemWidths();
+            this.container.style.width = (this.domHandler.width(this.container)) + 'px';
+        }
+        
+        if(this.autoplayInterval) {
+            this.circular = true;
+            this.startAutoplay();
+        }
+    }
+    
+    calculateItemWidths () {
+        let firstItem = this.items[0];
+        if(firstItem) {
+            let itemFrameWidth = this.domHandler.getOuterWidth(firstItem,true) - this.domHandler.width(firstItem);    //sum of margin, border and padding
+            for (let i = 0; i < this.items.length; i++) {
+                this.items[i].style.width = ((this.domHandler.innerWidth(this.viewport) - itemFrameWidth * this.columns) / this.columns) + 'px';
+            }
+        }
+    }
+    
+    onNextNav() {
+        let lastPage = (this.page === (this.totalPages - 1));
+
+        if(!lastPage) {
+            this.setPage(this.page + 1);
+        }
+        else if(this.circular) {
+            this.setPage(0);
+        }
+    }
+    
+    onPrevNav() {
+        if(this.page !== 0) {
+            this.setPage(this.page - 1);
+        }
+        else if(this.circular) {
+            this.setPage(this.totalPages - 1);
+        }
+    }
+    
+    setPage (p) {
+        if(p !== this.page) {
+            this.itemsContainer.style.left = (-1 * (this.domHandler.innerWidth(this.viewport) * p)) + 'px';
+            this.page = p;
+            this.first = this.page * this.columns;
+        }
+    }
+    
+    refreshDimensions() {
+        let win = window;
+        if(win.innerWidth <= this.breakpoint) {
+            this.columns = 1;
+            this.calculateItemWidths();
+            this.totalPages = this.itemsCount;
+            this.mobileDropdown.style.display = "block";
+            this.pageLinksEl.style.display = "none";
+        }
+        else {
+            this.columns = this.numVisible;
+            this.calculateItemWidths();
+            this.totalPages = Math.ceil(this.itemsCount / this.numVisible);
+            this.mobileDropdown.style.display = "none";
+            this.pageLinksEl.style.display = "block";
+        }
+
+        this.page = this.first / this.columns;
+        this.itemsContainer.style.left = (-1 * (this.domHandler.innerWidth(this.viewport) * this.page)) + 'px';
+    }
+    
+    startAutoplay() {
+        this.interval = setInterval(() => {
+            if(this.page === (this.totalPages - 1))
+                this.setPage(0);
+            else
+                this.setPage(this.page + 1);
+        }, this.autoplayInterval);
     }
 
+    stopAutoplay() {
+        clearInterval(this.interval);
+    }
+    
 }
--- showcase/demo/carousel/carouseldemo.ts
+++ showcase/demo/carousel/carouseldemo.ts
@@ -34,19 +34,32 @@ export class CarouselDemo {
 
     constructor() {
         this.msgs = [];
-        this.cars = [
-            {vin: 'r3278r2', year: 2010, brand: 'Audi', color: 'Black'},
-            {vin: 'jhto2g2', year: 2015, brand: 'BMW', color: 'White'},
-            {vin: 'h453w54', year: 2012, brand: 'Honda', color: 'Blue'},
-            {vin: 'g43gwwg', year: 1998, brand: 'Renault', color: 'White'},
-            {vin: 'gf45wg5', year: 2011, brand: 'VW', color: 'Red'},
-            {vin: 'bhv5y5w', year: 2015, brand: 'Jaguar', color: 'Blue'},
-            {vin: 'ybw5fsd', year: 2012, brand: 'Ford', color: 'Yellow'},
-            {vin: '45665e5', year: 2011, brand: 'Mercedes', color: 'Brown'},
-            {vin: 'he6sb5v', year: 2015, brand: 'Ford', color: 'Black'}
-        ];
+        setTimeout(() =>{
+            this.cars = [
+                {vin: 'r3278r2', year: 2010, brand: 'Audi', color: 'Black'},
+                {vin: 'jhto2g2', year: 2015, brand: 'BMW', color: 'White'},
+                {vin: 'h453w54', year: 2012, brand: 'Honda', color: 'Blue'},
+                {vin: 'g43gwwg', year: 1998, brand: 'Renault', color: 'White'},
+                {vin: 'gf45wg5', year: 2011, brand: 'VW', color: 'Red'},
+                {vin: 'bhv5y5w', year: 2015, brand: 'Jaguar', color: 'Blue'},
+                {vin: 'ybw5fsd', year: 2012, brand: 'Ford', color: 'Yellow'},
+                {vin: '45665e5', year: 2011, brand: 'Mercedes', color: 'Brown'},
+                {vin: 'he6sb5v', year: 2015, brand: 'Ford', color: 'Black'}
+            ];
+        },1000);
+        
     }
-
+    
+    update() {
+        this.cars = [];
+        this.cars.push({vin: 'he6sb5v', year: 2015, brand: 'Ford', color: 'Black'});
+        this.cars.push({vin: 'h453w54', year: 2012, brand: 'Honda', color: 'Blue'});
+        this.cars.push({vin: 'gf45wg5', year: 2011, brand: 'VW', color: 'Red'});
+        this.cars.push({vin: 'bhv5y5w', year: 2015, brand: 'Jaguar', color: 'Blue'});
+        this.cars.push({vin: 'jhto2g2', year: 2015, brand: 'BMW', color: 'White'});
+        this.cars.push({vin: 'r3278r2', year: 2010, brand: 'Audi', color: 'Black'});
+    }
+    
     selectCar(car: Car) {
         this.msgs = [];
         this.msgs.push({severity: 'info', summary: 'Car Selected', detail: 'Vin:' + car.vin});
