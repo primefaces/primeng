cd6f0d5fb1ace103dbbdab95940671826fbb1340
Çağatay Çivici <cagatay.civici@github.com>
2016-05-03 22:50:38 +0300

    Refactor config

--- systemjs.config.js
+++ systemjs.config.js
@@ -2,7 +2,6 @@
 
   // map tells the System loader where to look for things
   var map = {
-    'showcase':                   'showcase',
     'rxjs':                       'node_modules/rxjs',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     '@angular':                   'node_modules/@angular'
@@ -10,7 +9,7 @@
 
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
-    'showcase':                   { main: 'boot.js',  defaultExtension: 'js' },
+    'showcase':                   { main: 'boot.js', defaultExtension: 'js' },
     'components':                 { defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { defaultExtension: 'js' }
