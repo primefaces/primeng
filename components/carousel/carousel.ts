35cbbb9318f9cd566ed8c2cbf6c31c3d26efc36b
Çağatay Çivici <cagatay.civici@github.com>
2016-05-04 16:54:12 +0300

    Refactored Carousel

--- components/carousel/carousel.ts
+++ components/carousel/carousel.ts
@@ -1,31 +1,38 @@
-import {Component,ElementRef,AfterViewInit,AfterViewChecked,DoCheck,OnDestroy,OnChanges,Input,Output,SimpleChange,IterableDiffers,EventEmitter,TemplateRef,ContentChild,NgZone} from '@angular/core';
+import {Component,ElementRef,OnInit,AfterViewInit,AfterViewChecked,DoCheck,OnDestroy,Input,Output,IterableDiffers,TemplateRef,ContentChild,Renderer} from '@angular/core';
 import {DomHandler} from '../dom/domhandler';
 
 @Component({
     selector: 'p-carousel',
     template: `
         <div [ngClass]="{'ui-carousel ui-widget ui-widget-content ui-corner-all':true}" [attr.style]="style" [class]="styleClass">
-            <div [ngClass]="{'ui-carousel-header ui-widget-header':true}">
+            <div class="ui-carousel-header ui-widget-header">
                 <div class="ui-carousel-header-title">{{headerText}}</div>
-                <span class="ui-carousel-button ui-carousel-next-button fa fa-arrow-circle-right" (click)="onNextNav()" [ngClass]="{'ui-state-disabled':page === totalPages-1 && !circular}"></span>
-                <span class="ui-carousel-button ui-carousel-prev-button fa fa-arrow-circle-left" (click)="onPrevNav()" [ngClass]="{'ui-state-disabled':page === 0 && !circular}"></span>
-                <div class="ui-carousel-page-links">
+                <span class="ui-carousel-button ui-carousel-next-button fa fa-arrow-circle-right" (click)="onNextNav()" 
+                        [ngClass]="{'ui-state-disabled':(page === (totalPages-1)) && !circular}"></span>
+                <span class="ui-carousel-button ui-carousel-prev-button fa fa-arrow-circle-left" (click)="onPrevNav()" 
+                        [ngClass]="{'ui-state-disabled':(page === 0 && !circular)}"></span>
+                <div *ngIf="displayPageLinks" class="ui-carousel-page-links">
                     <a href="#" class="ui-carousel-page-link fa fa-circle-o" *ngFor="let links of anchorPageLinks;let i=index" [ngClass]="{'fa-dot-circle-o':page===i}"></a>
                 </div>
-                <select class="ui-carousel-mobiledropdown ui-widget ui-state-default ui-corner-left" (change)="setPage($event.target.value)">
+                <select *ngIf="displayPageDropdown" class="ui-carousel-dropdown ui-widget ui-state-default ui-corner-left" [value]="page" (change)="setPage($event.target.value)">
+                    <option *ngFor="let option of selectDropdownOptions" [value]="option" [selected]="value == option">{{option+1}}</option>
+                </select>
+                <select *ngIf="responsive" class="ui-carousel-mobiledropdown ui-widget ui-state-default ui-corner-left" [value]="page" (change)="setPage($event.target.value)"
+                    [style.display]="shrinked ? 'block' : 'none'">
                     <option *ngFor="let option of mobileDropdownOptions" [value]="option" [selected]="value == option">{{option+1}}</option>
                 </select>
             </div>
-            <div [ngClass]="{'ui-carousel-viewport':true}">
-                <ul class="ui-carousel-items" [style.transition]="easing">
-                    <template ngFor [ngForOf]="dataToRender" [ngForTemplate]="itemTemplate"></template>
+            <div class="ui-carousel-viewport">
+                <ul class="ui-carousel-items" [style.left.px]="left" [style.transitionProperty]="'left'" 
+                            [style.transitionDuration]="effectDuration" [style.transitionTimingFunction]="effect">
+                    <template ngFor [ngForOf]="value" [ngForTemplate]="itemTemplate"></template>
                 </ul>
             </div>
         </div>
     `,
     providers: [DomHandler]
 })
-export class Carousel implements AfterViewChecked,AfterViewInit,DoCheck{
+export class Carousel implements OnInit,AfterViewChecked,AfterViewInit,DoCheck,OnDestroy{
     
     @Input() value: any[];
 
@@ -35,8 +42,6 @@ export class Carousel implements AfterViewChecked,AfterViewInit,DoCheck{
 
     @Input() headerText: string;
 
-    @Input() effectDuration: any = 500;
-
     @Input() circular: boolean = false;
 
     @Input() breakpoint: number = 560;
@@ -44,8 +49,10 @@ export class Carousel implements AfterViewChecked,AfterViewInit,DoCheck{
     @Input() responsive: boolean = true;
 
     @Input() autoplayInterval: number = 0;
-
-    @Input() easing: string = 'left 1s';
+    
+    @Input() effectDuration: any = '1s';
+        
+    @Input() effect: string = 'ease-out';
 
     @Input() pageLinks: number = 3;
 
@@ -54,31 +61,21 @@ export class Carousel implements AfterViewChecked,AfterViewInit,DoCheck{
     @Input() styleClass: string;
     
     @ContentChild(TemplateRef) itemTemplate: TemplateRef<any>;
+        
+    private container: any;    
     
-    private dataToRender: any[];
-    
-    private container: any;
+    private left: any = 0;
     
     private viewport: any;
     
     private itemsContainer: any;
     
-    private itemsCount: any;
-    
     private items: any;
     
     private columns: any;
-    
-    private first: any;
-    
-    private page: any;
-    
-    private totalPages: any;
-    
-    private mobileDropdown: any;
-    
-    private pageLinksEl: any;
-    
+        
+    private page: number;
+                    
     private valuesChanged: any;
     
     private interval: any;
@@ -87,9 +84,15 @@ export class Carousel implements AfterViewChecked,AfterViewInit,DoCheck{
     
     private mobileDropdownOptions: any[];
     
+    private selectDropdownOptions: any[];
+    
+    private shrinked: boolean;
+    
+    documentResponsiveListener: any;
+    
     differ: any;
 
-    constructor(private el: ElementRef, private domHandler: DomHandler, differs: IterableDiffers,private zone:NgZone) {
+    constructor(private el: ElementRef, private domHandler: DomHandler, differs: IterableDiffers, private renderer: Renderer) {
         this.differ = differs.find([]).create(null);
     }
     
@@ -97,62 +100,82 @@ export class Carousel implements AfterViewChecked,AfterViewInit,DoCheck{
         let changes = this.differ.diff(this.value);
         
         if(changes) {
-            this.dataToRender = this.value;
+            if(this.value && this.value.length) {
+                if(this.value.length && this.firstVisible >= this.value.length) {
+                    this.setPage(this.totalPages - 1);
+                }
+            }
+            else {
+                this.setPage(0);
+            }
+
             this.valuesChanged = true;
+            
+            if(this.autoplayInterval) {
+                this.stopAutoplay();
+            }
+            
+            this.updateMobileDropdown();
+            this.updateLinks();
+            this.updateDropdown();
         }
-    
-        this.generateLinks();
-        this.generateMobileDropdown();
     }
     
     ngAfterViewChecked() {
         if(this.valuesChanged) {
-            this.stopAutoplay();
             this.render();
             this.valuesChanged = false;
         }
     }
+    
+    ngOnInit() {
+        this.columns = this.numVisible;
+        this.page = Math.floor(this.firstVisible / this.columns);
+    }
 
     ngAfterViewInit() {
         this.container = this.el.nativeElement.children[0];
-        this.viewport = this.domHandler.findSingle(this.el.nativeElement,'div.ui-carousel-viewport');
-        this.itemsContainer = this.domHandler.findSingle(this.el.nativeElement,'ul.ui-carousel-items');
+        this.viewport = this.domHandler.findSingle(this.el.nativeElement, 'div.ui-carousel-viewport');
+        this.itemsContainer = this.domHandler.findSingle(this.el.nativeElement, 'ul.ui-carousel-items');
+        this.shrinked = (window.innerWidth <= this.breakpoint);
+
+        if(this.responsive) {
+            this.documentResponsiveListener = this.renderer.listenGlobal('window', 'resize', (event) => {
+                this.updateState();
+            });
+        }
         
         if(this.value && this.value.length) {
             this.render();
-        } 
+        }
     }
     
-    generateLinks() {
+    updateLinks() {
         this.anchorPageLinks = [];
-        for (let i = 0; i < this.pageLinks; i++) {
+        for (let i = 0; i < this.totalPages; i++) {
             this.anchorPageLinks.push(i);
         }
     }
     
-    generateMobileDropdown() {
+    updateDropdown() {
+        this.selectDropdownOptions = [];
+        for (let i = 0; i < this.totalPages; i++) {
+            this.selectDropdownOptions.push(i);
+        }
+    }
+    
+    updateMobileDropdown() {
         this.mobileDropdownOptions = [];
-        for (let i = 0; i < this.numVisible * this.pageLinks; i++) {
+        for (let i = 0; i < this.value.length; i++) {
             this.mobileDropdownOptions.push(i);
         }
     }
     
     render() {
         this.items = this.domHandler.find(this.itemsContainer,'li');
-        this.itemsCount = this.itemsContainer.children.length;
-        this.columns = this.numVisible;
-        this.first = this.firstVisible;
-        this.page = this.first/this.columns;
-        this.totalPages = Math.ceil(this.itemsCount/this.columns);
+        this.calculateItemWidths();
         
-        this.mobileDropdown = this.domHandler.findSingle(this.el.nativeElement,'select.ui-carousel-mobiledropdown');
-        this.pageLinksEl = this.domHandler.findSingle(this.el.nativeElement,'div.ui-carousel-page-links');
-        
-        if(this.responsive) {
-            this.refreshDimensions();
-        }
-        else {
-            this.calculateItemWidths();
+        if(!this.responsive) {
             this.container.style.width = (this.domHandler.width(this.container)) + 'px';
         }
         
@@ -174,50 +197,60 @@ export class Carousel implements AfterViewChecked,AfterViewInit,DoCheck{
     onNextNav() {
         let lastPage = (this.page === (this.totalPages - 1));
 
-        if(!lastPage) {
+        if(!lastPage)
             this.setPage(this.page + 1);
-        }
-        else if(this.circular) {
+        else if(this.circular)
             this.setPage(0);
-        }
     }
     
     onPrevNav() {
-        if(this.page !== 0) {
+        if(this.page !== 0)
             this.setPage(this.page - 1);
-        }
-        else if(this.circular) {
+        else if(this.circular)
             this.setPage(this.totalPages - 1);
-        }
     }
     
-    setPage (p) {
-        if(p !== this.page) {
-            this.itemsContainer.style.left = (-1 * (this.domHandler.innerWidth(this.viewport) * p)) + 'px';
+    setPage(p, enforce?: boolean) {
+        if(p !== this.page || enforce) {
             this.page = p;
-            this.first = this.page * this.columns;
+            this.left = (-1 * (this.domHandler.innerWidth(this.viewport) * this.page));
+            this.firstVisible = this.page * this.columns;
         }
     }
     
-    refreshDimensions() {
+    get displayPageLinks(): boolean {
+        return (this.totalPages <= this.pageLinks && !this.shrinked);
+    }
+    
+    get displayPageDropdown(): boolean {
+        return (this.totalPages > this.pageLinks && !this.shrinked);
+    }
+    
+    get totalPages(): number {
+        return (this.value && this.value.length) ? Math.ceil(this.value.length / this.columns) : 0;
+    }
+        
+    routerDisplay () {
+        let win = window;
+        if(win.innerWidth <= this.breakpoint)
+            return true;
+        else
+            return false;
+    }
+    
+    updateState() {
         let win = window;
         if(win.innerWidth <= this.breakpoint) {
+            this.shrinked = true;
             this.columns = 1;
-            this.calculateItemWidths();
-            this.totalPages = this.itemsCount;
-            this.mobileDropdown.style.display = "block";
-            this.pageLinksEl.style.display = "none";
         }
         else {
+            this.shrinked = false;
             this.columns = this.numVisible;
-            this.calculateItemWidths();
-            this.totalPages = Math.ceil(this.itemsCount / this.numVisible);
-            this.mobileDropdown.style.display = "none";
-            this.pageLinksEl.style.display = "block";
         }
-
-        this.page = this.first / this.columns;
-        this.itemsContainer.style.left = (-1 * (this.domHandler.innerWidth(this.viewport) * this.page)) + 'px';
+        
+        this.calculateItemWidths();
+        this.setPage(Math.floor(this.firstVisible / this.columns), true);
     }
     
     startAutoplay() {
@@ -226,11 +259,22 @@ export class Carousel implements AfterViewChecked,AfterViewInit,DoCheck{
                 this.setPage(0);
             else
                 this.setPage(this.page + 1);
-        }, this.autoplayInterval);
+        }, 
+        this.autoplayInterval);
     }
 
     stopAutoplay() {
         clearInterval(this.interval);
+    }
+    
+    ngOnDestroy() {
+        if(this.responsive) {
+            this.documentResponsiveListener();
+        }
+        
+        if(this.autoplayInterval) {
+            this.stopAutoplay();
+        }
     }
     
 }
