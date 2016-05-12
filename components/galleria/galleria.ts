e34e57a5fbcaf40a2b8498105654532c7ab0f9f5
Nick Richardson <nrichardson@istreamfs.com>
2016-05-12 14:04:21 -0500

    Hide nav buttons when unusable. Also fixed issue where title and alt attributes were being looked for when the images array is empty

--- components/galleria/galleria.ts
+++ components/galleria/galleria.ts
@@ -22,10 +22,10 @@ import {DomHandler} from '../dom/domhandler';
                     </li>
                 </ul>
             </div>
-            <div class="ui-galleria-nav-prev fa fa-fw fa-chevron-circle-left" (click)="clickNavLeft()" [style.bottom.px]="frameHeight/2"></div>
-            <div class="ui-galleria-nav-next fa fa-fw fa-chevron-circle-right" (click)="clickNavRight()" [style.bottom.px]="frameHeight/2"></div>
+            <div class="ui-galleria-nav-prev fa fa-fw fa-chevron-circle-left" (click)="clickNavLeft()" [style.bottom.px]="frameHeight/2" *ngIf="activeIndex !== 0"></div>
+            <div class="ui-galleria-nav-next fa fa-fw fa-chevron-circle-right" (click)="clickNavRight()" [style.bottom.px]="frameHeight/2" *ngIf="images?.length > 0 && activeIndex !== (images.length -1)"></div>
             <div class="ui-galleria-caption" *ngIf="showCaption&&images" style="display:block">
-                <h4>{{images[activeIndex].title}}</h4><p>{{images[activeIndex].alt}}</p>
+                <h4>{{images[activeIndex]?.title}}</h4><p>{{images[activeIndex]?.alt}}</p>
             </div>
         </div>
     `,
