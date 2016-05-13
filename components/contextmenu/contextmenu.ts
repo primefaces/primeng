4906b538910c938c5e16e5fc065331ae174ea787
Çağatay Çivici <cagatay.civici@github.com>
2016-05-13 16:12:05 +0300

    ContextMenu reimpl as native

--- components/contextmenu/contextmenu.ts
+++ components/contextmenu/contextmenu.ts
@@ -1,54 +1,167 @@
-import {Component,ElementRef,AfterViewInit,OnDestroy,OnChanges,Input,Output,SimpleChange} from '@angular/core';
+import {Component,ElementRef,AfterViewInit,OnDestroy,Input,Output,Renderer,EventEmitter} from '@angular/core';
+import {DomHandler} from '../dom/domhandler';
+import {MenuItem} from '../api/menumodel';
+
+@Component({
+    selector: 'p-contextMenuSub',
+    template: `
+        <ul [ngClass]="{'ui-helper-reset':root, 'ui-widget-content ui-corner-all ui-helper-clearfix ui-menu-child ui-shadow':!root}" class="ui-menu-list"
+            (click)="listClick($event)">
+            <template ngFor let-child [ngForOf]="(root ? item : item.items)">
+                <li #item [ngClass]="{'ui-menuitem ui-widget ui-corner-all':true,'ui-menu-parent':child.items,'ui-menuitem-active':item==activeItem}"
+                    (mouseenter)="onItemMouseEnter($event, item)" (mouseleave)="onItemMouseLeave($event, item)">
+                    <a #link [href]="child.url||'#'" class="ui-menuitem-link ui-corner-all" [ngClass]="{'ui-state-hover':link==activeLink}" (click)="itemClick($event, child)">
+                        <span class="ui-submenu-icon fa fa-fw fa-caret-right" *ngIf="child.items"></span>
+                        <span class="ui-menuitem-icon fa fa-fw" *ngIf="child.icon" [ngClass]="child.icon"></span>
+                        <span class="ui-menuitem-text">{{child.label}}</span>
+                    </a>
+                    <p-contextMenuSub class="ui-submenu" [item]="child" *ngIf="child.items"></p-contextMenuSub>
+                </li>
+            </template>
+        </ul>
+    `,
+    directives: [ContextMenuSub],
+    providers: [DomHandler]
+})
+export class ContextMenuSub {
+
+    @Input() item: MenuItem;
+    
+    @Input() root: boolean;
+    
+    constructor(private domHandler: DomHandler) {}
+    
+    activeItem: any;
+    
+    activeLink: any;
+            
+    onItemMouseEnter(event, item) {
+        this.activeItem = item;
+        this.activeLink = item.children[0];
+        let nextElement =  item.children[0].nextElementSibling;
+        if(nextElement) {
+            let sublist = nextElement.children[0];
+            sublist.style.zIndex = ++DomHandler.zindex;
+                        
+            sublist.style.top = '0px';
+            sublist.style.left = this.domHandler.getOuterWidth(item.children[0]) + 'px';
+        }
+    }
+    
+    onItemMouseLeave(event, link) {
+        this.activeItem = null;
+        this.activeLink = null;
+    }
+    
+    itemClick(event, item: MenuItem) {
+        if(item.command) {
+            if(!item.eventEmitter) {
+                item.eventEmitter = new EventEmitter();
+                item.eventEmitter.subscribe(item.command);
+            }
+            
+            item.eventEmitter.emit(event);
+        }
+        
+        if(!item.url) {
+            event.preventDefault();
+        }
+    }
+    
+    listClick(event) {
+        this.activeItem = null;
+        this.activeLink = null;
+    }
+
+}
 
 @Component({
     selector: 'p-contextMenu',
     template: `
-        <div [class]="styleClass" [ngStyle]="style" [ngClass]="{'ui-menu ui-widget ui-widget-content ui-corner-all ui-helper-clearfix ui-contextmenu ui-menu-dynamic ui-shadow':true}">
-            <ng-content></ng-content>
+        <div [ngClass]="'ui-contextmenu ui-menu ui-widget ui-widget-content ui-corner-all ui-helper-clearfix ui-menu-dynamic ui-shadow'" 
+            [class]="styleClass" [ngStyle]="style" [style.display]="visible ? 'block' : 'none'" [style.left.px]="left" [style.top.px]="top">
+            <p-contextMenuSub [item]="model" root="root"></p-contextMenuSub>
         </div>
-    `
+    `,
+    providers: [DomHandler],
+    directives: [ContextMenuSub]
 })
-export class ContextMenu {
+export class ContextMenu implements AfterViewInit,OnDestroy {
+
+    @Input() model: MenuItem[];
     
     @Input() global: boolean;
 
     @Input() style: any;
 
     @Input() styleClass: string;
-
-    initialized: boolean;
-
-    menuElement: any;
-
-    constructor(private el: ElementRef) {
-        this.initialized = false;
-    }
+    
+    visible: boolean;
+    
+    left: number;
+    
+    top: number;
+    
+    container: any;
+    
+    documentClickListener: any;
+    
+    documentRightClickListener: any;
+        
+    constructor(private el: ElementRef, private domHandler: DomHandler, private renderer: Renderer) {}
 
     ngAfterViewInit() {
-        this.menuElement = jQuery(this.el.nativeElement).find('> div > ul');
-        this.menuElement.puicontextmenu({
-            target: this.global ? document : null,
-            enhanced: true
+        this.container = this.el.nativeElement.children[0];
+        
+        this.documentClickListener = this.renderer.listenGlobal('body', 'click', () => {
+            this.hide();
         });
-        this.initialized = true;
+        
+        this.documentRightClickListener = this.renderer.listenGlobal('body', 'contextmenu', (event) => {
+            this.show(event);
+            event.preventDefault();
+        });
     }
-
-    ngOnChanges(changes: {[key: string]: SimpleChange}) {
-        if (this.initialized) {
-            for (var key in changes) {
-                this.menuElement.puicontextmenu('option', key, changes[key].currentValue);
-            }
-        }
-    }
-
-    ngOnDestroy() {
-        this.menuElement.puicontextmenu('destroy');
-        this.initialized = false;
-        this.menuElement = null;
+    
+    toggle(event) {
+        if(this.container.offsetParent)
+            this.hide();
+        else
+            this.show(event);
     }
     
     show(event) {
-        this.menuElement.puicontextmenu('show', event);
+        this.left = event.pageX;
+        this.top = event.pageY;
+        this.visible = true;
+        this.domHandler.fadeIn(this.container, 250);
+    }
+    
+    hide() {
+        this.visible = false;
+    }
+
+    unsubscribe(item: any) {
+        if(item.eventEmitter) {
+            item.eventEmitter.unsubscribe();
+        }
+        
+        if(item.items) {
+            for(let childItem of item.items) {
+                this.unsubscribe(childItem);
+            }
+        }
+    }
+    
+    ngOnDestroy() {
+        this.documentClickListener();
+        this.documentRightClickListener();
+        
+        if(this.model) {
+            for(let item of this.model) {
+                this.unsubscribe(item);
+            }
+        }
     }
 
 }
