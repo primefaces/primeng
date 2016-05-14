dab5a6f6fda73f5fe31f0bc5e32c5fb2c32eac35
Çağatay Çivici <cagatay.civici@github.com>
2016-05-14 21:00:12 +0300

    Initial run at native slide menu

--- components/slidemenu/slidemenu.ts
+++ components/slidemenu/slidemenu.ts
@@ -1,71 +1,176 @@
-import {Component,ElementRef,AfterViewInit,OnDestroy,OnChanges,Input,Output,SimpleChange} from '@angular/core';
+import {Component,ElementRef,AfterViewInit,OnDestroy,Input,Output,Renderer,EventEmitter} from '@angular/core';
+import {DomHandler} from '../dom/domhandler';
+import {MenuItem} from '../api/menumodel';
+
+@Component({
+    selector: 'p-slideMenuSub',
+    template: `
+        <ul [ngClass]="{'ui-helper-reset ui-menu-rootlist':root, 'ui-widget-content ui-corner-all ui-helper-clearfix ui-menu-child':!root}" class="ui-menu-list"
+            (click)="listClick($event)" [style.width.px]="menuWidth" 
+            [style.transitionProperty]="root ? 'left' : 'none'" [style.transitionDuration]="effectDuration" [style.transitionTimingFunction]="easing">
+            <template ngFor let-child [ngForOf]="(root ? item : item.items)">
+                <li #item [ngClass]="{'ui-menuitem ui-widget ui-corner-all':true,'ui-menu-parent':child.items,'ui-menuitem-active':item==activeItem}"
+                    (mouseenter)="onItemMouseEnter($event, item)" (mouseleave)="onItemMouseLeave($event, item)">
+                    <a #link [href]="child.url||'#'" class="ui-menuitem-link ui-corner-all" [ngClass]="{'ui-state-hover':link==activeLink}" (click)="itemClick($event, child)">
+                        <span class="ui-submenu-icon fa fa-fw fa-caret-right" *ngIf="child.items"></span>
+                        <span class="ui-menuitem-icon fa fa-fw" *ngIf="child.icon" [ngClass]="child.icon"></span>
+                        <span class="ui-menuitem-text">{{child.label}}</span>
+                    </a>
+                    <p-slideMenuSub [item]="child" [menuWidth]="menuWidth" *ngIf="child.items"></p-slideMenuSub>
+                </li>
+            </template>
+        </ul>
+    `,
+    directives: [SlideMenuSub],
+    providers: [DomHandler]
+})
+export class SlideMenuSub {
+
+    @Input() item: MenuItem;
+    
+    @Input() root: boolean;
+    
+    @Input() backLabel: string = 'Back';
+    
+    @Input() menuWidth: string;
+    
+    @Input() effectDuration: any = '1s';
+        
+    @Input() easing: string = 'ease-out';
+        
+    constructor(private domHandler: DomHandler) {}
+    
+    activeItem: any;
+    
+    activeLink: any;
+            
+    onItemMouseEnter(event, item) {
+        this.activeItem = item;
+        this.activeLink = item.children[0];
+    }
+    
+    onItemMouseLeave(event, link) {
+        this.activeItem = null;
+        this.activeLink = null;
+    }
+    
+    itemClick(event, item: MenuItem) {
+        if(item.command) {
+            if(!item.eventEmitter && item.command) {
+                item.eventEmitter = new EventEmitter();
+                item.eventEmitter.subscribe(item.command);
+            }
+            
+            item.eventEmitter.emit(event);
+        }
+        else if(!item.url) {
+            event.preventDefault();
+        }
+    }
+    
+    listClick(event) {
+        this.activeItem = null;
+        this.activeLink = null;
+    }
+
+}
 
 @Component({
     selector: 'p-slideMenu',
     template: `
-        <div [class]="styleClass" [ngStyle]="style" [ngClass]="{'ui-menu ui-slidemenu ui-widget ui-widget-content ui-corner-all':true}">
-            <div class="ui-slidemenu-wrapper">
-                <div class="ui-slidemenu-content">
-                    <ng-content></ng-content>
+        <div [ngClass]="{'ui-menu ui-slidemenu ui-widget ui-widget-content ui-corner-all':true,'ui-menu-dynamic ui-shadow':popup}" 
+            [class]="styleClass" [ngStyle]="style">
+            <div class="ui-slidemenu-wrapper" style="height: 172.76px">
+                <div class="ui-slidemenu-content" style="height: 143px">
+                    <p-slideMenuSub [item]="model" root="root" [menuWidth]="menuWidth" [effectDuration]="effectDuration" [easing]="easing"></p-slideMenuSub>
                 </div>
                 <div class="ui-slidemenu-backward ui-widget-header ui-corner-all">
                     <span class="fa fa-fw fa-caret-left"></span>{{backLabel}}
                 </div>
             </div>
         </div>
-    `
+    `,
+    providers: [DomHandler],
+    directives: [SlideMenuSub]
 })
-export class SlideMenu {
+export class SlideMenu implements AfterViewInit,OnDestroy {
+
+    @Input() model: MenuItem[];
 
     @Input() popup: boolean;
-
-    @Input() trigger: any;
-
-    @Input() my: string;
-
-    @Input() at: string;
-
-    @Input() triggerEvent: string;
-
-    @Input() backLabel: string = 'Back';
 
     @Input() style: any;
 
     @Input() styleClass: string;
-
-    initialized: boolean;
-
-    menuElement: any;
-
-    constructor(private el: ElementRef) {
-        this.initialized = false;
-    }
+    
+    @Input() menuWidth: number = 180;
+    
+    @Input() effectDuration: any = '1s';
+        
+    @Input() easing: string = 'ease-out';
+    
+    container: any;
+    
+    documentClickListener: any;
+    
+    preventDocumentDefault: any;
+        
+    constructor(private el: ElementRef, private domHandler: DomHandler, private renderer: Renderer) {}
 
     ngAfterViewInit() {
-        this.menuElement = jQuery(this.el.nativeElement).find('> div > div > div > ul');
-        this.menuElement.puislidemenu({
-            enhanced: true,
-            popup: this.popup,
-            trigger: this.trigger ? jQuery(this.trigger): null,
-            my: this.my,
-            at: this.at,
-            triggerEvent: this.triggerEvent
-        });
-        this.initialized = true;
+        this.container = this.el.nativeElement.children[0];
+        
+        if(this.popup) {
+            this.documentClickListener = this.renderer.listenGlobal('body', 'click', () => {
+                if(!this.preventDocumentDefault) {
+                    this.hide();
+                }
+                this.preventDocumentDefault = false;
+            });
+        }
+    }
+    
+    toggle(event) {
+        if(this.container.offsetParent)
+            this.hide();
+        else
+            this.show(event);
+            
+        this.preventDocumentDefault = true;
+    }
+    
+    show(event) {
+        this.container.style.display = 'block';
+        this.domHandler.absolutePosition(this.container, event.target);
+        this.domHandler.fadeIn(this.container, 250);
+    }
+    
+    hide() {
+        this.container.style.display = 'none';
     }
 
-    ngOnChanges(changes: {[key: string]: SimpleChange}) {
-        if (this.initialized) {
-            for (var key in changes) {
-                this.menuElement.puislidemenu('option', key, changes[key].currentValue);
+    unsubscribe(item: any) {
+        if(item.eventEmitter) {
+            item.eventEmitter.unsubscribe();
+        }
+        
+        if(item.items) {
+            for(let childItem of item.items) {
+                this.unsubscribe(childItem);
             }
         }
     }
-
+    
     ngOnDestroy() {
-        this.menuElement.puislidemenu('destroy');
-        this.initialized = false;
-        this.menuElement = null;
+        if(this.popup) {
+            this.documentClickListener();
+        }
+        
+        if(this.model) {
+            for(let item of this.model) {
+                this.unsubscribe(item);
+            }
+        }
     }
 
 }
