aec3b1b85f15768ee8a2f37d5ab3c77ee4f973f4
Çağatay Çivici <cagatay.civici@github.com>
2016-05-14 13:10:50 +0300

    Fixed #354

---
+++ showcase/demo/tabmenu/tabmenudemo.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {TabMenu} from '../../../components/tabmenu/tabmenu';
+import {CodeHighlighter} from '../../../components/codehighlighter/codehighlighter';
+import {Button} from '../../../components/button/button';
+import {TabView} from '../../../components/tabview/tabview';
+import {TabPanel} from '../../../components/tabview/tabpanel';
+import {ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+import {MenuItem} from '../../../components/api/menumodel';
+
+@Component({
+    templateUrl: 'showcase/demo/tabmenu/tabmenudemo.html',
+    directives: [TabMenu,Button,TabPanel,TabView,CodeHighlighter,ROUTER_DIRECTIVES]
+})
+export class TabMenuDemo {
+    
+    private items: MenuItem[];
+
+    ngOnInit() {
+        this.items = [
+            {label: 'Stats', icon: 'fa-bar-chart'},
+            {label: 'Calendar', icon: 'fa-calendar'},
+            {label: 'Documentation', icon: 'fa-book'},
+            {label: 'Support', icon: 'fa-support'},
+            {label: 'Social', icon: 'fa-twitter'}
+        ];
+    }
+}
