c1f69fda373833537537849e4cfa76cf81d4e79b
Çağatay Çivici <cagatay.civici@github.com>
2016-06-18 14:01:30 +0300

    Base config for new forms

--- showcase/boot.ts
+++ showcase/boot.ts
@@ -4,10 +4,13 @@ import {AppComponent} from './app.component'
 import {APP_ROUTER_PROVIDERS} from './app.routes';
 import {enableProdMode} from '@angular/core';
 import {LocationStrategy,HashLocationStrategy} from '@angular/common';
+import {disableDeprecatedForms, provideForms} from '@angular/forms';
 import 'rxjs/Rx';
 
 //enableProdMode();
 bootstrap(AppComponent, [
   APP_ROUTER_PROVIDERS,
+  disableDeprecatedForms(),
+  provideForms(),
   provide(LocationStrategy, {useClass: HashLocationStrategy})
 ]);
