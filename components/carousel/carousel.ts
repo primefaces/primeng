c124fc0a6352df01b6e0361354b0be252c18e787
Çağatay Çivici <cagatay.civici@github.com>
2016-08-08 16:23:20 +0300

    Fixed #554

--- components/carousel/carousel.ts
+++ components/carousel/carousel.ts
@@ -13,7 +13,7 @@ import {TemplateWrapper} from '../common';
                 <span class="ui-carousel-button ui-carousel-prev-button fa fa-arrow-circle-left" (click)="onPrevNav()" 
                         [ngClass]="{'ui-state-disabled':(page === 0 && !circular)}"></span>
                 <div *ngIf="displayPageLinks" class="ui-carousel-page-links">
-                    <a href="#" class="ui-carousel-page-link fa fa-circle-o" *ngFor="let links of anchorPageLinks;let i=index" [ngClass]="{'fa-dot-circle-o':page===i}"></a>
+                    <a href="#" (click)="setPageWithLink($event,i)" class="ui-carousel-page-link fa fa-circle-o" *ngFor="let links of anchorPageLinks;let i=index" [ngClass]="{'fa-dot-circle-o':page===i}"></a>
                 </div>
                 <select *ngIf="displayPageDropdown" class="ui-carousel-dropdown ui-widget ui-state-default ui-corner-left" [value]="page" (change)="onDropdownChange($event.target.value)">
                     <option *ngFor="let option of selectDropdownOptions" [value]="option" [selected]="value == option">{{option+1}}</option>
@@ -218,6 +218,11 @@ export class Carousel implements OnInit,AfterViewChecked,AfterViewInit,DoCheck,O
             this.setPage(this.page - 1);
         else if(this.circular)
             this.setPage(this.totalPages - 1);
+    }
+    
+    setPageWithLink(event, p: number) {
+        this.setPage(p);
+        event.preventDefault();
     }
     
     setPage(p, enforce?: boolean) {
