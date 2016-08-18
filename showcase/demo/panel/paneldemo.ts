c8e5978708c5a9c803fc6dea12b2612c6a849365
Çağatay Çivici <cagatay.civici@github.com>
2016-08-18 15:59:52 +0300

    Demo update

--- showcase/demo/panel/paneldemo.ts
+++ showcase/demo/panel/paneldemo.ts
@@ -1,12 +1,28 @@
-import {Component} from '@angular/core';
+import {Component,OnInit} from '@angular/core';
 import {Message} from '../../../components/common/api';
+import {MenuItem} from '../../../components/common/api';
 
 @Component({
     templateUrl: 'showcase/demo/panel/paneldemo.html'
 })
-export class PanelDemo {
+export class PanelDemo implements OnInit {
 
     msgs: Message[] = [];
+    
+    items: MenuItem[];
+    
+    ngOnInit() {
+        this.items = [
+            {label: 'Update', icon: 'fa-refresh', command: () => {
+                this.update();
+            }},
+            {label: 'Delete', icon: 'fa-close', command: () => {
+                this.delete();
+            }},
+            {label: 'Angular.io', icon: 'fa-link', url: 'http://angular.io'},
+            {label: 'Theming', icon: 'fa-paint-brush', routerLink: ['/theming']}
+        ];
+    }
 
     save() {
         this.msgs = [];
--- showcase/demo/toolbar/toolbardemo.ts
+++ showcase/demo/toolbar/toolbardemo.ts
@@ -1,8 +1,17 @@
-import {Component} from '@angular/core';
+import {Component,OnInit} from '@angular/core';
+import {MenuItem} from '../../../components/common/api';
 
 @Component({
     templateUrl: 'showcase/demo/toolbar/toolbardemo.html'
 })
-export class ToolbarDemo {
+export class ToolbarDemo implements OnInit {
 
+    items: MenuItem[];
+    
+    ngOnInit() {
+        this.items = [
+            {label: 'Angular.io', icon: 'fa-link', url: 'http://angular.io'},
+            {label: 'Theming', icon: 'fa-paint-brush', routerLink: ['/theming']}
+        ];
+    }
 }
