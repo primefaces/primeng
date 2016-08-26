32fd2f96b86f4f85e03377b3ec355ba29dcdb196
Çağatay Çivici <cagatay.civici@github.com>
2016-08-26 11:53:18 +0300

    Refactor

--- components/panelmenu/panelmenu.ts
+++ components/panelmenu/panelmenu.ts
@@ -4,91 +4,14 @@ import {MenuItem} from '../common/api';
 import {Location} from '@angular/common';
 import {Router} from '@angular/router';
 
-@Component({
-    selector: 'p-panelMenuSub',
-    template: `
-        <ul class="ui-menu-list ui-helper-reset" [style.display]="expanded ? 'block' : 'none'">
-            <li *ngFor="let child of item.items" class="ui-menuitem ui-corner-all" [ngClass]="{'ui-menu-parent':child.items}">
-                <a #link [href]="child.url||'#'" class="ui-menuitem-link ui-corner-all" 
-                    [ngClass]="{'ui-menuitem-link-hasicon':child.icon&&child.items,'ui-state-hover':(hoveredLink==link)}" (click)="onClick($event,child)"
-                    (mouseenter)="hoveredLink=link" (mouseleave)="hoveredLink=null">
-                    <span class="ui-panelmenu-icon fa fa-fw" [ngClass]="{'fa-caret-right':!child.expanded,'fa-caret-down':child.expanded}" *ngIf="child.items"></span>
-                    <span class="ui-menuitem-icon fa fa-fw" [ngClass]="child.icon" *ngIf="child.icon"></span>
-                    <span class="ui-menuitem-text">{{child.label}}</span>
-                </a>
-                <p-panelMenuSub [item]="child" [expanded]="child.expanded" *ngIf="child.items"></p-panelMenuSub>
-            </li>
-        </ul>
-    `
-})
-export class PanelMenuSub {
-    
-    @Input() item: MenuItem;
-    
-    @Input() expanded: boolean;
+export class BasePanelMenuItem {
     
     constructor(protected router: Router) {}
-                
-    onClick(event, item: MenuItem) {
+    
+    handleClick(event, item) {
         if(item.items) {
             item.expanded = !item.expanded;
             event.preventDefault();
-        }
-        else {
-            if(!item.url||item.routerLink) {
-                event.preventDefault();
-            }
-            
-            if(item.command) {
-                if(!item.eventEmitter) {
-                    item.eventEmitter = new EventEmitter();
-                    item.eventEmitter.subscribe(item.command);
-                }
-                
-                item.eventEmitter.emit(event);
-            }
-            
-            if(item.routerLink) {
-                this.router.navigate(item.routerLink);
-            }
-        }
-    }
-}
-
-@Component({
-    selector: 'p-panelMenu',
-    template: `
-        <div [class]="styleClass" [ngStyle]="style" [ngClass]="'ui-panelmenu ui-widget'">
-            <div *ngFor="let item of model" class="ui-panelmenu-panel">
-                <div tabindex="0" [ngClass]="{'ui-widget ui-panelmenu-header ui-state-default':true,'ui-corner-all':!item.expanded,
-                    'ui-state-active ui-corner-top':item.expanded,'ui-state-hover':(item == hoveredItem)}">
-                    <a [href]="item.url||'#'" [ngClass]="{'ui-panelmenu-headerlink-hasicon':item.icon}"
-                        (mouseenter)="hoveredItem=item" (mouseleave)="hoveredItem=null" (click)="headerClick($event,item)">
-                        <span class="ui-panelmenu-icon fa fa-fw" [ngClass]="{'fa-caret-right':!item.expanded,'fa-caret-down':item.expanded}"></span>
-                        <span class="ui-menuitem-icon fa fa-fw" [ngClass]="item.icon" *ngIf="item.icon"></span>
-                        <span>{{item.label}}</span>
-                    </a>
-                </div>
-                <div class="ui-panelmenu-content ui-widget-content" [style.display]="item.expanded ? 'block' : 'none'">
-                    <p-panelMenuSub [item]="item" [expanded]="true"></p-panelMenuSub>
-                </div>
-            </div>
-        </div>
-    `
-})
-export class PanelMenu {
-    
-    @Input() model: MenuItem[];
-
-    @Input() style: any;
-
-    @Input() styleClass: string;
-    
-    constructor(protected router: Router) {}
-    
-    headerClick(event, item ) {
-        if(item.items) {
-            item.expanded = !item.expanded;
         }
         else {
             if(!item.url||item.routerLink) {
@@ -109,7 +32,69 @@ export class PanelMenu {
             }
         }
     }
-        
+}
+
+@Component({
+    selector: 'p-panelMenuSub',
+    template: `
+        <ul class="ui-menu-list ui-helper-reset" [style.display]="expanded ? 'block' : 'none'">
+            <li *ngFor="let child of item.items" class="ui-menuitem ui-corner-all" [ngClass]="{'ui-menu-parent':child.items}">
+                <a #link [href]="child.url||'#'" class="ui-menuitem-link ui-corner-all" 
+                    [ngClass]="{'ui-menuitem-link-hasicon':child.icon&&child.items,'ui-state-hover':(hoveredLink==link)}" (click)="handleClick($event,child)"
+                    (mouseenter)="hoveredLink=link" (mouseleave)="hoveredLink=null">
+                    <span class="ui-panelmenu-icon fa fa-fw" [ngClass]="{'fa-caret-right':!child.expanded,'fa-caret-down':child.expanded}" *ngIf="child.items"></span>
+                    <span class="ui-menuitem-icon fa fa-fw" [ngClass]="child.icon" *ngIf="child.icon"></span>
+                    <span class="ui-menuitem-text">{{child.label}}</span>
+                </a>
+                <p-panelMenuSub [item]="child" [expanded]="child.expanded" *ngIf="child.items"></p-panelMenuSub>
+            </li>
+        </ul>
+    `
+})
+export class PanelMenuSub extends BasePanelMenuItem {
+    
+    @Input() item: MenuItem;
+    
+    @Input() expanded: boolean;
+    
+    constructor(router: Router) {
+        super(router);
+    }
+}
+
+@Component({
+    selector: 'p-panelMenu',
+    template: `
+        <div [class]="styleClass" [ngStyle]="style" [ngClass]="'ui-panelmenu ui-widget'">
+            <div *ngFor="let item of model" class="ui-panelmenu-panel">
+                <div tabindex="0" [ngClass]="{'ui-widget ui-panelmenu-header ui-state-default':true,'ui-corner-all':!item.expanded,
+                    'ui-state-active ui-corner-top':item.expanded,'ui-state-hover':(item == hoveredItem)}">
+                    <a [href]="item.url||'#'" [ngClass]="{'ui-panelmenu-headerlink-hasicon':item.icon}"
+                        (mouseenter)="hoveredItem=item" (mouseleave)="hoveredItem=null" (click)="handleClick($event,item)">
+                        <span class="ui-panelmenu-icon fa fa-fw" [ngClass]="{'fa-caret-right':!item.expanded,'fa-caret-down':item.expanded}"></span>
+                        <span class="ui-menuitem-icon fa fa-fw" [ngClass]="item.icon" *ngIf="item.icon"></span>
+                        <span>{{item.label}}</span>
+                    </a>
+                </div>
+                <div class="ui-panelmenu-content ui-widget-content" [style.display]="item.expanded ? 'block' : 'none'">
+                    <p-panelMenuSub [item]="item" [expanded]="true"></p-panelMenuSub>
+                </div>
+            </div>
+        </div>
+    `
+})
+export class PanelMenu extends BasePanelMenuItem {
+    
+    @Input() model: MenuItem[];
+
+    @Input() style: any;
+
+    @Input() styleClass: string;
+    
+    constructor(router: Router) {
+        super(router);
+    }
+            
     unsubscribe(item: any) {
         if(item.eventEmitter) {
             item.eventEmitter.unsubscribe();
