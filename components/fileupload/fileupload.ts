8ea74f71835165c970cd69bd9ecca84526c4b49f
Çağatay Çivici <cagatay.civici@github.com>
2016-08-26 19:04:50 +0300

    FileUpload initial impl

--- components/fileupload/fileupload.ts
+++ components/fileupload/fileupload.ts
@@ -1,21 +1,130 @@
-import {NgModule,Component,Input,Output,EventEmitter} from '@angular/core';
+import {NgModule,Component,OnInit,Input,Output,EventEmitter} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {DomSanitizationService} from '@angular/platform-browser';
+import {ButtonModule} from '../button/button';
+import {ProgressBarModule} from '../progressbar/progressbar';
 
 @Component({
     selector: 'p-fileUpload',
     template: `
-        <input type="file">
+        <div class="ui-fileupload ui-widget">
+            <div class="ui-fileupload-buttonbar ui-widget-header ui-corner-top">
+                <button type="button" label="Choose" icon="fa-plus" pButton class="ui-fileupload-choose" (click)="fileinput.value=null"> 
+                    <input #fileinput type="file" (change)="onFileSelect($event)" [multiple]="multiple" [accept]="accept">
+                </button>
+
+                <button type="button" label="Upload" icon="fa-upload" pButton (click)="upload()"></button>
+                <button type="button" label="Cancel" icon="fa-close" pButton (click)="clear()"></button>
+            </div>
+            <div class="ui-fileupload-content ui-widget-content ui-corner-bottom" *ngIf="files && files.length">
+                <p-progressBar [value]="progress" [showValue]="false"></p-progressBar>
+            
+                <div class="ui-fileupload-files">
+                    <div class="ui-fileupload-row" *ngFor="let file of files;let i = index;">
+                        <div><img [src]="file.objectURL" *ngIf="isImage(file)" width="50" /></div>
+                        <div>{{file.name}}</div>
+                        <div>{{file.size}}</div>
+                        <div><button type="button" icon="fa-close" pButton (click)="remove(i)"></button></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     `,
 })
-export class FileUpload {
-
+export class FileUpload implements OnInit {
     
+    @Input() name: string;
+    
+    @Input() url: string;
+    
+    @Input() multiple: boolean;
+    
+    @Input() accept: string;
+    
+    @Output() onUpload: EventEmitter<any> = new EventEmitter();
+    
+    @Output() onError: EventEmitter<any> = new EventEmitter();
+    
+    @Output() onClear: EventEmitter<any> = new EventEmitter();
+    
+    @Output() onSelect: EventEmitter<any> = new EventEmitter();
+     
+    files: File[];
+    
+    progress: number = 0;
+    
+    constructor(private sanitizer:DomSanitizationService){}
+    
+    ngOnInit() {
+        this.files = [];
+    }
+    
+    onFileSelect(event) {
+        let files = event.target.files;
+        for(let i = 0; i < files.length; i++) {
+            let file = files[i];
+            if(this.isImage(file)) {
+                file.objectURL = this.sanitizer.bypassSecurityTrustUrl((window.URL.createObjectURL(files[i])));
+            }
+            
+            this.files.push(files[i]);
+        }
+        
+        this.onSelect.emit({originalEvent: event, files: files});
+    }
+    
+    isImage(file: File): boolean {
+        return /^image\//.test(file.type);
+    }
+    
+    onImageLoad(img: any) {
+        window.URL.revokeObjectURL(img.src);
+    }
+    
+    upload() {
+        let xhr = new XMLHttpRequest(),
+        formData = new FormData();
+        
+        for(let i = 0; i < this.files.length; i++) {
+            formData.append(this.name, this.files[i], this.files[i].name);
+        }
+        
+        xhr.upload.addEventListener('progress', (e: ProgressEvent) => {
+            if(e.lengthComputable) {
+              this.progress = Math.round((e.loaded * 100) / e.total);
+            }
+          }, false);
 
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState == 4) {
+                this.progress = 0;
+                
+                if(xhr.status == 200)
+                    this.onUpload.emit({xhr: xhr});
+                else
+                    this.onError.emit({xhr: xhr});
+                
+                this.clear();
+            }
+        };
+        
+        xhr.open('POST', this.url, true);
+        xhr.send(formData);
+    }
+
+    clear() {
+        this.files = [];
+        this.onClear.emit();
+    }
+    
+    remove(index: number) {
+        this.files.splice(index, 1);
+    }
 }
 
 @NgModule({
-    imports: [CommonModule],
-    exports: [FileUpload],
+    imports: [CommonModule,ButtonModule,ProgressBarModule],
+    exports: [FileUpload,ButtonModule,ProgressBarModule],
     declarations: [FileUpload]
 })
 export class FileUploadModule { }
--- showcase/demo/fileupload/fileuploaddemo.ts
+++ showcase/demo/fileupload/fileuploaddemo.ts
@@ -1,8 +1,15 @@
 import {Component} from '@angular/core';
+import {Message} from '../../../components/common/api';
 
 @Component({
     templateUrl: 'showcase/demo/fileupload/fileuploaddemo.html'
 })
 export class FileUploadDemo {
 
+    msgs: Message[];
+
+    onUpload(event) {
+        this.msgs = [];
+        this.msgs.push({severity: 'info', summary: 'File Uploaded', detail: ''});
+    }
 }
