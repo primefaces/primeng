a391b35469bc25e93fa6c748415329430b16bbe8
adam.andrzejewski@jitsolutions.pl <adam.andrzejewski@jitsolutions.pl>
2016-09-16 13:50:44 +0200

    fix problem when filter dropdown is used with sorting

--- components/datatable/datatable.ts
+++ components/datatable/datatable.ts
@@ -113,11 +113,9 @@ export class RowExpansionLoader {
                                 <span class="ui-sortable-column-icon fa fa-fw fa-sort" *ngIf="col.sortable"
                                      [ngClass]="{'fa-sort-desc': (getSortOrder(col) == -1),'fa-sort-asc': (getSortOrder(col) == 1)}"></span>
                                 <input type="text" pInputText class="ui-column-filter" *ngIf="col.filter && !col.filterValues" [value]="filters[col.field] ? filters[col.field].value : ''" (click)="onFilterInputClick($event)" (keyup)="onFilterKeyup($event.target.value, col.field, col.filterMatchMode)"/>
-                                <span class="ui-column-filter" *ngIf="col.filter && col.filterValues">
-                                      <select [value]="filters[col.field] ? filters[col.field].value : ''" (change)="onFilterKeyup($event.target.value, col.field, col.filterMatchMode)">
-                                          <option [ngValue]="elem.value" *ngFor="let elem of col.filterValues">{{elem.label}}</option>
-                                      </select>
-                                </span>
+                                <select class="ui-column-filter" *ngIf="col.filter && col.filterValues" (change)="onFilterKeyup($event.target.value, col.field, col.filterMatchMode)" (click)="onFilterInputClick($event)">
+                                   <option [ngValue]="elem.value" [value]="elem.value" *ngFor="let elem of col.filterValues">{{elem.label}}</option>
+                                </select>
                                 <p-dtCheckbox *ngIf="col.selectionMode=='multiple'" (onChange)="toggleRowsWithCheckbox($event)" [checked]="allSelected" [disabled]="isEmpty()"></p-dtCheckbox>
                             </th>
                         </tr>
