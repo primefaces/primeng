c5b780e9cdc4006107d250d42b9e366c52eec23b
Çağatay Çivici <cagatay.civici@github.com>
2016-09-17 10:04:00 +0300

    Implemented range slider and events

--- components/slider/slider.ts
+++ components/slider/slider.ts
@@ -15,8 +15,11 @@ export const SLIDER_VALUE_ACCESSOR: any = {
         <div [ngStyle]="style" [class]="styleClass" [ngClass]="{'ui-slider ui-widget ui-widget-content ui-corner-all':true,
             'ui-slider-horizontal':orientation == 'horizontal','ui-slider-vertical':orientation == 'vertical','ui-slider-animate':animate}"
             (click)="onBarClick($event)">
-            <span class="ui-slider-handle ui-state-default ui-corner-all" (mousedown)="onMouseDown($event)" [style.transition]="dragging ? 'none': null"
+            <span *ngIf="!range" class="ui-slider-handle ui-state-default ui-corner-all" (mousedown)="onMouseDown($event)" [style.transition]="dragging ? 'none': null"
                 [ngStyle]="{'left': orientation == 'horizontal' ? value + '%' : null,'bottom': orientation == 'vertical' ? value + '%' : null}"></span>
+            <span *ngIf="range" class="ui-slider-range ui-widget-header ui-corner-all" [ngStyle]="{'left':values[0] + '%',width: (values[1] - values[0] + '%')}"></span>
+            <span *ngIf="range" (mousedown)="onMouseDown($event,0)" [style.transition]="dragging ? 'none': null" class="ui-slider-handle ui-state-default ui-corner-all" [ngStyle]="{'left':values[0] + '%'}"></span>
+            <span *ngIf="range" (mousedown)="onMouseDown($event,1)" [style.transition]="dragging ? 'none': null" class="ui-slider-handle ui-state-default ui-corner-all" [ngStyle]="{'left':values[1] + '%'}"></span>
         </div>
     `,
     providers: [SLIDER_VALUE_ACCESSOR,DomHandler]
@@ -47,6 +50,8 @@ export class Slider implements AfterViewInit,OnDestroy,ControlValueAccessor {
     
     protected value: number;
     
+    protected values: number;
+    
     protected handleValue: number;
         
     protected onModelChange: Function = () => {};
@@ -69,9 +74,11 @@ export class Slider implements AfterViewInit,OnDestroy,ControlValueAccessor {
     
     protected sliderHandleClick: boolean;
     
+    protected handleIndex: number;
+    
     constructor(protected el: ElementRef, protected domHandler: DomHandler, protected renderer: Renderer) {}
     
-    onMouseDown(event) {
+    onMouseDown(event:Event,index?:number) {
         this.dragging = true;
         let rect = this.el.nativeElement.children[0].getBoundingClientRect();
         this.initX = rect.left + this.domHandler.getWindowScrollLeft();
@@ -79,6 +86,7 @@ export class Slider implements AfterViewInit,OnDestroy,ControlValueAccessor {
         this.barWidth = this.el.nativeElement.children[0].offsetWidth;
         this.barHeight = this.el.nativeElement.children[0].offsetHeight;
         this.sliderHandleClick = true;
+        this.handleIndex = index;
     }
     
     onBarClick(event) {
@@ -94,41 +102,67 @@ export class Slider implements AfterViewInit,OnDestroy,ControlValueAccessor {
         this.dragListener = this.renderer.listenGlobal('body', 'mousemove', (event) => {
             if(this.dragging) {
                 let value = this.calculateValue(event);
+                let otherHandleIndex = this.handleIndex == 0 ? 1 : 0;
                                 
-                if(this.orientation == 'horizontal') {
-                    if(event.pageX < this.initX)
-                        value = this.min;
-                    else if (event.pageX > (this.initX + this.barWidth))
-                        value = this.max;
-                } 
-                else {
-                    if(event.pageY < this.initY)
-                        value = this.max;
-                    else if (event.pageY > (this.initY + this.barHeight))
-                        value = this.min;
+                if(this.range) {
+                    if(this.handleIndex == 0) {
+                        if(value < this.min)
+                            value = this.min;
+                        else if (value > this.values[1])
+                            value = this.values[1];
+                    }
+                    else {
+                        if(value > this.max)
+                            value = this.max;
+                        else if (value < this.values[0])
+                            value = this.values[0];
+                    }
+                        
+                    this.values[this.handleIndex] = value;
+                    this.onModelChange(this.values);
+                    this.onChange.emit({event: event, values: this.values});
                 }
-                
-                this.value = value;
-                this.onModelChange(Math.floor(this.value));
+                else {
+                    if(this.orientation == 'horizontal') {
+                        if(value < this.min)
+                            value = this.min;
+                        else if (value > this.max)
+                            value = this.max;
+                    } 
+                    else {
+                        if(event.pageY < this.initY)
+                            value = this.max;
+                        else if (event.pageY > (this.initY + this.barHeight))
+                            value = this.min;
+                    }
+                    
+                    this.value = value
+                    this.onModelChange(Math.floor(this.value));
+                    this.onChange.emit({event: event, value: this.value});
+                }
             }
         });
         
         this.mouseupListener = this.renderer.listenGlobal('body', 'mouseup', (event) => {
             if(this.dragging) {
                 this.dragging = false;
+                this.onSlideEnd.emit({event: event});
             }
         });
     }
     
     calculateValue(event): number {
         if(this.orientation === 'horizontal')
-            return ((event.pageX - this.initX) * 100) / (this.barWidth);
+            return Math.floor(((event.pageX - this.initX) * 100) / (this.barWidth));
         else
-            return (((this.initY + this.barHeight) - event.pageY) * 100) / (this.barHeight);
+            return Math.floor((((this.initY + this.barHeight) - event.pageY) * 100) / (this.barHeight));
     }
     
     writeValue(value: any) : void {
-        this.value = value||0;
+        if(this.range)
+            this.values = value||[0,0];
+        else
+            this.value = value||0;
     }
     
     registerOnChange(fn: Function): void {
