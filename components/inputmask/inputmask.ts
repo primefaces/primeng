e2900956f5621e4fd9b4317e9dc3bc3a7e780a5d
Çağatay Çivici <cagatay.civici@github.com>
2016-09-28 01:09:18 +0300

    Clear value on backspace

--- components/inputmask/inputmask.ts
+++ components/inputmask/inputmask.ts
@@ -1,6 +1,29 @@
 /* 
-    Port of jQuery MaskedInput by DigitalBush as a Native Angular2 Component 
+    Port of jQuery MaskedInput by DigitalBush as a Native Angular2 Component in Typescript without jQuery
     https://github.com/digitalBush/jquery.maskedinput/
+    
+    Copyright (c) 2007-2014 Josh Bush (digitalbush.com)
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
 */
 import {NgModule,Component,ElementRef,AfterViewInit,OnDestroy,HostBinding,HostListener,Input,forwardRef,Output,EventEmitter} from '@angular/core';
 import {CommonModule} from '@angular/common';
@@ -233,7 +256,7 @@ export class InputMask implements AfterViewInit,OnDestroy,ControlValueAccessor {
                 j = this.seekNext(j);
             }
         }
-        //this.writeBuffer();
+        this.writeBuffer();
         this.caret(Math.max(this.firstNonMaskPos, begin));
     }
     
@@ -307,13 +330,15 @@ export class InputMask implements AfterViewInit,OnDestroy,ControlValueAccessor {
             pos = this.caret();
             begin = pos.begin;
             end = pos.end;
+            
 
             if (end - begin === 0) {
                 begin=k!==46?this.seekPrev(begin):(end=this.seekNext(begin-1));
                 end=k===46?this.seekNext(end):end;
             }
+
             this.clearBuffer(begin, end);
-            this.shiftL(begin, end - 1);
+			this.shiftL(begin, end - 1);
 
             e.preventDefault();
         } else if( k === 13 ) { // enter
