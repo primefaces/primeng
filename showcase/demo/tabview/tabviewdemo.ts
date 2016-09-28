cb992887c0726953845c47b0a85593cac21bab18
Eduard Bogatskiy <eduard.bogatskiy@donriver.com>
2016-09-28 10:09:27 +0300

    Implemented #876. Added canClose guard callout to support custom business logic validations to be performed just before tab is closed

--- showcase/demo/tabview/tabviewdemo.ts
+++ showcase/demo/tabview/tabviewdemo.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {Message} from '../../../components/common/api';
+import { TabButton } from '../../../components/tabview/tabview';
 
 @Component({
     templateUrl: 'showcase/demo/tabview/tabviewdemo.html'
@@ -7,7 +8,23 @@ import {Message} from '../../../components/common/api';
 export class TabViewDemo {
 
     msgs: Message[];
-    
+
+    buttons: TabButton[] = [
+        {
+            action: () => {
+                this.msgs = [];
+                this.msgs.push({severity:'info', summary:'Add Button Clicked'});
+            },
+            rightIcon: 'fa-plus-circle fa-lg',
+        }
+    ];
+
+    closeGuard = (event) => {
+        this.msgs = [];
+        this.msgs.push({severity:'info', summary:'This tab is guarded and cannot be closed', detail: 'Index: ' + event.index});
+        return false;
+    }
+
     onTabChange(event) {
         this.msgs = [];
         this.msgs.push({severity:'info', summary:'Tab Expanded', detail: 'Index: ' + event.index});
