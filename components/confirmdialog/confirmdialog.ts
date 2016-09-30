0dbe5b6a32741e9472279feaffeedc240e172172
Çağatay Çivici <cagatay.civici@github.com>
2016-09-30 13:40:34 +0300

    Fixed #994

--- components/confirmdialog/confirmdialog.ts
+++ components/confirmdialog/confirmdialog.ts
@@ -26,8 +26,8 @@ import {Subscription}   from 'rxjs/Subscription';
                 <ng-content select="footer"></ng-content>
             </div>
             <div class="ui-dialog-buttonpane ui-widget-content ui-helper-clearfix" *ngIf="!footer">
-                <button type="button" pButton [icon]="rejectIcon" [label]="rejectLabel" (click)="reject()"></button>
-                <button type="button" pButton [icon]="acceptIcon" [label]="acceptLabel" (click)="accept()"></button>
+                <button type="button" pButton [icon]="rejectIcon" [label]="rejectLabel" (click)="reject()" *ngIf="rejectVisible"></button>
+                <button type="button" pButton [icon]="acceptIcon" [label]="acceptLabel" (click)="accept()" *ngIf="acceptVisible"></button>
             </div>
         </div>
     `,
@@ -56,10 +56,14 @@ export class ConfirmDialog implements AfterViewInit,OnDestroy {
     @Input() acceptIcon: string = 'fa-check';
     
     @Input() acceptLabel: string = 'Yes';
+    
+    @Input() acceptVisible: boolean = true;
 
     @Input() rejectIcon: string = 'fa-close';
     
     @Input() rejectLabel: string = 'No';
+    
+    @Input() rejectVisible: boolean = true;
         
     @Input() width: any;
 
@@ -100,6 +104,8 @@ export class ConfirmDialog implements AfterViewInit,OnDestroy {
             this.message = this.confirmation.message||this.message;
             this.icon = this.confirmation.icon||this.icon;
             this.header = this.confirmation.header||this.header;
+            this.rejectVisible = this.confirmation.rejectVisible === false ? false : this.rejectVisible;
+            this.acceptVisible = this.confirmation.acceptVisible === false ? false : this.acceptVisible;
             
             if(this.confirmation.accept) {
                 this.confirmation.acceptEvent = new EventEmitter();
