201ced35f0dbfc83e4f926d8c24c5e58d7d418d3
Eduard Bogatskiy <eduard.bogatskiy@donriver.com>
2016-10-17 09:37:30 +0300

    DONRIVER-4 TabView: Implement ability to cancel close tab action

--- showcase/demo/tabview/tabviewdemo.ts
+++ showcase/demo/tabview/tabviewdemo.ts
@@ -7,7 +7,13 @@ import {Message} from '../../../components/common/api';
 export class TabViewDemo {
 
     msgs: Message[];
-    
+
+    closeGuard = (event) => {
+        this.msgs = [];
+        this.msgs.push({severity:'info', summary:'This tab is guarded and cannot be closed', detail: 'Index: ' + event.index});
+        return false;
+    }
+
     onTabChange(event) {
         this.msgs = [];
         this.msgs.push({severity:'info', summary:'Tab Expanded', detail: 'Index: ' + event.index});
