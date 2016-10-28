e55f911d1f6a3929f56f9e0df9b11484fd33b1ab
Çağatay Çivici <cagatay.civici@github.com>
2016-10-28 10:13:46 +0300

    Let webpack compile ts

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,29 @@ var webpack = require("webpack");
 
 module.exports = {
     entry: {
-        'polyfills': './showcase/polyfills.js',
-        'vendor': './showcase/vendor.js',
-        'application': './showcase/main.js'
+        'polyfills': './showcase/polyfills.ts',
+        'vendor': './showcase/vendor.ts',
+        'application': './showcase/main.ts'
     },
     output: {
         path: __dirname,
         filename: "./prod/[name].js",
         chunkFilename: "./prod/[id].js"
     },
+    
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: ['ts-loader']
+            }
+        ]
+    },
+    
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
             name: ['application', 'vendor', 'polyfills']
