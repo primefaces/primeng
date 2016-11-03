d6c5012306fb73186f24489b98d2723096ac92f7
Çağatay Çivici <cagatay.civici@github.com>
2016-11-03 19:47:59 +0300

    Fix paths

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,7 +2,6 @@ var webpackMerge = require('webpack-merge');
 var commonConfig = require('./webpack.common.js');
 
 module.exports = webpackMerge(commonConfig, {
-    devtool: 'cheap-module-eval-source-map',
     devServer: {
         historyApiFallback: true,
         stats: 'minimal'
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,7 +5,6 @@ var commonConfig = require('./webpack.common.js');
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
 module.exports = webpackMerge(commonConfig, {
-    devtool: 'source-map',
     plugins: [
         new webpack.NoErrorsPlugin(),
         new webpack.optimize.DedupePlugin(),
