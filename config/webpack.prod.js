664e33717f63a1b19c55862646fbd6fd4ea8b64f
Çağatay Çivici <cagatay.civici@github.com>
2016-11-03 20:18:28 +0300

    Remove warnings at console

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -12,7 +12,10 @@ module.exports = webpackMerge(commonConfig, {
           beautify: false,
           mangle: {screw_ie8: true, keep_fnames: true},
           compress: {screw_ie8: true},
-          comments: false
+          comments: false,
+          compressor: {
+              warnings: false
+          } 
         }),
         new webpack.DefinePlugin({
             'process.env': {
