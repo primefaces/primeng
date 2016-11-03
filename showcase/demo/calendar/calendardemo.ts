b309f1a45204851abcff2c5613e04a8def18e174
kryzys <krzysztof.bilinski@gmail.com>
2016-11-03 13:30:38 +0100

    Calendar localization

--- showcase/demo/calendar/calendardemo.ts
+++ showcase/demo/calendar/calendardemo.ts
@@ -28,14 +28,7 @@ export class CalendarDemo {
     es: any;
 
     ngOnInit() {
-        this.es = {
-        	firstDayOfWeek: 1,
-        	dayNames: [ "domingo","lunes","martes","miércoles","jueves","viernes","sábado" ],
-        	dayNamesShort: [ "dom","lun","mar","mié","jue","vie","sáb" ],
-        	dayNamesMin: [ "D","L","M","X","J","V","S" ],
-            monthNames: [ "enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre" ],
-        	monthNamesShort: [ "ene","feb","mar","abr","may","jun","jul","ago","sep","oct","nov","dic" ]
-        };
+        this.onChangeLocaleES()
         
         let today = new Date();
         let month = today.getMonth();
@@ -45,5 +38,38 @@ export class CalendarDemo {
         this.minDate.setMonth(prevMonth);
         this.maxDate = new Date();
         this.maxDate.setMonth(nextMonth);
+    }
+
+    onChangeLocaleES() {
+        this.es = {
+            firstDayOfWeek: 1,
+            dayNames: ["domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"],
+            dayNamesShort: ["dom", "lun", "mar", "mié", "jue", "vie", "sáb"],
+            dayNamesMin: ["D", "L", "M", "X", "J", "V", "S"],
+            monthNames: ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"],
+            monthNamesShort: ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"]
+        }
+    };
+
+    onChangeLocalePL() {
+        this.es = {
+            firstDayOfWeek: 1,
+            dayNames: ['poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota', 'niedziela'],
+            dayNamesShort: ['pon.', 'wt.', 'śr.', 'czw.', 'piąt.', 'sob.', 'niedz.'],
+            dayNamesMin: ['pn.', 'wt.', 'śr.', 'czw.', 'pt.', 'so.', 'ni.'],
+            monthNames: ['styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień'],
+            monthNamesShort: ['sty', 'lut', 'mar', 'kwi', 'maj', 'cze', 'lip', 'sie', 'wrz', 'paź', 'lis', 'gru'],
+        };
+    }
+
+    onChangeLocaleEN() {
+        this.es = {
+            firstDayOfWeek: 0,
+            dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+            dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+            dayNamesMin: ["Su","Mo","Tu","We","Th","Fr","Sa"],
+            monthNames: [ "January","February","March","April","May","June","July","August","September","October","November","December" ],
+            monthNamesShort: [ "Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ]
+        };
     }
 }
