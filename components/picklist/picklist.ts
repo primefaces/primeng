3d09785d65ab006cc815f978b0498ed78d2bb562
Çağatay Çivici <cagatay.civici@github.com>
2016-11-13 12:35:29 +0300

    Fixed #1292

--- components/picklist/picklist.ts
+++ components/picklist/picklist.ts
@@ -10,17 +10,17 @@ import {DomHandler} from '../dom/domhandler';
         <div [class]="styleClass" [ngStyle]="style" [ngClass]="{'ui-picklist ui-widget ui-helper-clearfix': true,'ui-picklist-responsive': responsive}">
             <div class="ui-picklist-source-controls ui-picklist-buttons" *ngIf="showSourceControls">
                 <div class="ui-picklist-buttons-cell">
-                    <button type="button" pButton icon="fa-angle-up" (click)="moveUp(sourcelist,source)"></button>
-                    <button type="button" pButton icon="fa-angle-double-up" (click)="moveTop(sourcelist,source)"></button>
-                    <button type="button" pButton icon="fa-angle-down" (click)="moveDown(sourcelist,source)"></button>
-                    <button type="button" pButton icon="fa-angle-double-down" (click)="moveBottom(sourcelist,source)"></button>
+                    <button type="button" pButton icon="fa-angle-up" (click)="moveUp(sourcelist,source,selectedItemsSource)"></button>
+                    <button type="button" pButton icon="fa-angle-double-up" (click)="moveTop(sourcelist,source,selectedItemsSource)"></button>
+                    <button type="button" pButton icon="fa-angle-down" (click)="moveDown(sourcelist,source,selectedItemsSource)"></button>
+                    <button type="button" pButton icon="fa-angle-double-down" (click)="moveBottom(sourcelist,source,selectedItemsSource)"></button>
                 </div>
             </div>
             <div class="ui-picklist-listwrapper ui-picklist-source-wrapper" [ngClass]="{'ui-picklist-listwrapper-nocontrols':!showSourceControls}">
                 <div class="ui-picklist-caption ui-widget-header ui-corner-tl ui-corner-tr" *ngIf="sourceHeader">{{sourceHeader}}</div>
                 <ul #sourcelist class="ui-widget-content ui-picklist-list ui-picklist-source ui-corner-bottom" [ngStyle]="sourceStyle">
-                    <li *ngFor="let item of source" [ngClass]="{'ui-picklist-item':true,'ui-state-hover':(hoveredItem==item),'ui-state-highlight':isSelected(item)}"
-                        (mouseenter)="hoveredItem=item" (mouseleave)="hoveredItem=null" (click)="selectItem($event,item)">
+                    <li *ngFor="let item of source" [ngClass]="{'ui-picklist-item':true,'ui-state-hover':(hoveredItem==item),'ui-state-highlight':isSelected(item,selectedItemsSource)}"
+                        (mouseenter)="hoveredItem=item" (mouseleave)="hoveredItem=null" (click)="onItemClick($event,item,selectedItemsSource)">
                         <template [pTemplateWrapper]="itemTemplate" [item]="item"></template>
                     </li>
                 </ul>
@@ -36,18 +36,18 @@ import {DomHandler} from '../dom/domhandler';
             <div class="ui-picklist-listwrapper ui-picklist-target-wrapper" [ngClass]="{'ui-picklist-listwrapper-nocontrols':!showSourceControls}">
                 <div class="ui-picklist-caption ui-widget-header ui-corner-tl ui-corner-tr" *ngIf="targetHeader">{{targetHeader}}</div>
                 <ul #targetlist class="ui-widget-content ui-picklist-list ui-picklist-target ui-corner-bottom" [ngStyle]="targetStyle">
-                    <li *ngFor="let item of target" [ngClass]="{'ui-picklist-item':true,'ui-state-hover':(hoveredItem==item),'ui-state-highlight':isSelected(item)}"
-                        (mouseenter)="hoveredItem=item" (mouseleave)="hoveredItem=null" (click)="selectItem($event,item)">
+                    <li *ngFor="let item of target" [ngClass]="{'ui-picklist-item':true,'ui-state-hover':(hoveredItem==item),'ui-state-highlight':isSelected(item,selectedItemsTarget)}"
+                        (mouseenter)="hoveredItem=item" (mouseleave)="hoveredItem=null" (click)="onItemClick($event,item,selectedItemsTarget)">
                         <template [pTemplateWrapper]="itemTemplate" [item]="item"></template>
                     </li>
                 </ul>
             </div>
             <div class="ui-picklist-target-controls ui-picklist-buttons" *ngIf="showTargetControls">
                 <div class="ui-picklist-buttons-cell">
-                    <button type="button" pButton icon="fa-angle-up" (click)="moveUp(targetlist,target)"></button>
-                    <button type="button" pButton icon="fa-angle-double-up" (click)="moveTop(targetlist,target)"></button>
-                    <button type="button" pButton icon="fa-angle-down" (click)="moveDown(targetlist,target)"></button>
-                    <button type="button" pButton icon="fa-angle-double-down" (click)="moveBottom(targetlist,target)"></button>
+                    <button type="button" pButton icon="fa-angle-up" (click)="moveUp(targetlist,target,selectedItemsTarget)"></button>
+                    <button type="button" pButton icon="fa-angle-double-up" (click)="moveTop(targetlist,target,selectedItemsTarget)"></button>
+                    <button type="button" pButton icon="fa-angle-down" (click)="moveDown(targetlist,target,selectedItemsTarget)"></button>
+                    <button type="button" pButton icon="fa-angle-double-down" (click)="moveBottom(targetlist,target,selectedItemsTarget)"></button>
                 </div>
             </div>
         </div>
@@ -86,7 +86,9 @@ export class PickList implements OnDestroy,AfterViewChecked {
     
     hoveredItem: any;
     
-    selectedItems: any[];
+    selectedItemsSource: any[] = [];
+    
+    selectedItemsTarget: any[] = [];
         
     reorderedListElement: any;
     
@@ -113,24 +115,26 @@ export class PickList implements OnDestroy,AfterViewChecked {
         }
     }
     
-    selectItem(event, item: any) {
+    onItemClick(event, item: any, selectedItems: any[]) {
         let metaKey = (event.metaKey||event.ctrlKey);
-        let index = this.findIndexInSelection(item);
+        let index = this.findIndexInSelection(item,selectedItems);
         let selected = (index != -1);
         
         if(selected && metaKey) {
-            this.selectedItems.splice(index, 1);
+            selectedItems.splice(index, 1);
         }
         else {
-            this.selectedItems = (metaKey) ? this.selectedItems||[] : [];            
-            this.selectedItems.push(item);
+            if(!metaKey) {
+                selectedItems.length = 0;
+            }         
+            selectedItems.push(item);
         }
     }
 
-    moveUp(listElement, list) {
-        if(this.selectedItems && this.selectedItems.length) {
-            for(let i = 0; i < this.selectedItems.length; i++) {
-                let selectedItem = this.selectedItems[i];
+    moveUp(listElement, list, selectedItems) {
+        if(selectedItems && selectedItems.length) {
+            for(let i = 0; i < selectedItems.length; i++) {
+                let selectedItem = selectedItems[i];
                 let selectedItemIndex: number = this.findIndexInList(selectedItem, list);
 
                 if(selectedItemIndex != 0) {
@@ -149,10 +153,10 @@ export class PickList implements OnDestroy,AfterViewChecked {
         }
     }
 
-    moveTop(listElement, list) {
-        if(this.selectedItems && this.selectedItems.length) {
-            for(let i = 0; i < this.selectedItems.length; i++) {
-                let selectedItem = this.selectedItems[i];
+    moveTop(listElement, list, selectedItems) {
+        if(selectedItems && selectedItems.length) {
+            for(let i = 0; i < selectedItems.length; i++) {
+                let selectedItem = selectedItems[i];
                 let selectedItemIndex: number = this.findIndexInList(selectedItem, list);
 
                 if(selectedItemIndex != 0) {
@@ -168,10 +172,10 @@ export class PickList implements OnDestroy,AfterViewChecked {
         }
     }
 
-    moveDown(listElement, list) {
-        if(this.selectedItems && this.selectedItems.length) {
-            for(let i = this.selectedItems.length - 1; i >= 0; i--) {
-                let selectedItem = this.selectedItems[i];
+    moveDown(listElement, list, selectedItems) {
+        if(selectedItems && selectedItems.length) {
+            for(let i = selectedItems.length - 1; i >= 0; i--) {
+                let selectedItem = selectedItems[i];
                 let selectedItemIndex: number = this.findIndexInList(selectedItem, list);
 
                 if(selectedItemIndex != (list.length - 1)) {
@@ -190,10 +194,10 @@ export class PickList implements OnDestroy,AfterViewChecked {
         }
     }
 
-    moveBottom(listElement, list) {
-        if(this.selectedItems && this.selectedItems.length) {
-            for(let i = this.selectedItems.length - 1; i >= 0; i--) {
-                let selectedItem = this.selectedItems[i];
+    moveBottom(listElement, list, selectedItems) {
+        if(selectedItems && selectedItems.length) {
+            for(let i = selectedItems.length - 1; i >= 0; i--) {
+                let selectedItem = selectedItems[i];
                 let selectedItemIndex: number = this.findIndexInList(selectedItem, list);
 
                 if(selectedItemIndex != (list.length - 1)) {
@@ -210,17 +214,17 @@ export class PickList implements OnDestroy,AfterViewChecked {
     }
 
     moveRight(targetListElement) {
-        if(this.selectedItems && this.selectedItems.length) {
-            for(let i = 0; i < this.selectedItems.length; i++) {
-                let selectedItem = this.selectedItems[i];
+        if(this.selectedItemsSource && this.selectedItemsSource.length) {
+            for(let i = 0; i < this.selectedItemsSource.length; i++) {
+                let selectedItem = this.selectedItemsSource[i];
                 if(this.findIndexInList(selectedItem, this.target) == -1) {
                     this.target.push(this.source.splice(this.findIndexInList(selectedItem, this.source),1)[0]);
                 }
             }
             this.onMoveToTarget.emit({
-                items: this.selectedItems
+                items: this.selectedItemsSource
             });
-            this.selectedItems = [];
+            this.selectedItemsSource = [];
         }
     }
 
@@ -233,22 +237,22 @@ export class PickList implements OnDestroy,AfterViewChecked {
                 items: this.source
             });
             this.source.splice(0, this.source.length);
-            this.selectedItems = [];
+            this.selectedItemsSource = [];
         }
     }
 
     moveLeft(sourceListElement) {
-        if(this.selectedItems && this.selectedItems.length) {
-            for(let i = 0; i < this.selectedItems.length; i++) {
-                let selectedItem = this.selectedItems[i];
+        if(this.selectedItemsTarget && this.selectedItemsTarget.length) {
+            for(let i = 0; i < this.selectedItemsTarget.length; i++) {
+                let selectedItem = this.selectedItemsTarget[i];
                 if(this.findIndexInList(selectedItem, this.source) == -1) {
                     this.source.push(this.target.splice(this.findIndexInList(selectedItem, this.target),1)[0]);
                 }
             }
             this.onMovetoSource.emit({
-                items: this.selectedItems
+                items: this.selectedItemsTarget
             });
-            this.selectedItems = [];
+            this.selectedItemsTarget = [];
         }
     }
 
@@ -261,16 +265,16 @@ export class PickList implements OnDestroy,AfterViewChecked {
                 items: this.target
             });
             this.target.splice(0, this.target.length);
-            this.selectedItems = [];
+            this.selectedItemsTarget = [];
         }
     }
 
-    isSelected(item: any) {
-        return this.findIndexInSelection(item) != -1;
+    isSelected(item: any, selectedItems: any[]) {
+        return this.findIndexInSelection(item, selectedItems) != -1;
     }
     
-    findIndexInSelection(item: any): number {
-        return this.findIndexInList(item, this.selectedItems);
+    findIndexInSelection(item: any, selectedItems: any[]): number {
+        return this.findIndexInList(item, selectedItems);
     }
     
     findIndexInList(item: any, list: any): number {
