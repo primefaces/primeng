fb8c5f59f5ca169988ffb8b7bf01442162896c9e
Michael Klein <michael.klein@itsv.at>
2016-11-21 16:09:19 +0100

    Show active state on menuBar items that use routerLinks

--- components/menubar/menubar.ts
+++ components/menubar/menubar.ts
@@ -1,9 +1,8 @@
-import {NgModule,Component,ElementRef,AfterViewInit,OnDestroy,Input,Output,Renderer,EventEmitter} from '@angular/core';
+import {NgModule,Component,ElementRef,OnDestroy,Input,EventEmitter} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DomHandler} from '../dom/domhandler';
 import {MenuItem} from '../common/api';
-import {Location} from '@angular/common';
-import {Router} from '@angular/router';
+import {Router, RouterModule} from '@angular/router';
 
 @Component({
     selector: 'p-menubarSub',
@@ -13,7 +12,7 @@ import {Router} from '@angular/router';
             <template ngFor let-child [ngForOf]="(root ? item : item.items)">
                 <li #item [ngClass]="{'ui-menuitem ui-widget ui-corner-all':true,'ui-menu-parent':child.items,'ui-menuitem-active':item==activeItem}"
                     (mouseenter)="onItemMouseEnter($event,item,child)" (mouseleave)="onItemMouseLeave($event,item)">
-                    <a #link [href]="child.url||'#'" class="ui-menuitem-link ui-corner-all" 
+                    <a #link [href]="child.url||'#'" class="ui-menuitem-link ui-corner-all" [routerLink]="child.routerLink" routerLinkActive="ui-state-active"
                         [ngClass]="{'ui-state-hover':link==activeLink&&!child.disabled,'ui-state-disabled':child.disabled}" (click)="itemClick($event, child)">
                         <span class="ui-submenu-icon fa fa-fw" *ngIf="child.items" [ngClass]="{'fa-caret-down':root,'fa-caret-right':!root}"></span>
                         <span class="ui-menuitem-icon fa fa-fw" *ngIf="child.icon" [ngClass]="child.icon"></span>
@@ -29,27 +28,27 @@ import {Router} from '@angular/router';
 export class MenubarSub {
 
     @Input() item: MenuItem;
-    
+
     @Input() root: boolean;
-    
+
     constructor(public domHandler: DomHandler, public router: Router) {}
-    
+
     activeItem: any;
-    
+
     activeLink: any;
-            
+
     onItemMouseEnter(event, item, menuitem: MenuItem) {
         if(menuitem.disabled) {
             return;
         }
-        
+
         this.activeItem = item;
         this.activeLink = item.children[0];
         let nextElement =  item.children[0].nextElementSibling;
         if(nextElement) {
             let sublist = nextElement.children[0];
             sublist.style.zIndex = ++DomHandler.zindex;
-            
+
             if(this.root) {
                 sublist.style.top = this.domHandler.getOuterHeight(item.children[0]) + 'px';
                 sublist.style.left = '0px'
@@ -60,42 +59,38 @@ export class MenubarSub {
             }
         }
     }
-    
+
     onItemMouseLeave(event, link) {
         this.activeItem = null;
         this.activeLink = null;
     }
-    
+
     itemClick(event, item: MenuItem) {
         if(item.disabled) {
             event.preventDefault();
             return;
         }
-        
+
         if(!item.url||item.routerLink) {
             event.preventDefault();
         }
-        
+
         if(item.command) {
             if(!item.eventEmitter) {
                 item.eventEmitter = new EventEmitter();
                 item.eventEmitter.subscribe(item.command);
             }
-            
+
             item.eventEmitter.emit({
                 originalEvent: event,
                 item: item
             });
         }
 
-        if(item.routerLink) {
-            this.router.navigate(item.routerLink);
-        }
-        
         this.activeItem = null;
         this.activeLink = null;
     }
-        
+
     listClick(event) {
         this.activeItem = null;
         this.activeLink = null;
@@ -120,22 +115,22 @@ export class Menubar implements OnDestroy {
     @Input() style: any;
 
     @Input() styleClass: string;
-            
-    constructor(public el: ElementRef, public domHandler: DomHandler, public renderer: Renderer) {}
-    
+
+    constructor(public el: ElementRef, public domHandler: DomHandler) {}
+
     unsubscribe(item: any) {
         if(item.eventEmitter) {
             item.eventEmitter.unsubscribe();
         }
-        
+
         if(item.items) {
             for(let childItem of item.items) {
                 this.unsubscribe(childItem);
             }
         }
     }
-        
-    ngOnDestroy() {        
+
+    ngOnDestroy() {
         if(this.model) {
             for(let item of this.model) {
                 this.unsubscribe(item);
@@ -146,7 +141,7 @@ export class Menubar implements OnDestroy {
 }
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     exports: [Menubar],
     declarations: [Menubar,MenubarSub]
 })
