f44109ce9da32c725c1c1e7a46b8c9ab3d48fc8e
Thavarajan <Thavarajan@gmail.com>
2016-11-26 14:58:16 +0530

    Button Demo updated for altest change applicable

--- showcase/demo/button/buttondemo.ts
+++ showcase/demo/button/buttondemo.ts
@@ -1,13 +1,24 @@
-import {Component} from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-    templateUrl: 'showcase/demo/button/buttondemo.html'
+    templateUrl: 'showcase/demo/button/buttondemo.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ButtonDemo {
 
     clicks: number = 0;
-
+    btnicon: string = 'fa-check';
+    btnlabel = `click - ${this.clicks}`;
+    btnIconpos: string = 'right';
     count() {
         this.clicks++;
+        if (this.clicks % 2 === 0) {
+            this.btnicon = 'fa-check';
+            this.btnIconpos = 'right';
+        } else {
+            this.btnicon = 'fa-times';
+            this.btnIconpos = 'left';
+        }
+        this.btnlabel = `click - ${this.clicks}`
     }
 }
