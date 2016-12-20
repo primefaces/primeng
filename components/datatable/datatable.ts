d20cf869cc732728fbcecc1cd4698c7ee1394c9f
Çağatay Çivici <cagatay.civici@github.com>
2016-12-20 13:58:35 +0300

    Merged #1519

--- components/datatable/datatable.ts
+++ components/datatable/datatable.ts
@@ -238,7 +238,7 @@ export class RowExpansionLoader {
                                         </span>
                                         <span class="ui-sortable-column-icon fa fa-fw fa-sort" *ngIf="col.sortable"
                                              [ngClass]="{'fa-sort-desc': (col.field === sortField) && (sortOrder == -1),'fa-sort-asc': (col.field === sortField) && (sortOrder == 1)}"></span>
-                                        <input type="text" pInputText class="ui-column-filter" [attr.placeholder]="col.filterPlaceholder" *ngIf="col.filter" (click)="onFilterInputClick($event)" (keyup)="onFilterKeyup($event.target.value, col.field, col.filterMatchMode)"/>
+                                        <input type="text" pInputText class="ui-column-filter" [attr.placeholder]="col.filterPlaceholder" *ngIf="col.filter" [value]="filters[col.field] ? filters[col.field].value : ''" (click)="onFilterInputClick($event)" (keyup)="onFilterKeyup($event.target.value, col.field, col.filterMatchMode)"/>
                                         <p-dtCheckbox *ngIf="col.selectionMode=='multiple'" (onChange)="toggleRowsWithCheckbox($event)" [checked]="allSelected" [disabled]="isEmpty()"></p-dtCheckbox>
                                     </th>
                                 </template>
