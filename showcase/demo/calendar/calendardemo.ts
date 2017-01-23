ba4c3f7269dcdb7cc4f93894181975c5ede8aee7
mertsincan <sincan.mert@gmail.com>
2017-01-23 23:54:24 +0300

    Refactor calendar demo

--- showcase/demo/calendar/calendardemo.ts
+++ showcase/demo/calendar/calendardemo.ts
@@ -39,11 +39,16 @@ export class CalendarDemo {
         
         let today = new Date();
         let month = today.getMonth();
+        let year = today.getFullYear();
         let prevMonth = (month === 0) ? 11 : month -1;
+        let prevYear = (prevMonth === 11) ? year - 1 : year;
         let nextMonth = (month === 11) ? 0 : month + 1;
+        let nextYear = (nextMonth === 0) ? year + 1 : year;
         this.minDate = new Date();
         this.minDate.setMonth(prevMonth);
+        this.minDate.setFullYear(prevYear);
         this.maxDate = new Date();
         this.maxDate.setMonth(nextMonth);
+        this.maxDate.setFullYear(nextYear);
     }
 }
