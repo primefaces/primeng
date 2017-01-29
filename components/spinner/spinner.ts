925196d88ed04b5a2d31401bf4d264b19990f856
Tomasz Foltyński <folek1@gmail.com>
2017-01-29 13:09:34 +0100

    Added placeholder to spinner component

--- components/spinner/spinner.ts
+++ components/spinner/spinner.ts
@@ -15,7 +15,7 @@ export const SPINNER_VALUE_ACCESSOR: any = {
     template: `
         <span class="ui-spinner ui-widget ui-corner-all">
             <input #in pInputText type="text" class="ui-spinner-input" [value]="valueAsString"
-            [attr.size]="size" [attr.maxlength]="maxlength" [attr.tabindex]="tabindex" [disabled]="disabled" [readonly]="readonly"
+            [attr.size]="size" [attr.maxlength]="maxlength" [attr.tabindex]="tabindex" [attr.placeholder]="placeholder" [disabled]="disabled" [readonly]="readonly"
             (keydown)="onInputKeydown($event)" (keyup)="onInput($event,in.value)" (keypress)="onInputKeyPress($event)" (blur)="onBlur()" (change)="handleChange($event)" (focus)="onFocus()">
             <button [ngClass]="{'ui-spinner-button ui-spinner-up ui-corner-tr ui-button ui-widget ui-state-default':true,'ui-state-disabled':disabled}" [disabled]="disabled"
                 (mouseleave)="onUpButtonMouseleave($event)" (mousedown)="onUpButtonMousedown($event,in)" (mouseup)="onUpButtonMouseup($event)">
@@ -46,6 +46,8 @@ export class Spinner implements OnInit,ControlValueAccessor {
     @Input() maxlength: number;
     
     @Input() size: number;
+
+    @Input() placeholder: string;
 
     @Input() disabled: boolean;
     
