6553bd81c8c49c936819b28f8eee299b634ab3af
cezar_a <cezar.augusto@tcm.go.gov.br>
2017-03-16 09:01:49 -0300

    fix issue #1937

--- components/utils/ObjectUtils.ts
+++ components/utils/ObjectUtils.ts
@@ -1,8 +1,8 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ObjectUtils {
-    
+
     public equals(obj1: any, obj2: any): boolean {
         if (obj1 == null && obj2 == null) {
             return true;
@@ -21,26 +21,38 @@ export class ObjectUtils {
             for (var p in obj1) {
                 if (p === "_$visited") continue;
                 if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) {
+                    delete obj1._$visited;
                     return false;
                 }
-
                 switch (typeof (obj1[p])) {
                     case 'object':
-                        if (obj1[p] && obj1[p]._$visited || !this.equals(obj1[p], obj2[p])) return false;
+                        if (obj1[p] && obj1[p]._$visited || !this.equals(obj1[p], obj2[p])) {
+                          delete obj1._$visited;
+                          return false;
+                        } 
                         break;
 
                     case 'function':
-                        if (typeof (obj2[p]) == 'undefined' || (p != 'compare' && obj1[p].toString() != obj2[p].toString())) return false;
+                        if (typeof (obj2[p]) == 'undefined' || (p != 'compare' && obj1[p].toString() != obj2[p].toString())) {
+                            delete obj1._$visited;
+                            return false;
+                        }
                         break;
 
                     default:
-                        if (obj1[p] != obj2[p]) return false;
+                        if (obj1[p] != obj2[p]) {
+                            delete obj1._$visited;
+                            return false;
+                        } 
                         break;
                 }
             }
 
             for (var p in obj2) {
-                if (typeof (obj1[p]) == 'undefined') return false;
+                if (typeof (obj1[p]) == 'undefined') {
+                    delete obj1._$visited;
+                    return false;
+                } 
             }
 
             delete obj1._$visited;
@@ -49,16 +61,16 @@ export class ObjectUtils {
 
         return false;
     }
-    
+
     resolveFieldData(data: any, field: string): any {
-        if(data && field) {
-            if(field.indexOf('.') == -1) {
+        if (data && field) {
+            if (field.indexOf('.') == -1) {
                 return data[field];
             }
             else {
                 let fields: string[] = field.split('.');
                 let value = data;
-                for(var i = 0, len = fields.length; i < len; ++i) {
+                for (var i = 0, len = fields.length; i < len; ++i) {
                     if (value == null) {
                         return null;
                     }
