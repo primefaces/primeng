875439859a2af0555428405c4058c7c78280e957
Paulo Marques <paulo.marques@polarising.com>
2017-04-06 15:12:29 +0100

    Add z-index input and contentchild

--- components/blockui/blockui.ts
+++ components/blockui/blockui.ts
@@ -7,7 +7,7 @@ import {BlockableUI} from '../common/api';
     selector: 'p-blockUI',
     template: `
         <div class="ui-blockui ui-widget-overlay" [ngClass]="{'ui-blockui-document':!target}" [ngStyle]="{display: blocked ? 'block' : 'none'}">
-            <ng-content></ng-content>
+        <ng-content></ng-content>
         </div>
     `,
     providers: [DomHandler]
@@ -19,20 +19,20 @@ export class BlockUI implements AfterViewInit,OnDestroy {
     @Input() zindex: string;
 
     @ContentChild(TemplateRef) template: TemplateRef<any>;
-
+    
     _blocked: boolean;
-
+    
     _mask: HTMLDivElement;
-
+    
     constructor(public el: ElementRef,public domHandler: DomHandler) {}
-
+    
     @Input() get blocked(): boolean {
         return this._blocked;
     }
-
+    
     set blocked(val: boolean) {
         this._blocked = val;
-
+        
         if(this._mask) {
             if(this._blocked)
                 this.block();
@@ -40,15 +40,15 @@ export class BlockUI implements AfterViewInit,OnDestroy {
                 this.unblock();
         }
     }
-
+    
     ngAfterViewInit() {
         this._mask = this.el.nativeElement.children[0];
-
+        
         if(this.target && !this.target.getBlockableElement) {
             throw 'Target of BlockUI must implement BlockableUI interface';
         }
     }
-
+        
     block() {
         if(this.target) {
             this.target.getBlockableElement().appendChild(this._mask);
@@ -59,16 +59,16 @@ export class BlockUI implements AfterViewInit,OnDestroy {
         else {
             document.body.appendChild(this._mask);
         }
-
+        
         this._mask.style.zIndex = (this.zindex)? this.zindex: String(++DomHandler.zindex);
     }
-
+    
     unblock() {
         this.el.nativeElement.appendChild(this._mask);
     }
-
+    
     ngOnDestroy() {
-
+        
     }
 }
 
