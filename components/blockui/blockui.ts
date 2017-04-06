672f5d1fea3676525d92f99733c0679d3c5b5b3f
Paulo Marques <paulo.marques@polarising.com>
2017-04-06 11:06:25 +0100

    Add z-index input and contentchild

--- components/blockui/blockui.ts
+++ components/blockui/blockui.ts
@@ -1,4 +1,4 @@
-import {NgModule,Component,Input,AfterViewInit,OnDestroy,EventEmitter,ElementRef} from '@angular/core';
+import {NgModule,Component,Input,AfterViewInit,OnDestroy,EventEmitter,ElementRef,TemplateRef,ContentChild} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DomHandler} from '../dom/domhandler';
 import {BlockableUI} from '../common/api';
@@ -6,27 +6,33 @@ import {BlockableUI} from '../common/api';
 @Component({
     selector: 'p-blockUI',
     template: `
-        <div class="ui-blockui ui-widget-overlay" [ngClass]="{'ui-blockui-document':!target}" [ngStyle]="{display: blocked ? 'block' : 'none'}"></div>
+        <div class="ui-blockui ui-widget-overlay" [ngClass]="{'ui-blockui-document':!target}" [ngStyle]="{display: blocked ? 'block' : 'none'}">
+            <ng-content></ng-content>
+        </div>
     `,
     providers: [DomHandler]
 })
 export class BlockUI implements AfterViewInit,OnDestroy {
 
     @Input() target: any;
-    
+
+    @Input() zindex: string;
+
+    @ContentChild(TemplateRef) template: TemplateRef<any>;
+
     _blocked: boolean;
-    
+
     _mask: HTMLDivElement;
-    
+
     constructor(public el: ElementRef,public domHandler: DomHandler) {}
-    
+
     @Input() get blocked(): boolean {
         return this._blocked;
     }
-    
+
     set blocked(val: boolean) {
         this._blocked = val;
-        
+
         if(this._mask) {
             if(this._blocked)
                 this.block();
@@ -34,15 +40,15 @@ export class BlockUI implements AfterViewInit,OnDestroy {
                 this.unblock();
         }
     }
-    
+
     ngAfterViewInit() {
         this._mask = this.el.nativeElement.children[0];
-        
+
         if(this.target && !this.target.getBlockableElement) {
             throw 'Target of BlockUI must implement BlockableUI interface';
         }
     }
-        
+
     block() {
         if(this.target) {
             this.target.getBlockableElement().appendChild(this._mask);
@@ -53,16 +59,16 @@ export class BlockUI implements AfterViewInit,OnDestroy {
         else {
             document.body.appendChild(this._mask);
         }
-        
-        this._mask.style.zIndex = String(++DomHandler.zindex);
+
+        this._mask.style.zIndex = (this.zindex)? this.zindex: String(++DomHandler.zindex);
     }
-    
+
     unblock() {
         this.el.nativeElement.appendChild(this._mask);
     }
-    
+
     ngOnDestroy() {
-        
+
     }
 }
 
