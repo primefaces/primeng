e0287a597eaef6c213416250abd7c64261f9a62a
Çağatay Çivici <cagatay.civici@github.com>
2017-04-12 01:01:16 +0300

    Merged 2279

--- components/datatable/datatable.ts
+++ components/datatable/datatable.ts
@@ -1980,7 +1980,6 @@ export class DataTable implements AfterViewChecked,AfterViewInit,AfterContentIni
             if(link.download !== undefined) {
                 link.setAttribute('href', URL.createObjectURL(blob));
                 link.setAttribute('download', this.exportFilename + '.csv');
-                document.body.appendChild(link);
                 link.click();
             }
             else {
