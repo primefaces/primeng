0ff4602aeec33804d5970fe590cc9433c5cfe0eb
Alfonso Concha <alfonso@coddde.com>
2017-05-29 15:35:24 -0400

    Fixing fix for #2035:
    Chart generateLegend() should return chart.js generateLegend, not just call it.

--- components/chart/chart.ts
+++ components/chart/chart.ts
@@ -73,7 +73,7 @@ export class UIChart implements AfterViewInit, OnDestroy {
     
     generateLegend() {
         if(this.chart) {
-            this.chart.generateLegend();
+            return this.chart.generateLegend();
         }
     }
     
