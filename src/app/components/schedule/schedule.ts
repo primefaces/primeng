e8f4845242b5833a3db948573b1abe27260f0727
Brock Ellis <brock@sharproot.com>
2017-06-06 15:10:19 -0500

    Add in official editEvent method, rename existing, internal method to _updateEvent

--- src/app/components/schedule/schedule.ts
+++ src/app/components/schedule/schedule.ts
@@ -221,7 +221,7 @@ export class Schedule implements DoCheck,OnDestroy,OnInit,OnChanges,AfterViewChe
                 });
             },
             eventDrop: (event, delta, revertFunc, jsEvent, ui, view) => {
-                this.updateEvent(event);
+                this._updateEvent(event);
                 
                 this.onEventDrop.emit({
                     'event': event,
@@ -246,7 +246,7 @@ export class Schedule implements DoCheck,OnDestroy,OnInit,OnChanges,AfterViewChe
                 });
             },
             eventResize: (event, delta, revertFunc, jsEvent, ui, view) => {
-                this.updateEvent(event);
+                this._updateEvent(event);
                 
                 this.onEventResize.emit({
                     'event': event,
@@ -349,8 +349,12 @@ export class Schedule implements DoCheck,OnDestroy,OnInit,OnChanges,AfterViewChe
     getDate() {
         return this.schedule.fullCalendar('getDate');
     }
-    
-    findEvent(id: string) {
+   
+    updateEvent(event: any) {
+        this.schedule.fullCalendar('updateEvent', event);
+    }
+ 
+    _findEvent(id: string) {
         let event;
         if(this.events) {
             for(let e of this.events) {
@@ -363,8 +367,8 @@ export class Schedule implements DoCheck,OnDestroy,OnInit,OnChanges,AfterViewChe
         return event;
     }
     
-    updateEvent(event: any) {
-        let sourceEvent = this.findEvent(event.id);
+    _updateEvent(event: any) {
+        let sourceEvent = this._findEvent(event.id);
         if(sourceEvent) {
             sourceEvent.start = event.start.format();
             if(event.end) {
