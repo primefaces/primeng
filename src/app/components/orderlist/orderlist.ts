58979424dd4a9283e9450c4fc63d64636622d7ad
Çağatay Çivici <cagatay.civici@github.com>
2017-06-13 18:25:07 +0300

    Refactor to use ngIF

--- src/app/components/orderlist/orderlist.ts
+++ src/app/components/orderlist/orderlist.ts
@@ -23,8 +23,8 @@ import {ObjectUtils} from '../utils/objectutils';
                 </div>
                 <ul #listelement class="ui-widget-content ui-orderlist-list ui-corner-bottom" [ngStyle]="listStyle" (dragover)="onListMouseMove($event)">
                     <ng-template ngFor let-item [ngForOf]="value" let-i="index" let-l="last">
-                        <li class="ui-orderlist-droppoint" *ngIf="dragdrop" (dragover)="onDragOver($event, i)" (drop)="onDrop($event, i)" (dragleave)="onDragLeave($event)" 
-                            [ngClass]="{'ui-state-highlight': (i === dragOverItemIndex)}" [style.display]="isItemVisible(item) ? 'block' : 'none'"></li>
+                        <li class="ui-orderlist-droppoint" *ngIf="dragdrop && isItemVisible(item)" (dragover)="onDragOver($event, i)" (drop)="onDrop($event, i)" (dragleave)="onDragLeave($event)" 
+                            [ngClass]="{'ui-state-highlight': (i === dragOverItemIndex)}"></li>
                         <li class="ui-orderlist-item"
                             [ngClass]="{'ui-state-highlight':isSelected(item)}" 
                             (click)="onItemClick($event,item)" (touchend)="onItemTouchEnd($event)"
@@ -32,7 +32,7 @@ import {ObjectUtils} from '../utils/objectutils';
                             [draggable]="dragdrop" (dragstart)="onDragStart($event, i)" (dragend)="onDragEnd($event)">
                             <ng-template [pTemplateWrapper]="itemTemplate" [item]="item"></ng-template>
                         </li>
-                        <li class="ui-orderlist-droppoint" *ngIf="dragdrop&&l" (dragover)="onDragOver($event, i + 1)" (drop)="onDrop($event, i + 1)" (dragleave)="onDragLeave($event)" 
+                        <li class="ui-orderlist-droppoint" *ngIf="dragdrop && l" (dragover)="onDragOver($event, i + 1)" (drop)="onDrop($event, i + 1)" (dragleave)="onDragLeave($event)" 
                             [ngClass]="{'ui-state-highlight': (i + 1 === dragOverItemIndex)}"></li>
                     </ng-template>
                 </ul>
