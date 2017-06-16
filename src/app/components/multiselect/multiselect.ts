ee63e1349964d98a2375cc31c152e1b98ec6556e
UMBIAN\oolive <oded.olive@resmed.com>
2017-06-16 10:49:26 -0300

    Change to the way the label is built to handle cases where the label is null

--- src/app/components/multiselect/multiselect.ts
+++ src/app/components/multiselect/multiselect.ts
@@ -315,10 +315,13 @@ export class MultiSelect implements OnInit,AfterViewInit,AfterViewChecked,DoChec
         if(this.value && this.options && this.value.length && this.displaySelectedLabel) {
             let label = '';
             for(let i = 0; i < this.value.length; i++) {
-                if(i != 0) {
-                    label = label + ', ';
+                let itemLabel = this.findLabelByValue(this.value[i]);
+                if (itemLabel) {
+                    if(label.length > 0) {
+                        label = label + ',';
+                    }
+                    label = label + itemLabel;
                 }
-                label = label + this.findLabelByValue(this.value[i]);
             }
             
             if(this.value.length <= this.maxSelectedLabels) {
