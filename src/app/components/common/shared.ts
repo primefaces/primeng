08c89fc53d57237fd2b61114088f8f4899d2fe63
Mrtcndkn <mrtcndkn@gmail.com>
2017-06-19 15:40:47 +0300

    fixed #3142

--- src/app/components/common/shared.ts
+++ src/app/components/common/shared.ts
@@ -81,6 +81,7 @@ export class Column implements AfterContentInit{
     @Input() expander: boolean;
     @Input() selectionMode: string;
     @Input() filterPlaceholder: string;
+    @Input() filterMaxlength: number;
     @Input() frozen: boolean;
     @Output() sortFunction: EventEmitter<any> = new EventEmitter();
     @ContentChildren(PrimeTemplate) templates: QueryList<any>;
