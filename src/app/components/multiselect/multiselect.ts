44fe457e83c64281272b635f32864ee47b5db443
Zakharevich.L <Zakharevich.L@digdes.com>
2017-06-23 18:34:57 +0300

    add init options for multiselect

--- src/app/components/multiselect/multiselect.ts
+++ src/app/components/multiselect/multiselect.ts
@@ -74,7 +74,7 @@ export class MultiSelect implements OnInit,AfterViewInit,AfterViewChecked,DoChec
     @Output() onBlur: EventEmitter<any> = new EventEmitter();
 
     @Input() scrollHeight: string = '200px';
-    
+
     @Input() defaultLabel: string = 'Choose';
 
     @Input() style: any;
@@ -84,91 +84,94 @@ export class MultiSelect implements OnInit,AfterViewInit,AfterViewChecked,DoChec
     @Input() inputId: string;
 
     @Input() disabled: boolean;
-    
+
     @Input() filter: boolean = true;
-    
+
     @Input() overlayVisible: boolean;
 
     @Input() tabindex: number;
-    
+
     @Input() appendTo: any;
-    
+
     @Input() dataKey: string;
-    
+
     @Input() displaySelectedLabel: boolean = true;
-    
+
     @Input() maxSelectedLabels: number = 3;
-    
+
     @Input() selectedItemsLabel: string = '{0} items selected';
-        
+
+    @Input() selectedOptions: SelectItem[] = [];
+
     @ViewChild('container') containerViewChild: ElementRef;
-    
+
     @ViewChild('panel') panelViewChild: ElementRef;
-    
+
     public value: any[];
-    
+
     public onModelChange: Function = () => {};
-    
+
     public onModelTouched: Function = () => {};
-    
+
     public valuesAsString: string;
-        
+
     public focus: boolean;
-    
+
     public documentClickListener: any;
-    
+
     public container: HTMLDivElement;
-    
+
     public panel: HTMLDivElement;
-        
+
     public selfClick: boolean;
-    
+
     public panelClick: boolean;
-    
+
     public filterValue: string;
-    
+
     public visibleOptions: SelectItem[];
-    
+
     public filtered: boolean;
-        
+
     public valueDiffer: any;
-    
+
     public optionsDiffer: any;
-    
+
     constructor(public el: ElementRef, public domHandler: DomHandler, public renderer: Renderer2, differs: IterableDiffers, public objectUtils: ObjectUtils) {
         this.valueDiffer = differs.find([]).create(null);
         this.optionsDiffer = differs.find([]).create(null);
     }
-    
+
     ngOnInit() {
+        this.initSelectedOptions();
         this.updateLabel();
-        
+
         this.documentClickListener = this.renderer.listen('document', 'click', () => {
             if(!this.selfClick && !this.panelClick && this.overlayVisible) {
                 this.hide();
             }
-            
+
             this.selfClick = false;
             this.panelClick = false;
         });
     }
-    
+
     ngAfterViewInit() {
         this.container = <HTMLDivElement> this.containerViewChild.nativeElement;
-        this.panel = <HTMLDivElement> this.panelViewChild.nativeElement; 
-        
+        this.panel = <HTMLDivElement> this.panelViewChild.nativeElement;
+
         if(this.appendTo) {
             if(this.appendTo === 'body')
                 document.body.appendChild(this.panel);
             else
                 this.domHandler.appendChild(this.panel, this.appendTo);
         }
-        
+
         if(this.overlayVisible) {
             this.show();
         }
     }
-    
+
     ngAfterViewChecked() {
         if(this.filtered) {
             if(this.appendTo)
@@ -179,21 +182,25 @@ export class MultiSelect implements OnInit,AfterViewInit,AfterViewChecked,DoChec
             this.filtered = false;
         }
     }
-    
+
     ngDoCheck() {
         let valueChanges = this.valueDiffer.diff(this.value);
         let optionChanges = this.optionsDiffer.diff(this.options);
-        
+
         if(valueChanges||optionChanges) {
             this.updateLabel();
         }
     }
-    
+
+    initSelectedOptions() {
+      this.selectedOptions.forEach((option) => this.value = [...this.value||[],option.value]);
+    }
+
     writeValue(value: any) : void {
         this.value = value;
         this.updateLabel();
     }
-    
+
     registerOnChange(fn: Function): void {
         this.onModelChange = fn;
     }
@@ -201,29 +208,29 @@ export class MultiSelect implements OnInit,AfterViewInit,AfterViewChecked,DoChec
     registerOnTouched(fn: Function): void {
         this.onModelTouched = fn;
     }
-    
+
     setDisabledState(val: boolean): void {
         this.disabled = val;
     }
-    
+
     onItemClick(event, value) {
         let selectionIndex = this.findSelectionIndex(value);
         if(selectionIndex != -1)
             this.value = this.value.filter((val,i) => i!=selectionIndex);
         else
             this.value = [...this.value||[],value];
-        
+
         this.onModelChange(this.value);
         this.onChange.emit({originalEvent: event, value: this.value});
-    }   
-    
+    }
+
     isSelected(value) {
         return this.findSelectionIndex(value) != -1;
     }
-    
+
     findSelectionIndex(val: any): number {
         let index = -1;
-        
+
         if(this.value) {
             for(let i = 0; i < this.value.length; i++) {
                 if(this.objectUtils.equals(this.value[i], val, this.dataKey)) {
@@ -232,10 +239,10 @@ export class MultiSelect implements OnInit,AfterViewInit,AfterViewChecked,DoChec
                 }
             }
         }
-        
+
         return index;
     }
-    
+
     toggleAll(event, checkbox) {
         if(checkbox.checked) {
             this.value = [];
@@ -246,25 +253,25 @@ export class MultiSelect implements OnInit,AfterViewInit,AfterViewChecked,DoChec
                 this.value = [];
                 for(let i = 0; i < opts.length; i++) {
                     this.value.push(opts[i].value);
-                } 
+                }
             }
         }
         checkbox.checked = !checkbox.checked;
         this.onModelChange(this.value);
         this.onChange.emit({originalEvent: event, value: this.value});
-    } 
-    
+    }
+
     isAllChecked() {
         if(this.filterValue && this.filterValue.trim().length)
             return this.value&&this.visibleOptions&&this.visibleOptions.length&&(this.value.length == this.visibleOptions.length);
         else
             return this.value&&this.options&&(this.value.length == this.options.length);
-    } 
-    
+    }
+
     show() {
         this.overlayVisible = true;
         this.panel.style.zIndex = String(++DomHandler.zindex);
-        
+
         if(this.appendTo)
             this.domHandler.absolutePosition(this.panel, this.container);
         else
@@ -272,22 +279,22 @@ export class MultiSelect implements OnInit,AfterViewInit,AfterViewChecked,DoChec
 
         this.domHandler.fadeIn(this.panel, 250);
     }
-    
+
     hide() {
         this.overlayVisible = false;
     }
-    
+
     close(event) {
         this.hide();
         event.preventDefault();
         event.stopPropagation();
     }
-         
+
     onMouseclick(event,input) {
         if(this.disabled) {
             return;
         }
-        
+
         if(!this.panelClick) {
             if(this.overlayVisible) {
                 this.hide();
@@ -297,20 +304,20 @@ export class MultiSelect implements OnInit,AfterViewInit,AfterViewChecked,DoChec
                 this.show();
             }
         }
-        
+
         this.selfClick = true;
     }
-    
+
     onFocus(event) {
         this.focus = true;
     }
-    
+
     onInputBlur(event) {
         this.focus = false;
         this.onBlur.emit({originalEvent: event});
         this.onModelTouched();
     }
-    
+
     updateLabel() {
         if(this.value && this.options && this.value.length && this.displaySelectedLabel) {
             let label = '';
@@ -320,7 +327,7 @@ export class MultiSelect implements OnInit,AfterViewInit,AfterViewChecked,DoChec
                 }
                 label = label + this.findLabelByValue(this.value[i]);
             }
-            
+
             if(this.value.length <= this.maxSelectedLabels) {
                 this.valuesAsString = label;
             }
@@ -334,19 +341,19 @@ export class MultiSelect implements OnInit,AfterViewInit,AfterViewChecked,DoChec
             this.valuesAsString = this.defaultLabel;
         }
     }
-    
+
     findLabelByValue(val: any): string {
         let label = null;
         for(let i = 0; i < this.options.length; i++) {
             let option = this.options[i];
             if(option.value == val) {
                 label = option.label;
-                break; 
+                break;
             }
         }
         return label;
     }
-    
+
     onFilter(event) {
         this.filterValue = event.target.value.trim().toLowerCase();
         this.visibleOptions = [];
@@ -358,7 +365,7 @@ export class MultiSelect implements OnInit,AfterViewInit,AfterViewChecked,DoChec
         }
         this.filtered = true;
     }
-        
+
     isItemVisible(option: SelectItem): boolean {
         if(this.filterValue && this.filterValue.trim().length) {
             for(let i = 0; i < this.visibleOptions.length; i++) {
@@ -371,7 +378,7 @@ export class MultiSelect implements OnInit,AfterViewInit,AfterViewChecked,DoChec
             return true;
         }
     }
-    
+
     getVisibleOptions(): SelectItem[] {
         if(this.filterValue && this.filterValue.trim().length) {
             let items = [];
@@ -392,7 +399,7 @@ export class MultiSelect implements OnInit,AfterViewInit,AfterViewChecked,DoChec
         if(this.documentClickListener) {
             this.documentClickListener();
         }
-        
+
         if(this.appendTo) {
             this.container.appendChild(this.panel);
         }
