3a68b9b563fbb20135cf345629809f78f27732ad
Sarp Saatçıoğlu <sarpsaatci@gmail.com>
2017-07-11 09:37:43 +0300

    (new feature) slide down animation for Messages

--- src/app/components/messages/messages.ts
+++ src/app/components/messages/messages.ts
@@ -1,27 +1,43 @@
 import {NgModule,Component,Input,Output,EventEmitter} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Message} from '../common/message';
+import {trigger,state,style,transition,animate} from '@angular/animations';
 
 @Component({
     selector: 'p-messages',
     template: `
         <div *ngIf="hasMessages()" class="ui-messages ui-widget ui-corner-all" style="display:block"
-                    [ngClass]="{'ui-messages-info':(value[0].severity === 'info'),
-                    'ui-messages-warn':(value[0].severity === 'warn'),
-                    'ui-messages-error':(value[0].severity === 'error'),
-                    'ui-messages-success':(value[0].severity === 'success')}">
+                    [ngClass]="{'ui-messages-info':(getSeverityClass() === 'info'),
+                    'ui-messages-warn':(getSeverityClass() === 'warn'),
+                    'ui-messages-error':(getSeverityClass() === 'error'),
+                    'ui-messages-success':(getSeverityClass() === 'success')}"
+                    [@slideIn]="hasMessages() ? '*' : 'void'"><div class ="ui-messages-container">
             <a href="#" class="ui-messages-close" (click)="clear($event)" *ngIf="closable">
                 <i class="fa fa-close"></i>
             </a>
             <span class="ui-messages-icon fa fa-fw fa-2x" [ngClass]="icon"></span>
             <ul>
                 <li *ngFor="let msg of value">
-                    <span class="ui-messages-summary">{{msg.summary}}</span>
-                    <span class="ui-messages-detail">{{msg.detail}}</span>
+                    <span class="ui-messages-summary">{{getSummary()}}</span>
+                    <span class="ui-messages-detail">{{getDetail()}}</span>
                 </li>
             </ul>
-        </div>
-    `
+        </div></div>
+    `,
+    animations: [
+      trigger('slideIn', [
+      state('*', style({ height : '*' })),
+      state('void', style({ height: '0' })),
+      transition('* => void', [
+          style({ height: '*' }),
+          animate(200, style({ height: '0' }))
+      ]),
+      transition('void => *', [
+          style({ height: '0' }),
+          animate(200 , style({ height: '*' }))
+      ])
+    ])
+  ]
 })
 export class Messages {
 
@@ -34,9 +50,26 @@ export class Messages {
     hasMessages() {
         return this.value && this.value.length > 0;
     }
-
+    
     getSeverityClass() {
+      if(this.value && this.value[0])
         return this.value[0].severity;
+      else
+        return null;
+    }
+    
+    getSummary() {
+      if(this.value && this.value[0])
+        return this.value[0].summary;
+      else
+        return null;
+    }
+    
+    getDetail() {
+      if(this.value && this.value[0])
+        return this.value[0].detail;
+      else
+        return null;
     }
 
     clear(event) {
