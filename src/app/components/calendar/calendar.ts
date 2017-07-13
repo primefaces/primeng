043ef9561dd94c7203a45b2f4d55a310d82b3e26
Muratcan Sefa KATIRCI <katirci@ug.bilkent.edu.tr>
2017-07-13 11:40:50 +0300

    #3337  Fixed: Calendar navigator year bug
    The bug has been fixed for both start and end of the range.

--- src/app/components/calendar/calendar.ts
+++ src/app/components/calendar/calendar.ts
@@ -458,7 +458,11 @@ export class Calendar implements AfterViewInit,AfterViewChecked,OnInit,OnDestroy
         
         if(this.currentMonth === 0) {
             this.currentMonth = 11;
-            this.currentYear--;
+            if(this.yearRange && this.currentYear === this.yearOptions[0]) {
+              this.currentYear = this.yearOptions[this.yearOptions.length - 1];
+            }          
+            else
+              this.currentYear--;
         }
         else {
             this.currentMonth--;
@@ -476,7 +480,11 @@ export class Calendar implements AfterViewInit,AfterViewChecked,OnInit,OnDestroy
         
         if(this.currentMonth === 11) {
             this.currentMonth = 0;
-            this.currentYear++;
+            if(this.yearRange && this.currentYear === this.yearOptions[this.yearOptions.length - 1 ]) {
+              this.currentYear = this.yearOptions[0];
+            }          
+            else
+              this.currentYear++;
         }
         else {
             this.currentMonth++;
