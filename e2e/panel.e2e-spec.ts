95a46756a5abde5dcb3faa0ea351ae36e0b8ad26
Mrtcndkn <mrtcndkn@gmail.com>
2017-07-31 01:36:04 +0300

    remove warning from panel test

--- e2e/panel.e2e-spec.ts
+++ e2e/panel.e2e-spec.ts
@@ -1,19 +1,26 @@
-import {browser, by, element, ElementFinder} from 'protractor';
+import {browser, by, element, ElementFinder, ElementArrayFinder} from 'protractor';
 
 describe('Panel', () => {
     let toggleIcon: ElementFinder;
-    let contentWrapper: ElementFinder;
+    let contentWrapper: ElementArrayFinder;
     
     describe('Toggle Icon Click', () => {
         beforeEach(() => {
           browser.get('#/panel');
           toggleIcon = element(by.css('.ui-panel-titlebar-icon'));
-          contentWrapper = element(by.css('.ui-panel-content-wrapper'));
+          contentWrapper = element.all(by.css('.ui-panel-content-wrapper'));
         });
 
         it('should close active content', () => {
             toggleIcon.click();
-            expect(contentWrapper.getCssValue('overflow')).toBe('hidden');
+            expect(contentWrapper.get(0).getCssValue('overflow')).toBe('hidden');
+        });
+        
+        it('should toggle content', () => {
+            toggleIcon.click();
+            browser.sleep(1000);
+            toggleIcon.click();
+            expect(contentWrapper.get(0).getCssValue('height')).not.toBe('0');
         });
         
     });
