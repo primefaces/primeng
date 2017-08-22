259886be7cadbf101a9018ab26224e82f39d2c4b
Çağatay Çivici <cagatay.civici@github.com>
2017-08-22 15:40:32 +0300

    Fixed #3707

--- src/app/components/breadcrumb/breadcrumb.ts
+++ src/app/components/breadcrumb/breadcrumb.ts
@@ -12,11 +12,11 @@ import {RouterModule} from '@angular/router';
                 <li class="ui-breadcrumb-home fa fa-home" *ngIf="!home"></li>
                 <li class="ui-breadcrumb-home" *ngIf="home">
                     <a *ngIf="!home.routerLink" [href]="home.url||'#'" class="ui-menuitem-link" (click)="itemClick($event, home)" 
-                        [ngClass]="{'ui-state-disabled':home.disabled}" [attr.target]="home.target">
+                        [ngClass]="{'ui-state-disabled':home.disabled}" [attr.target]="home.target" [attr.title]="home.title">
                         <span class="fa fa-home"></span>
                     </a>
                     <a *ngIf="home.routerLink" [routerLink]="home.routerLink" [routerLinkActive]="'ui-state-active'" [routerLinkActiveOptions]="home.routerLinkActiveOptions||{exact:false}" class="ui-menuitem-link" (click)="itemClick($event, home)" 
-                        [ngClass]="{'ui-state-disabled':home.disabled}" [attr.target]="home.target">
+                        [ngClass]="{'ui-state-disabled':home.disabled}" [attr.target]="home.target" [attr.title]="home.title">
                         <span class="fa fa-home"></span>
                     </a>
                 </li>
@@ -24,11 +24,11 @@ import {RouterModule} from '@angular/router';
                 <ng-template ngFor let-item let-end="last" [ngForOf]="model">
                     <li role="menuitem">
                         <a *ngIf="!item.routerLink" [href]="item.url||'#'" class="ui-menuitem-link" (click)="itemClick($event, item)" 
-                            [ngClass]="{'ui-state-disabled':item.disabled}" [attr.target]="item.target">
+                            [ngClass]="{'ui-state-disabled':item.disabled}" [attr.target]="item.target" [attr.title]="item.title">
                             <span class="ui-menuitem-text">{{item.label}}</span>
                         </a>
                         <a *ngIf="item.routerLink" [routerLink]="item.routerLink" [routerLinkActive]="'ui-state-active'" [routerLinkActiveOptions]="item.routerLinkActiveOptions||{exact:false}" class="ui-menuitem-link" (click)="itemClick($event, item)" 
-                            [ngClass]="{'ui-state-disabled':item.disabled}" [attr.target]="item.target">
+                            [ngClass]="{'ui-state-disabled':item.disabled}" [attr.target]="item.target" [attr.title]="item.title">
                             <span class="ui-menuitem-text">{{item.label}}</span>
                         </a>
                     </li>
