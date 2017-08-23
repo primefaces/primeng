61b8aaa82bbe875b881da9c926c95f2a4ac30bbd
Çağatay Çivici <cagatay.civici@github.com>
2017-08-23 16:25:23 +0300

    Cleanup subscription

--- src/app/showcase/components/terminal/terminaldemo.ts
+++ src/app/showcase/components/terminal/terminaldemo.ts
@@ -1,16 +1,25 @@
-import {Component} from '@angular/core';
+import {Component,OnDestroy} from '@angular/core';
 import {TerminalService} from '../../../components/terminal/terminalservice';
+import {Subscription}   from 'rxjs/Subscription';
 
 @Component({
     templateUrl: './terminaldemo.html',
     providers: [TerminalService]
 })
-export class TerminalDemo {
+export class TerminalDemo implements OnDestroy {
+    
+    subscription: Subscription;
     
     constructor(private terminalService: TerminalService) {
-        this.terminalService.commandHandler.subscribe(command => {
+        this.subscription = this.terminalService.commandHandler.subscribe(command => {
             let response = (command === 'date') ? new Date().toDateString() : 'Unknown command: ' + command;
             this.terminalService.sendResponse(response);
         });
+    }
+    
+    ngOnDestroy() {
+        if(this.subscription) {
+            this.subscription.unsubscribe();
+        }
     }
 }
