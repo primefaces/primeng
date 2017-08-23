27a3df7d91acdf9561ee67d5b586665d00b2a8aa
sumragen <gennadiy.varava@logicify.com>
2017-08-23 17:28:06 +0300

    #3724 added flag "loadingIndicatorVisible" to display simple "loading" message when process to search suggestions is started

--- src/app/components/autocomplete/autocomplete.ts
+++ src/app/components/autocomplete/autocomplete.ts
@@ -44,6 +44,9 @@ export const AUTOCOMPLETE_VALUE_ACCESSOR: any = {
                     <li *ngIf="noResults && emptyMessage" class="ui-autocomplete-list-item ui-corner-all">{{emptyMessage}}</li>
                 </ul>
             </div>
+            <div [style.display]="loadingIndicatorVisible ? 'block' : 'none'">
+              Loading...
+            </div>
         </span>
     `,
     host: {
@@ -53,133 +56,135 @@ export const AUTOCOMPLETE_VALUE_ACCESSOR: any = {
     providers: [DomHandler,ObjectUtils,AUTOCOMPLETE_VALUE_ACCESSOR]
 })
 export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,ControlValueAccessor {
-    
+
     @Input() minLength: number = 1;
-    
+
     @Input() delay: number = 300;
-    
+
     @Input() style: any;
-    
+
     @Input() styleClass: string;
-    
+
     @Input() inputStyle: any;
 
     @Input() inputId: string;
-    
+
     @Input() inputStyleClass: string;
-    
+
     @Input() placeholder: string;
-    
+
     @Input() readonly: boolean;
-        
+
     @Input() disabled: boolean;
-    
+
     @Input() maxlength: number;
-    
+
     @Input() required: boolean;
-    
+
     @Input() size: number;
-    
+
     @Input() appendTo: any;
-    
+
     @Input() autoHighlight: boolean;
-    
+
     @Input() type: string = 'text';
 
     @Output() completeMethod: EventEmitter<any> = new EventEmitter();
-    
+
     @Output() onSelect: EventEmitter<any> = new EventEmitter();
-    
+
     @Output() onUnselect: EventEmitter<any> = new EventEmitter();
 
     @Output() onFocus: EventEmitter<any> = new EventEmitter();
-    
+
     @Output() onBlur: EventEmitter<any> = new EventEmitter();
-    
+
     @Output() onDropdownClick: EventEmitter<any> = new EventEmitter();
-	
+
 	@Output() onClear: EventEmitter<any> = new EventEmitter();
-    
+
     @Output() onKeyUp: EventEmitter<any> = new EventEmitter();
-    
+
     @Input() field: string;
-    
+
     @Input() scrollHeight: string = '200px';
-    
+
     @Input() dropdown: boolean;
-    
+
     @Input() multiple: boolean;
 
     @Input() tabindex: number;
-    
+
     @Input() dataKey: string;
-    
+
     @Input() emptyMessage: string;
-    
+
     @Input() immutable: boolean = true;
-    
+
     @ViewChild('in') inputEL: ElementRef;
-    
+
     @ViewChild('multiIn') multiInputEL: ElementRef;
-    
+
     @ViewChild('panel') panelEL: ElementRef;
-    
+
     @ViewChild('multiContainer') multiContainerEL: ElementRef;
-        
+
     @ContentChildren(PrimeTemplate) templates: QueryList<any>;
-    
+
     itemTemplate: TemplateRef<any>;
-    
+
     selectedItemTemplate: TemplateRef<any>;
-    
+
     value: any;
-    
+
     _suggestions: any[];
-    
+
     onModelChange: Function = () => {};
-    
+
     onModelTouched: Function = () => {};
-    
+
     timeout: any;
-            
+
     panelVisible: boolean = false;
-    
+
+    loadingIndicatorVisible: boolean = false;
+
     documentClickListener: any;
-    
+
     suggestionsUpdated: boolean;
-    
+
     highlightOption: any;
-    
+
     highlightOptionChanged: boolean;
-    
+
     focus: boolean = false;
-        
+
     filled: boolean;
-    
+
     inputClick: boolean;
 
     inputKeyDown: boolean;
-    
+
     noResults: boolean;
-    
+
     differ: any;
-        
+
     constructor(public el: ElementRef, public domHandler: DomHandler, public renderer: Renderer2, public objectUtils: ObjectUtils, public cd: ChangeDetectorRef, public differs: IterableDiffers) {
         this.differ = differs.find([]).create(null);
     }
-    
+
     @Input() get suggestions(): any[] {
         return this._suggestions;
     }
 
     set suggestions(val:any[]) {
         this._suggestions = val;
-                
+
         if(this.immutable) {
             this.handleSuggestionsChange();
         }
     }
-    
+
     ngDoCheck() {
         if(!this.immutable) {
             let changes = this.differ.diff(this.suggestions);
@@ -188,7 +193,7 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
             }
         }
     }
-    
+
     handleSuggestionsChange() {
         if(this.panelEL && this.panelEL.nativeElement) {
             this.highlightOption = null;
@@ -196,15 +201,15 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
                 this.noResults = false;
                 this.show();
                 this.suggestionsUpdated = true;
-                
+
                 if(this.autoHighlight) {
                     this.highlightOption = this._suggestions[0];
                 }
             }
             else {
                 this.noResults = true;
-                
-                if(this.emptyMessage) {    
+
+                if(this.emptyMessage) {
                     this.show();
                     this.suggestionsUpdated = true;
                 }
@@ -214,7 +219,7 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
             }
         }
     }
-        
+
     ngAfterContentInit() {
         this.templates.forEach((item) => {
             switch(item.getType()) {
@@ -225,14 +230,14 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
                 case 'selectedItem':
                     this.selectedItemTemplate = item.template;
                 break;
-                
+
                 default:
                     this.itemTemplate = item.template;
                 break;
             }
         });
     }
-    
+
     ngAfterViewInit() {
         if(this.appendTo) {
             if(this.appendTo === 'body')
@@ -241,14 +246,14 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
                 this.domHandler.appendChild(this.panelEL.nativeElement, this.appendTo);
         }
     }
-    
+
     ngAfterViewChecked() {
         //Use timeouts as since Angular 4.2, AfterViewChecked is broken and not called after panel is updated
         if(this.suggestionsUpdated && this.panelEL.nativeElement && this.panelEL.nativeElement.offsetParent) {
             setTimeout(() => this.align(), 1);
             this.suggestionsUpdated = false;
         }
-        
+
         if(this.highlightOptionChanged) {
             setTimeout(() => {
                 let listItem = this.domHandler.findSingle(this.panelEL.nativeElement, 'li.ui-state-highlight');
@@ -259,12 +264,12 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
             this.highlightOptionChanged = false;
         }
     }
-    
+
     writeValue(value: any) : void {
         this.value = value;
         this.filled = this.value && this.value != '';
     }
-    
+
     registerOnChange(fn: Function): void {
         this.onModelChange = fn;
     }
@@ -272,7 +277,7 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
     registerOnTouched(fn: Function): void {
         this.onModelTouched = fn;
     }
-    
+
     setDisabledState(val: boolean): void {
         this.disabled = val;
     }
@@ -286,12 +291,12 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
         if(!this.multiple) {
             this.onModelChange(value);
         }
-        
+
         if(value.length === 0) {
            this.hide();
 		   this.onClear.emit(event);
         }
-        
+
         if(value.length >= this.minLength) {
             //Cancel the search request if user types within the timeout
             if(this.timeout) {
@@ -308,25 +313,29 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
         this.updateFilledState();
         this.inputKeyDown = false;
     }
-    
+
     onInputClick(event: MouseEvent) {
         if(this.documentClickListener) {
             this.inputClick = true;
         }
     }
-    
+
     search(event: any, query: string) {
         //allow empty string but not undefined or null
        if(query === undefined || query === null) {
            return;
        }
-       
-       this.completeMethod.emit({
+
+      if(!this.panelVisible) {
+        this.loadingIndicatorVisible = true;
+      }
+
+      this.completeMethod.emit({
            originalEvent: event,
            query: query
        });
     }
-            
+
     selectItem(option: any) {
         if(this.multiple) {
             this.multiInputEL.nativeElement.value = '';
@@ -341,36 +350,37 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
             this.value = option;
             this.onModelChange(this.value);
         }
-        
+
         this.onSelect.emit(option);
-        
+
         this.focusInput();
     }
-    
+
     show() {
         if(this.multiInputEL || this.inputEL) {
             let hasFocus = this.multiple ? document.activeElement == this.multiInputEL.nativeElement : document.activeElement == this.inputEL.nativeElement ;
             if(!this.panelVisible && hasFocus) {
+                this.loadingIndicatorVisible = false;
                 this.panelVisible = true;
                 this.panelEL.nativeElement.style.zIndex = ++DomHandler.zindex;
                 this.domHandler.fadeIn(this.panelEL.nativeElement, 200);
                 this.bindDocumentClickListener();
-            }   
+            }
         }
     }
-    
+
     align() {
         if(this.appendTo)
             this.domHandler.absolutePosition(this.panelEL.nativeElement, (this.multiple ? this.multiContainerEL.nativeElement : this.inputEL.nativeElement));
         else
             this.domHandler.relativePosition(this.panelEL.nativeElement, (this.multiple ? this.multiContainerEL.nativeElement : this.inputEL.nativeElement));
     }
-    
+
     hide() {
         this.panelVisible = false;
         this.unbindDocumentClickListener();
     }
-    
+
     handleDropdownClick(event) {
         this.focusInput();
         let queryValue = this.multiple ? this.multiInputEL.nativeElement.value : this.inputEL.nativeElement.value;
@@ -379,14 +389,14 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
             query: queryValue
         });
     }
-    
+
     focusInput() {
         if(this.multiple)
             this.multiInputEL.nativeElement.focus();
         else
             this.inputEL.nativeElement.focus();
     }
-    
+
     removeItem(item: any) {
         let itemIndex = this.domHandler.index(item);
         let removedValue = this.value[itemIndex];
@@ -394,11 +404,11 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
         this.onUnselect.emit(removedValue);
         this.onModelChange(this.value);
     }
-        
+
     onKeydown(event) {
         if(this.panelVisible) {
             let highlightItemIndex = this.findOptionIndex(this.highlightOption);
-            
+
             switch(event.which) {
                 //down
                 case 40:
@@ -412,10 +422,10 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
                     else {
                         this.highlightOption = this.suggestions[0];
                     }
-                    
+
                     event.preventDefault();
                 break;
-                
+
                 //up
                 case 38:
                     if(highlightItemIndex > 0) {
@@ -423,10 +433,10 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
                         this.highlightOption = this.suggestions[prevItemIndex];
                         this.highlightOptionChanged = true;
                     }
-                    
+
                     event.preventDefault();
                 break;
-                
+
                 //enter
                 case 13:
                     if(this.highlightOption) {
@@ -435,14 +445,14 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
                     }
                     event.preventDefault();
                 break;
-                
+
                 //escape
                 case 27:
                     this.hide();
                     event.preventDefault();
                 break;
 
-                
+
                 //tab
                 case 9:
                     if(this.highlightOption) {
@@ -456,7 +466,7 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
                 this.search(event,event.target.value);
             }
         }
-        
+
         if(this.multiple) {
             switch(event.which) {
                 //backspace
@@ -473,22 +483,22 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
 
         this.inputKeyDown = true;
     }
-    
+
     onKeyup(event) {
         this.onKeyUp.emit(event);
     }
-    
+
     onInputFocus(event) {
         this.focus = true;
         this.onFocus.emit(event);
     }
-    
+
     onInputBlur(event) {
         this.focus = false;
         this.onModelTouched();
         this.onBlur.emit(event);
     }
-            
+
     isSelected(val: any): boolean {
         let selected: boolean = false;
         if(this.value && this.value.length) {
@@ -501,8 +511,8 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
         }
         return selected;
     }
-    
-    findOptionIndex(option): number {        
+
+    findOptionIndex(option): number {
         let index: number = -1;
         if(this.suggestions) {
             for(let i = 0; i < this.suggestions.length; i++) {
@@ -512,41 +522,41 @@ export class AutoComplete implements AfterViewInit,AfterViewChecked,DoCheck,Cont
                 }
             }
         }
-                
+
         return index;
     }
-    
+
     updateFilledState() {
         if(this.multiple)
             this.filled = (this.value && this.value.length) || (this.multiInputEL && this.multiInputEL.nativeElement && this.multiInputEL.nativeElement.value != '');
         else
             this.filled = this.inputEL && this.inputEL.nativeElement && this.inputEL.nativeElement.value != '';
     }
-    
+
     bindDocumentClickListener() {
         if(!this.documentClickListener) {
             this.documentClickListener = this.renderer.listen('document', 'click', (event) => {
                 if(event.which === 3) {
                     return;
                 }
-                
+
                 if(this.inputClick)
                     this.inputClick = false;
                 else
                     this.hide();
-                    
+
                 this.cd.markForCheck();
             });
         }
     }
-    
+
     unbindDocumentClickListener() {
         if(this.documentClickListener) {
             this.documentClickListener();
             this.documentClickListener = null;
         }
     }
-    
+
     ngOnDestroy() {
         this.unbindDocumentClickListener();
 
