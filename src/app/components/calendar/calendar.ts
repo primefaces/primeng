9e237adf5dbbbd66529de850657ede368f4c9ee0
Çağatay Çivici <cagatay.civici@github.com>
2017-10-04 10:44:55 +0300

    Fixed #3927

--- src/app/components/calendar/calendar.ts
+++ src/app/components/calendar/calendar.ts
@@ -1482,7 +1482,11 @@ export class Calendar implements AfterViewInit,AfterViewChecked,OnInit,OnDestroy
 			} while (true);
 		}
 
-		date = this.daylightSavingAdjust(new Date(year, month - 1, day));
+        if (this.utc)
+            date = new Date(Date.UTC(year, month - 1, day));
+        else
+            date = this.daylightSavingAdjust(new Date(year, month - 1, day));
+
 		if(date.getFullYear() !== year || date.getMonth() + 1 !== month || date.getDate() !== day) {
 			throw "Invalid date"; // E.g. 31/02/00
 		}
