dea9214139e12e12745a3a17034041504c1b149a
Norman Cano <norman.cano@outlook.com>
2017-12-08 18:12:25 -0500

    Dropdown: Add `filterMode` property

--- src/app/components/dropdown/dropdown.ts
+++ src/app/components/dropdown/dropdown.ts
@@ -77,6 +77,8 @@ export class Dropdown implements OnInit,AfterViewInit,AfterContentInit,AfterView
     @Input() scrollHeight: string = '200px';
 
     @Input() filter: boolean;
+	
+	@Input() filterMode: string = 'contains';
     
     @Input() name: string;
 
@@ -544,7 +546,7 @@ export class Dropdown implements OnInit,AfterViewInit,AfterContentInit,AfterView
     activateFilter() {
         let searchFields: string[] = this.filterBy.split(',');
         if(this.options && this.options.length) {
-            this.optionsToDisplay = this.objectUtils.filter(this.options, searchFields, this.filterValue);
+            this.optionsToDisplay = this.objectUtils.filter(this.options, searchFields, this.filterValue, this.filterMode);
             this.optionsChanged = true;
         }
     }
