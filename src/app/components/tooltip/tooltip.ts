4fd7b24c3469d779073854f96ddc3f36242f3706
Çağatay Çivici <cagatay.civici@github.com>
2017-12-19 15:44:34 +0300

    Fixed #4053

--- src/app/components/tooltip/tooltip.ts
+++ src/app/components/tooltip/tooltip.ts
@@ -1,137 +1,150 @@
-import {NgModule,Directive,ElementRef,OnDestroy,HostBinding,HostListener,Input,Renderer2} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {DomHandler} from '../dom/domhandler';
+import { NgModule, Directive, ElementRef, AfterViewInit, OnDestroy, HostBinding, HostListener, Input, NgZone } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DomHandler } from '../dom/domhandler';
 
 @Directive({
     selector: '[pTooltip]',
     providers: [DomHandler]
 })
-export class Tooltip implements OnDestroy {
+export class Tooltip implements AfterViewInit, OnDestroy {
 
     @Input() tooltipPosition: string = 'right';
-    
-    @Input() tooltipEvent: string = 'hover';
-    
-    @Input() appendTo: any = 'body';
-    
-    @Input() positionStyle: string;
-    
-    @Input() tooltipStyleClass: string;
-    
-    @Input() tooltipZIndex: string = 'auto';
-    
-    @Input("tooltipDisabled") disabled: boolean;
-    
-    @Input() escape: boolean = true;
-    
-    @Input() showDelay: number;
-    
-    @Input() hideDelay: number;
-    
-    @Input() life: number;
-    
-    container: any;
-    
-    styleClass: string;
-    
-    tooltipText: any;
-    
-    showTimeout: any;
-    
-    hideTimeout: any;
-    
-    lifeTimeout: any;
-    
-    documentResizeListener: Function;
-    
-    active: boolean;
-    
-    public _text: string;
-    
-    constructor(public el: ElementRef, public domHandler: DomHandler, public renderer: Renderer2) {}
-    
-    @HostListener('mouseenter', ['$event']) 
-    onMouseEnter(e: Event) {
-        if(this.tooltipEvent === 'hover') {
-            if(this.hideTimeout) {
-                clearTimeout(this.hideTimeout);
-                this.destroy();
-            }
 
-            this.activate();
+    @Input() tooltipEvent: string = 'hover';
+
+    @Input() appendTo: any = 'body';
+
+    @Input() positionStyle: string;
+
+    @Input() tooltipStyleClass: string;
+
+    @Input() tooltipZIndex: string = 'auto';
+
+    @Input("tooltipDisabled") disabled: boolean;
+
+    @Input() escape: boolean = true;
+
+    @Input() showDelay: number;
+
+    @Input() hideDelay: number;
+
+    @Input() life: number;
+
+    container: any;
+
+    styleClass: string;
+
+    tooltipText: any;
+
+    showTimeout: any;
+
+    hideTimeout: any;
+
+    lifeTimeout: any;
+
+    active: boolean;
+
+    _text: string;
+
+    mouseEnterListener: Function;
+
+    mouseLeaveListener: Function;
+
+    clickListener: Function;
+
+    focusListener: Function;
+
+    blurListener: Function;
+
+    resizeListener: any;
+
+    constructor(public el: ElementRef, public domHandler: DomHandler, public zone: NgZone) { }
+
+    ngAfterViewInit() {
+        this.zone.runOutsideAngular(() => {
+            if (this.tooltipEvent === 'hover') {
+                this.mouseEnterListener = this.onMouseEnter.bind(this);
+                this.mouseLeaveListener = this.onMouseLeave.bind(this);
+                this.clickListener = this.onClick.bind(this);
+                this.el.nativeElement.addEventListener('mouseenter', this.mouseEnterListener);
+                this.el.nativeElement.addEventListener('mouseleave', this.mouseLeaveListener);
+                this.el.nativeElement.addEventListener('click', this.clickListener);
+            }
+            else if (this.tooltipEvent === 'focus') {
+                this.focusListener = this.onFocus.bind(this);
+                this.blurListener = this.onBlur.bind(this);
+                this.el.nativeElement.addEventListener('focus', this.focusListener);
+                this.el.nativeElement.addEventListener('blur', this.blurListener);
+            }
+        });
+    }
+
+    onMouseEnter(e: Event) {
+        if (this.hideTimeout) {
+            clearTimeout(this.hideTimeout);
+            this.remove();
         }
+
+        this.activate();
     }
     
-    @HostListener('mouseleave', ['$event'])
     onMouseLeave(e: Event) {
-        if(this.tooltipEvent === 'hover') {
-            this.deactivate(true);
-        }
-    }
-    
-    @HostListener('focus', ['$event'])
-    onFocus(e: Event) {
-        if(this.tooltipEvent === 'focus') {
-            this.activate();
-        }
-    }
-    
-    @HostListener('blur', ['$event'])
-    onBlur(e: Event) {
-        if(this.tooltipEvent === 'focus') {
-            this.deactivate(true);
-        }
-    }
-  
-  
-    @HostListener('click', ['$event'])
-    onClick(e: Event) {
-      if(this.tooltipEvent === 'hover') {
         this.deactivate(true);
-      }
     }
     
+    onFocus(e: Event) {
+        this.activate();
+    }
+    
+    onBlur(e: Event) {
+        this.deactivate(true);
+    }
+  
+    onClick(e: Event) {
+        this.deactivate(true);
+    }
+
     activate() {
         this.active = true;
-        if(this.hideTimeout) {
+        if (this.hideTimeout) {
             clearTimeout(this.hideTimeout);
         }
-        
-        if(this.showDelay)
+
+        if (this.showDelay)
             this.showTimeout = setTimeout(() => { this.show() }, this.showDelay);
         else
             this.show();
-            
-        if(this.life) {
+
+        if (this.life) {
             this.lifeTimeout = setTimeout(() => { this.deactivate(false) }, this.life);
         }
     }
-    
+
     deactivate(useDelay) {
         this.active = false;
-        if(this.showTimeout) {
+        if (this.showTimeout) {
             clearTimeout(this.showTimeout);
         }
-        
-        if(this.lifeTimeout) {
+
+        if (this.lifeTimeout) {
             clearTimeout(this.lifeTimeout);
         }
-        
-        if(this.hideDelay && useDelay)
+
+        if (this.hideDelay && useDelay)
             this.hideTimeout = setTimeout(() => { this.hide() }, this.hideDelay);
         else
             this.hide();
     }
-    
+
     get text(): string {
         return this._text;
     }
 
     @Input('pTooltip') set text(text: string) {
         this._text = text;
-        if(this.active) {
-            if(this._text) {
-                if(this.container && this.container.offsetParent)
+        if (this.active) {
+            if (this._text) {
+                if (this.container && this.container.offsetParent)
                     this.updateText();
                 else
                     this.show();
@@ -141,126 +154,126 @@ export class Tooltip implements OnDestroy {
             }
         }
     }
-    
+
     create() {
         this.container = document.createElement('div');
-        
+
         let tooltipArrow = document.createElement('div');
         tooltipArrow.className = 'ui-tooltip-arrow';
         this.container.appendChild(tooltipArrow);
-        
+
         this.tooltipText = document.createElement('div');
         this.tooltipText.className = 'ui-tooltip-text ui-shadow ui-corner-all';
-		
-		this.updateText();
-        
-        if(this.positionStyle) {
+
+        this.updateText();
+
+        if (this.positionStyle) {
             this.container.style.position = this.positionStyle;
         }
-        
+
         this.container.appendChild(this.tooltipText);
-        
-        if(this.appendTo === 'body')
+
+        if (this.appendTo === 'body')
             document.body.appendChild(this.container);
-        else if(this.appendTo === 'target')
+        else if (this.appendTo === 'target')
             this.domHandler.appendChild(this.container, this.el.nativeElement);
         else
             this.domHandler.appendChild(this.container, this.appendTo);
-            
+
         this.container.style.display = 'inline-block';
     }
-    
+
     show() {
-        if(!this.text || this.disabled) {
+        if (!this.text || this.disabled) {
             return;
         }
-        
+
         this.create();
         this.align();
-        if(this.tooltipStyleClass) {
+        if (this.tooltipStyleClass) {
             this.container.className = this.container.className + ' ' + this.tooltipStyleClass;
         }
         this.domHandler.fadeIn(this.container, 250);
-        if(this.tooltipZIndex === 'auto')
+        if (this.tooltipZIndex === 'auto')
             this.container.style.zIndex = ++DomHandler.zindex;
         else
             this.container.style.zIndex = this.tooltipZIndex;
-        
+
         this.bindDocumentResizeListener();
     }
-    
+
     hide() {
-        this.destroy();
+        this.remove();
     }
-    
-    updateText () {
-        if(this.escape) {
+
+    updateText() {
+        if (this.escape) {
             this.tooltipText.innerHTML = '';
             this.tooltipText.appendChild(document.createTextNode(this._text));
         }
-		else {
+        else {
             this.tooltipText.innerHTML = this._text;
         }
     }
-    
+
     align() {
         let position = this.tooltipPosition;
-        
-        switch(position) {
+
+        switch (position) {
             case 'top':
                 this.alignTop();
-                if(this.isOutOfBounds()) {
+                if (this.isOutOfBounds()) {
                     this.alignBottom();
                 }
-            break;
-            
+                break;
+
             case 'bottom':
                 this.alignBottom();
-                if(this.isOutOfBounds()) {
+                if (this.isOutOfBounds()) {
                     this.alignTop();
                 }
-            break;
-            
+                break;
+
             case 'left':
                 this.alignLeft();
-                if(this.isOutOfBounds()) {
+                if (this.isOutOfBounds()) {
                     this.alignRight();
-                    
-                    if(this.isOutOfBounds()) {
+
+                    if (this.isOutOfBounds()) {
                         this.alignTop();
-                        
-                        if(this.isOutOfBounds()) {
+
+                        if (this.isOutOfBounds()) {
                             this.alignBottom();
                         }
                     }
                 }
-            break;
-            
+                break;
+
             case 'right':
                 this.alignRight();
-                if(this.isOutOfBounds()) {
+                if (this.isOutOfBounds()) {
                     this.alignLeft();
-                    
-                    if(this.isOutOfBounds()) {
+
+                    if (this.isOutOfBounds()) {
                         this.alignTop();
-                        
-                        if(this.isOutOfBounds()) {
+
+                        if (this.isOutOfBounds()) {
                             this.alignBottom();
                         }
                     }
                 }
-            break;
+                break;
         }
     }
-    
+
     getHostOffset() {
         let offset = this.el.nativeElement.getBoundingClientRect();
         let targetLeft = offset.left + this.domHandler.getWindowScrollLeft();
         let targetTop = offset.top + this.domHandler.getWindowScrollTop();
-        
-        return {left: targetLeft, top: targetTop};
+
+        return { left: targetLeft, top: targetTop };
     }
-    
+
     alignRight() {
         this.preAlign();
         this.container.className = 'ui-tooltip ui-widget ui-tooltip-right';
@@ -270,7 +283,7 @@ export class Tooltip implements OnDestroy {
         this.container.style.left = left + 'px';
         this.container.style.top = top + 'px';
     }
-    
+
     alignLeft() {
         this.preAlign();
         this.container.className = 'ui-tooltip ui-widget ui-tooltip-left';
@@ -280,7 +293,7 @@ export class Tooltip implements OnDestroy {
         this.container.style.left = left + 'px';
         this.container.style.top = top + 'px';
     }
-    
+
     alignTop() {
         this.preAlign();
         this.container.className = 'ui-tooltip ui-widget ui-tooltip-top';
@@ -290,7 +303,7 @@ export class Tooltip implements OnDestroy {
         this.container.style.left = left + 'px';
         this.container.style.top = top + 'px';
     }
-    
+
     alignBottom() {
         this.preAlign();
         this.container.className = 'ui-tooltip ui-widget ui-tooltip-bottom';
@@ -300,12 +313,12 @@ export class Tooltip implements OnDestroy {
         this.container.style.left = left + 'px';
         this.container.style.top = top + 'px';
     }
-    
+
     preAlign() {
         this.container.style.left = -999 + 'px';
         this.container.style.top = -999 + 'px';
     }
-    
+
     isOutOfBounds(): boolean {
         let offset = this.container.getBoundingClientRect();
         let targetTop = offset.top;
@@ -316,36 +329,55 @@ export class Tooltip implements OnDestroy {
 
         return (targetLeft + width > viewport.width) || (targetLeft < 0) || (targetTop < 0) || (targetTop + height > viewport.height);
     }
-    
+
+    onWindowResize(e: Event) {
+        this.hide();
+    }
+
     bindDocumentResizeListener() {
-        this.documentResizeListener = this.renderer.listen('window', 'resize', (event) => {
-            this.hide();
+        this.zone.runOutsideAngular(() => {
+            this.resizeListener = this.onWindowResize.bind(this);
+            window.addEventListener('resize', this.resizeListener);
         });
     }
-    
+
     unbindDocumentResizeListener() {
-        if(this.documentResizeListener) {
-            this.documentResizeListener();
-            this.documentResizeListener = null;
+        if (this.resizeListener) {
+            window.removeEventListener('resize', this.resizeListener);
+            this.resizeListener = null;
         }
     }
-    
-    destroy() {
+
+    unbindEvents() {
+        if (this.tooltipEvent === 'hover') {
+            this.el.nativeElement.removeEventListener('mouseenter', this.mouseEnterListener);
+            this.el.nativeElement.removeEventListener('mouseleave', this.mouseLeaveListener);
+            this.el.nativeElement.removeEventListener('click', this.clickListener);
+        }
+        else if (this.tooltipEvent === 'focus') {
+            this.el.nativeElement.removeEventListener('focus', this.focusListener);
+            this.el.nativeElement.removeEventListener('blur', this.blurListener);
+        }
+
         this.unbindDocumentResizeListener();
-        
-        if(this.container && this.container.parentElement) {
-            if(this.appendTo === 'body')
+    }
+
+    remove() {
+        if (this.container && this.container.parentElement) {
+            if (this.appendTo === 'body')
                 document.body.removeChild(this.container);
-            else if(this.appendTo === 'target')
+            else if (this.appendTo === 'target')
                 this.el.nativeElement.removeChild(this.container);
-            else
+            else 
                 this.domHandler.removeChild(this.container, this.appendTo);
         }
+        
         this.container = null;
     }
-    
+
     ngOnDestroy() {
-        this.destroy();
+        this.unbindEvents();
+        this.remove();
     }
 }
 
