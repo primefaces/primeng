051a5ee984f9cd0f35d21627a02b027d9c9e7b18
Çağatay Çivici <jagatai@Cagatays-MacBook-Pro.local>
2018-01-22 12:43:47 +0300

    Fixed #1097

--- src/app/components/tieredmenu/tieredmenu.ts
+++ src/app/components/tieredmenu/tieredmenu.ts
@@ -11,6 +11,7 @@ import {RouterModule} from '@angular/router';
             <ng-template ngFor let-child [ngForOf]="(root ? item : item.items)">
                 <li *ngIf="child.separator" class="ui-menu-separator ui-widget-content">
                 <li *ngIf="!child.separator" #listItem [ngClass]="{'ui-menuitem ui-widget ui-corner-all':true,'ui-menuitem-active':listItem==activeItem}"
+                    [class]="child.styleClass" [ngStyle]="child.style"
                     (mouseenter)="onItemMouseEnter($event, listItem, child)" (mouseleave)="onItemMouseLeave($event)">
                     <a *ngIf="!child.routerLink" [href]="child.url||'#'" class="ui-menuitem-link ui-corner-all" [attr.target]="child.target" [attr.title]="child.title" [attr.id]="child.id"
                         [ngClass]="{'ui-state-disabled':child.disabled}" (click)="itemClick($event, child)">
