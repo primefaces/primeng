55835eac716cc55102b548f1be8acc1fadcea5de
Çağatay Çivici <jagatai@Cagatays-MacBook-Pro.local>
2018-01-23 12:13:34 +0300

    refactor prop name

--- src/app/components/dropdown/dropdown.ts
+++ src/app/components/dropdown/dropdown.ts
@@ -130,7 +130,7 @@ export class Dropdown implements OnInit,AfterViewInit,AfterContentInit,AfterView
     
     @Input() optionLabel: string;
 
-    @Input() autoSelectFirst: boolean = true;
+    @Input() autoDisplayFirst: boolean = true;
 
     @Input() emptyFilterMessage: string = 'No results found';
     
@@ -345,7 +345,7 @@ export class Dropdown implements OnInit,AfterViewInit,AfterContentInit,AfterView
     
     updateSelectedOption(val: any): void {
         this.selectedOption = this.findOption(val, this.optionsToDisplay);
-        if(this.autoSelectFirst && !this.placeholder && !this.selectedOption && this.optionsToDisplay && this.optionsToDisplay.length && !this.editable) {
+        if(this.autoDisplayFirst && !this.placeholder && !this.selectedOption && this.optionsToDisplay && this.optionsToDisplay.length && !this.editable) {
             this.selectedOption = this.optionsToDisplay[0];
         }
         this.selectedOptionUpdated = true;
