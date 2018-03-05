7c9583c5eb20a860a34bb862458bfd65202367f3
Çağatay Çivici <jagatai@Cagatays-MacBook-Pro.local>
2018-03-05 15:26:34 +0300

    Use click instead to move on top

--- src/app/components/dialog/dialog.ts
+++ src/app/components/dialog/dialog.ts
@@ -11,7 +11,7 @@ let idx: number = 0;
     selector: 'p-dialog',
     template: `
         <div #container [ngClass]="{'ui-dialog ui-widget ui-widget-content ui-corner-all ui-shadow':true, 'ui-dialog-rtl':rtl,'ui-dialog-draggable':draggable}" [style.display]="visible ? 'block' : 'none'"
-            [ngStyle]="style" [class]="styleClass" [style.width.px]="width" [style.height.px]="height" [style.minWidth.px]="minWidth" (mousedown)="moveOnTop()" [@dialogState]="visible ? 'visible' : 'hidden'"
+            [ngStyle]="style" [class]="styleClass" [style.width.px]="width" [style.height.px]="height" [style.minWidth.px]="minWidth" (click)="moveOnTop()" [@dialogState]="visible ? 'visible' : 'hidden'"
             role="dialog" [attr.aria-labelledby]="id + '-label'">
             <div #titlebar class="ui-dialog-titlebar ui-widget-header ui-helper-clearfix ui-corner-top"
                 (mousedown)="initDrag($event)" (mouseup)="endDrag($event)" *ngIf="showHeader">
@@ -323,8 +323,7 @@ export class Dialog implements AfterViewInit,AfterViewChecked,OnDestroy {
     moveOnTop() {
         if(this.autoZIndex) {
             this.containerViewChild.nativeElement.style.zIndex = String(this.baseZIndex + (++DomHandler.zindex));
-        }
-        
+        } 
     }
     
     onCloseMouseDown(event: Event) {
