731f4f9688137279b94c4d5d89e80f0378ddc858
Çağatay Çivici <jagatai@Cagatays-MacBook-Pro.local>
2018-03-09 20:50:57 +0300

    Remove unused code demo

--- src/app/showcase/components/table/tableeditdemo.ts
+++ src/app/showcase/components/table/tableeditdemo.ts
@@ -9,18 +9,9 @@ export class TableEditDemo implements OnInit {
 
     cars: Car[];
 
-    cols: any[];
-
     constructor(private carService: CarService) { }
 
     ngOnInit() {
         this.carService.getCarsSmall().then(cars => this.cars = cars);
-
-        this.cols = [
-            { field: 'vin', header: 'Vin' },
-            { field: 'year', header: 'Year' },
-            { field: 'brand', header: 'Brand' },
-            { field: 'color', header: 'Color' }
-        ];
     }
 }
