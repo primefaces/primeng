01d4f9bd1bbf57ba2a25238d654aee2efb79f11e
MI53RE <MI53RE@users.noreply.github.com>
2018-04-17 11:24:40 +0200

    :sparkles: New filter constraint `containsIn`
    This new constraints allow to filter with a mutliselect over several values (as string for now) instead of only one.
    Rule is as following : If at least one element of the selected list is present in the cell return true (keep the row), else return false (exclude the row)
    Here is a small example on how it works in the html
    
    ```html
    <p-table #dt [value]="entities" emptyMessage="empty">
      <ng-template pTemplate="header">
        <tr>
          <th>
            <p-multiSelect 
              [options]="groups"
              defaultLabel="Tous les groupes"
              (onChange)="dt.filter($event.value, 'groups', 'containsIn')">
            </p-multiSelect>
          </th>
        </tr>
      </ng-template>
      <ng-template let-rowData pTemplate="body">
        <tr>
          <td>
            {{rowData.['groups']}}
          </td>
        </tr>
      </ng-template>
    </p-table>
    ```
    
    And the following explaining the result on the interfaces
    
    |  Groups  |
    |  ------  |
    |  g1      |
    |  g1, g2  |
    |  g2      |
    |  g3      |
    |  g4, g3  |
    
    [x] g1
    
    |  Groups  |
    |  ------  |
    |  g1      |
    |  g1, g2  |
    
    [x] g1
    [x] g4
    
    |  Groups  |
    |  ------  |
    |  g1      |
    |  g1, g2  |
    |  g4, g3  |

--- src/app/components/table/table.ts
+++ src/app/components/table/table.ts
@@ -1203,6 +1203,29 @@ export class Table implements OnInit, AfterContentInit {
             }
 
             return value > filter;
+        },
+
+        containsIn: function (value, filter) {
+            if (filter === undefined || filter === null || filter.length === 0) {
+                return true;
+            }
+            if (value === undefined || value === null) {
+                return false;
+            }
+            for (let _i = 0; _i < filter.length; _i++) {
+                if (filter[_i] === undefined || filter[_i] === null || (typeof filter[_i] === 'string' && filter[_i].trim() === '')) {
+                    return true;
+                }
+                if (value === undefined || value === null) {
+                    return false;
+                }
+                const _regExp = new RegExp('\\b' + filter[_i].toLowerCase() + '\\b');
+                const _result = _regExp.test(value.toString().toLowerCase());
+                if (_result) {
+                    return _result;
+                }
+            }
+            return false;
         }
     }
 
