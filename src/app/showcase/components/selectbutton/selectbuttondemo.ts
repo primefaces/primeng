ef71dae07b3bcf23503d95bfacbdb9d39e6cba65
Çağatay Çivici <jagatai@Cagatays-MacBook-Pro.local>
2018-04-19 10:36:36 +0300

    Fixed #5124

--- src/app/showcase/components/selectbutton/selectbuttondemo.ts
+++ src/app/showcase/components/selectbutton/selectbuttondemo.ts
@@ -16,6 +16,10 @@ export class SelectButtonDemo {
 
     modes: SelectItem[];
 
+    countries: any[];
+
+    selectedCountry: any;
+
     constructor() {
         this.types = [
             {label: 'Paypal', value: 'PayPal', icon: 'fa fa-fw fa-cc-paypal'},
@@ -28,11 +32,18 @@ export class SelectButtonDemo {
             {value: 'Italic', title: 'Italic', icon: 'fa fa-fw fa-italic'},
             {value: 'Underline', title: 'Underline', icon: 'fa fa-fw fa-underline'}
         ];
+
+        this.countries = [
+            {name: 'USA', flag: 'usa.png'},
+            {name: 'Germany', flag: 'germany.png'},
+            {name: 'Japan', flag: 'japan.png'}
+        ];
     }
 
     clear() {
         this.selectedType = null;
         this.selectedTypes = [];
         this.selectedModes = [];
+        this.selectedCountry = null;
     }
 }
