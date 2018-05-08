cae507053b11bac8c05145a333421de1e1cba4d0
Çağatay Çivici <jagatai@Prime-MacBook.local>
2018-05-08 10:07:03 +0300

    Fix AOT issue

--- src/app/components/radiobutton/radiobutton.ts
+++ src/app/components/radiobutton/radiobutton.ts
@@ -16,7 +16,7 @@ export const RADIO_VALUE_ACCESSOR: any = {
                 <input #rb type="radio" [attr.id]="inputId" [attr.name]="name" [attr.value]="value" [attr.tabindex]="tabindex" 
                     [checked]="checked" (change)="onChange($event)" (focus)="onFocus($event)" (blur)="onBlur($event)" [disabled]="disabled">
             </div>
-            <div (click)="handleClick($event, rb, focus)"
+            <div (click)="handleClick($event, rb, true)"
                 [ngClass]="{'ui-radiobutton-box ui-widget ui-state-default':true,
                 'ui-state-active':rb.checked,'ui-state-disabled':disabled,'ui-state-focus':focused}">
                 <span class="ui-radiobutton-icon ui-clickable" [ngClass]="{'fa fa-circle':rb.checked}"></span>
