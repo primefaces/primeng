21641f436f47ab92f0013f7075e6a95278975908
Himanshu Arora <himanshu_arora@csx.com>
2018-05-16 16:35:00 -0400

    updated failing test

--- src/app/showcase/app.component.spec.ts
+++ src/app/showcase/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-        imports: [ RouterTestingModule ],
+        imports: [ RouterTestingModule, FormsModule ],
         declarations: [ AppComponent ]
     }).compileComponents();
   }));
