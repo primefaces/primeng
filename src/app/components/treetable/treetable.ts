62ce9ab612481161ea2c580049fda15c28428806
Çağatay Çivici <jagatai@Prime-MacBook.local>
2018-05-23 11:08:41 +0300

    Pagination and Sections for new TreeTable

--- src/app/components/treetable/treetable.ts
+++ src/app/components/treetable/treetable.ts
@@ -1,8 +1,9 @@
-import {NgModule, AfterContentInit, Component, Input, Output, EventEmitter, ContentChildren, TemplateRef, QueryList} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {TreeNode} from '../common/treenode';
-import {Subscription} from 'rxjs';
-import {DomHandler} from '../dom/domhandler';
+import { NgModule, AfterContentInit, Component, Input, Output, EventEmitter, ContentChildren, TemplateRef, QueryList} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TreeNode } from '../common/treenode';
+import { Subscription } from 'rxjs';
+import { DomHandler } from '../dom/domhandler';
+import { PaginatorModule } from '../paginator/paginator';
 import { PrimeTemplate } from '../common/shared';
 
 @Component({
@@ -13,6 +14,9 @@ import { PrimeTemplate } from '../common/shared';
             <div *ngIf="captionTemplate" class="ui-treetable-caption ui-widget-header">
                 <ng-container *ngTemplateOutlet="captionTemplate"></ng-container>
             </div>
+            <p-paginator [rows]="rows" [first]="first" [totalRecords]="totalRecords" [pageLinkSize]="pageLinks" styleClass="ui-paginator-top" [alwaysShow]="alwaysShowPaginator"
+                (onPageChange)="onPageChange($event)" [rowsPerPageOptions]="rowsPerPageOptions" *ngIf="paginator && (paginatorPosition === 'top' || paginatorPosition =='both')"
+                [templateLeft]="paginatorLeftTemplate" [templateRight]="paginatorRightTemplate" [dropdownAppendTo]="paginatorDropdownAppendTo"></p-paginator>
             <div class="ui-treetable-wrapper" *ngIf="!scrollable">
                 <table #table class="ui-treetable-table">
                     <ng-container *ngTemplateOutlet="colGroupTemplate; context {$implicit: columns}"></ng-container>
@@ -24,6 +28,12 @@ import { PrimeTemplate } from '../common/shared';
                     </tfoot>
                     <tbody class="ui-treetable-tbody" [pTreeTableBody]="columns" [pTreeTableBodyTemplate]="bodyTemplate"></tbody>
                 </table>
+            </div>
+            <p-paginator [rows]="rows" [first]="first" [totalRecords]="totalRecords" [pageLinkSize]="pageLinks" styleClass="ui-paginator-bottom" [alwaysShow]="alwaysShowPaginator"
+                (onPageChange)="onPageChange($event)" [rowsPerPageOptions]="rowsPerPageOptions" *ngIf="paginator && (paginatorPosition === 'bottom' || paginatorPosition =='both')"
+                [templateLeft]="paginatorLeftTemplate" [templateRight]="paginatorRightTemplate" [dropdownAppendTo]="paginatorDropdownAppendTo"></p-paginator>
+            <div *ngIf="summaryTemplate" class="ui-treetable-summary ui-widget-header">
+                <ng-container *ngTemplateOutlet="summaryTemplate"></ng-container>
             </div>
         </div>
     `,
@@ -41,9 +51,33 @@ export class TreeTable implements AfterContentInit{
 
     @Input() scrollable: boolean;
 
+    @Input() lazy: boolean = false;
+
+    @Input() paginator: boolean;
+
+    @Input() rows: number;
+
+    @Input() first: number = 0;
+
+    @Input() totalRecords: number = 0;
+
+    @Input() pageLinks: number = 5;
+
+    @Input() rowsPerPageOptions: number[];
+
+    @Input() alwaysShowPaginator: boolean = true;
+
+    @Input() paginatorPosition: string = 'bottom';
+
+    @Input() paginatorDropdownAppendTo: any;
+
     @Input() rowTrackBy: Function = (index: number, item: any) => item;
 
     @Output() onNodeExpand: EventEmitter<any> = new EventEmitter();
+
+    @Output() onNodeCollapse: EventEmitter<any> = new EventEmitter();
+
+    @Output() onPage: EventEmitter<any> = new EventEmitter();
 
     @ContentChildren(PrimeTemplate) templates: QueryList<PrimeTemplate>;
 
@@ -61,7 +95,13 @@ export class TreeTable implements AfterContentInit{
 
     footerTemplate: TemplateRef<any>;
 
+    summaryTemplate: TemplateRef<any>;
+
     emptyMessageTemplate: TemplateRef<any>;
+
+    paginatorLeftTemplate: TemplateRef<any>;
+
+    paginatorRightTemplate: TemplateRef<any>;
 
     ngAfterContentInit() {
         this.templates.forEach((item) => {
@@ -82,8 +122,20 @@ export class TreeTable implements AfterContentInit{
                     this.footerTemplate = item.template;
                 break;
 
+                case 'summary':
+                    this.summaryTemplate = item.template;
+                break;
+
                 case 'emptymessage':
                     this.emptyMessageTemplate = item.template;
+                break;
+
+                case 'paginatorleft':
+                    this.paginatorLeftTemplate = item.template;
+                break;
+
+                case 'paginatorright':
+                    this.paginatorRightTemplate = item.template;
                 break;
             }
         });
@@ -94,12 +146,21 @@ export class TreeTable implements AfterContentInit{
     }
     set value(val: any[]) {
         this._value = val;
+
+        if (!this.lazy) {
+            this.totalRecords = (this._value ? this._value.length : 0);
+        }
+
         this.updateSerializedValue();
     }
 
     updateSerializedValue() {
         this.serializedValue = [];
-        this.serializeNodes(null, this.value, 0);
+
+        if(this.paginator)
+            this.serializePageNodes();
+        else
+            this.serializeNodes(null, this.value, 0);
     }
 
     serializeNodes(parent, nodes, level) {
@@ -114,6 +175,39 @@ export class TreeTable implements AfterContentInit{
                 this.serializeNodes(node, node.children, level + 1);
             }
         }
+    }
+
+    serializePageNodes() {
+        this.serializedValue = []; 
+        if(this.value && this.value.length) {
+            for(let i = this.first; i < (this.first + this.rows); i++) {
+                let node = this.value[i];
+    
+                this.serializedValue.push({
+                    node: node,
+                    parent: null,
+                    level: 0
+                });
+    
+                this.serializeNodes(node, node.children, 1);
+            }
+        }
+    }
+
+    onPageChange(event) {
+        this.first = event.first;
+        this.rows = event.rows;
+
+        /*if (this.lazy) {
+            this.onLazyLoad.emit(this.createLazyLoadMetadata());
+        }*/
+
+        this.onPage.emit({
+            first: this.first,
+            rows: this.rows
+        });
+
+        this.serializePageNodes();
     }
 
     isEmpty() {
@@ -173,7 +267,7 @@ export class TreeTableToggler {
 
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule,PaginatorModule],
     exports: [TreeTable,TreeTableToggler],
     declarations: [TreeTable,TreeTableBody,TreeTableToggler]
 })
