91139583c16c13c2525dd43c5c66098b3de32f3e
Çağatay Çivici <jagatai@Prime-MacBook.local>
2018-05-29 11:10:14 +0300

    Toggleable columns for new TreeTable

--- src/app/showcase/components/treetable/treetabledemo.module.ts
+++ src/app/showcase/components/treetable/treetabledemo.module.ts
@@ -1,13 +1,14 @@
-import {NgModule}     from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormsModule}    from '@angular/forms';
-import {TreeTableDemo} from './treetabledemo';
-import {TreeTableDemoRoutingModule} from './treetabledemo-routing.module';
-import {TreeTableModule} from '../../../components/treetable/treetable';
-import {GrowlModule} from '../../../components/growl/growl';
-import {TabViewModule} from '../../../components/tabview/tabview';
-import {ContextMenuModule} from '../../../components/contextmenu/contextmenu';
-import {CodeHighlighterModule} from '../../../components/codehighlighter/codehighlighter';
+import { NgModule }     from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule }    from '@angular/forms';
+import { TreeTableDemo } from './treetabledemo';
+import { TreeTableDemoRoutingModule } from './treetabledemo-routing.module';
+import { TreeTableModule } from '../../../components/treetable/treetable';
+import { GrowlModule } from '../../../components/growl/growl';
+import { TabViewModule } from '../../../components/tabview/tabview';
+import { MultiSelectModule } from '../../../components/multiselect/multiselect';
+import { ContextMenuModule } from '../../../components/contextmenu/contextmenu';
+import { CodeHighlighterModule } from '../../../components/codehighlighter/codehighlighter';
 import { TreeTableSubmenu } from './treetablesubmenu';
 import { TreeTablePageDemo } from './treetablepagedemo';
 import { TreeTableSortDemo } from './treetablesortdemo';
@@ -32,6 +33,7 @@ import { TreeTableEditDemo } from './treetableeditdemo';
         TreeTableModule,
         GrowlModule,
         TabViewModule,
+        MultiSelectModule,
         ContextMenuModule,
         CodeHighlighterModule
 	],
