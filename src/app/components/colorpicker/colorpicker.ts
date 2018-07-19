a4c87b832d0f278b2a888236e1d009380c650dfa
Matt Filbert <Matthew.C.Filbert@associates.ice.dhs.gov>
2018-07-19 09:13:44 -0500

    colorpicker aria

--- src/app/components/colorpicker/colorpicker.ts
+++ src/app/components/colorpicker/colorpicker.ts
@@ -15,7 +15,7 @@ export const COLORPICKER_VALUE_ACCESSOR: any = {
     template: `
         <div [ngStyle]="style" [class]="styleClass" [ngClass]="{'ui-colorpicker ui-widget':true,'ui-colorpicker-overlay':!inline,'ui-colorpicker-dragging':colorDragging||hueDragging}">
             <input #input type="text" *ngIf="!inline" class="ui-colorpicker-preview ui-inputtext ui-state-default ui-corner-all" readonly="readonly" [ngClass]="{'ui-state-disabled': disabled}"
-                (focus)="onInputFocus()" (click)="onInputClick()" (keydown)="onInputKeydown($event)" [attr.id]="inputId" [attr.tabindex]="tabindex" [disabled]="disabled"
+                (focus)="onInputFocus()" (click)="onInputClick()" (keydown)="onInputKeydown($event)" [attr.id]="inputId" [attr.tabindex]="tabindex" [attr.aria-label]="ariaLabel" [attr.aria-required]="ariaRequired" [disabled]="disabled"
                 [style.backgroundColor]="inputBgColor">
             <div #panel [ngClass]="{'ui-colorpicker-panel ui-corner-all': true, 'ui-colorpicker-overlay-panel ui-shadow':!inline, 'ui-state-disabled': disabled}" (click)="onPanelClick()"
                 [@panelState]="inline ? 'visible' : (panelVisible ? 'visible' : 'hidden')" [style.display]="inline ? 'block' : (panelVisible ? 'block' : 'none')">
@@ -51,63 +51,67 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
     @Input() style: any;
 
     @Input() styleClass: string;
-    
+
     @Input() inline: boolean;
-    
+
     @Input() format: string = 'hex';
-    
+
     @Input() appendTo: string;
-    
+
     @Input() disabled: boolean;
-    
+
     @Input() tabindex: string;
-    
+
     @Input() inputId: string;
-    
+
+    @Input() ariaLabel: string;
+
+    @Input() ariaRequired: boolean;
+
     @Output() onChange: EventEmitter<any> = new EventEmitter();
-    
+
     @ViewChild('panel') panelViewChild: ElementRef;
-    
+
     @ViewChild('colorSelector') colorSelectorViewChild: ElementRef;
-    
+
     @ViewChild('colorHandle') colorHandleViewChild: ElementRef;
-    
+
     @ViewChild('hue') hueViewChild: ElementRef;
-    
+
     @ViewChild('hueHandle') hueHandleViewChild: ElementRef;
-    
+
     @ViewChild('input') inputViewChild: ElementRef;
-    
+
     value: any;
-    
+
     inputBgColor: string;
-    
+
     shown: boolean;
-    
+
     panelVisible: boolean;
-    
+
     defaultColor: string = 'ff0000';
-    
+
     onModelChange: Function = () => {};
-    
+
     onModelTouched: Function = () => {};
-    
+
     documentClickListener: Function;
-    
+
     documentMousemoveListener: Function;
-    
+
     documentMouseupListener: Function;
-    
+
     documentHueMoveListener: Function;
-                
+
     selfClick: boolean;
-    
+
     colorDragging: boolean;
-    
+
     hueDragging: boolean;
-                
+
     constructor(public el: ElementRef, public domHandler: DomHandler, public renderer: Renderer2, public cd: ChangeDetectorRef) {}
-    
+
     ngAfterViewInit() {
         if (this.appendTo) {
             if (this.appendTo === 'body')
@@ -123,19 +127,19 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
             this.shown = false;
         }
     }
-    
+
     onHueMousedown(event: MouseEvent) {
         if(this.disabled) {
             return;
         }
-        
+
         this.bindDocumentMousemoveListener();
         this.bindDocumentMouseupListener();
-        
+
         this.hueDragging = true;
         this.pickHue(event);
     }
-    
+
     pickHue(event: MouseEvent) {
         let top: number = this.hueViewChild.nativeElement.getBoundingClientRect().top + (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);
         this.value = this.validateHSB({
@@ -143,25 +147,25 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
             s: this.value.s,
             b: this.value.b
         });
-        
+
         this.updateColorSelector();
         this.updateUI();
         this.updateModel();
         this.onChange.emit({originalEvent: event, value: this.getValueToUpdate()});
     }
-    
+
     onColorMousedown(event: MouseEvent) {
         if(this.disabled) {
             return;
         }
-        
+
         this.bindDocumentMousemoveListener();
         this.bindDocumentMouseupListener();
-        
+
         this.colorDragging = true;
         this.pickColor(event);
     }
-    
+
     pickColor(event: MouseEvent) {
         let rect = this.colorSelectorViewChild.nativeElement.getBoundingClientRect();
         let top = rect.top + (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0);
@@ -173,31 +177,31 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
             s: saturation,
             b: brightness
         });
-        
+
         this.updateUI();
         this.updateModel();
         this.onChange.emit({originalEvent: event, value: this.getValueToUpdate()});
     }
-    
+
     getValueToUpdate() {
         let val: any;
         switch(this.format) {
             case 'hex':
                 val = '#' + this.HSBtoHEX(this.value);
             break;
-            
+
             case 'rgb':
                 val = this.HSBtoRGB(this.value);
             break;
-            
+
             case 'hsb':
                 val = this.value;
             break;
         }
-        
+
         return val;
     }
-    
+
     updateModel(): void {
         this.onModelChange(this.getValueToUpdate());
     }
@@ -208,11 +212,11 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
                 case 'hex':
                     this.value = this.HEXtoHSB(value);
                 break;
-                
+
                 case 'rgb':
                     this.value = this.RGBtoHSB(value);
                 break;
-                
+
                 case 'hsb':
                     this.value = value;
                 break;
@@ -221,61 +225,61 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
         else {
             this.value = this.HEXtoHSB(this.defaultColor);
         }
-        
+
         this.updateColorSelector();
         this.updateUI();
     }
-    
+
     updateColorSelector() {
         this.colorSelectorViewChild.nativeElement.style.backgroundColor = '#' + this.HSBtoHEX(this.value);
     }
-        
+
     updateUI() {
         this.colorHandleViewChild.nativeElement.style.left =  Math.floor(150 * this.value.s / 100) + 'px';
         this.colorHandleViewChild.nativeElement.style.top =  Math.floor(150 * (100 - this.value.b) / 100) + 'px';
         this.hueHandleViewChild.nativeElement.style.top = Math.floor(150 - (150 * this.value.h / 360)) + 'px';
         this.inputBgColor = '#' + this.HSBtoHEX(this.value);
     }
-    
+
     onInputFocus() {
         this.onModelTouched();
     }
-    
+
     show() {
         this.panelViewChild.nativeElement.style.zIndex = String(++DomHandler.zindex);
         this.panelVisible = true;
         this.shown = true;
     }
-    
+
     hide() {
         this.panelVisible = false;
         this.unbindDocumentClickListener();
     }
-    
+
     onShow() {
         this.alignPanel();
         this.bindDocumentClickListener();
     }
-     
+
     alignPanel() {
         if(this.appendTo)
             this.domHandler.absolutePosition(this.panelViewChild.nativeElement, this.inputViewChild.nativeElement);
         else
             this.domHandler.relativePosition(this.panelViewChild.nativeElement, this.inputViewChild.nativeElement);
     }
-    
+
     onInputClick() {
         this.selfClick = true;
         this.togglePanel();
     }
-    
+
     togglePanel() {
         if(!this.panelVisible)
             this.show();
         else
             this.hide();
     }
-    
+
     onInputKeydown(event: KeyboardEvent) {
         switch(event.which) {
             //space
@@ -283,7 +287,7 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
                 this.togglePanel();
                 event.preventDefault();
             break;
-                        
+
             //escape and tab
             case 27:
             case 9:
@@ -291,11 +295,11 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
             break;
         }
     }
-        
+
     onPanelClick() {
         this.selfClick = true;
     }
-    
+
     registerOnChange(fn: Function): void {
         this.onModelChange = fn;
     }
@@ -303,11 +307,11 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
     registerOnTouched(fn: Function): void {
         this.onModelTouched = fn;
     }
-    
+
     setDisabledState(val: boolean): void {
         this.disabled = val;
     }
-    
+
     bindDocumentClickListener() {
         if(!this.documentClickListener) {
             this.documentClickListener = this.renderer.listen('document', 'click', () => {
@@ -315,41 +319,41 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
                     this.panelVisible = false;
                     this.unbindDocumentClickListener();
                 }
-                
+
                 this.selfClick = false;
                 this.cd.markForCheck();
             });
-        }    
+        }
     }
-    
+
     unbindDocumentClickListener() {
         if(this.documentClickListener) {
             this.documentClickListener();
             this.documentClickListener = null;
         }
     }
-    
+
     bindDocumentMousemoveListener() {
         if(!this.documentMousemoveListener) {
             this.documentMousemoveListener = this.renderer.listen('document', 'mousemove', (event: MouseEvent) => {
                 if(this.colorDragging) {
                     this.pickColor(event);
                 }
-                
+
                 if(this.hueDragging) {
                     this.pickHue(event);
                 }
             });
         }
     }
-    
+
     unbindDocumentMousemoveListener() {
         if(this.documentMousemoveListener) {
             this.documentMousemoveListener();
             this.documentMousemoveListener = null;
         }
     }
-    
+
     bindDocumentMouseupListener() {
         if(!this.documentMouseupListener) {
             this.documentMouseupListener = this.renderer.listen('document', 'mouseup', () => {
@@ -360,7 +364,7 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
             });
         }
     }
-    
+
     unbindDocumentMouseupListener() {
         if(this.documentMouseupListener) {
             this.documentMouseupListener();
@@ -375,7 +379,7 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
             b: Math.min(100, Math.max(0, hsb.b))
         };
     }
-    
+
     validateRGB(rgb) {
         return {
             r: Math.min(255, Math.max(0, rgb.r)),
@@ -383,7 +387,7 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
             b: Math.min(255, Math.max(0, rgb.b))
         };
     }
-    
+
     validateHEX(hex) {
         var len = 6 - hex.length;
         if (len > 0) {
@@ -396,16 +400,16 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
         }
         return hex;
     }
-    
+
     HEXtoRGB(hex) {
         let hexValue = parseInt(((hex.indexOf('#') > -1) ? hex.substring(1) : hex), 16);
         return {r: hexValue >> 16, g: (hexValue & 0x00FF00) >> 8, b: (hexValue & 0x0000FF)};
     }
-    
+
     HEXtoHSB(hex) {
         return this.RGBtoHSB(this.HEXtoRGB(hex));
     }
-    
+
     RGBtoHSB(rgb) {
         var hsb = {
             h: 0,
@@ -417,7 +421,7 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
         var delta = max - min;
         hsb.b = max;
         if (max != 0) {
-            
+
         }
         hsb.s = max != 0 ? 255 * delta / max : 0;
         if (hsb.s != 0) {
@@ -439,7 +443,7 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
         hsb.b *= 100/255;
         return hsb;
     }
-    
+
     HSBtoRGB(hsb) {
         var rgb = {
             r: null, g: null, b: null
@@ -453,7 +457,7 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
                 g: v,
                 b: v
             }
-        } 
+        }
         else {
             var t1 = v;
             var t2 = (255-s)*v/255;
@@ -469,27 +473,27 @@ export class ColorPicker implements ControlValueAccessor, AfterViewInit, AfterVi
         }
         return {r:Math.round(rgb.r), g:Math.round(rgb.g), b:Math.round(rgb.b)};
     }
-    
+
     RGBtoHEX(rgb) {
         var hex = [
             rgb.r.toString(16),
             rgb.g.toString(16),
             rgb.b.toString(16)
         ];
-        
+
         for(var key in hex) {
             if(hex[key].length == 1) {
                 hex[key] = '0' + hex[key];
             }
-        }        
+        }
 
         return hex.join('');
     }
-    
+
     HSBtoHEX(hsb) {
         return this.RGBtoHEX(this.HSBtoRGB(hsb));
     }
-    
+
     ngOnDestroy() {
         this.unbindDocumentClickListener();
 
