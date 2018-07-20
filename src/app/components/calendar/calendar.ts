6fbd37b9475de082ba140e5773d4de6f12c9dcdc
Çağatay Çivici <jagatai@Prime-MacBook.local>
2018-07-20 14:03:02 +0300

    Check for state

--- src/app/components/calendar/calendar.ts
+++ src/app/components/calendar/calendar.ts
@@ -37,7 +37,7 @@ export interface LocaleSettings {
             </ng-template>
             <div #datepicker [class]="panelStyleClass" [ngClass]="{'ui-datepicker ui-widget ui-widget-content ui-helper-clearfix ui-corner-all': true, 'ui-datepicker-inline':inline,'ui-shadow':!inline,
                 'ui-state-disabled':disabled,'ui-datepicker-timeonly':timeOnly,'ui-datepicker-multiple-month': this.numberOfMonths > 1, 'ui-datepicker-monthpicker': (view === 'month'), 'ui-datepicker-touch-ui': touchUI}"
-                (click)="onDatePickerClick($event)" [@overlayState]="'visible'" [@.disabled]="inline" (@overlayState.start)="onOverlayShowStart($event)" *ngIf="inline || overlayVisible">
+                (click)="onDatePickerClick($event)" [@animation]="'visible'" [@.disabled]="inline" (@animation.start)="onAnimationStart($event)" *ngIf="inline || overlayVisible">
                 <ng-container *ngIf="!timeOnly">
                     <div class="ui-datepicker-group ui-widget-content" *ngFor="let month of months; let i = index;">
                         <div class="ui-datepicker-header ui-widget-header ui-helper-clearfix ui-corner-all">
@@ -161,7 +161,7 @@ export interface LocaleSettings {
         </span>
     `,
     animations: [
-        trigger('overlayState', [
+        trigger('animation', [
             state('void', style({
                 transform: 'translateY(5%)',
                 opacity: 0
@@ -1432,12 +1432,14 @@ export class Calendar implements AfterViewInit,OnInit,OnDestroy,ControlValueAcce
         this.bindDocumentClickListener();
     }
 
-    onOverlayShowStart(event: AnimationEvent) {
-        if (!this.inline) {
-            this.alignOverlay(event.element);
-
-            if(this.autoZIndex) {
-                event.element.style.zIndex = String(this.baseZIndex + (++DomHandler.zindex));
+    onAnimationStart(event: AnimationEvent) {
+        if (event.toState === 'visible') {
+            if (!this.inline) {
+                this.alignOverlay(event.element);
+    
+                if(this.autoZIndex) {
+                    event.element.style.zIndex = String(this.baseZIndex + (++DomHandler.zindex));
+                }
             }
         }
     }
