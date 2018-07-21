8770e334c97a9def2b1ae3a457102913cb46b9a7
Çağatay Çivici <jagatai@Prime-MacBook.local>
2018-07-22 00:55:27 +0300

    Improve animations of Calendar

--- src/app/components/calendar/calendar.ts
+++ src/app/components/calendar/calendar.ts
@@ -37,7 +37,7 @@ export interface LocaleSettings {
             </ng-template>
             <div #datepicker [class]="panelStyleClass" [ngClass]="{'ui-datepicker ui-widget ui-widget-content ui-helper-clearfix ui-corner-all': true, 'ui-datepicker-inline':inline,'ui-shadow':!inline,
                 'ui-state-disabled':disabled,'ui-datepicker-timeonly':timeOnly,'ui-datepicker-multiple-month': this.numberOfMonths > 1, 'ui-datepicker-monthpicker': (view === 'month'), 'ui-datepicker-touch-ui': touchUI}"
-                (click)="onDatePickerClick($event)" [@animation]="'visible'" [@.disabled]="inline" (@animation.start)="onAnimationStart($event)" *ngIf="inline || overlayVisible">
+                (click)="onDatePickerClick($event)" [@overlayAnimation]="'visible'" [@.disabled]="inline" (@overlayAnimation.start)="onOverlayAnimationStart($event)" *ngIf="inline || overlayVisible">
                 <ng-container *ngIf="!timeOnly">
                     <div class="ui-datepicker-group ui-widget-content" *ngFor="let month of months; let i = index;">
                         <div class="ui-datepicker-header ui-widget-header ui-helper-clearfix ui-corner-all">
@@ -161,7 +161,7 @@ export interface LocaleSettings {
         </span>
     `,
     animations: [
-        trigger('animation', [
+        trigger('overlayAnimation', [
             state('void', style({
                 transform: 'translateY(5%)',
                 opacity: 0
@@ -170,7 +170,8 @@ export interface LocaleSettings {
                 transform: 'translateY(0)',
                 opacity: 1
             })),
-            transition('* => *', animate('150ms ease-in'))
+            transition('void => visible', animate('225ms ease-out')),
+            transition('visible => void', animate('195ms ease-in'))
         ])
     ],
     host: {
@@ -1432,7 +1433,7 @@ export class Calendar implements AfterViewInit,OnInit,OnDestroy,ControlValueAcce
         this.bindDocumentClickListener();
     }
 
-    onAnimationStart(event: AnimationEvent) {
+    onOverlayAnimationStart(event: AnimationEvent) {
         if (event.toState === 'visible') {
             if (!this.inline) {
                 this.alignOverlay(event.element);
