5d7cf30ac5922c37d71df52f9b0630c27a2b7a58
Marc Levine <marc.levine@8451.com>
2018-08-07 21:17:01 -0500

    fix formatting in DataView

--- src/app/components/dataview/dataview.ts
+++ src/app/components/dataview/dataview.ts
@@ -205,15 +205,15 @@ export class DataView implements OnInit,AfterContentInit,BlockableUI {
             let result = null;
             
             if (value1 == null && value2 != null)
-            result = -1;
+                result = -1;
             else if (value1 != null && value2 == null)
-            result = 1;
+                result = 1;
             else if (value1 == null && value2 == null)
-            result = 0;
+                result = 0;
             else if (typeof value1 === 'string' && typeof value2 === 'string')
-            result = value1.localeCompare(value2);
+                result = value1.localeCompare(value2);
             else
-            result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
+                result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
             
             return (this.sortOrder * result);
         });
