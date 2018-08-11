33798435b82803fa4a9b86e7048b4ecc9ebf9dc2
yigitfindikli <yigitfindikli@gmail.com>
2018-08-11 20:13:36 +0300

    Button spec test added

--- src/app/components/button/button.spec.ts
+++ src/app/components/button/button.spec.ts
@@ -4,21 +4,83 @@ import { Button } from './button';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('Button', () => {
-  
-  let button: Button;
-  let fixture: ComponentFixture<Button>;
-  
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        NoopAnimationsModule
-      ],
-      declarations: [
-        Button
-      ]
-    });
-    
-    fixture = TestBed.createComponent(Button);
-    button = fixture.componentInstance;
-  });
+
+      let button: Button;
+      let fixture: ComponentFixture<Button>;
+
+      beforeEach(() => {
+        TestBed.configureTestingModule({
+          imports: [
+              NoopAnimationsModule
+          ],
+          declarations: [
+              Button
+          ]
+        });
+
+        fixture = TestBed.createComponent(Button);
+        button = fixture.componentInstance;
+      });
+
+      it('should disabled when disabled is true', () => {
+        button.disabled = true;
+        fixture.detectChanges();
+        const buttonEl = fixture.debugElement.query(By.css('.ui-button'));
+        expect(buttonEl.nativeElement.className).toContain('ui-state-disabled');
+      });
+
+      it('should display the label and have a text only class', () => {
+        button.label = 'PrimeNG';
+        fixture.detectChanges();
+        const buttonEl = fixture.debugElement.query(By.css('.ui-button'));
+        expect(buttonEl.nativeElement.className).toContain('ui-button-text-only');
+        expect(buttonEl.nativeElement.textContent).toContain('PrimeNG');
+        expect(buttonEl.nativeElement.children.length).toEqual(1);
+      });
+
+      it('should display icon and icon to be on left by default ', () => {
+        button.label = 'PrimeNG';
+        button.icon = 'pi pi-times';
+        fixture.detectChanges();
+        const buttonEl = fixture.debugElement.query(By.css('.ui-button'));
+        expect(buttonEl.nativeElement.className).toContain('ui-button-text-icon-left');
+        expect(buttonEl.nativeElement.children[0].className).toContain('ui-button-icon-left');
+      });
+
+      it('should display icon', () => {
+        button.icon = 'pi pi-times';
+        fixture.detectChanges();
+        const buttonEl = fixture.debugElement.query(By.css('.ui-button'));
+        expect(buttonEl.nativeElement.className).toContain('ui-button-icon-only');
+      });
+
+      it('should display icon on right and have a label', () => {
+        button.label = 'PrimeNG';
+        button.icon = 'pi pi-times';
+        button.iconPos = 'right';
+        fixture.detectChanges();
+        const buttonEl = fixture.debugElement.query(By.css('.ui-button'));
+        expect(buttonEl.nativeElement.className).toContain('ui-button-text-icon-right');
+        expect(buttonEl.nativeElement.children[0].className).toContain('ui-button-icon-right');
+        expect(buttonEl.nativeElement.children[1].textContent).toContain('PrimeNG');
+      });
+      it('should display icon on right', () => {
+        button.icon = 'pi pi-times';
+        button.iconPos = 'right';
+        fixture.detectChanges();
+        const buttonEl = fixture.debugElement.query(By.css('.ui-button'));
+        expect(buttonEl.nativeElement.className).toContain('ui-button-icon-only');
+        expect(buttonEl.nativeElement.children[0].className).toContain('ui-button-icon-right');
+      });
+      it('should be enabled by default', () => {
+        fixture.detectChanges();
+        const buttonEl = fixture.debugElement.query(By.css('.ui-button'));
+        expect(buttonEl.nativeElement.className).not.toContain('ui-state-disabled');
+      });
+      it('should have a empty label and icon', () => {
+        fixture.detectChanges();
+        const buttonEl = fixture.debugElement.query(By.css('.ui-button'));
+        expect(buttonEl.nativeElement.className).toContain('ui-button-text-empty');
+      });
+
 });
