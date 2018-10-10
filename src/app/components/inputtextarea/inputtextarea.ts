f6c1ddc7f7be75cce98343645b1a22d8ab8dbcd1
Çağatay Çivici <jagatai@Prime-MacBook.local>
2018-10-10 15:54:35 +0300

    Refactored #6626 - New autoresize for textatea

--- src/app/components/inputtextarea/inputtextarea.ts
+++ src/app/components/inputtextarea/inputtextarea.ts
@@ -1,4 +1,4 @@
-import {NgModule,Directive,ElementRef,HostListener,Input,Output,OnInit,DoCheck,EventEmitter,Optional} from '@angular/core';
+import {NgModule,Directive,ElementRef,HostListener,Input,Output,DoCheck,EventEmitter,Optional} from '@angular/core';
 import {NgModel} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 
@@ -7,94 +7,74 @@ import {CommonModule} from '@angular/common';
     host: {
         '[class.ui-inputtext]': 'true',
         '[class.ui-corner-all]': 'true',
+        '[class.ui-inputtextarea-resizable]': 'autoResize',
         '[class.ui-state-default]': 'true',
         '[class.ui-widget]': 'true',
-        '[class.ui-state-filled]': 'filled',
-        '[attr.rows]': 'rows',
-        '[attr.cols]': 'cols'
+        '[class.ui-state-filled]': 'filled'
     }
 })
-export class InputTextarea implements OnInit,DoCheck {
+export class InputTextarea implements DoCheck {
     
     @Input() autoResize: boolean;
     
-    @Input() rows: number = 2;
-    
-    @Input() cols: number = 20;
-    
     @Output() onResize: EventEmitter<any> = new EventEmitter();
-    
-    rowsDefault: number;
-    
-    colsDefault: number;
-    
+        
     filled: boolean;
 
     cachedScrollHeight:number;
 
     constructor(public el: ElementRef, @Optional() public ngModel: NgModel) {}
-    
-    ngOnInit() {
-        this.rowsDefault = this.rows;
-        this.colsDefault = this.cols;
-        var style = window.getComputedStyle(this.el.nativeElement, null);
-        if (style.resize === 'vertical') {
-            this.el.nativeElement.style.resize = 'none';
-        } 
-        else if (style.resize === 'both') {
-            this.el.nativeElement.style.resize = 'horizontal';
-        }
-    }
-    
+        
     ngDoCheck() {
         this.updateFilledState();
+        
+        if (this.autoResize) {
+            this.resize();
+        }
     }
     
     //To trigger change detection to manage ui-state-filled for material labels when there is no value binding
     @HostListener('input', ['$event'])
     onInput(e) {
         this.updateFilledState();
-        if(this.autoResize) {
+        if (this.autoResize) {
             this.resize(e);
         }
     }
     
     updateFilledState() {
-        this.filled = (this.el.nativeElement.value && this.el.nativeElement.value.length) ||
-                        (this.ngModel && this.ngModel.model);
+        this.filled = (this.el.nativeElement.value && this.el.nativeElement.value.length) || (this.ngModel && this.ngModel.model);
     }
     
     @HostListener('focus', ['$event'])
     onFocus(e) {
-        if(this.autoResize) {
+        if (this.autoResize) {
             this.resize(e);
         }
     }
     
     @HostListener('blur', ['$event'])
     onBlur(e) {
-        if(this.autoResize) {
+        if (this.autoResize) {
             this.resize(e);
         }
     }
     
     resize(event?: Event) {
-        var style = window.getComputedStyle(this.el.nativeElement, null);
-
-        if(!this.cachedScrollHeight) {
+        if (!this.cachedScrollHeight) {
             this.cachedScrollHeight = this.el.nativeElement.scrollHeight;
             this.el.nativeElement.style.overflow = "hidden";
         }
 
-        if(this.cachedScrollHeight != this.el.nativeElement.scrollHeight) {
+        if (this.cachedScrollHeight != this.el.nativeElement.scrollHeight) {
             this.el.nativeElement.style.height = ''
             this.el.nativeElement.style.height = this.el.nativeElement.scrollHeight + 'px';
 
-            if(parseFloat(this.el.nativeElement.style.height) >= parseFloat(this.el.nativeElement.style.maxHeight)) {
+            if (parseFloat(this.el.nativeElement.style.height) >= parseFloat(this.el.nativeElement.style.maxHeight)) {
                 this.el.nativeElement.style.overflowY = "scroll";
                 this.el.nativeElement.style.height = this.el.nativeElement.style.maxHeight;
             }
-            else{
+            else {
                 this.el.nativeElement.style.overflow = "hidden";
             }
 
