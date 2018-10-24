4edaafa28bbbd3de9f8821c0e207f89a5537f268
cagataycivici <cagatay.civici@gmail.com>
2018-10-24 18:53:39 +0300

    Updated incell editing to add a dropdown

--- src/app/showcase/components/table/tableeditdemo.ts
+++ src/app/showcase/components/table/tableeditdemo.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from '../../components/domain/car';
 import { CarService } from '../../service/carservice';
+import {SelectItem} from '../../../components/common/api';
 
 @Component({
     templateUrl: './tableeditdemo.html'
@@ -9,9 +10,24 @@ export class TableEditDemo implements OnInit {
 
     cars: Car[];
 
+    brands: SelectItem[];
+
     constructor(private carService: CarService) { }
 
     ngOnInit() {
         this.carService.getCarsSmall().then(cars => this.cars = cars);
+
+        this.brands = [
+            {label: 'Audi', value: 'Audi'},
+            {label: 'BMW', value: 'BMW'},
+            {label: 'Fiat', value: 'Fiat'},
+            {label: 'Ford', value: 'Ford'},
+            {label: 'Honda', value: 'Honda'},
+            {label: 'Jaguar', value: 'Jaguar'},
+            {label: 'Mercedes', value: 'Mercedes'},
+            {label: 'Renault', value: 'Renault'},
+            {label: 'VW', value: 'VW'},
+            {label: 'Volvo', value: 'Volvo'}
+        ];
     }
 }
