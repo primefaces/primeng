ec960adf5c0b85b68eb93468362e31b4d134b322
cagataycivici <cagatay.civici@gmail.com>
2018-11-15 14:14:42 +0300

    Lazy loading for the new VirtualScroller

--- src/app/showcase/components/virtualscroller/virtualscrollerdemo.ts
+++ src/app/showcase/components/virtualscroller/virtualscrollerdemo.ts
@@ -1,6 +1,7 @@
 import {Component,OnInit} from '@angular/core';
 import {Car} from '../../components/domain/car';
 import {CarService} from '../../service/carservice';
+import {LazyLoadEvent} from '../../../components/common/api';
 
 @Component({
     templateUrl: './virtualscrollerdemo.html',
@@ -17,6 +18,27 @@ import {CarService} from '../../service/carservice';
             font-weight: bold;
         }
 
+        .empty-car-item-index {
+            background-color: #f1f1f1;
+            width: 60px;
+            height: 60px;
+            margin: 36px auto 0 auto;
+            animation: pulse 1s infinite ease-in-out;
+        }
+
+        .empty-car-item-image {
+            background-color: #f1f1f1;
+            width: 120px;
+            height: 120px;
+            animation: pulse 1s infinite ease-in-out;
+        }
+
+        .empty-car-item-text {
+            background-color: #f1f1f1;
+            height: 18px;
+            animation: pulse 1s infinite ease-in-out;
+        }
+
         @media (max-width: 40em) {
             .car-item {
                 text-align: center;
@@ -27,10 +49,16 @@ import {CarService} from '../../service/carservice';
 export class VirtualScrollerDemo implements OnInit {
 
     cars: Car[] = [];
+
+    lazyCars: Car[];
     
     brands: string[];
 
     colors: string[];
+
+    totalLazyCarsLength: number;
+
+    timeout: any;
 
     constructor(private carService: CarService) { }
 
@@ -46,6 +74,9 @@ export class VirtualScrollerDemo implements OnInit {
         for (let i = 0; i < 10000; i++) {
             this.cars.push(this.generateCar());
         }
+
+        //in a real application, make a remote request to retrieve the number of records only, not the actual records
+        this.totalLazyCarsLength = 10000;
     }
 
     generateCar(): Car {
@@ -54,7 +85,6 @@ export class VirtualScrollerDemo implements OnInit {
             brand: this.generateBrand(),
             color: this.generateColor(),
             year: this.generateYear()
-
         }
     }
 
@@ -80,5 +110,23 @@ export class VirtualScrollerDemo implements OnInit {
     generateYear() {
         return 2000 + Math.floor(Math.random() * Math.floor(19));
     }
-    
+
+    loadCarsLazy(event: LazyLoadEvent) {
+        console.log('loading lazy ' + event.first); 
+        //in a real application, make a remote request to load data using state metadata from event
+        //event.first = First row offset
+        //event.rows = Number of rows per page
+
+        //imitate db connection over a network
+        if (this.timeout) {
+            clearTimeout(this.timeout);
+        }
+        
+        this.timeout = setTimeout(() => {
+            this.lazyCars = [];
+            if (this.cars) {
+                this.lazyCars = this.cars.slice(event.first, (event.first + event.rows));
+            }
+        }, 1000);
+    }
 }
