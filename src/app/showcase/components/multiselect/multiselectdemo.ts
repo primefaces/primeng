2534bf6897e976eb88c16b060d5414cd9974e5f3
cagataycivici <cagatay.civici@gmail.com>
2018-11-29 10:57:17 +0300

    Demo for selectedItems template of MultiSelect

--- src/app/showcase/components/multiselect/multiselectdemo.ts
+++ src/app/showcase/components/multiselect/multiselectdemo.ts
@@ -2,7 +2,27 @@ import {Component} from '@angular/core';
 import {SelectItem} from '../../../components/common/api';
 
 @Component({
-    templateUrl: './multiselectdemo.html'
+    templateUrl: './multiselectdemo.html',
+    styles: [`
+        :host ::ng-deep .ui-multiselected-item-token,
+        :host ::ng-deep .ui-multiselected-empty-token {
+            padding: 2px 4px;
+            margin: 0 0.286em 0 0;
+            display: inline-block;
+            vertical-align:middle;
+            height: 1.857em;
+        }
+
+        :host ::ng-deep .ui-multiselected-item-token {
+            background: #007ad9;
+            color: #ffffff;
+        }
+
+        :host ::ng-deep .ui-multiselected-empty-token {
+            background: #d95f00;
+            color: #ffffff;
+        }
+    `]
 })
 export class MultiSelectDemo {
 
