5f2279bcb31c3ff511eaa5adcd2d98997bb7cff7
cagataycivici <cagatay.civici@gmail.com>
2018-12-11 14:13:34 +0300

    Fixed #6966 - Acentuation insensitive filtering for componens having filters

--- src/app/components/utils/objectutils.ts
+++ src/app/components/utils/objectutils.ts
@@ -90,11 +90,13 @@ export class ObjectUtils {
 
     public filter(value: any[], fields: any[], filterValue: string) {
         let filteredItems: any[] = [];
+        let filterText = this.removeAccents(filterValue).toLowerCase();
 
         if(value) {
             for(let item of value) {
                 for(let field of fields) {
-                    if(String(this.resolveFieldData(item, field)).toLowerCase().indexOf(filterValue.toLowerCase()) > -1) {
+                    let fieldValue = this.removeAccents(String(this.resolveFieldData(item, field))).toLowerCase();
+                    if(fieldValue.indexOf(filterText) > -1) {
                         filteredItems.push(item);
                         break;
                     }
@@ -165,4 +167,33 @@ export class ObjectUtils {
 
         return index;
     }
+
+    public removeAccents(str) {
+        if (str.search(/[\xC0-\xFF]/g) > -1) {
+            str = str
+                    .replace(/[\xC0-\xC5]/g, "A")
+                    .replace(/[\xC6]/g, "AE")
+                    .replace(/[\xC7]/g, "C")
+                    .replace(/[\xC8-\xCB]/g, "E")
+                    .replace(/[\xCC-\xCF]/g, "I")
+                    .replace(/[\xD0]/g, "D")
+                    .replace(/[\xD1]/g, "N")
+                    .replace(/[\xD2-\xD6\xD8]/g, "O")
+                    .replace(/[\xD9-\xDC]/g, "U")
+                    .replace(/[\xDD]/g, "Y")
+                    .replace(/[\xDE]/g, "P")
+                    .replace(/[\xE0-\xE5]/g, "a")
+                    .replace(/[\xE6]/g, "ae")
+                    .replace(/[\xE7]/g, "c")
+                    .replace(/[\xE8-\xEB]/g, "e")
+                    .replace(/[\xEC-\xEF]/g, "i")
+                    .replace(/[\xF1]/g, "n")
+                    .replace(/[\xF2-\xF6\xF8]/g, "o")
+                    .replace(/[\xF9-\xFC]/g, "u")
+                    .replace(/[\xFE]/g, "p")
+                    .replace(/[\xFD\xFF]/g, "y");
+        }
+      
+        return str;
+      }
 }
