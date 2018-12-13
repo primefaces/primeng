f0394b2b947b14a67a598b53fa3b7456a28286b0
cagataycivici <cagatay.civici@gmail.com>
2018-12-13 11:04:05 +0300

    Remove unused import

--- src/app/components/tabmenu/tabmenu.ts
+++ src/app/components/tabmenu/tabmenu.ts
@@ -2,7 +2,6 @@ import {NgModule,Component,ElementRef,Input,Output} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DomHandler} from '../dom/domhandler';
 import {MenuItem} from '../common/menuitem';
-import {Location} from '@angular/common';
 import {RouterModule} from '@angular/router';
 
 @Component({
