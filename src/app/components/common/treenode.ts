8c06ad2bedd0e8c5cef9b34a58bbc8ae5ecf2bbc
Daniel Gray <daniel.0a0a0a@gmail.com>
2018-12-18 13:54:20 +0100

    Add generic SelectItem and TreeNode (fix #2089 and #2876)

--- src/app/components/common/treenode.ts
+++ src/app/components/common/treenode.ts
@@ -1,14 +1,14 @@
-export interface TreeNode {
+export interface TreeNode<T = any> {
     label?: string;
-    data?: any;
+    data?: T;
     icon?: any;
     expandedIcon?: any;
     collapsedIcon?: any;
-    children?: TreeNode[];
+    children?: TreeNode<T>[];
     leaf?: boolean;
     expanded?: boolean;
     type?: string;
-    parent?: TreeNode;
+    parent?: TreeNode<T>;
     partialSelected?: boolean;
     styleClass?: string;
     draggable?: boolean;
