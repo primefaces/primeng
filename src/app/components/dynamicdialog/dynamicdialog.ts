e31b405cbeea866950cf59995216a765e8715604
Michel Selerin <michel.selerin@outlook.com>
2018-12-19 17:07:07 +0100

    fix: DynamicDialog should not use translate3d and translateX/Y to center itself (#7024)

--- src/app/components/dynamicdialog/dynamicdialog.ts
+++ src/app/components/dynamicdialog/dynamicdialog.ts
@@ -10,32 +10,33 @@ import { DynamicDialogRef } from './dynamicdialog-ref';
 	selector: 'p-dynamicDialog',
 	template: `
 		<div #mask class="ui-widget-overlay ui-dialog-mask ui-dialog-mask-scrollblocker" *ngIf="visible" (click)="onMaskClick()"></div>
-		<div [ngClass]="{'ui-dialog ui-dynamicdialog ui-widget ui-widget-content ui-corner-all ui-shadow':true, 'ui-dialog-rtl': config.rtl}" [ngStyle]="config.style" [class]="config.styleClass"
-			[@animation]="{value: 'visible', params: {transitionParams: config.transitionOptions || '400ms cubic-bezier(0.25, 0.8, 0.25, 1)'}}" 
-			(@animation.start)="onAnimationStart($event)" (@animation.done)="onAnimationEnd($event)" role="dialog" *ngIf="visible"
-			[style.width]="config.width" [style.height]="config.height">
-            <div class="ui-dialog-titlebar ui-widget-header ui-helper-clearfix ui-corner-top" *ngIf="config.showHeader === false ? false: true">
-                <span class="ui-dialog-title">{{config.header}}</span>
-                <a [ngClass]="'ui-dialog-titlebar-icon ui-dialog-titlebar-close ui-corner-all'" tabindex="0" role="button" (click)="close()" (keydown.enter)="close()" *ngIf="config.closable === false ? false : true">
-                    <span class="pi pi-times"></span>
-                </a>
+		<div class="ui-dynamicdialog-wrapper">
+            <div [ngClass]="{'ui-dialog ui-dynamicdialog ui-widget ui-widget-content ui-corner-all ui-shadow':true, 'ui-dialog-rtl': config.rtl}" [ngStyle]="config.style" [class]="config.styleClass"
+                [@animation]="{value: 'visible', params: {transitionParams: config.transitionOptions || '400ms cubic-bezier(0.25, 0.8, 0.25, 1)'}}" 
+                (@animation.start)="onAnimationStart($event)" (@animation.done)="onAnimationEnd($event)" role="dialog" *ngIf="visible"
+                [style.width]="config.width" [style.height]="config.height">
+                <div class="ui-dialog-titlebar ui-widget-header ui-helper-clearfix ui-corner-top" *ngIf="config.showHeader === false ? false: true">
+                    <span class="ui-dialog-title">{{config.header}}</span>
+                    <a [ngClass]="'ui-dialog-titlebar-icon ui-dialog-titlebar-close ui-corner-all'" tabindex="0" role="button" (click)="close()" (keydown.enter)="close()" *ngIf="config.closable === false ? false : true">
+                        <span class="pi pi-times"></span>
+                    </a>
+                </div>
+                <div class="ui-dialog-content ui-widget-content" [ngStyle]="config.contentStyle">
+                    <ng-template pDynamicDialogContent></ng-template>
+                </div>
+                <div class="ui-dialog-footer ui-widget-content" *ngIf="config.footer">
+                    {{config.footer}}
+                </div>
             </div>
-            <div class="ui-dialog-content ui-widget-content" [ngStyle]="config.contentStyle">
-				<ng-template pDynamicDialogContent></ng-template>
-			</div>
-			<div class="ui-dialog-footer ui-widget-content" *ngIf="config.footer">
-				{{config.footer}}
-            </div>
-		</div>
+        </div>
 	`,
 	animations: [
         trigger('animation', [
             state('void', style({
-                transform: 'translate3d(-50%, -25%, 0) scale(0.9)',
+                transform: 'scale(0.9)',
                 opacity: 0
             })),
             state('visible', style({
-                transform: 'translateX(-50%) translateY(-50%)',
                 opacity: 1
             })),
             transition('* => *', animate('{{transitionParams}}'))
@@ -91,22 +92,22 @@ export class DynamicDialogComponent implements AfterViewInit, OnDestroy {
     }
     
     center() {
-        let elementWidth = this.domHandler.getOuterWidth(this.container);
-        let elementHeight = this.domHandler.getOuterHeight(this.container);
-        if(elementWidth == 0 && elementHeight == 0) {
-            this.container.style.visibility = 'hidden';
-            this.container.style.display = 'block';
-            elementWidth = this.domHandler.getOuterWidth(this.container);
-            elementHeight = this.domHandler.getOuterHeight(this.container);
-            this.container.style.display = 'none';
-            this.container.style.visibility = 'visible';
-        }
-        let viewport = this.domHandler.getViewport();
-        let x = Math.max(Math.floor((viewport.width - elementWidth) / 2), 0);
-        let y = Math.max(Math.floor((viewport.height - elementHeight) / 2), 0);
-
-        this.container.style.left = x + 'px';
-        this.container.style.top = y + 'px';
+        // let elementWidth = this.domHandler.getOuterWidth(this.container);
+        // let elementHeight = this.domHandler.getOuterHeight(this.container);
+        // if(elementWidth == 0 && elementHeight == 0) {
+        //     this.container.style.visibility = 'hidden';
+        //     this.container.style.display = 'block';
+        //     elementWidth = this.domHandler.getOuterWidth(this.container);
+        //     elementHeight = this.domHandler.getOuterHeight(this.container);
+        //     this.container.style.display = 'none';
+        //     this.container.style.visibility = 'visible';
+        // }
+        // let viewport = this.domHandler.getViewport();
+        // let x = Math.max(Math.floor((viewport.width - elementWidth) / 2), 0);
+        // let y = Math.max(Math.floor((viewport.height - elementHeight) / 2), 0);
+        //
+        // this.container.style.left = x + 'px';
+        // this.container.style.top = y + 'px';
 	}
 	
 	moveOnTop() {
