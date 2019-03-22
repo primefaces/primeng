66a0d75986962901debe927040242c85c897b60f
mertsincan <sincan.mert@gmail.com>
2019-03-22 11:44:15 +0300

    Added virtual scroll demo

--- src/app/showcase/components/treetable/treetablescrolldemo.ts
+++ src/app/showcase/components/treetable/treetablescrolldemo.ts
@@ -15,11 +15,17 @@ export class TreeTableScrollDemo {
 
     files4: TreeNode[];
 
+    virtualFiles: TreeNode[];
+
     cols: any[];
 
     frozenCols: any[];
 
     scrollableCols: any[];
+
+    totalRecords: number;
+
+    loading: boolean;
 
     constructor(private nodeService: NodeService) { }
 
@@ -47,5 +53,78 @@ export class TreeTableScrollDemo {
         this.frozenCols = [
             { field: 'name', header: 'Name' }
         ];
+
+        //in a production application, retrieve the logical number of rows from a remote datasource
+        this.totalRecords = 250000;
+
+        this.loading = true;
+    }
+
+    loadNodes(event) {
+        this.loading = true;
+
+        //in a production application, make a remote request to load data using state metadata from event
+        //event.first = First row offset
+        //event.rows = Number of rows per page
+        //event.sortField = Field name to sort with
+        //event.sortOrder = Sort order as number, 1 for asc and -1 for dec
+        //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
+
+        //imitate db connection over a network
+        setTimeout(() => {
+            this.virtualFiles = [];
+
+            //last chunk
+            if (event.first === 249980)
+                this.createLazyNodes(event.first, 20);
+            else
+                this.createLazyNodes(event.first, event.rows);
+
+            this.loading = false;
+        }, 250);
+    }
+
+    createLazyNodes(index, length) {
+        for(let i = 0; i < length; i++) {
+            let node = {
+                data: {  
+                    name: 'Item ' + (index + i),
+                    size: Math.floor(Math.random() * 1000) + 1 + 'kb',
+                    type: 'Type ' + (index + i)
+                },
+                leaf: false
+            };
+
+            this.virtualFiles.push(node);
+        }
+    }
+
+    onNodeExpand(event) {
+        this.loading = true;
+
+        setTimeout(() => {
+            this.loading = false;
+            const node = event.node;
+
+            node.children = [
+                {
+                    data: {  
+                        name: node.data.name + ' - 0',
+                        size: Math.floor(Math.random() * 1000) + 1 + 'kb',
+                        type: 'File'
+                    },
+                },
+                {
+                    data: {  
+                        name: node.data.name + ' - 1',
+                        size: Math.floor(Math.random() * 1000) + 1 + 'kb',
+                        type: 'File'
+                    }
+                }
+            ];
+
+            this.virtualFiles = [...this.virtualFiles];
+        }, 250);
+        
     }
 }
