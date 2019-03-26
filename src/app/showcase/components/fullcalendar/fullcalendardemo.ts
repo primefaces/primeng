863321bede53d1acc8cc62e621d6794055f12978
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2019-03-26 17:59:29 +0300

    fixed #7450 FullCalendar 4.1.0 Support

--- src/app/showcase/components/fullcalendar/fullcalendardemo.ts
+++ src/app/showcase/components/fullcalendar/fullcalendardemo.ts
@@ -1,5 +1,8 @@
 import {Component,OnInit} from '@angular/core';
 import {EventService} from '../../service/eventservice';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 @Component({
     templateUrl: './fullcalendardemo.html'
@@ -18,11 +21,12 @@ export class FullCalendarDemo implements OnInit {
         this.eventService.getEvents().then(events => {this.events = events;});
         
         this.options = {
+            plugins: [interactionPlugin,dayGridPlugin,timeGridPlugin],
             defaultDate: '2017-02-01',
             header: {
                 left: 'prev,next',
                 center: 'title',
-                right: 'month,agendaWeek,agendaDay'
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
             },
             editable: true
         };
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -2,36 +2,44 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
-    config.set({
-      basePath: '',
-      frameworks: ['jasmine', '@angular-devkit/build-angular'],
-      plugins: [
-        require('karma-jasmine'),
-        require('karma-chrome-launcher'),
-        require('karma-jasmine-html-reporter'),
-        require('karma-coverage-istanbul-reporter'),
-        require('@angular-devkit/build-angular/plugins/karma')
-      ],
-      client: {
-        clearContext: false // leave Jasmine Spec Runner output visible in browser
-      },
-      coverageIstanbulReporter: {
-        dir: require('path').join(__dirname, '../coverage'),
-        reports: ['html', 'lcovonly'],
-        fixWebpackSourcePaths: true
-      },
-      files: [
-        '../node_modules/moment/moment.js',
-        '../node_modules/fullcalendar/dist/fullcalendar.js',
-        '../node_modules/chart.js/dist/Chart.js',
-        '../node_modules/quill/dist/quill.js'
-      ],
-      reporters: ['progress', 'kjhtml'],
-      port: 9876,
-      colors: true,
-      logLevel: config.LOG_INFO,
-      autoWatch: true,
-      browsers: ['Chrome'],
-      singleRun: false
-    });
-  };
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../coverage'),
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
+    },
+    files: [
+      '../node_modules/moment/moment.js',
+      '../node_modules/@fullcalendar/core/main.js',
+      '../node_modules/@fullcalendar/daygrid/main.js',
+      '../node_modules/@fullcalendar/timegrid/main.js',
+      '../node_modules/@fullcalendar/interaction/main.js',
+      '../node_modules/@fullcalendar/resource-common/main.js',
+      '../node_modules/@fullcalendar/resource-daygrid/main.js',
+      '../node_modules/@fullcalendar/resource-timegrid/main.js',
+      '../node_modules/@fullcalendar/timeline/main.js',
+      '../node_modules/@fullcalendar/timegrid/main.js',
+      '../node_modules/chart.js/dist/Chart.js',
+      '../node_modules/quill/dist/quill.js'
+    ],
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false
+  });
+};
