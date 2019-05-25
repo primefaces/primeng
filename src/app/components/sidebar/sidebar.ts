c021e1dd90b790abd5304c5736e2d5e67bde290c
yigitfindikli <yigitfindikli@gmail.com>
2019-05-25 14:47:58 +0300

    fixed #4793 dismissableMask and closeOnEscape functionality for Sidebar

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -10,7 +10,7 @@ import {DomHandler} from '../dom/domhandler';
             'ui-sidebar-left': (position === 'left'), 'ui-sidebar-right': (position === 'right'),
             'ui-sidebar-top': (position === 'top'), 'ui-sidebar-bottom': (position === 'bottom'), 
             'ui-sidebar-full': fullScreen}"
-            [@panelState]="visible ? 'visible' : 'hidden'" [ngStyle]="style" [class]="styleClass">
+            [@panelState]="visible ? 'visible' : 'hidden'" (@panelState.start)="onAnimationStart($event)" [ngStyle]="style" [class]="styleClass">
             <a [ngClass]="{'ui-sidebar-close ui-corner-all':true}" *ngIf="showCloseIcon" tabindex="0" role="button" (click)="close($event)" (keydown.enter)="close($event)">
                 <span class="pi pi-times"></span>
             </a>
@@ -54,6 +54,8 @@ export class Sidebar implements AfterViewInit, AfterViewChecked, OnDestroy {
 
     @Input() showCloseIcon: boolean = true;
 
+    @Input() closeOnEscape: boolean = true;
+
     @ViewChild('container') containerViewChild: ElementRef;
 
     @Output() onShow: EventEmitter<any> = new EventEmitter();
@@ -71,6 +73,8 @@ export class Sidebar implements AfterViewInit, AfterViewChecked, OnDestroy {
     mask: HTMLDivElement;
 
     maskClickListener: Function;
+
+    documentEscapeListener: Function;
 
     executePostDisplayActions: boolean;
 
@@ -164,9 +168,31 @@ export class Sidebar implements AfterViewInit, AfterViewChecked, OnDestroy {
         }
     }
 
+    onAnimationStart(event){
+        switch(event.toState) {
+            case 'visible':
+                if (this.closeOnEscape) {
+                    this.bindDocumentEscapeListener();
+                }
+            break;
+            case 'hidden':
+                this.unbindGlobalListeners();
+            break;
+        }
+    }
+
+    bindDocumentEscapeListener() {
+        this.documentEscapeListener = this.renderer.listen('document', 'keydown', (event) => {
+            if (event.which == 27) {
+                if (parseInt(this.containerViewChild.nativeElement.style.zIndex) === (DomHandler.zindex + this.baseZIndex)) {
+                    this.close(event);
+                }
+            }
+        });
+    }
+
     disableModality() {
         if(this.mask) {
-            this.unbindMaskClickListener();
             document.body.removeChild(this.mask);
             if(this.blockScroll) {
                 DomHandler.removeClass(document.body, 'ui-overflow-hidden');
@@ -182,6 +208,18 @@ export class Sidebar implements AfterViewInit, AfterViewChecked, OnDestroy {
         }
     }
 
+    unbindDocumentEscapeListener() {
+        if(this.documentEscapeListener) {
+            this.documentEscapeListener();
+            this.documentEscapeListener = null;
+        }
+    }
+
+    unbindGlobalListeners() {
+        this.unbindMaskClickListener();
+        this.unbindDocumentEscapeListener();
+    }
+
     ngOnDestroy() {
         this.initialized = false;
 
@@ -193,7 +231,7 @@ export class Sidebar implements AfterViewInit, AfterViewChecked, OnDestroy {
             this.el.nativeElement.appendChild(this.containerViewChild.nativeElement);
         }
 
-		this.unbindMaskClickListener();
+		this.unbindGlobalListeners();
     }
 }
 
