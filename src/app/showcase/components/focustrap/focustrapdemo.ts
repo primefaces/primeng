a2cb45c9025ebf0469351dbe70019cf4a1717253
cagataycivici <cagatay.civici@gmail.com>
2019-06-03 12:31:24 +0300

    Fixed AOT issues

--- src/app/showcase/components/focustrap/focustrapdemo.ts
+++ src/app/showcase/components/focustrap/focustrapdemo.ts
@@ -5,6 +5,8 @@ import {Component} from '@angular/core';
 })
 export class FocusTrapDemo {
 
+    selectedCity: string;
+
     cities = [
         {name: 'New York', code: 'NY'},
         {name: 'Rome', code: 'RM'},
