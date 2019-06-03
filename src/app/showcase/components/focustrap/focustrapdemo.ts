6e32dbe2215f05058d05f5c9ee58241d4562ded6
cagataycivici <cagatay.civici@gmail.com>
2019-06-03 11:20:34 +0300

    Documentation for FocusTrap

--- src/app/showcase/components/focustrap/focustrapdemo.ts
+++ src/app/showcase/components/focustrap/focustrapdemo.ts
@@ -5,20 +5,6 @@ import {Component} from '@angular/core';
 })
 export class FocusTrapDemo {
 
-    brands: string[] = ['Audi','BMW','Fiat','Ford','Honda','Jaguar','Mercedes','Renault','Volvo','VW'];
-    
-    filteredBrands: any[];
-
-    filterBrands(event) {
-        this.filteredBrands = [];
-        for(let i = 0; i < this.brands.length; i++) {
-            let brand = this.brands[i];
-            if(brand.toLowerCase().indexOf(event.query.toLowerCase()) == 0) {
-                this.filteredBrands.push(brand);
-            }
-        }
-    }
-
     cities = [
         {name: 'New York', code: 'NY'},
         {name: 'Rome', code: 'RM'},
@@ -26,17 +12,5 @@ export class FocusTrapDemo {
         {name: 'Istanbul', code: 'IST'},
         {name: 'Paris', code: 'PRS'}
     ];
-
-    cars = [
-        {label: 'Audi', value: 'Audi'},
-        {label: 'BMW', value: 'BMW'},
-        {label: 'Fiat', value: 'Fiat'},
-        {label: 'Ford', value: 'Ford'},
-        {label: 'Honda', value: 'Honda'},
-        {label: 'Jaguar', value: 'Jaguar'},
-        {label: 'Mercedes', value: 'Mercedes'},
-        {label: 'Renault', value: 'Renault'},
-        {label: 'VW', value: 'VW'},
-        {label: 'Volvo', value: 'Volvo'},
-    ];
+    
 }
