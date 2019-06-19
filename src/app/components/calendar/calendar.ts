c4d190f8b1ac230005d660e06eaf7c1241cd21f4
mertsincan <sincan.mert@gmail.com>
2019-06-19 16:24:07 +0300

    Fixed #7799 - Times between 12:00AM-12:59AM save as PM when defaulted or typed in

--- src/app/components/calendar/calendar.ts
+++ src/app/components/calendar/calendar.ts
@@ -858,10 +858,10 @@ export class Calendar implements OnInit,OnDestroy,ControlValueAccessor {
         let date = new Date(dateMeta.year, dateMeta.month, dateMeta.day);
         
         if (this.showTime) {
-            if (this.hourFormat === '12' && this.pm && this.currentHour != 12)
-                date.setHours(this.currentHour + 12);
+            if (this.currentHour === 12)
+                date.setHours(this.pm ? 12 : 0);
             else
-                date.setHours(this.currentHour);
+                date.setHours(this.pm ? this.currentHour + 12 : this.currentHour);
 
             date.setMinutes(this.currentMinute);
             date.setSeconds(this.currentSecond);
