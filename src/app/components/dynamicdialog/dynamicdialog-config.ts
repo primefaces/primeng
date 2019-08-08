d1387e6991536d066eb75f4e27cafa07feb25150
leo <lsamalea@gmail.com>
2019-08-07 22:46:42 -0400

    extending dynamidialog-config to support all dialog component inputs

--- src/app/components/dynamicdialog/dynamicdialog-config.ts
+++ src/app/components/dynamicdialog/dynamicdialog-config.ts
@@ -1,18 +1,36 @@
 export class DynamicDialogConfig {
 	data?: any;
-	header?: string;
-	footer?: string;
-	width?: string;
-	height?: string;
-	closeOnEscape?: boolean;
-	baseZIndex?: number;
-	autoZIndex?: boolean;
+	footer?: string = '';
+	visible?: boolean;
+	header?: string = '';
+	draggable?: boolean = true;
+	resizable?: boolean = true;
+	positionLeft?: number;
+	positionTop?: number;
+	contentStyle?: any;
+	modal?: boolean;
+	closeOnEscape?: boolean = true;
 	dismissableMask?: boolean;
 	rtl?: boolean;
+	closable?: boolean = true;
+	responsive?: boolean = true;
+	appendTo?: any;
 	style?: any;
-	contentStyle?: any;
-	styleClass?: string;
-	transitionOptions?: string;
-	closable?: boolean;
-	showHeader?: boolean;
+	styleClass?: string = '';
+	showHeader?: boolean = true;
+	breakpoint?: number = 640;
+	blockScroll?: boolean = false;
+	autoZIndex?: boolean = true;
+	baseZIndex?: number = 0;
+	minX?: number = 0;
+	minY?: number = 0;
+	focusOnShow?: boolean = true;
+	maximizable?: boolean;
+	focusTrap?: boolean = true;
+	transitionOptions?: string = '150ms cubic-bezier(0, 0, 0.2, 1)';
+	closeIcon?: string = 'pi pi-times';
+	minimizeIcon?: string = 'pi pi-window-minimize';
+	maximizeIcon?: string = 'pi pi-window-maximize';
+	width?: string = ''
+	height?: string = ''
 }
