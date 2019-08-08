73f3b8ae527aeb95191c7d0b6a8498b7833c89e4
leo <lsamalea@gmail.com>
2019-08-07 22:45:53 -0400

    adding multi dialog support

--- src/app/components/dynamicdialog/dialogservice.ts
+++ src/app/components/dynamicdialog/dialogservice.ts
@@ -6,46 +6,40 @@ import { DynamicDialogRef } from './dynamicdialog-ref';
 
 @Injectable()
 export class DialogService {
-    
-    dialogComponentRef: ComponentRef<DynamicDialogComponent>;
 
     constructor(private componentFactoryResolver: ComponentFactoryResolver, private appRef: ApplicationRef, private injector: Injector) { }
 
     public open(componentType: Type<any>, config: DynamicDialogConfig) {
-        const dialogRef = this.appendDialogComponentToBody(config);
-
-        this.dialogComponentRef.instance.childComponentType = componentType;
-
+        const dialogRef = this.appendDialogComponentToBody(config, componentType);
         return dialogRef;
     }
 
-    private appendDialogComponentToBody(config: DynamicDialogConfig) {
+    private appendDialogComponentToBody(config: DynamicDialogConfig, componentType: Type<any>) {
         const map = new WeakMap();
         map.set(DynamicDialogConfig, config);
 
         const dialogRef = new DynamicDialogRef();
         map.set(DynamicDialogRef, dialogRef);
 
-        const sub = dialogRef.onClose.subscribe(() => {
-            this.removeDialogComponentFromBody();
-            sub.unsubscribe();
-        });
-
         const componentFactory = this.componentFactoryResolver.resolveComponentFactory(DynamicDialogComponent);
         const componentRef = componentFactory.create(new DynamicDialogInjector(this.injector, map));
+        componentRef.instance.childComponentType = componentType;
 
         this.appRef.attachView(componentRef.hostView);
 
         const domElem = (componentRef.hostView as EmbeddedViewRef<any>).rootNodes[0] as HTMLElement;
         document.body.appendChild(domElem);
 
-        this.dialogComponentRef = componentRef;
+        const sub = dialogRef.onClose.subscribe(() => {
+            this.removeDialogComponentFromBody(componentRef);
+            sub.unsubscribe();
+        });
 
         return dialogRef;
     }
 
-    private removeDialogComponentFromBody() {
-        this.appRef.detachView(this.dialogComponentRef.hostView);
-        this.dialogComponentRef.destroy();
+    private removeDialogComponentFromBody(componentRef: ComponentRef<DynamicDialogComponent>) {
+        this.appRef.detachView(componentRef.hostView);
+        componentRef.destroy();
     }
 }
--- src/app/components/dynamicdialog/dynamicdialog.ts
+++ src/app/components/dynamicdialog/dynamicdialog.ts
@@ -5,42 +5,58 @@ import { DynamicDialogConfig } from './dynamicdialog-config';
 import { CommonModule } from '@angular/common';
 import { DomHandler } from '../dom/domhandler';
 import { DynamicDialogRef } from './dynamicdialog-ref';
+import { DialogModule, Dialog } from '../dialog/dialog';
+
+//todo how to set all inputs of a component 
+/*
+[transitionOptions]="config.transitionOptions"
+	[header]="config.header"
+		[draggable]="config.draggable"
+		[resizable]="config.resizable"
+		[positionLeft]="config.positionLeft"
+		[positionTop]="config.positionTop"
+		[contentStyle]="config.contentStyle"
+		[modal]="config.modal"
+		[closeOnEscape]="config.closeOnEscape"
+		[dismissableMask]="config.dismissableMask"
+		[rtl]="config.rtl"
+		[closable]="config.closable"
+		[responsive]="config.responsive"
+		[appendTo]="config.appendTo"
+		[style]="config.style"
+		[styleClass]="config.styleClass"
+		[showHeader]="config.showHeader"
+		[breakpoint]="config.breakpoint"
+		[blockScroll]="config.blockScroll"
+		[autoZIndex]="config.autoZIndex"
+		[baseZIndex]="config.baseZIndex"
+		[minX]="config.minX"
+		[minY]="config.minY"
+		[focusOnShow]="config.focusOnShow"
+		[maximizable]="config.maximizable"
+		[focusTrap]="config.focusTrap"
+		
+		[closeIcon]="config.closeIcon"
+		[minimizeIcon]="config.minimizeIcon"
+		[maximizeIcon]="config.maximizeIcon"
+
+*/
 
 @Component({
 	selector: 'p-dynamicDialog',
 	template: `
-		<div #mask class="ui-widget-overlay ui-dialog-mask ui-dialog-mask-scrollblocker" *ngIf="visible" (click)="onMaskClick()"></div>
-		<div [ngClass]="{'ui-dialog ui-dynamicdialog ui-widget ui-widget-content ui-corner-all ui-shadow':true, 'ui-dialog-rtl': config.rtl}" [ngStyle]="config.style" [class]="config.styleClass"
-			[@animation]="{value: 'visible', params: {transitionParams: config.transitionOptions || '150ms cubic-bezier(0, 0, 0.2, 1)'}}" 
-			(@animation.start)="onAnimationStart($event)" (@animation.done)="onAnimationEnd($event)" role="dialog" *ngIf="visible"
-			[style.width]="config.width" [style.height]="config.height">
-            <div class="ui-dialog-titlebar ui-widget-header ui-helper-clearfix ui-corner-top" *ngIf="config.showHeader === false ? false: true">
-                <span class="ui-dialog-title">{{config.header}}</span>
-                <a [ngClass]="'ui-dialog-titlebar-icon ui-dialog-titlebar-close ui-corner-all'" tabindex="0" role="button" (click)="close()" (keydown.enter)="close()" *ngIf="config.closable === false ? false : true">
-                    <span class="pi pi-times"></span>
-                </a>
-            </div>
-            <div class="ui-dialog-content ui-widget-content" [ngStyle]="config.contentStyle">
-				<ng-template pDynamicDialogContent></ng-template>
-			</div>
-			<div class="ui-dialog-footer ui-widget-content" *ngIf="config.footer">
-				{{config.footer}}
-            </div>
-		</div>
+	<p-dialog #dialog
+	    [header]="config.header"
+		(onHide)="close()"
+		(click)="moveOnTop()"
+		[(visible)]="visible"
+
+	
+		
+		>
+		<ng-template pDynamicDialogContent></ng-template>
+	</p-dialog>
 	`,
-	animations: [
-        trigger('animation', [
-            state('void', style({
-                transform: 'translateX(-50%) translateY(-50%) scale(0.7)',
-                opacity: 0
-            })),
-            state('visible', style({
-                transform: 'translateX(-50%) translateY(-50%) scale(1)',
-                opacity: 1
-            })),
-            transition('* => *', animate('{{transitionParams}}'))
-        ])
-	]
 })
 export class DynamicDialogComponent implements AfterViewInit, OnDestroy {
 
@@ -48,17 +64,11 @@ export class DynamicDialogComponent implements AfterViewInit, OnDestroy {
 
 	componentRef: ComponentRef<any>;
 
-	mask: HTMLDivElement;
-
 	@ViewChild(DynamicDialogContent, { static: false }) insertionPoint: DynamicDialogContent;
 	
-	@ViewChild('mask', { static: false }) maskViewChild: ElementRef;
+	@ViewChild('dialog', { static: false }) dialog: Dialog;
 
 	childComponentType: Type<any>;
-
-	container: HTMLDivElement;
-
-	documentEscapeListener: Function;
 
 	constructor(private componentFactoryResolver: ComponentFactoryResolver, private cd: ChangeDetectorRef, public renderer: Renderer2,
 			public config: DynamicDialogConfig, private dialogRef: DynamicDialogRef, public zone: NgZone) { }
@@ -66,14 +76,6 @@ export class DynamicDialogComponent implements AfterViewInit, OnDestroy {
 	ngAfterViewInit() {
 		this.loadChildComponent(this.childComponentType);
 		this.cd.detectChanges();
-	}
-
-	onOverlayClicked(evt: MouseEvent) {
-		this.dialogRef.close();
-	}
-
-	onDialogClicked(evt: MouseEvent) {
-		evt.stopPropagation();
 	}
 
 	loadChildComponent(componentType: Type<any>) {
@@ -88,78 +90,15 @@ export class DynamicDialogComponent implements AfterViewInit, OnDestroy {
 	moveOnTop() {
         if (this.config.autoZIndex !== false) {
 			const zIndex = this.config.baseZIndex||0 + (++DomHandler.zindex);
-			this.container.style.zIndex = String(zIndex);
-			this.maskViewChild.nativeElement.style.zIndex = String(zIndex - 1);
-		}
-    }
-
-	onAnimationStart(event: AnimationEvent) {
-		switch(event.toState) {
-			case 'visible':
-				this.container = event.element;
-				this.moveOnTop();
-				this.bindGlobalListeners();
-				DomHandler.addClass(document.body, 'ui-overflow-hidden');
-			break;
-
-			case 'void':
-				this.onContainerDestroy();
-			break;
+			this.dialog.container.style.zIndex = String(zIndex);
 		}
 	}
 	
-	onAnimationEnd(event: AnimationEvent) {
-		if (event.toState === 'void') {
-			this.dialogRef.close();
-		}
-	}
-
-	onContainerDestroy() {
-		DomHandler.removeClass(document.body, 'ui-overflow-hidden');
-		this.unbindGlobalListeners();
-		this.container = null;
-	}
-	    
 	close() {
-		this.visible = false;
+		this.dialogRef.close();
 	}
-
-	onMaskClick() {
-		if (this.config.dismissableMask) {
-			this.close();
-		}
-	}
-
-	bindGlobalListeners() {
-        if (this.config.closeOnEscape !== false && this.config.closable !== false) {
-            this.bindDocumentEscapeListener();
-        }
-    }
-    
-    unbindGlobalListeners() {
-        this.unbindDocumentEscapeListener();
-	}
-	
-	bindDocumentEscapeListener() {
-        this.documentEscapeListener = this.renderer.listen('document', 'keydown', (event) => {
-            if (event.which == 27) {
-                if (parseInt(this.container.style.zIndex) == DomHandler.zindex) {
-					this.close();
-				}
-            }
-        });
-    }
-    
-    unbindDocumentEscapeListener() {
-        if(this.documentEscapeListener) {
-            this.documentEscapeListener();
-            this.documentEscapeListener = null;
-        }
-    }
 
 	ngOnDestroy() {
-		this.onContainerDestroy();
-
 		if (this.componentRef) {
 			this.componentRef.destroy();
 		}
@@ -167,7 +106,7 @@ export class DynamicDialogComponent implements AfterViewInit, OnDestroy {
 }
 
 @NgModule({
-	imports: [CommonModule],
+	imports: [CommonModule, DialogModule],
 	declarations: [DynamicDialogComponent, DynamicDialogContent],
 	entryComponents: [DynamicDialogComponent]
 })
