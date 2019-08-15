6bc9bd0fefa8872950e46a92ae156d50d2501b3b
Heo Sangmin <heo@mapiacompany.com>
2019-08-15 13:46:46 +0900

    feat(ng-packagr): support ng-packagr library build

---
+++ src/app/components/dynamicdialog/public_api.ts
@@ -0,0 +1,6 @@
+export * from './dialogservice';
+export * from './dynamicdialog';
+export * from './dynamicdialog-config';
+export * from './dynamicdialog-injector';
+export * from './dynamicdialog-ref';
+export * from './dynamicdialog';
