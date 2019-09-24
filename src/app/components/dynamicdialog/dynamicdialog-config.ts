29d08db7915ce8e911fd8f4eda7d0b699dbb9b06
Brandon Blaylock <brandon@null.pub>
2019-09-24 10:28:44 -0700

    feat: parameterized DynamicDialogConfig

--- src/app/components/dynamicdialog/dynamicdialog-config.ts
+++ src/app/components/dynamicdialog/dynamicdialog-config.ts
@@ -1,18 +1,18 @@
-export class DynamicDialogConfig {
-	data?: any;
-	header?: string;
-	footer?: string;
-	width?: string;
-	height?: string;
-	closeOnEscape?: boolean;
-	baseZIndex?: number;
-	autoZIndex?: boolean;
-	dismissableMask?: boolean;
-	rtl?: boolean;
-	style?: any;
-	contentStyle?: any;
-	styleClass?: string;
-	transitionOptions?: string;
-	closable?: boolean;
-	showHeader?: boolean;
+export class DynamicDialogConfig<D = any> {
+    data?: D;
+    header?: string;
+    footer?: string;
+    width?: string;
+    height?: string;
+    closeOnEscape?: boolean;
+    baseZIndex?: number;
+    autoZIndex?: boolean;
+    dismissableMask?: boolean;
+    rtl?: boolean;
+    style?: any;
+    contentStyle?: any;
+    styleClass?: string;
+    transitionOptions?: string;
+    closable?: boolean;
+    showHeader?: boolean;
 }
