3f03fb93ee7336205ac8009db9adb309d8871f4e
mertsincan <sincan.mert@gmail.com>
2019-10-17 15:52:25 +0300

    Refactor #3896

--- src/app/showcase/components/carousel/carouseldemo.ts
+++ src/app/showcase/components/carousel/carouseldemo.ts
@@ -4,36 +4,36 @@ import { CarService } from '../../service/carservice';
 @Component({
     templateUrl: './carouseldemo.html',
     styles: [`
-		.carousel-demo .p-carousel .p-carousel-content .p-carousel-item .car-details > .p-grid {
+		.carousel-demo .ui-carousel .ui-carousel-content .ui-carousel-item .car-details > .p-grid {
 			border: 1px solid #b3c2ca;
 			border-radius: 3px;
 			margin: 0.3em;
 			text-align: center;
 			padding: 2em 0 2.25em 0;
 		}
-		.carousel-demo .p-carousel .p-carousel-content .p-carousel-item .car-data .car-title {
+		.carousel-demo .ui-carousel .ui-carousel-content .ui-carousel-item .car-data .car-title {
 			font-weight: 700;
 			font-size: 20px;
 			margin-top: 24px;
 		}
-		.carousel-demo .p-carousel .p-carousel-content .p-carousel-item .car-data .car-subtitle {
+		.carousel-demo .ui-carousel .ui-carousel-content .ui-carousel-item .car-data .car-subtitle {
 			margin: 0.25em 0 2em 0;
 		}
-		.carousel-demo .p-carousel .p-carousel-content .p-carousel-item .car-data button {
+		.carousel-demo .ui-carousel .ui-carousel-content .ui-carousel-item .car-data button {
 			margin-left: 0.5em;
 		}
-		.carousel-demo .p-carousel .p-carousel-content .p-carousel-item .car-data button:first-child {
+		.carousel-demo .ui-carousel .ui-carousel-content .ui-carousel-item .car-data button:first-child {
 			margin-left: 0;
 		}
-		.carousel-demo .p-carousel.custom-carousel .p-carousel-dot-icon {
+		.carousel-demo .ui-carousel.custom-carousel .ui-carousel-dot-icon {
 			width: 16px !important;
 			height: 16px !important;
 			border-radius: 50%;
 		}
-		.carousel-demo .p-carousel.p-carousel-horizontal .p-carousel-content .p-carousel-item.p-carousel-item-start .car-details > .p-grid {
+		.carousel-demo .ui-carousel.ui-carousel-horizontal .ui-carousel-content .ui-carousel-item.ui-carousel-item-start .car-details > .p-grid {
 			margin-left: 0.6em;
 		}
-		.carousel-demo .p-carousel.p-carousel-horizontal .p-carousel-content .p-carousel-item.p-carousel-item-end .car-details > .p-grid {
+		.carousel-demo .ui-carousel.ui-carousel-horizontal .ui-carousel-content .ui-carousel-item.ui-carousel-item-end .car-details > .p-grid {
 			margin-right: 0.6em;
 		}
 	`],
