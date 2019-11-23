02d6da509fc2b89c6afd19a4e26312efdac8ef4d
cagataycivici <cagatay.civici@gmail.com>
2019-11-23 13:09:44 +0300

    Terminate string

--- src/app/showcase/components/blockui/blockuidemo.module.ts
+++ src/app/showcase/components/blockui/blockuidemo.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {BlockUIDemo} from './blockuidemo';
 import {BlockUIDemoRoutingModule} from './blockuidemo-routing.module';
-import {BlockUIModule} from 'primeng/blockui;
+import {BlockUIModule} from 'primeng/blockui';
 import {ButtonModule} from 'primeng/button';
 import {PanelModule} from 'primeng/panel';
 import {TabViewModule} from 'primeng/tabview';
