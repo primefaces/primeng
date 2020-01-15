38775776c10e3622bca9d44b28fd811de8c963e9
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2020-01-15 14:49:05 +0300

    Router Demo  added to TabMenu Demo
    u

--- src/app/showcase/components/tabmenu/tabmenudemo.module.ts
+++ src/app/showcase/components/tabmenu/tabmenudemo.module.ts
@@ -1,9 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {TabMenuDemo} from './tabmenudemo';
+import {TabMenuDemo, InfoComponent, MessageComponent} from './tabmenudemo';
 import {TabMenuDemoRoutingModule} from './tabmenudemo-routing.module';
 import {TabMenuModule} from 'primeng/tabmenu';
 import {TabViewModule} from 'primeng/tabview';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 import {CodeHighlighterModule} from 'primeng/codehighlighter';
 
 @NgModule({
@@ -11,11 +15,14 @@ import {CodeHighlighterModule} from 'primeng/codehighlighter';
 		CommonModule,
 		TabMenuDemoRoutingModule,
         TabMenuModule,
-        TabViewModule,
+		TabViewModule,
+		MessageModule,
         CodeHighlighterModule
 	],
 	declarations: [
-		TabMenuDemo
+		TabMenuDemo,
+		InfoComponent,
+		MessageComponent
 	]
 })
 export class TabMenuDemoModule {}
