ebe83d83265ce3e04bbb91d3a42ac784052678de
cagataycivici <cagatay.civici@gmail.com>
2020-03-18 21:41:15 +0300

    Cosmetics on Calendar Demo

--- src/app/showcase/components/calendar/calendardemo.ts
+++ src/app/showcase/components/calendar/calendardemo.ts
@@ -1,7 +1,12 @@
 import {Component} from '@angular/core';
 
 @Component({
-    templateUrl: './calendardemo.html'
+    templateUrl: './calendardemo.html',
+    styles: [`
+        .ui-grid {
+            margin-top: -30px;
+        }
+    `]
 })
 export class CalendarDemo {
 
