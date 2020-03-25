f9d01238844523dd88fa0f1eaacb98a860388f39
cagataycivici <cagatay.civici@gmail.com>
2020-03-25 18:00:16 +0300

    Destroy dynamic dialog on route change

--- src/app/showcase/components/dynamicdialog/dynamicdialogdemo.ts
+++ src/app/showcase/components/dynamicdialog/dynamicdialogdemo.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import {Component,OnDestroy} from '@angular/core';
 import {DialogService} from '../../../components/dynamicdialog/dialogservice';
 import {MessageService} from 'primeng/api';
 import {CarsListDemo} from './carslistdemo';
 import {Car} from '../../components/domain/car';
 import { AppComponent } from '../../app.component';
+import { DynamicDialogRef } from '../../../components/dynamicdialog/dynamicdialog-ref';
 
 @Component({
     templateUrl: './dynamicdialogdemo.html',
@@ -28,19 +29,21 @@ import { AppComponent } from '../../app.component';
         }
     `]
 })
-export class DynamicDialogDemo {
+export class DynamicDialogDemo implements OnDestroy {
 
     constructor(public dialogService: DialogService, public messageService: MessageService, private app: AppComponent) {}
 
+    ref: DynamicDialogRef;
+
     show() {
-        const ref = this.dialogService.open(CarsListDemo, {
+        this.ref = this.dialogService.open(CarsListDemo, {
             header: 'Choose a Car',
             width: '70%',
             contentStyle: {"max-height": "350px", "overflow": "auto"},
             baseZIndex: 10000
         });
 
-        ref.onClose.subscribe((car: Car) =>{
+        this.ref.onClose.subscribe((car: Car) =>{
             if (car) {
                 this.messageService.add({severity:'info', summary: 'Car Selected', detail:'Vin:' + car.vin});
             }
@@ -49,6 +52,10 @@ export class DynamicDialogDemo {
 
     isNewsActive() {
         return this.app.newsActive;
+    }
+
+    ngOnDestroy() {
+        this.ref.close();
     }
 }
 
