36a19881378bce403ab89aff91bcca950dd05322
mertsincan <sincan.mert@gmail.com>
2020-03-25 16:07:58 +0300

    Fixed #8653 - Migrate DynamicDialog to new Dialog Impl

--- src/app/components/dynamicdialog/dynamicdialog.ts
+++ src/app/components/dynamicdialog/dynamicdialog.ts
@@ -1,46 +1,53 @@
 import { Component, NgModule, Type, ComponentFactoryResolver, ViewChild, OnDestroy, ComponentRef, AfterViewInit, ChangeDetectorRef, Renderer2, NgZone, ElementRef } from '@angular/core';
-import { trigger,state,style,transition,animate,AnimationEvent } from '@angular/animations';
+import { trigger,style,transition,animate,AnimationEvent, animation, useAnimation } from '@angular/animations';
 import { DynamicDialogContent } from './dynamicdialogcontent';
 import { DynamicDialogConfig } from './dynamicdialog-config';
 import { CommonModule } from '@angular/common';
 import { DomHandler } from 'primeng/dom';
 import { DynamicDialogRef } from './dynamicdialog-ref';
 
+const showAnimation = animation([
+    style({ transform: '{{transform}}', opacity: 0 }),
+    animate('{{transition}}', style({ transform: 'none', opacity: 1 }))
+]);
+
+const hideAnimation = animation([
+    animate('{{transition}}', style({ transform: '{{transform}}', opacity: 0 }))
+]);
+
 @Component({
 	selector: 'p-dynamicDialog',
 	template: `
-		<div #mask class="ui-widget-overlay ui-dialog-mask ui-dialog-mask-scrollblocker" *ngIf="visible" (click)="onMaskClick()"></div>
-		<div [ngClass]="{'ui-dialog ui-dynamicdialog ui-widget ui-widget-content ui-corner-all ui-shadow':true, 'ui-dialog-rtl': config.rtl}" [ngStyle]="config.style" [class]="config.styleClass"
-			[@animation]="{value: 'visible', params: {transitionParams: config.transitionOptions || '150ms cubic-bezier(0, 0, 0.2, 1)'}}" 
-			(@animation.start)="onAnimationStart($event)" (@animation.done)="onAnimationEnd($event)" role="dialog" *ngIf="visible"
-			[style.width]="config.width" [style.height]="config.height">
-            <div class="ui-dialog-titlebar ui-widget-header ui-helper-clearfix ui-corner-top" *ngIf="config.showHeader === false ? false: true">
-                <span class="ui-dialog-title">{{config.header}}</span>
-                <div class="ui-dialog-titlebar-icons">
-                    <a [ngClass]="'ui-dialog-titlebar-icon ui-dialog-titlebar-close ui-corner-all'" tabindex="0" role="button" (click)="close()" (keydown.enter)="close()" *ngIf="config.closable !== false">
-                        <span class="pi pi-times"></span>
-                    </a>
+        <div #mask class="ui-widget-overlay ui-dialog-mask ui-dialog-visible ui-dialog-mask-scrollblocker">
+            <div [ngClass]="{'ui-dialog ui-dynamicdialog ui-widget ui-widget-content ui-corner-all ui-shadow':true, 'ui-dialog-rtl': config.rtl}" [ngStyle]="config.style" [class]="config.styleClass"
+                [@animation]="{value: 'visible', params: {transform: transformOptions, transition: config.transitionOptions || '150ms cubic-bezier(0, 0, 0.2, 1)'}}"
+                (@animation.start)="onAnimationStart($event)" (@animation.done)="onAnimationEnd($event)" role="dialog" *ngIf="visible"
+                [style.width]="config.width" [style.height]="config.height">
+                <div class="ui-dialog-titlebar ui-widget-header ui-helper-clearfix ui-corner-top" *ngIf="config.showHeader === false ? false: true">
+                    <span class="ui-dialog-title">{{config.header}}</span>
+                    <div class="ui-dialog-titlebar-icons">
+                        <a [ngClass]="'ui-dialog-titlebar-icon ui-dialog-titlebar-close ui-corner-all'" tabindex="0" role="button" (click)="close()" (keydown.enter)="close()" *ngIf="config.closable !== false">
+                            <span class="pi pi-times"></span>
+                        </a>
+                    </div>
+                </div>
+                <div class="ui-dialog-content ui-widget-content" [ngStyle]="config.contentStyle">
+                    <ng-template pDynamicDialogContent></ng-template>
+                </div>
+                <div class="ui-dialog-footer ui-widget-content" *ngIf="config.footer">
+                    {{config.footer}}
                 </div>
             </div>
-            <div class="ui-dialog-content ui-widget-content" [ngStyle]="config.contentStyle">
-				<ng-template pDynamicDialogContent></ng-template>
-			</div>
-			<div class="ui-dialog-footer ui-widget-content" *ngIf="config.footer">
-				{{config.footer}}
-            </div>
-		</div>
+        </div>
 	`,
 	animations: [
         trigger('animation', [
-            state('void', style({
-                transform: 'translateX(-50%) translateY(-50%) scale(0.7)',
-                opacity: 0
-            })),
-            state('visible', style({
-                transform: 'translateX(-50%) translateY(-50%) scale(1)',
-                opacity: 1
-            })),
-            transition('* => *', animate('{{transitionParams}}'))
+            transition('void => visible', [
+                useAnimation(showAnimation)
+            ]),
+            transition('visible => void', [
+                useAnimation(hideAnimation)
+            ])
         ])
 	]
 })
@@ -53,16 +60,18 @@ export class DynamicDialogComponent implements AfterViewInit, OnDestroy {
 	mask: HTMLDivElement;
 
 	@ViewChild(DynamicDialogContent, { static: false }) insertionPoint: DynamicDialogContent;
-	
+
 	@ViewChild('mask', { static: false }) maskViewChild: ElementRef;
 
 	childComponentType: Type<any>;
 
     container: HTMLDivElement;
-    
+
     documentKeydownListener: any;
 
-	documentEscapeListener: Function;
+    documentEscapeListener: Function;
+
+    transformOptions: string = "scale(0.7)";
 
 	constructor(private componentFactoryResolver: ComponentFactoryResolver, private cd: ChangeDetectorRef, public renderer: Renderer2,
 			public config: DynamicDialogConfig, private dialogRef: DynamicDialogRef, public zone: NgZone) { }
@@ -88,7 +97,7 @@ export class DynamicDialogComponent implements AfterViewInit, OnDestroy {
 
 		this.componentRef = viewContainerRef.createComponent(componentFactory);
 	}
-	
+
 	moveOnTop() {
         if (this.config.autoZIndex !== false) {
 			const zIndex = this.config.baseZIndex||0 + (++DomHandler.zindex);
@@ -112,7 +121,7 @@ export class DynamicDialogComponent implements AfterViewInit, OnDestroy {
 			break;
 		}
 	}
-	
+
 	onAnimationEnd(event: AnimationEvent) {
 		if (event.toState === 'void') {
 			this.dialogRef.close();
@@ -122,9 +131,9 @@ export class DynamicDialogComponent implements AfterViewInit, OnDestroy {
 	onContainerDestroy() {
 		DomHandler.removeClass(document.body, 'ui-overflow-hidden');
 		this.unbindGlobalListeners();
-		this.container = null;
+        this.container = null;
 	}
-	    
+
 	close() {
 		this.visible = false;
 	}
@@ -134,7 +143,7 @@ export class DynamicDialogComponent implements AfterViewInit, OnDestroy {
 			this.close();
 		}
     }
-    
+
     onKeydown(event: KeyboardEvent) {
         if (event.which === 9) {
             event.preventDefault();
@@ -181,12 +190,12 @@ export class DynamicDialogComponent implements AfterViewInit, OnDestroy {
             this.bindDocumentEscapeListener();
         }
     }
-    
+
     unbindGlobalListeners() {
         this.unbindDocumentKeydownListener();
         this.unbindDocumentEscapeListener();
     }
-    
+
     bindDocumentKeydownListener() {
         this.zone.runOutsideAngular(() => {
             this.documentKeydownListener = this.onKeydown.bind(this);
@@ -200,7 +209,7 @@ export class DynamicDialogComponent implements AfterViewInit, OnDestroy {
             this.documentKeydownListener = null;
         }
     }
-	
+
 	bindDocumentEscapeListener() {
         this.documentEscapeListener = this.renderer.listen('document', 'keydown', (event) => {
             if (event.which == 27) {
@@ -210,7 +219,7 @@ export class DynamicDialogComponent implements AfterViewInit, OnDestroy {
             }
         });
     }
-    
+
     unbindDocumentEscapeListener() {
         if (this.documentEscapeListener) {
             this.documentEscapeListener();
