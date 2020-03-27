29423981ea68320eb8fecb4b89eb1e285c5cbd93
mertsincan <sincan.mert@gmail.com>
2020-03-27 15:52:11 +0300

    Refactor on dynamicdialog demo

--- src/app/showcase/components/dynamicdialog/dynamicdialogdemo.ts
+++ src/app/showcase/components/dynamicdialog/dynamicdialogdemo.ts
@@ -55,7 +55,9 @@ export class DynamicDialogDemo implements OnDestroy {
     }
 
     ngOnDestroy() {
-        this.ref.close();
+        if (this.ref) {
+            this.ref.close();
+        }
     }
 }
 
