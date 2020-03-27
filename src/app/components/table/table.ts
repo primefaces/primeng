8796a585a44250839f9a63614b0822159b6a81ef
cagataycivici <cagatay.civici@gmail.com>
2020-03-27 12:47:03 +0300

    Fixed #7843 - aria-label for p-tableCheckbox, p-tableHeaderCheckbox and p-tableRadioButton

--- src/app/components/table/table.ts
+++ src/app/components/table/table.ts
@@ -3394,7 +3394,8 @@ export class CellEditor implements AfterContentInit {
     template: `
         <div class="ui-radiobutton ui-widget" (click)="onClick($event)">
             <div class="ui-helper-hidden-accessible">
-                <input type="radio" [attr.id]="inputId" [attr.name]="name" [checked]="checked" (focus)="onFocus()" (blur)="onBlur()" [disabled]="disabled">
+                <input type="radio" [attr.id]="inputId" [attr.name]="name" [checked]="checked" (focus)="onFocus()" (blur)="onBlur()" 
+                [disabled]="disabled" [attr.aria-label]="ariaLabel">
             </div>
             <div #box [ngClass]="{'ui-radiobutton-box ui-widget ui-state-default':true,
                 'ui-state-active':checked, 'ui-state-disabled':disabled}" role="radio" [attr.aria-checked]="checked">
@@ -3414,6 +3415,8 @@ export class TableRadioButton  {
     @Input() inputId: string;
 
     @Input() name: string;
+
+    @Input() ariaLabel: string;
 
     @ViewChild('box') boxViewChild: ElementRef;
 
@@ -3463,7 +3466,7 @@ export class TableRadioButton  {
         <div class="ui-chkbox ui-widget" (click)="onClick($event)">
             <div class="ui-helper-hidden-accessible">
                 <input type="checkbox" [attr.id]="inputId" [attr.name]="name" [checked]="checked" (focus)="onFocus()" (blur)="onBlur()" [disabled]="disabled"
-                [attr.required]="required">
+                [attr.required]="required" [attr.aria-label]="ariaLabel">
             </div>
             <div #box [ngClass]="{'ui-chkbox-box ui-widget ui-state-default':true,
                 'ui-state-active':checked, 'ui-state-disabled':disabled}" role="checkbox" [attr.aria-checked]="checked">
@@ -3485,6 +3488,8 @@ export class TableCheckbox  {
     @Input() name: string;
 
     @Input() required: boolean;
+    
+    @Input() ariaLabel: string;
 
     @ViewChild('box') boxViewChild: ElementRef;
 
@@ -3533,7 +3538,8 @@ export class TableCheckbox  {
     template: `
         <div class="ui-chkbox ui-widget" (click)="onClick($event)">
             <div class="ui-helper-hidden-accessible">
-                <input #cb type="checkbox" [attr.id]="inputId" [attr.name]="name" [checked]="checked" (focus)="onFocus()" (blur)="onBlur()" [disabled]="isDisabled()">
+                <input #cb type="checkbox" [attr.id]="inputId" [attr.name]="name" [checked]="checked" (focus)="onFocus()" (blur)="onBlur()" 
+                [disabled]="isDisabled()" [attr.aria-label]="ariaLabel">
             </div>
             <div #box [ngClass]="{'ui-chkbox-box ui-widget ui-state-default':true,
                 'ui-state-active':checked, 'ui-state-disabled': isDisabled()}" role="checkbox" [attr.aria-checked]="checked">
@@ -3551,6 +3557,8 @@ export class TableHeaderCheckbox  {
     @Input() inputId: string;
 
     @Input() name: string;
+
+    @Input() ariaLabel: string;
 
     checked: boolean;
 
