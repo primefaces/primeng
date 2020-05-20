e08274a9b6ec835f0c7f6d07f674bd7435b6db69
cagataycivici <cagatay.civici@gmail.com>
2020-05-20 22:37:55 +0300

    Updated scroll demo

--- src/app/showcase/components/table/tablescrolldemo.ts
+++ src/app/showcase/components/table/tablescrolldemo.ts
@@ -11,12 +11,6 @@ export class TableScrollDemo implements OnInit {
 
     cars2: Car[];
 
-    cars3: Car[];
-    
-    cars4: Car[];
-
-    cars5: Car[];
-
     cols: any[];
 
     frozenCars: Car[];
@@ -24,8 +18,6 @@ export class TableScrollDemo implements OnInit {
     frozenCols: any[];
 
     scrollableCols: any[];
-
-    sales: any[];
     
     dialogVisible: boolean;
 
@@ -34,9 +26,6 @@ export class TableScrollDemo implements OnInit {
     ngOnInit() {
         this.carService.getCarsMedium().then(cars => this.cars1 = cars);
         this.carService.getCarsSmall().then(cars => this.cars2 = cars);
-        this.carService.getCarsMedium().then(cars => this.cars3 = cars);
-        this.carService.getCarsMedium().then(cars => this.cars4 = cars);
-        this.carService.getCarsMedium().then(cars => this.cars5 = cars);
 
         this.cols = [
             { field: 'vin', header: 'Vin' },
@@ -61,19 +50,6 @@ export class TableScrollDemo implements OnInit {
         this.frozenCars = [
             { "brand": "BMW", "year": 2013, "color": "Grey", "vin": "fh2uf23" },
             { "brand": "Chevrolet", "year": 2011, "color": "Black", "vin": "4525g23" }
-        ];
-
-        this.sales = [
-            { brand: 'Apple', lastYearSale: '51%', thisYearSale: '40%', lastYearProfit: '$54,406.00', thisYearProfit: '$43,342' },
-            { brand: 'Samsung', lastYearSale: '83%', thisYearSale: '96%', lastYearProfit: '$423,132', thisYearProfit: '$312,122' },
-            { brand: 'Microsoft', lastYearSale: '38%', thisYearSale: '5%', lastYearProfit: '$12,321', thisYearProfit: '$8,500' },
-            { brand: 'Philips', lastYearSale: '49%', thisYearSale: '22%', lastYearProfit: '$745,232', thisYearProfit: '$650,323,' },
-            { brand: 'Song', lastYearSale: '17%', thisYearSale: '79%', lastYearProfit: '$643,242', thisYearProfit: '500,332' },
-            { brand: 'LG', lastYearSale: '52%', thisYearSale: ' 65%', lastYearProfit: '$421,132', thisYearProfit: '$150,005' },
-            { brand: 'Sharp', lastYearSale: '82%', thisYearSale: '12%', lastYearProfit: '$131,211', thisYearProfit: '$100,214' },
-            { brand: 'Panasonic', lastYearSale: '44%', thisYearSale: '45%', lastYearProfit: '$66,442', thisYearProfit: '$53,322' },
-            { brand: 'HTC', lastYearSale: '90%', thisYearSale: '56%', lastYearProfit: '$765,442', thisYearProfit: '$296,232' },
-            { brand: 'Toshiba', lastYearSale: '75%', thisYearSale: '54%', lastYearProfit: '$21,212', thisYearProfit: '$12,533' }
         ];
     }
 
