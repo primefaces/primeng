a333367455ad2f3f123d5bc6e3810d0318cb9b0e
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2020-05-27 20:17:37 +0300

    Stackblitz Demos Added to Overlay Components

--- src/app/showcase/app.module.ts
+++ src/app/showcase/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule }    from '@angular/forms';
-import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
