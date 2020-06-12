4971852b4f29ddd70d66f491d0116d3143d8329a
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2020-06-12 16:33:59 +0300

    Fixed #8939 - Tabview: disabled tab control can be focused

--- src/app/components/tabview/tabview.ts
+++ src/app/components/tabview/tabview.ts
@@ -18,7 +18,7 @@ let idx: number = 0;
     },
     template: `
         <ng-template ngFor let-tab [ngForOf]="tabs">
-            <li [class]="getDefaultHeaderClass(tab)" [ngStyle]="tab.headerStyle" role="presentation"
+            <li [class]="getDefaultHeaderClass(tab)" [ngStyle]="tab.headerStyle" role="presentation" [attr.tabindex]="tab.disabled ? -1 : 0"
                 [ngClass]="{'ui-tabview-selected ui-state-active': tab.selected, 'ui-state-disabled': tab.disabled}"
                 (click)="clickTab($event,tab)" *ngIf="!tab.closed" tabindex="0" (keydown.enter)="clickTab($event,tab)">
                 <a [attr.id]="tab.id + '-label'" role="tab" [attr.aria-selected]="tab.selected" [attr.aria-controls]="tab.id" [pTooltip]="tab.tooltip" [tooltipPosition]="tab.tooltipPosition"
