e182f387ef9850c91aefdcce922db5e615553602
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2020-07-16 17:44:27 +0300

    Checkbox Demo Update

--- src/app/showcase/components/checkbox/checkboxdemo.ts
+++ src/app/showcase/components/checkbox/checkboxdemo.ts
@@ -7,7 +7,13 @@ export class CheckboxDemo {
 
     selectedCities: string[] = [];
 
-    selectedCategories: string[] = ['Technology', 'Sports'];
+    selectedCategories: any[] = ['Technology', 'Sports'];
     
+    categories: any[] = [{name: 'Accounting', key: 'A'}, {name: 'Marketing', key: 'M'}, {name: 'Production', key: 'P'}, {name: 'Research', key: 'R'}];
+
     checked: boolean = false;
+
+    ngOnInit() {
+        this.selectedCategories = this.categories.slice(1,3);
+    }
 }
