dc3ab5b880e6309679357e8797e0f349e944ad33
cagataycivici <cagatay.civici@gmail.com>
2020-07-20 18:08:06 +0300

    Update logo

--- src/app/showcase/app.topbar.component.ts
+++ src/app/showcase/app.topbar.component.ts
@@ -13,7 +13,7 @@ import { Subscription } from 'rxjs';
                 <i class="pi pi-bars"></i>
             </a>
             <a [routerLink]="['/']" class="logo">
-                <img alt="logo" src="assets/showcase/images/primeng-logo.svg" />
+                <img alt="logo" [src]="'assets/showcase/images/' + (config.dark ? 'primeng-logo-light.svg' : 'primeng-logo-dark.svg')" />
             </a>
             <div class="app-theme" [pTooltip]="theme" tooltipPosition="bottom">
                 <img [src]="'assets/showcase/images/themes/' + logoMap[config.theme]" />
