0e992dd3d45b2fe00918ac5ebae4cda092a7f3bb
yigitfindikli <yigitfindikli@gmail.com>
2020-07-22 17:23:23 +0300

    Setup Demo Update

--- src/app/showcase/components/setup/setup.module.ts
+++ src/app/showcase/components/setup/setup.module.ts
@@ -3,11 +3,13 @@ import {CommonModule} from '@angular/common';
 import {SetupComponent} from './setup.component';
 import {SetupRoutingModule} from './setup-routing.module';
 import {CodeHighlighterModule} from 'primeng/codehighlighter';
+import {AppCodeModule} from '../../app.code.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CodeHighlighterModule,
+    AppCodeModule,
     SetupRoutingModule
   ],
   declarations: [
