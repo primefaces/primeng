42a5361e5da0bf5d356b67989f93697453b661c3
yigitfindikli <yigitfindikli@gmail.com>
2020-07-24 18:24:51 +0300

    OverlayPanel demo update

--- src/app/showcase/components/overlaypanel/overlaypaneldemo.ts
+++ src/app/showcase/components/overlaypanel/overlaypaneldemo.ts
@@ -4,25 +4,16 @@ import {Car} from '../../components/domain/car';
 import {CarService} from '../../service/carservice';
 
 @Component({
-    templateUrl: './overlaypaneldemo.html'
+    templateUrl: './overlaypaneldemo.html',
+    styleUrls: ['./overlaypanel.scss']
 })
 export class OverlayPanelDemo {
 
     cars1: Car[];
     
-    cars2: Car[];
-    
-    selectedCar: Car;
-    
     constructor(private carService: CarService) { }
 
     ngOnInit() {
         this.carService.getCarsSmall().then(cars => this.cars1 = cars);
-        this.carService.getCarsSmall().then(cars => this.cars2 = cars);
-    }
-    
-    selectCar(event,car: Car, overlaypanel: OverlayPanel) {
-        this.selectedCar = car;
-        overlaypanel.toggle(event);
     }
 }
