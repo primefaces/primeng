bdddcba86c04771bc432e2d1fdb29726142d3a78
cagataycivici <cagatay.civici@gmail.com>
2020-07-27 15:25:00 +0300

    Cosmetics on Icons Page

--- src/app/showcase/components/icons/icons.module.ts
+++ src/app/showcase/components/icons/icons.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { IconsComponent } from './icons.component';
 import { IconsRoutingModule } from './icons-routing.module';
 import { InputTextModule } from 'primeng/inputtext';
+import { AppCodeModule } from '../../app.code.component';
 import { CodeHighlighterModule } from 'primeng/codehighlighter';
 
 @NgModule({
     imports: [
         CommonModule,
         InputTextModule,
+        AppCodeModule,
         CodeHighlighterModule,
         IconsRoutingModule
     ],
