b903436dcee08123a829ef59775b12cbf2f9fd5c
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2020-07-28 16:10:17 +0300

    Paginator demo update

--- src/app/showcase/components/paginator/paginatordemo.ts
+++ src/app/showcase/components/paginator/paginatordemo.ts
@@ -1,8 +1,21 @@
 import {Component} from '@angular/core';
 
 @Component({
-    templateUrl: './paginatordemo.html'
+    templateUrl: './paginatordemo.html',
+    styleUrls: ['./paginatordemo.scss']
 })
 export class PaginatorDemo {
+    first: number = 0;
 
+    totalRecords: number = 120;
+
+    totalRecords2: number = 12;
+
+    onPageChange(event) {
+        this.first = event.first;
+    }
+
+    refresh() {
+        this.first = 0;
+    }
 }
