08c24bcfaf00d7adeaab44d3b6531ade03567ac7
cagataycivici <cagatay.civici@gmail.com>
2020-07-29 10:53:30 +0300

    Update lazy table demo

--- src/app/showcase/components/table/tablelazydemo.ts
+++ src/app/showcase/components/table/tablelazydemo.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Car } from '../../components/domain/car';
-import { CarService } from '../../service/carservice';
+import { Customer } from '../../domain/customer';
+import { CustomerService } from '../../service/customerservice';
 import { LazyLoadEvent } from 'primeng/api';
 import { FilterMetadata } from 'primeng/api';
 
@@ -9,9 +9,9 @@ import { FilterMetadata } from 'primeng/api';
 })
 export class TableLazyDemo implements OnInit {
 
-    datasource: Car[];
+    datasource: Customer[];
 
-    cars: Car[];
+    customers: Customer[];
 
     totalRecords: number;
 
@@ -19,26 +19,19 @@ export class TableLazyDemo implements OnInit {
 
     loading: boolean;
 
-    constructor(private carService: CarService) { }
+    constructor(private customerService: CustomerService) { }
 
     ngOnInit() {
         //datasource imitation
-        this.carService.getCarsLarge().then(cars => {
-            this.datasource = cars;
-            this.totalRecords = this.datasource.length;
+        this.customerService.getCustomersLarge().then(data => {
+            this.datasource = data;
+            this.totalRecords = data.length;
         });
-
-        this.cols = [
-            { field: 'vin', header: 'Vin' },
-            { field: 'year', header: 'Year' },
-            { field: 'brand', header: 'Brand' },
-            { field: 'color', header: 'Color' }
-        ];
 
         this.loading = true;
     }
 
-    loadCarsLazy(event: LazyLoadEvent) {  
+    loadCustomers(event: LazyLoadEvent) {  
         this.loading = true;
 
         //in a real application, make a remote request to load data using state metadata from event
@@ -51,7 +44,7 @@ export class TableLazyDemo implements OnInit {
         //imitate db connection over a network
         setTimeout(() => {
             if (this.datasource) {
-                this.cars = this.datasource.slice(event.first, (event.first + event.rows));
+                this.customers = this.datasource.slice(event.first, (event.first + event.rows));
                 this.loading = false;
             }
         }, 1000);
