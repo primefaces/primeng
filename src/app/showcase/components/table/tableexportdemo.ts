942e6d1373fd5661a498cda0a52bf230ccdec6be
cagataycivici <cagatay.civici@gmail.com>
2020-07-29 14:35:55 +0300

    Update export demo

--- src/app/showcase/components/table/tableexportdemo.ts
+++ src/app/showcase/components/table/tableexportdemo.ts
@@ -1,30 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { Car } from '../../components/domain/car';
-import { CarService } from '../../service/carservice';
+import { Product } from '../../domain/product';
+import { ProductService } from '../../service/productservice';
 
 @Component({
     templateUrl: './tableexportdemo.html'
 })
 export class TableExportDemo implements OnInit {
 
-    cars: Car[];
+    products: Product[];
 
-    selectedCars: Car[];
+    selectedProducts: Product[];
+
+    constructor(private productService: ProductService) { }
 
     cols: any[];
 
     exportColumns: any[];
 
-    constructor(private carService: CarService) { }
-
     ngOnInit() {
-        this.carService.getCarsSmall().then(cars => this.cars = cars);
+        this.productService.getProductsSmall().then(data => this.products = data);
 
         this.cols = [
-            { field: 'vin', header: 'Vin' },
-            { field: 'year', header: 'Year' },
-            { field: 'brand', header: 'Brand' },
-            { field: 'color', header: 'Color' }
+            { field: 'code', header: 'Code' },
+            { field: 'name', header: 'Name' },
+            { field: 'category', header: 'Category' },
+            { field: 'quantity', header: 'Quantity' }
         ];
 
         this.exportColumns = this.cols.map(col => ({title: col.header, dataKey: col.field}));
@@ -34,18 +34,18 @@ export class TableExportDemo implements OnInit {
         import("jspdf").then(jsPDF => {
             import("jspdf-autotable").then(x => {
                 const doc = new jsPDF.default(0,0);
-                doc.autoTable(this.exportColumns, this.cars);
-                doc.save('primengTable.pdf');
+                doc.autoTable(this.exportColumns, this.products);
+                doc.save('products.pdf');
             })
         })
     }
 
     exportExcel() {
         import("xlsx").then(xlsx => {
-            const worksheet = xlsx.utils.json_to_sheet(this.getCars());
+            const worksheet = xlsx.utils.json_to_sheet(this.products);
             const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
             const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
-            this.saveAsExcelFile(excelBuffer, "primengTable");
+            this.saveAsExcelFile(excelBuffer, "products");
         });
     }
 
@@ -58,14 +58,5 @@ export class TableExportDemo implements OnInit {
             });
             FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
         });
-    }
-
-    getCars() {
-        let cars = [];
-        for(let car of this.cars) {
-            car.year = car.year.toString();
-            cars.push(car);
-        }
-        return cars;
     }
 }
