722b1d18ab374574ea878926fc35307797291717
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2020-07-30 12:21:32 +0300

    MenuModel demo updated

--- src/app/showcase/components/menumodel/menumodeldemo.module.ts
+++ src/app/showcase/components/menumodel/menumodeldemo.module.ts
@@ -3,11 +3,13 @@ import {CommonModule} from '@angular/common';
 import {MenuModelDemo} from './menumodeldemo';
 import {MenuModelDemoRoutingModule} from './menumodeldemo-routing.module';
 import {CodeHighlighterModule} from 'primeng/codehighlighter';
+import {AppCodeModule} from '../../app.code.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		MenuModelDemoRoutingModule,
+		AppCodeModule,
         CodeHighlighterModule
 	],
 	declarations: [
