e7e86b08908be64f009bdb44046427f6171812ae
cagataycivici <cagatay.civici@gmail.com>
2020-08-10 10:51:21 +0300

    New dev iteration

--- src/app/showcase/app.footer.component.ts
+++ src/app/showcase/app.footer.component.ts
@@ -5,7 +5,7 @@ import {Component} from '@angular/core';
     template: `
         <div class="layout-footer">
             <div class="layout-footer-left">
-                <span>PrimeNG 10.0.0-rc.3 by </span>
+                <span>PrimeNG 10.0.0-rc.4 by </span>
                 <a href="https://www.primetek.com.tr">PrimeTek</a>
             </div>
 
