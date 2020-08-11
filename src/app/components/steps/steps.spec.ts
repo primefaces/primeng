251a2c94c5d0ab019b87a5e0d2f2a678226ea74d
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2020-08-11 17:08:33 +0300

    #9116 - Unit tests should be pass for Steps

--- src/app/components/steps/steps.spec.ts
+++ src/app/components/steps/steps.spec.ts
@@ -8,172 +8,166 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { MenuItem } from 'primeng/api';
 
 @Component({
-  template: `<p-steps [model]="items"></p-steps>
+	template: `<p-steps [model]="items"></p-steps>
   `,
 
 })
 class TestStpesComponent {
-    items: MenuItem[];
+	items: MenuItem[];
 
-    activeIndex: number = 1;
+	activeIndex: number = 1;
 
-    constructor() {}
+	constructor() { }
 
-    ngOnInit() {
-        this.items = [{
-                label: 'Personal',
-                command: (event: any) => {
-                    this.activeIndex = 0;
-                }
-            },
-            {
-                label: 'Seat',
-                command: (event: any) => {
-                    this.activeIndex = 1;
-                }
-            },
-            {
-                label: 'Payment',
-                command: (event: any) => {
-                    this.activeIndex = 2;
-                }
-            },
-            {
-                label: 'Confirmation',
-                command: (event: any) => {
-                    this.activeIndex = 3;
-                }
-            }
-        ];
-    }
+	ngOnInit() {
+		this.items = [{
+			label: 'Personal',
+			command: (event: any) => {
+				this.activeIndex = 0;
+			}
+		},
+		{
+			label: 'Seat',
+			command: (event: any) => {
+				this.activeIndex = 1;
+			}
+		},
+		{
+			label: 'Payment',
+			command: (event: any) => {
+				this.activeIndex = 2;
+			}
+		},
+		{
+			label: 'Confirmation',
+			command: (event: any) => {
+				this.activeIndex = 3;
+			}
+		}
+		];
+	}
 }
 
 describe('Steps', () => {
-  
-  let steps: Steps;
-  let testComponent :TestStpesComponent;
-  let fixture: ComponentFixture<TestStpesComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      schemas: [NO_ERRORS_SCHEMA],
-      imports: [
-        NoopAnimationsModule,
-        RouterTestingModule,
-      ],
-      declarations: [
-        Steps,
-        Toast,
-        TestStpesComponent
-      ],
-    });
-      
-      fixture = TestBed.createComponent(TestStpesComponent);
-      steps = fixture.debugElement.children[0].componentInstance;
-      testComponent = fixture.debugElement.componentInstance;
-    });
+	let steps: Steps;
+	let testComponent: TestStpesComponent;
+	let fixture: ComponentFixture<TestStpesComponent>;
 
-    it('should change style & styleClass', () => {
-      steps.style = {'height' : '300px'};
-      steps.styleClass = "Primeng ROCKS!";
-      fixture.detectChanges();
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			schemas: [NO_ERRORS_SCHEMA],
+			imports: [
+				NoopAnimationsModule,
+				RouterTestingModule,
+			],
+			declarations: [
+				Steps,
+				Toast,
+				TestStpesComponent
+			],
+		});
 
-      const stepsEl = fixture.debugElement.children[0].query(By.css('div')).nativeElement;
-      expect(stepsEl.className).toContain("Primeng ROCKS!");
-      expect(stepsEl.style.height).toContain("300px");
-    });
+		fixture = TestBed.createComponent(TestStpesComponent);
+		steps = fixture.debugElement.children[0].componentInstance;
+		testComponent = fixture.debugElement.componentInstance;
+	});
 
-    it('should readonly by default', () => {
-      
-      fixture.detectChanges();
+	it('should change style & styleClass', () => {
+		steps.style = { 'height': '300px' };
+		steps.styleClass = "Primeng ROCKS!";
+		fixture.detectChanges();
 
-      const items = fixture.debugElement.children[0].queryAll(By.css('li'));
+		const stepsEl = fixture.debugElement.children[0].query(By.css('div')).nativeElement;
+		expect(stepsEl.className).toContain("Primeng ROCKS!");
+		expect(stepsEl.style.height).toContain("300px");
+	});
 
-      for(let x =0; x < testComponent.items.length; x++ ){
-        if (x==0)
-          expect(items[x].nativeElement.className).not.toContain("ui-state-disabled ui-steps-incomplete");
-        else
-          expect(items[x].nativeElement.className).toContain("ui-state-disabled ui-steps-incomplete");
-      }
-    });
+	it('should readonly by default', () => {
 
-    it('should not readonly', () => {
-      steps.readonly = false;
-      fixture.detectChanges();
+		fixture.detectChanges();
 
-      const stepsEl = fixture.debugElement.children[0].query(By.css('div')).nativeElement;
-      const items = fixture.debugElement.children[0].queryAll(By.css('li'));
-      expect(stepsEl.className).not.toContain("ui-steps-readonly");
-      for(let x =0; x < testComponent.items.length; x++ ){
-          expect(items[x].nativeElement.className).not.toContain("ui-state-disabled ui-steps-incomplete");
-      }
-    });
+		const items = fixture.debugElement.children[0].queryAll(By.css('li'));
 
-    it('should show the step number', () => {
-      fixture.detectChanges();
+		for (let x = 0; x < testComponent.items.length; x++) {
+			if (x == 0)
+				expect(items[x].nativeElement.className).not.toContain("p-disabled");
+			else
+				expect(items[x].nativeElement.className).toContain("p-disabled");
+		}
+	});
 
-      const stepsEl = fixture.debugElement.children[0].query(By.css('div')).nativeElement;
-      const items = fixture.debugElement.children[0].queryAll(By.css('li'));
-      for(let x =0; x < testComponent.items.length; x++ ){
-          expect(items[x].query(By.css('.ui-steps-number')).nativeElement.textContent).toEqual((x+1).toString());
-      }
-    });
+	it('should not readonly', () => {
+		steps.readonly = false;
+		fixture.detectChanges();
 
-    it('should activate first item', () => {
-      fixture.detectChanges();
+		const stepsEl = fixture.debugElement.children[0].query(By.css('div')).nativeElement;
+		const items = fixture.debugElement.children[0].queryAll(By.css('li'));
+		expect(stepsEl.className).not.toContain("p-steps-readonly");
+		for (let x = 0; x < testComponent.items.length; x++) {
+			expect(items[x].nativeElement.className).not.toContain("p-disabled");
+		}
+	});
 
-      const items = fixture.debugElement.children[0].queryAll(By.css('li'));
-      for(let x =0; x < testComponent.items.length; x++ ){
-        if (x==0)
-          expect(items[x].nativeElement.className).toContain("ui-state-highlight ui-steps-current");
-        else
-          expect(items[x].nativeElement.className).toContain("ui-state-default");
-      }
-    });
+	it('should show the step number', () => {
+		fixture.detectChanges();
 
-    it('should change activeItem', () => {
-      steps.activeIndex = 2;
-      fixture.detectChanges();
+		const stepsEl = fixture.debugElement.children[0].query(By.css('div')).nativeElement;
+		const items = fixture.debugElement.children[0].queryAll(By.css('li'));
+		for (let x = 0; x < testComponent.items.length; x++) {
+			expect(items[x].query(By.css('.p-steps-number')).nativeElement.textContent).toEqual((x + 1).toString());
+		}
+	});
 
-      const items = fixture.debugElement.children[0].queryAll(By.css('li'));
-      for(let x =0; x < testComponent.items.length; x++ ){
-        if (x==2)
-          expect(items[x].nativeElement.className).toContain("ui-state-highlight ui-steps-current");
-        else
-          expect(items[x].nativeElement.className).toContain("ui-state-default");
-      }
-    });
+	it('should activate first item', () => {
+		fixture.detectChanges();
 
-    it('should change activeItem when click', () => {
-      steps.readonly = false;
-      const itemClickSpy = spyOn(steps, 'itemClick').and.callThrough();
-      fixture.detectChanges();
+		const items = fixture.debugElement.children[0].queryAll(By.css('li'));
+		for (let x = 0; x < testComponent.items.length; x++) {
+			if (x == 0)
+				expect(items[x].nativeElement.className).toContain("p-highlight p-steps-current");
+		}
+	});
 
-      const items = fixture.debugElement.children[0].queryAll(By.css('li'));
-      steps.activeIndexChange.subscribe(value => steps.activeIndex = value)
-      items[2].query(By.css('a')).nativeElement.click();
-      fixture.detectChanges();
+	it('should change activeItem', () => {
+		steps.activeIndex = 2;
+		fixture.detectChanges();
 
-      expect(itemClickSpy).toHaveBeenCalled();
-      for(let x =0; x < testComponent.items.length; x++ ){
-        if (x==2)
-          expect(items[x].nativeElement.className).toContain("ui-state-highlight ui-steps-current");
-        else
-          expect(items[x].nativeElement.className).toContain("ui-state-default");
-      }
-    });
+		const items = fixture.debugElement.children[0].queryAll(By.css('li'));
+		for (let x = 0; x < testComponent.items.length; x++) {
+			if (x == 2)
+				expect(items[x].nativeElement.className).toContain("p-highlight p-steps-current");
+		}
+	});
 
-    it('should listen even emitter', () => {
-      steps.readonly = false;
-      let data =0;
-      fixture.detectChanges();
+	it('should change activeItem when click', () => {
+		steps.readonly = false;
+		const itemClickSpy = spyOn(steps, 'itemClick').and.callThrough();
+		fixture.detectChanges();
 
-      const items = fixture.debugElement.children[0].queryAll(By.css('li'));
-      steps.activeIndexChange.subscribe(value => data = value)
-      items[2].query(By.css('a')).nativeElement.click();
-      fixture.detectChanges();
+		const items = fixture.debugElement.children[0].queryAll(By.css('li'));
+		steps.activeIndexChange.subscribe(value => steps.activeIndex = value)
+		items[2].query(By.css('a')).nativeElement.click();
+		fixture.detectChanges();
 
-      expect(data).toEqual(2);
-    });
+		expect(itemClickSpy).toHaveBeenCalled();
+		for (let x = 0; x < testComponent.items.length; x++) {
+			if (x == 2)
+				expect(items[x].nativeElement.className).toContain("p-highlight p-steps-current");
+		}
+	});
+
+	it('should listen even emitter', () => {
+		steps.readonly = false;
+		let data = 0;
+		fixture.detectChanges();
+
+		const items = fixture.debugElement.children[0].queryAll(By.css('li'));
+		steps.activeIndexChange.subscribe(value => data = value)
+		items[2].query(By.css('a')).nativeElement.click();
+		fixture.detectChanges();
+
+		expect(data).toEqual(2);
+	});
 
 });
