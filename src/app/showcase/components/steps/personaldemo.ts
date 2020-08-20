16043658a659ea906e9664ee194cbe67183b2a2e
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2020-08-20 17:01:26 +0300

    Steps Demo Update

--- src/app/showcase/components/steps/personaldemo.ts
+++ src/app/showcase/components/steps/personaldemo.ts
@@ -18,18 +18,18 @@ import { Router } from '@angular/router';
                     <div class="p-fluid">
                         <div class="p-field">
                             <label for="firstname">Firstname</label>
-                            <input #firstname="ngModel" id="firstname" type="text" required pInputText [(ngModel)]="personalInformation.firstname" [ngClass]="{'p-invalid': firstname.invalid && (firstname.dirty || firstname.touched || submitted)}"> 
-                            <small *ngIf="firstname.invalid && (firstname.dirty || firstname.touched || submitted)" class="p-error">Firstname is required.</small>
+                            <input #firstname="ngModel" id="firstname" type="text" required pInputText [(ngModel)]="personalInformation.firstname" [ngClass]="{'p-invalid': (firstname.invalid && submitted) || (firstname.dirty && firstname.invalid)}"> 
+                            <small *ngIf="(firstname.invalid && submitted) || (firstname.dirty && firstname.invalid)" class="p-error">Firstname is required.</small>
                         </div>
                         <div class="p-field">
                             <label for="lastname">Lastname</label>
-                            <input #lastname="ngModel" id="lastname" type="text" required pInputText [(ngModel)]="personalInformation.lastname" [ngClass]="{'p-invalid': lastname.invalid && (lastname.dirty || lastname.touched || submitted)}"> 
-                            <small class="p-error" *ngIf="lastname.invalid && (lastname.dirty || lastname.touched || submitted)">Lastname is required.</small>
+                            <input #lastname="ngModel" id="lastname" type="text" required pInputText [(ngModel)]="personalInformation.lastname" [ngClass]="{'p-invalid': (lastname.invalid && submitted) || (lastname.dirty && lastname.invalid)}"> 
+                            <small class="p-error" *ngIf="(lastname.invalid && submitted )|| (lastname.dirty && lastname.invalid)">Lastname is required.</small>
                         </div>
                         <div class="p-field">
                             <label for="age">Age</label>
-                            <input #age="ngModel" id="age" type="number" required pInputText [(ngModel)]="personalInformation.age" [ngClass]="{'p-invalid': age.invalid && (age.dirty || age.touched || submitted)}"> 
-                            <small class="p-error" *ngIf="age.invalid && (age.dirty || age.touched || submitted)">Age is required.</small>
+                            <input #age="ngModel" id="age" type="number" required pInputText [(ngModel)]="personalInformation.age" [ngClass]="{'p-invalid': (age.invalid && submitted) || (age.dirty && age.invalid)}"> 
+                            <small class="p-error" *ngIf="(age.invalid && submitted) || (age.dirty && age.invalid)">Age is required.</small>
                         </div>
                     </div>
                 </ng-template>
