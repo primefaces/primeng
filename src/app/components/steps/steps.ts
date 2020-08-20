b8137c7e90865df6977196b8e441d0b13c486f3b
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2020-08-20 12:37:02 +0300

    Fixed #9174 - Enhance Steps RouterNavigation

--- src/app/components/steps/steps.ts
+++ src/app/components/steps/steps.ts
@@ -1,7 +1,7 @@
 import {NgModule,Component,Input,Output,EventEmitter,ChangeDetectionStrategy, ViewEncapsulation} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MenuItem} from 'primeng/api';
-import {RouterModule} from '@angular/router';
+import {RouterModule, Router, ActivatedRoute} from '@angular/router';
 
 @Component({
     selector: 'p-steps',
@@ -9,23 +9,25 @@ import {RouterModule} from '@angular/router';
         <div [ngClass]="{'p-steps p-component':true,'p-readonly':readonly}" [ngStyle]="style" [class]="styleClass">
             <ul role="tablist">
                 <li *ngFor="let item of model; let i = index" class="p-steps-item" #menuitem [ngStyle]="item.style" [class]="item.styleClass" role="tab" [attr.aria-selected]="i === activeIndex" [attr.aria-expanded]="i === activeIndex"
-                    [ngClass]="{'p-highlight p-steps-current':(i === activeIndex), 'p-disabled':(item.disabled || (readonly && (i !== activeIndex)))}">
-                    <a *ngIf="!item.routerLink" [attr.href]="item.url" class="p-menuitem-link" role="presentation" (click)="itemClick($event, item, i)" (keydown.enter)="itemClick($event, item, i)" [attr.target]="item.target" [attr.id]="item.id" 
-                        [attr.tabindex]="item.disabled||(i !== activeIndex && readonly) ? null : (item.tabindex ? item.tabindex : '0')">
-                        <span class="p-steps-number">{{i + 1}}</span>
-                        <span class="p-steps-title">{{item.label}}</span>
-                    </a>
-                    <a *ngIf="item.routerLink" [routerLink]="item.routerLink" [queryParams]="item.queryParams" role="presentation" [routerLinkActive]="'p-menuitem-link-active'" [routerLinkActiveOptions]="item.routerLinkActiveOptions||{exact:false}" class="p-menuitem-link" 
-                        (click)="itemClick($event, item, i)" (keydown.enter)="itemClick($event, item, i)" [attr.target]="item.target" [attr.id]="item.id" [attr.tabindex]="item.disabled||(i !== activeIndex && readonly) ? null : (item.tabindex ? item.tabindex : '0')"
+                    [ngClass]="{'p-highlight p-steps-current': isActive(item, i), 'p-disabled':(item.disabled || !isActive(item, index))}">
+                    <a *ngIf="!isRouterLink(item); else elseBlock" [routerLink]="item.routerLink" [queryParams]="item.queryParams" role="presentation" [routerLinkActive]="'p-menuitem-link-active'" [routerLinkActiveOptions]="item.routerLinkActiveOptions||{exact:false}" class="p-menuitem-link" 
+                        (click)="itemClick($event, item, i)" (keydown.enter)="itemClick($event, item, i)" [attr.target]="item.target" [attr.id]="item.id" [attr.tabindex]="item.disabled || readonly ? null : (item.tabindex ? item.tabindex : '0')"
                         [fragment]="item.fragment" [queryParamsHandling]="item.queryParamsHandling" [preserveFragment]="item.preserveFragment" [skipLocationChange]="item.skipLocationChange" [replaceUrl]="item.replaceUrl" [state]="item.state">
                         <span class="p-steps-number">{{i + 1}}</span>
                         <span class="p-steps-title">{{item.label}}</span>
                     </a>
+                    <ng-template #elseBlock>
+                        <a [attr.href]="item.url" class="p-menuitem-link" role="presentation" (click)="itemClick($event, item, i)" (keydown.enter)="itemClick($event, item, i)" [attr.target]="item.target" [attr.id]="item.id" 
+                            [attr.tabindex]="item.disabled||(i !== activeIndex && readonly) ? null : (item.tabindex ? item.tabindex : '0')">
+                            <span class="p-steps-number">{{i + 1}}</span>
+                            <span class="p-steps-title">{{item.label}}</span>
+                        </a>
+                    </ng-template>
                 </li>
             </ul>
         </div>
     `,
-   changeDetection: ChangeDetectionStrategy.OnPush,
+    changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None,
     styleUrls: ['./steps.css']
 })
@@ -42,6 +44,8 @@ export class Steps {
     @Input() styleClass: string;
     
     @Output() activeIndexChange: EventEmitter<any> = new EventEmitter();
+
+    constructor(private router: Router, private route:ActivatedRoute) { }
     
     itemClick(event: Event, item: MenuItem, i: number) {
         if (this.readonly || item.disabled) {
@@ -63,7 +67,29 @@ export class Steps {
             });
         }
     }
-    
+
+    isRouterLink(item) {
+        if (item.routerLink)
+            return (item.disabled || (this.readonly && !this.isRouterLinkActive(item)));
+        
+        return false;
+    }
+
+    isRouterLinkActive(item) {
+        if (item.routerLink) {
+            return this.router.isActive(item.routerLink, false) || this.router.isActive(this.router.createUrlTree([item.routerLink], {relativeTo: this.route}).toString(), false);
+        }
+        
+        return false;
+    }
+
+    isActive(item, index) {
+        if (item.routerLink) {
+            return this.isRouterLinkActive(item)
+        }
+
+        return index === this.activeIndex;
+    }
 }
 
 @NgModule({
