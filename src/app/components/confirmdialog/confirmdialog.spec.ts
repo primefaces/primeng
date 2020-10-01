248196ea445e5378f807ffa6cf03a316dd70bf4b
o.pakers <o.pakers@intrum.com>
2020-10-01 14:05:19 +0300

    [issues/5448]: Converting spaces to tabs

--- src/app/components/confirmdialog/confirmdialog.spec.ts
+++ src/app/components/confirmdialog/confirmdialog.spec.ts
@@ -38,25 +38,25 @@ describe('ConfirmDialog', () => {
   let fixture: ComponentFixture<TestConfirmDialogComponent>;
 
   beforeEach(() => {
-      TestBed.configureTestingModule({
-        schemas: [NO_ERRORS_SCHEMA],
-      imports: [
-        NoopAnimationsModule
-      ],
-      declarations: [
-        ConfirmDialog,
-        TestConfirmDialogComponent,
-      ],
-      providers:[
-        ConfirmationService
-      ]
-      });
+	  TestBed.configureTestingModule({
+		schemas: [NO_ERRORS_SCHEMA],
+	  imports: [
+		NoopAnimationsModule
+	  ],
+	  declarations: [
+		ConfirmDialog,
+		TestConfirmDialogComponent,
+	  ],
+	  providers:[
+		ConfirmationService
+	  ]
+	  });
 
-      fixture = TestBed.createComponent(TestConfirmDialogComponent);
-      confirmDialog = fixture.debugElement.query(By.css('p-confirmDialog')).componentInstance;
-    });
+	  fixture = TestBed.createComponent(TestConfirmDialogComponent);
+	  confirmDialog = fixture.debugElement.query(By.css('p-confirmDialog')).componentInstance;
+	});
 
-    it('should display the header', () => {
+	it('should display the header', () => {
 		confirmDialog.header = "PrimengRocks!";
 		confirmDialog.visible = true;
 		fixture.detectChanges();
@@ -64,40 +64,40 @@ describe('ConfirmDialog', () => {
 		const confirmDialogEl = fixture.debugElement.query(By.css('.p-dialog-title')).nativeElement;
 		expect(confirmDialogEl).toBeTruthy();
 		expect(confirmDialogEl.textContent).toContain('PrimengRocks!');
-    });
+	});
 
-    it('should display the close icon when closable is true', () => {
-        confirmDialog.visible = true;
-        fixture.detectChanges();
+	it('should display the close icon when closable is true', () => {
+		confirmDialog.visible = true;
+		fixture.detectChanges();
 
-        const closeEl = fixture.debugElement.query(By.css('.p-dialog-header-close'));
-        expect(closeEl).not.toBeNull();
-    });
+		const closeEl = fixture.debugElement.query(By.css('.p-dialog-header-close'));
+		expect(closeEl).not.toBeNull();
+	});
 
-    it('should not create the container element by default', () => {
+	it('should not create the container element by default', () => {
 		fixture.detectChanges();
 
 		expect(fixture.debugElement.query(By.css('.p-dialog'))).toBeFalsy;
 		expect(confirmDialog.visible).toEqual(undefined);
-    });
+	});
 
-    it('should add rtl class when rtl is enabled', () => {
+	it('should add rtl class when rtl is enabled', () => {
 		confirmDialog.visible = true;
 		confirmDialog.rtl = true;
 		fixture.detectChanges();
 
 		expect(fixture.debugElement.query(By.css('.p-dialog')).classes['p-dialog-rtl']).toEqual(true);
-    });
+	});
 
-    it('should not have a close icon', () => {
+	it('should not have a close icon', () => {
 		confirmDialog.visible = true;
 		confirmDialog.closable = false;
 		fixture.detectChanges();
 
 		expect(fixture.debugElement.query(By.css('a'))).toBeFalsy();
-    });
+	});
 
-    it('should change buttonStyles', () => {
+	it('should change buttonStyles', () => {
 		confirmDialog.visible = true;
 		confirmDialog.rejectButtonStyleClass = "Primeng ROCKS!";
 		confirmDialog.acceptButtonStyleClass = "Primeng ROCKS!";
@@ -105,9 +105,9 @@ describe('ConfirmDialog', () => {
 
 		expect(fixture.debugElement.query(By.css('.p-dialog-footer')).children[0].nativeElement.className).toContain("Primeng ROCKS!");
 		expect(fixture.debugElement.query(By.css('.p-dialog-footer')).children[1].nativeElement.className).toContain("Primeng ROCKS!");
-    });
+	});
 
-    it('should change icons', () => {
+	it('should change icons', () => {
 		confirmDialog.visible = true;
 		confirmDialog.icon = "Primeng ROCKS!";
 		confirmDialog.acceptIcon = "Primeng ROCKS!";
@@ -117,25 +117,25 @@ describe('ConfirmDialog', () => {
 		expect(fixture.debugElement.query(By.css('.p-confirm-dialog-icon')).nativeElement.className).toContain("Primeng ROCKS!");
 		expect(fixture.debugElement.query(By.css('.p-dialog-footer')).children[0].nativeElement.icon).toContain("Primeng ROCKS!");
 		expect(fixture.debugElement.query(By.css('.p-dialog-footer')).children[1].nativeElement.icon).toContain("Primeng ROCKS!");
-    });
+	});
 
-    it('should not show accept button', () => {
+	it('should not show accept button', () => {
 		confirmDialog.visible = true;
 		confirmDialog.acceptVisible = false;
 		fixture.detectChanges();
 
 		expect(fixture.debugElement.query(By.css('.p-dialog-footer')).children[1]).toBeFalsy();
-    });
+	});
 
-    it('should not show reject button', () => {
+	it('should not show reject button', () => {
 		confirmDialog.visible = true;
 		confirmDialog.rejectVisible = false;
 		fixture.detectChanges();
 
 		expect(fixture.debugElement.query(By.css('.p-dialog-footer')).children[1]).toBeFalsy();
-    });
+	});
 
-    it('should run accept', () => {
+	it('should run accept', () => {
 		const confirmEl = fixture.debugElement.query(By.css('button')).nativeElement;
 		confirmEl.click();
 		fixture.detectChanges();
@@ -143,9 +143,9 @@ describe('ConfirmDialog', () => {
 		const acceptButtonEl = fixture.debugElement.query(By.css('.p-dialog-footer')).children[0].nativeElement;
 		acceptButtonEl.click();
 		expect(fixture.componentInstance.header).toEqual("accept");
-    });
+	});
 
-    it('should run reject', () => {
+	it('should run reject', () => {
 		const confirmEl = fixture.debugElement.query(By.css('button')).nativeElement;
 		confirmEl.click();
 		fixture.detectChanges();
@@ -188,67 +188,67 @@ describe('ConfirmDialog', () => {
 
 
 @Component({
-    template: `<p-confirmDialog key="confirmDialogTemplates">
-    <p-header>
-        <b>PrimengRocks!</b>
-    </p-header>
-    <ng-template pTemplate="contentTemplate">
-        <span class="p-confirm-dialog-message">Are you really sure?</span>
-    </ng-template>
-    </p-confirmDialog>
+	template: `<p-confirmDialog key="confirmDialogTemplates">
+	<p-header>
+		<b>PrimengRocks!</b>
+	</p-header>
+	<ng-template pTemplate="contentTemplate">
+		<span class="p-confirm-dialog-message">Are you really sure?</span>
+	</ng-template>
+	</p-confirmDialog>
 
   <button type="button" (click)="confirmTemplates()" pButton icon="pi pi-check" label="Confirm"></button>`
 })
 class TestConfirmDialogTemplateComponent {
 
-    constructor(private confirmationService: ConfirmationService) {}
+	constructor(private confirmationService: ConfirmationService) {}
 
-    confirmTemplates() {
-        this.confirmationService.confirm({});
-    }
+	confirmTemplates() {
+		this.confirmationService.confirm({});
+	}
 
 }
 
 describe('ConfirmDialogWithTemplates', () => {
 
-    let confirmDialog: ConfirmDialog;
-    let fixture: ComponentFixture<TestConfirmDialogTemplateComponent>;
+	let confirmDialog: ConfirmDialog;
+	let fixture: ComponentFixture<TestConfirmDialogTemplateComponent>;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            schemas: [NO_ERRORS_SCHEMA],
-            imports: [
-                NoopAnimationsModule,
-                SharedModule
-            ],
-            declarations: [
-                ConfirmDialog,
-                TestConfirmDialogTemplateComponent
-            ],
-            providers:[
-                ConfirmationService
-            ]
-        });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			schemas: [NO_ERRORS_SCHEMA],
+			imports: [
+				NoopAnimationsModule,
+				SharedModule
+			],
+			declarations: [
+				ConfirmDialog,
+				TestConfirmDialogTemplateComponent
+			],
+			providers:[
+				ConfirmationService
+			]
+		});
 
-        fixture = TestBed.createComponent(TestConfirmDialogTemplateComponent);
-        confirmDialog = fixture.debugElement.query(By.css('p-confirmDialog')).componentInstance;
-    });
+		fixture = TestBed.createComponent(TestConfirmDialogTemplateComponent);
+		confirmDialog = fixture.debugElement.query(By.css('p-confirmDialog')).componentInstance;
+	});
 
-    it('should display the header template', () => {
-        confirmDialog.visible = true;
-        fixture.detectChanges();
+	it('should display the header template', () => {
+		confirmDialog.visible = true;
+		fixture.detectChanges();
 
-        const confirmDialogEl = fixture.debugElement.query(By.css('.p-dialog-header')).nativeElement;
-        expect(confirmDialogEl).toBeTruthy();
-        expect(confirmDialogEl.innerHTML).toContain('PrimengRocks!');
-    });
+		const confirmDialogEl = fixture.debugElement.query(By.css('.p-dialog-header')).nativeElement;
+		expect(confirmDialogEl).toBeTruthy();
+		expect(confirmDialogEl.innerHTML).toContain('PrimengRocks!');
+	});
 
-    it('should display the content template', () => {
-        confirmDialog.visible = true;
-        fixture.detectChanges();
+	it('should display the content template', () => {
+		confirmDialog.visible = true;
+		fixture.detectChanges();
 
-        const confirmDialogEl = fixture.debugElement.query(By.css('.p-dialog-content')).nativeElement;
-        expect(confirmDialogEl).toBeTruthy();
-        expect(confirmDialogEl.innerHTML).toContain('Are you really sure?');
-    });
+		const confirmDialogEl = fixture.debugElement.query(By.css('.p-dialog-content')).nativeElement;
+		expect(confirmDialogEl).toBeTruthy();
+		expect(confirmDialogEl.innerHTML).toContain('Are you really sure?');
+	});
 });
