1b69091075ed8574078eb90d5b124628be57ed4f
mertsincan <sincan.mert@gmail.com>
2020-10-08 00:31:57 +0300

    Fixed #9372 - Problems when typing or pasting numbers into InputNumber

--- src/app/components/inputnumber/inputnumber.ts
+++ src/app/components/inputnumber/inputnumber.ts
@@ -116,6 +116,10 @@ export class InputNumber implements OnInit,ControlValueAccessor {
 
     groupChar: string = '';
 
+    prefixChar: string = '';
+
+    suffixChar: string = '';
+
     isSpecialChar: boolean;
 
     timer: any;
@@ -279,8 +283,8 @@ export class InputNumber implements OnInit,ControlValueAccessor {
         this._group = this.getGroupingExpression();
         this._minusSign = this.getMinusSignExpression();
         this._currency = this.getCurrencyExpression();
-        this._suffix = new RegExp(`${this.escapeRegExp(this.suffix||'')}`, 'g');
-        this._prefix = new RegExp(`${this.escapeRegExp(this.prefix||'')}`, 'g');
+        this._suffix = this.getSuffixExpression();
+        this._prefix = this.getPrefixExpression();
         this._index = d => index.get(d);
     }
 
@@ -317,6 +321,31 @@ export class InputNumber implements OnInit,ControlValueAccessor {
         }
 
         return new RegExp(`[]`,'g');
+    }
+
+    getPrefixExpression() {
+        if (this.prefix) {
+            this.prefixChar = this.prefix;
+        }
+        else {
+            const formatter = new Intl.NumberFormat(this.locale, {style: this.mode, currency: this.currency, currencyDisplay: this.currencyDisplay});
+            this.prefixChar = formatter.format(1).split('1')[0];
+        }
+
+        return new RegExp(`${this.escapeRegExp(this.prefixChar||'')}`, 'g');
+    }
+
+    getSuffixExpression() {
+        if (this.suffix) {
+            this.suffixChar = this.suffix;
+        }
+        else {
+            const formatter = new Intl.NumberFormat(this.locale, {style: this.mode, currency: this.currency, currencyDisplay: this.currencyDisplay,
+                minimumFractionDigits: 0, maximumFractionDigits: 0});
+            this.suffixChar = formatter.format(1).split('1')[1];
+        }
+
+        return new RegExp(`${this.escapeRegExp(this.suffixChar||'')}`, 'g');
     }
 
     formatValue(value) {
@@ -646,16 +675,16 @@ export class InputNumber implements OnInit,ControlValueAccessor {
         }
         else {
             const maxFractionDigits = this.numberFormat.resolvedOptions().maximumFractionDigits;
+            const operation = selectionStart !== selectionEnd ? 'range-insert' : 'insert';
 
             if (decimalCharIndex > 0 && selectionStart > decimalCharIndex) {
                 if ((selectionStart + text.length - (decimalCharIndex + 1)) <= maxFractionDigits) {
                     newValueStr = inputValue.slice(0, selectionStart) + text + inputValue.slice(selectionStart + text.length);
-                    this.updateValue(event, newValueStr, text, 'insert');
+                    this.updateValue(event, newValueStr, text, operation);
                 }
             }
             else {
                 newValueStr = this.insertText(inputValue, text, selectionStart, selectionEnd);
-                const operation = selectionStart !== selectionEnd ? 'range-insert' : 'insert';
                 this.updateValue(event, newValueStr, text, operation);
             }
         }
@@ -667,7 +696,7 @@ export class InputNumber implements OnInit,ControlValueAccessor {
         if (textSplit.length == 2) {
             const decimalCharIndex = value.slice(start, end).search(this._decimal);
             this._decimal.lastIndex = 0;
-            return (decimalCharIndex > 0) ? value.slice(0, start) + this.formatValue(text) + value.slice(end) : value;
+            return (decimalCharIndex > 0) ? value.slice(0, start) + this.formatValue(text) + value.slice(end) : (value || this.formatValue(text));
         }
         else if ((end - start) === value.length) {
             return this.formatValue(text);
@@ -820,7 +849,7 @@ export class InputNumber implements OnInit,ControlValueAccessor {
             this.input.nativeElement.value = newValue;
             this.input.nativeElement.setSelectionRange(0, 0);
             this.initCursor();
-            const prefixLength = (this.prefix || '').length;
+            const prefixLength = (this.prefixChar || '').length;
             const selectionEnd = prefixLength + insertedValueStr.length;
             this.input.nativeElement.setSelectionRange(selectionEnd, selectionEnd);
         }
