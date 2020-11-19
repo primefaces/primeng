54e5323de723b425712233762508fd0d99e66025
Cagatay Civici <cagatay.civici@gmail.com>
2020-11-19 12:24:40 +0300

    Formatting for values

--- src/app/components/api/primengconfig.ts
+++ src/app/components/api/primengconfig.ts
@@ -62,8 +62,8 @@ export class PrimeNGConfig {
         dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
         dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
         dayNamesMin: ["Su","Mo","Tu","We","Th","Fr","Sa"],
-        monthNames: [ "January","February","March","April","May","June","July","August","September","October","November","December" ],
-        monthNamesShort: [ "Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ],
+        monthNames: ["January","February","March","April","May","June","July","August","September","October","November","December"],
+        monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
         today: 'Today',
         weekHeader: 'Wk'
     }
