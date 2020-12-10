4194f67e579111572c9a96222abd53877d01ac36
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2020-12-10 15:18:47 +0300

    Doc fix

--- src/app/showcase/components/badge/badgedemo.ts
+++ src/app/showcase/components/badge/badgedemo.ts
@@ -4,20 +4,4 @@ import {Component} from '@angular/core';
     templateUrl: './badgedemo.html'
 })
 export class BadgeDemo {
-
-    value = "2";
-
-    ngOnInit() {
-        setTimeout(() => {
-            this.value = "24+";
-            this.hey()
-        },2000)
-    }
-
-    hey() {
-        setTimeout(() => {
-            this.value = null;
-        }, 2000);
-    }
-
 }
