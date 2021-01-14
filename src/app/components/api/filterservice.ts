631a9298e93aa748a79d7341417d8ae7bf7bc78b
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2021-01-14 17:21:20 +0300

    Fixed #9765 - [bug report] Filter function 'between' thow an exception when the second parameter value is null.

--- src/app/components/api/filterservice.ts
+++ src/app/components/api/filterservice.ts
@@ -129,7 +129,7 @@ export class FilterService {
         },
 
         between: (value, filter: any[]):boolean => {
-            if (filter == null || filter[0] == null || filter[0] == null) {
+            if (filter == null || filter[0] == null || filter[1] == null) {
                 return true;
             }
     
