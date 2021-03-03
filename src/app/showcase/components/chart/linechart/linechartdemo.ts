eae676d30fa39d739f286054a37c7d0fe51adb89
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2021-03-03 11:59:36 +0300

    #9961 for Line Chart

--- src/app/showcase/components/chart/linechart/linechartdemo.ts
+++ src/app/showcase/components/chart/linechart/linechartdemo.ts
@@ -8,11 +8,17 @@ import {AppConfig} from '../../../domain/appconfig';
     templateUrl: './linechartdemo.html',
     providers: [MessageService]
 })
-export class LineChartDemo {
+export class LineChartDemo implements OnInit {
 
-    data: any;
+    basicData: any;
 
-    chartOptions: any;
+    multiAxisData: any;
+
+    multiAxisOptions: any;
+
+    lineStylesData: any;
+
+    basicOptions: any;
     
     subscription: Subscription;
 
@@ -21,23 +27,88 @@ export class LineChartDemo {
     constructor(private messageService: MessageService, private configService: AppConfigService) {}
 
     ngOnInit() {
-        this.data = {
+        this.basicData = {
             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             datasets: [
                 {
                     label: 'First Dataset',
                     data: [65, 59, 80, 81, 56, 55, 40],
                     fill: false,
-                    borderColor: '#4bc0c0'
+                    borderColor: '#42A5F5'
                 },
                 {
                     label: 'Second Dataset',
                     data: [28, 48, 40, 19, 86, 27, 90],
                     fill: false,
-                    borderColor: '#565656'
+                    borderColor: '#FFA726'
                 }
             ]
         }
+
+        this.multiAxisData = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'Dataset 1',
+                fill: false,
+                borderColor: '#42A5F5',
+                yAxisID: 'y-axis-1',
+                data: [65, 59, 80, 81, 56, 55, 10]
+            }, {
+                label: 'Dataset 2',
+                fill: false,
+                borderColor: '#00bb7e',
+                yAxisID: 'y-axis-2',
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }]
+        };
+
+        this.multiAxisOptions = {
+            responsive: true,
+            hoverMode: 'index',
+            stacked: false,
+            scales: {
+                yAxes: [{
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                    id: 'y-axis-1',
+                }, {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    id: 'y-axis-2',
+                    gridLines: {
+                        drawOnChartArea: false
+                    }
+                }]
+            }
+        };
+
+        this.lineStylesData = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'First Dataset',
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    fill: false,
+                    borderColor: '#42A5F5'
+                },
+                {
+                    label: 'Second Dataset',
+                    data: [28, 48, 40, 19, 86, 27, 90],
+                    fill: false,
+                    borderDash: [5, 5],
+                    borderColor: '#66BB6A'
+                },
+                {
+                    label: 'Third Dataset',
+                    data: [12, 51, 62, 33, 21, 62, 45],
+                    fill: true,
+                    borderColor: '#FFA726',
+                    backgroundColor: 'rgba(255,167,38,0.2)'
+                }
+            ]
+        };
         
         this.config = this.configService.config;
         this.updateChartOptions();
@@ -48,33 +119,64 @@ export class LineChartDemo {
     }
 
     updateChartOptions() {
-        this.chartOptions = this.config && this.config.dark ? this.getDarkTheme() : this.getLightTheme();
+        if (this.config.dark) 
+            this.applyDarkTheme();
+        else
+            this.applyLightTheme();
     }
 
-    getLightTheme() {
-        return {
+    applyLightTheme() {
+        this.basicOptions = {
             legend: {
-                    labels: {
+                labels: {
+                    fontColor: '#495057'
+                }
+            },
+            scales: {
+                xAxes: [{
+                    ticks: {
                         fontColor: '#495057'
                     }
+                }],
+                yAxes: [{
+                    ticks: {
+                        fontColor: '#495057'
+                    }
+                }]
+            }
+        };
+
+        this.multiAxisOptions.scales.xAxes = [{
+                ticks: {
+                    fontColor: '#495057'
                 },
-                scales: {
-                    xAxes: [{
-                        ticks: {
-                            fontColor: '#495057'
-                        }
-                    }],
-                    yAxes: [{
-                        ticks: {
-                            fontColor: '#495057'
-                        }
-                    }]
+                gridLines: {
+                    color: '#ebedef'
                 }
-        }
+            }
+        ];
+        this.multiAxisOptions.scales.yAxes[0].ticks = {
+            fontColor: '#495057'
+        };
+        this.multiAxisOptions.scales.yAxes[0].gridLines = {
+            color: '#ebedef'
+        };
+        this.multiAxisOptions.scales.yAxes[1].ticks = {
+            fontColor: '#495057'
+        };
+        this.multiAxisOptions.scales.yAxes[1].gridLines = {
+            color: '#ebedef'
+        };
+        this.multiAxisOptions.legend = {
+            labels:  {
+                fontColor: '#495057'
+            }
+        };
+        this.multiAxisOptions = {...this.multiAxisOptions};
     }
 
-    getDarkTheme() {
-        return {
+    applyDarkTheme() {
+        this.basicOptions = {
             legend: {
                 labels: {
                     fontColor: '#ebedef'
@@ -98,10 +200,34 @@ export class LineChartDemo {
                     }
                 }]
             }
-        }
-    }
+        };
 
-    selectData(event) {
-        this.messageService.add({severity: 'info', summary: 'Data Selected', 'detail': this.data.datasets[event.element._datasetIndex].data[event.element._index]});
+        this.multiAxisOptions.scales.xAxes = [{
+                ticks: {
+                    fontColor: '#ebedef'
+                },
+                gridLines: {
+                    color: 'rgba(255,255,255,0.2)'
+                }
+            }
+        ];
+        this.multiAxisOptions.scales.yAxes[0].ticks = {
+            fontColor: '#ebedef'
+        };
+        this.multiAxisOptions.scales.yAxes[0].gridLines = {
+            color: 'rgba(255,255,255,0.2)'
+        };
+        this.multiAxisOptions.scales.yAxes[1].ticks = {
+            fontColor: '#ebedef'
+        };
+        this.multiAxisOptions.scales.yAxes[1].gridLines = {
+            color: 'rgba(255,255,255,0.2)'
+        };
+        this.multiAxisOptions.legend = {
+            labels:  {
+                fontColor: '#ebedef'
+            }
+        };
+        this.multiAxisOptions = {...this.multiAxisOptions};
     }
 }
