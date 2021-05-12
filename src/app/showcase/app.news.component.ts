c1740be50f48a90e000073f9641c43b367b0afb2
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2021-05-12 16:20:58 +0300

    News updated

--- src/app/showcase/app.news.component.ts
+++ src/app/showcase/app.news.component.ts
@@ -4,7 +4,7 @@ import {Component, Input, EventEmitter, Output} from '@angular/core';
     selector: 'app-news',
     template: `
         <div class="layout-news">
-            <div class="layout-news-container">
+            <div class="layout-news-container" (click)="redirect()">
                 <img class="layouts-news-mockup-image" src="assets/showcase/images/topbar-atlantis-device.png">
                 <a href="https://www.primefaces.org/layouts/atlantis-ng" target="_blank"  style="text-decoration: none" class="layout-news-button">
                     LEARN MORE<i class="pi pi-angle-right"></i>
@@ -25,6 +25,10 @@ export class AppNewsComponent {
     hideNews(event: Event) {
         this.onNewsHide.emit();
         event.preventDefault();
+    }
+
+    redirect() {
+        window.location.href = "https://www.primefaces.org/layouts/atlantis-ng";
     }
     
 }
