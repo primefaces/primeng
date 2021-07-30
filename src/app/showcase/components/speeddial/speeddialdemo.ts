2f116362b849e248931c4042c08a59632f1632c4
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2021-07-30 11:03:16 +0300

    SpeedDial demo update

--- src/app/showcase/components/speeddial/speeddialdemo.ts
+++ src/app/showcase/components/speeddial/speeddialdemo.ts
@@ -49,33 +49,43 @@ export class SpeedDialDemo implements OnInit {
 
         this.tooltipItems = [
             {
-                tooltip: 'Add',
+                tooltipOptions: {
+                    tooltipLabel: "Add"
+                },
                 icon: 'pi pi-pencil',
                 command: () => {
                     this.messageService.add({ severity: 'info', summary: 'Add', detail: 'Data Added' });
                 }
             },
             {
-                tooltip: 'Update',
+                tooltipOptions: {
+                    tooltipLabel: "Update"
+                },
                 icon: 'pi pi-refresh',
                 command: () => {
                     this.messageService.add({ severity: 'success', summary: 'Update', detail: 'Data Updated' });
                 }
             },
             {
-                tooltip: 'Delete',
+                tooltipOptions: {
+                    tooltipLabel: "Delete"
+                },
                 icon: 'pi pi-trash',
                 command: () => {
                     this.messageService.add({ severity: 'error', summary: 'Delete', detail: 'Data Deleted' });
                 }
             },
             {
-                tooltip: 'Upload',
+                tooltipOptions: {
+                    tooltipLabel: "Upload"
+                },
                 icon: 'pi pi-upload',
                 routerLink: ['/fileupload']
             },
             {
-                tooltip: 'Angular Website',
+                tooltipOptions: {
+                    tooltipLabel: "Angular Website"
+                },
                 icon: 'pi pi-external-link',
                 url: 'http://angular.io'
             }
@@ -83,39 +93,49 @@ export class SpeedDialDemo implements OnInit {
 
         this.leftTooltipItems = [
             {
-                tooltip: 'Add',
+                tooltipOptions: {
+                    tooltipLabel: "Add",
+                    tooltipPosition: "left",
+                },
                 icon: 'pi pi-pencil',
-                tooltipPosition: 'left',
                 command: () => {
                     this.messageService.add({ severity: 'info', summary: 'Add', detail: 'Data Added' });
                 }
             },
             {
-                tooltip: 'Update',
+                tooltipOptions: {
+                    tooltipLabel: "Update",
+                    tooltipPosition: "left",
+                },
                 icon: 'pi pi-refresh',
-                tooltipPosition: 'left',
                 command: () => {
                     this.messageService.add({ severity: 'success', summary: 'Update', detail: 'Data Updated' });
                 }
             },
             {
-                tooltip: 'Delete',
+                tooltipOptions: {
+                    tooltipLabel: "Delete",
+                    tooltipPosition: "left",
+                },
                 icon: 'pi pi-trash',
-                tooltipPosition: 'left',
                 command: () => {
                     this.messageService.add({ severity: 'error', summary: 'Delete', detail: 'Data Deleted' });
                 }
             },
             {
-                tooltip: 'Upload',
                 icon: 'pi pi-upload',
-                tooltipPosition: 'left',
+                tooltipOptions: {
+                    tooltipLabel: "Upload",
+                    tooltipPosition: "left",
+                },
                 routerLink: ['/fileupload']
             },
             {
-                tooltip: 'Angular Website',
+                tooltipOptions: {
+                    tooltipLabel: "Angular Website",
+                    tooltipPosition: "left",
+                },
                 icon: 'pi pi-external-link',
-                tooltipPosition: 'left',
                 url: 'http://angular.io'
             }
         ];
