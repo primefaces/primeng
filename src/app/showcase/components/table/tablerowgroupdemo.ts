8c24e9ef972d54844275f3cd3c8a737d247ee69d
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2021-08-03 11:01:26 +0300

    Table doc update

--- src/app/showcase/components/table/tablerowgroupdemo.ts
+++ src/app/showcase/components/table/tablerowgroupdemo.ts
@@ -10,42 +10,13 @@ export class TableRowGroupDemo implements OnInit {
 
     customers: Customer[];
 
-    rowGroupMetadata: any;
 
     constructor(private customerService: CustomerService) { }
 
     ngOnInit() {
         this.customerService.getCustomersMedium().then(data => {
             this.customers = data;
-            this.updateRowGroupMetaData();
         });
-    }
-
-    onSort() {
-        this.updateRowGroupMetaData();
-    }
-
-    updateRowGroupMetaData() {
-        this.rowGroupMetadata = {};
-
-        if (this.customers) {
-            for (let i = 0; i < this.customers.length; i++) {
-                let rowData = this.customers[i];
-                let representativeName = rowData.representative.name;
-
-                if (i == 0) {
-                    this.rowGroupMetadata[representativeName] = { index: 0, size: 1 };
-                }
-                else {
-                    let previousRowData = this.customers[i - 1];
-                    let previousRowGroup = previousRowData.representative.name;
-                    if (representativeName === previousRowGroup)
-                        this.rowGroupMetadata[representativeName].size++;
-                    else
-                        this.rowGroupMetadata[representativeName] = { index: i, size: 1 };
-                }
-            }
-        }
     }
 
     calculateCustomerTotal(name) {
