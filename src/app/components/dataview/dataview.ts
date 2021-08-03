24f3d53d5c5fd77dfcef75c248e6a6956fb7eae0
djay1977 <j.devos@nsi-sa.be>
2021-08-03 15:40:52 +0200

    fix: css class for PrimeFlex 3
    PrimeNG's DataView component is not compatible with PrimeFlex 3
    The pTemplate gridItem generates a p-grid class and p-nogutter

--- src/app/components/dataview/dataview.ts
+++ src/app/components/dataview/dataview.ts
@@ -24,7 +24,7 @@ import { Subscription } from 'rxjs';
                 [dropdownAppendTo]="paginatorDropdownAppendTo" [dropdownScrollHeight]="paginatorDropdownScrollHeight" [templateLeft]="paginatorLeftTemplate" [templateRight]="paginatorRightTemplate"
                 [currentPageReportTemplate]="currentPageReportTemplate" [showFirstLastIcon]="showFirstLastIcon" [dropdownItemTemplate]="paginatorDropdownItemTemplate" [showCurrentPageReport]="showCurrentPageReport" [showJumpToPageDropdown]="showJumpToPageDropdown" [showPageLinks]="showPageLinks"></p-paginator>
             <div class="p-dataview-content">
-                <div class="p-grid p-nogutter">
+                <div class="grid grid-nogutter">
                     <ng-template ngFor let-rowData let-rowIndex="index" [ngForOf]="paginator ? ((filteredValue||value) | slice:(lazy ? 0 : first):((lazy ? 0 : first) + rows)) : (filteredValue||value)" [ngForTrackBy]="trackBy">
                         <ng-container *ngTemplateOutlet="itemTemplate; context: {$implicit: rowData, rowIndex: rowIndex}"></ng-container>
                     </ng-template>
