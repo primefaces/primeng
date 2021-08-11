6af52c3f3def150491def6923de5070f44d30de1
Cagatay Civici <cagatay.civici@gmail.com>
2021-08-11 17:13:30 +0300

    Set version

--- src/app/showcase/app.footer.component.ts
+++ src/app/showcase/app.footer.component.ts
@@ -5,7 +5,7 @@ import {Component} from '@angular/core';
     template: `
         <div class="layout-footer">
             <div class="layout-footer-left">
-                <span>PrimeNG 12.0.1-SNAPSHOT by </span>
+                <span>PrimeNG 12.1.0-rc1 by </span>
                 <a href="https://www.primetek.com.tr">PrimeTek</a>
             </div>
 
