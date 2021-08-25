b6dacf65663704d0a06aa58895295307d37fbd0a
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2021-08-25 15:05:29 +0300

    Set version

--- src/app/showcase/app.footer.component.ts
+++ src/app/showcase/app.footer.component.ts
@@ -5,7 +5,7 @@ import {Component} from '@angular/core';
     template: `
         <div class="layout-footer">
             <div class="layout-footer-left">
-                <span>PrimeNG 12.1.0-rc.3-SNAPSHOT by </span>
+                <span>PrimeNG 12.1.0 by </span>
                 <a href="https://www.primetek.com.tr">PrimeTek</a>
             </div>
 
