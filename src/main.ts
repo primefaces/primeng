9eca709aa73030ba6b0786dbe970605d3109601c
tomer-regev <69435535+tomer-regev@users.noreply.github.com>
2021-09-01 13:19:05 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/showcase/app.module';
 import { environment } from './environments/environment';
+
 
 if (environment.production) {
   enableProdMode();
