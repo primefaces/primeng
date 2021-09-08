67808748e771bb92ed4fa1285f8821b36200ff28
mertsincan <sincan.mert@gmail.com>
2021-09-08 14:00:52 +0300

    Remove readonly tests from inputnumber.spec

--- src/app/components/inputnumber/inputnumber.spec.ts
+++ src/app/components/inputnumber/inputnumber.spec.ts
@@ -42,38 +42,4 @@ describe('InputNumber', () => {
         const inputMaskEl = fixture.debugElement.query(By.css('input'));
         expect(inputMaskEl.nativeElement).toBeTruthy();
     });
-
-    it('should not update when readonly', () => {
-        fixture.detectChanges();
-
-        const updateModelSpy = spyOn(inputNumber, "updateModel").and.callThrough();
-        const inputNumberEl = fixture.debugElement.query(By.css("input"));
-        const event: any = document.createEvent('CustomEvent');
-        event.which = 13;
-        event.initEvent('keydown', true, true);
-        inputNumberEl.nativeElement.dispatchEvent(event as KeyboardEvent);
-        event.initEvent('input', true, true);
-        inputNumberEl.nativeElement.dispatchEvent(event as KeyboardEvent);
-        event.initEvent('keypress', true, true);
-        inputNumberEl.nativeElement.dispatchEvent(event as KeyboardEvent);
-        inputNumberEl.nativeElement.dispatchEvent(new Event("focus"));
-        fixture.detectChanges();
-
-        expect(document.activeElement).not.toEqual(inputNumber.input.nativeElement);
-        expect(updateModelSpy).not.toHaveBeenCalled();
-    });
-
-    it('should not display buttons when readonly', () => {
-        inputNumber.showButtons = true;
-        fixture.detectChanges();
-        
-        const inputNumberButtonsElArrayReadonly = fixture.debugElement.queryAll(By.css("button"));
-        expect(inputNumberButtonsElArrayReadonly).toHaveSize(0);
-
-        testComponent.readonly = false;
-        fixture.detectChanges();
-        
-        const inputNumberButtonsElArray = fixture.debugElement.queryAll(By.css("button"));
-        expect(inputNumberButtonsElArray).toHaveSize(2);
-    })
 });
