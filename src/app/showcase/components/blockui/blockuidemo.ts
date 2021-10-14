7f6a53c62b5a0aa2530624eecc989674fcde2a8d
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2021-10-14 12:46:09 +0300

    Doc update

--- src/app/showcase/components/blockui/blockuidemo.ts
+++ src/app/showcase/components/blockui/blockuidemo.ts
@@ -6,14 +6,40 @@ import {Component,OnInit,EventEmitter} from '@angular/core';
         :host ::ng-deep button {
             margin-right: .25em;
         }
+
+        :host ::ng-deep .p-component-overlay-enter .pi.pi-lock {
+            animation: enter 150ms forwards;
+        }
+
+        :host ::ng-deep .p-component-overlay-leave .pi.pi-lock {
+            animation: leave 150ms forwards;
+        }
+
+        @keyframes enter {
+            from {
+                color: transparent;
+            }
+            to {
+                color: var(--text-color);
+            }
+        }
+
+        @keyframes leave {
+            from {
+                color: var(--text-color);
+            }
+            to {
+                color: transparent;
+            }
+        }
     `]
 })
 export class BlockUIDemo {
 
     blockedPanel: boolean = false;
-    
+
     blockedDocument: boolean = false;
-        
+
     blockDocument() {
         this.blockedDocument = true;
         setTimeout(() => {
