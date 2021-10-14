cc4ed6459dcb2eca6fb9efa3c74bc959232831e4
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2021-10-14 19:23:29 +0300

    New dev iteration

--- src/app/showcase/app.footer.component.ts
+++ src/app/showcase/app.footer.component.ts
@@ -5,7 +5,7 @@ import {Component} from '@angular/core';
     template: `
         <div class="layout-footer">
             <div class="layout-footer-left">
-                <span>PrimeNG 12.2.1 by </span>
+                <span>PrimeNG 12.2.2-SNAPSHOT by </span>
                 <a href="https://www.primetek.com.tr">PrimeTek</a>
             </div>
 
