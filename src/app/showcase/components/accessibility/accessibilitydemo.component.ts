85e917d5335c21cdde2d5492fa5cabfd6a35a04d
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-04-21 11:32:05 +0300

    refactor

--- src/app/showcase/components/accessibility/accessibilitydemo.component.ts
+++ src/app/showcase/components/accessibility/accessibilitydemo.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'accessibility',
-  templateUrl: './accessibilitydemo.component.html',
+    selector: 'accessibility',
+    templateUrl: './accessibilitydemo.component.html',
 })
 export class AccessibilityDemoComponent {}
