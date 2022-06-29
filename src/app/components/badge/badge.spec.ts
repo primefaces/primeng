65a3a1484a033e9662f12bad004dbceecefc8e44
yigitfindikli <yigitfindikli@gmail.com>
2022-06-29 15:48:56 +0300

    Unit test fixes

--- src/app/components/badge/badge.spec.ts
+++ src/app/components/badge/badge.spec.ts
@@ -1,23 +1,29 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { Badge, BadgeModule } from './badge';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
 
 describe('Badge', () => {
 
-	let button: Badge;
+	let badge: Badge;
 	let fixture: ComponentFixture<Badge>;
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
 			imports: [
-				NoopAnimationsModule
-			],
-			declarations: [
+				NoopAnimationsModule,
 				BadgeModule
 			]
 		});
 
 		fixture = TestBed.createComponent(Badge);
-		button = fixture.componentInstance;
+		badge = fixture.componentInstance;
+	});
+
+	it('should display by default', () => {
+		fixture.detectChanges();
+
+		const badgeEl = fixture.debugElement.query(By.css('.p-badge'));
+		expect(badgeEl.nativeElement).toBeTruthy();
 	});
 });
