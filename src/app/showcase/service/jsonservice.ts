253d4f8180ddebd9c0dc2d14e1279435912398b2
Cagatay Civici <cagatay.civici@gmail.com>
2022-06-30 17:25:18 +0300

    Add no cache

--- src/app/showcase/service/jsonservice.ts
+++ src/app/showcase/service/jsonservice.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -7,14 +7,14 @@ export class JsonService {
     constructor(private http: HttpClient) { }
 
     getVersions() {
-        return this.http.get<any>('https://www.primefaces.org/primeng/versions.json')
+        return this.http.get<any>('https://www.primefaces.org/primeng/versions.json', {headers:new HttpHeaders({'cache': 'no-store'})})
         .toPromise()
         .then(res => res.versions)
         .then(data => { return data; });
     }
 
     getAnnouncement() {
-        return this.http.get<any>('https://www.primefaces.org/cdn/news/primeng.json')
+        return this.http.get<any>('https://www.primefaces.org/cdn/news/primeng.json', {headers:new HttpHeaders({'cache': 'no-store'})})
         .toPromise()
         .then(data => { return data; });
     }
