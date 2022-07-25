2b140b959049dcbaee2da1f28f39d8a664068cfc
Cagatay Civici <cagatay.civici@gmail.com>
2022-07-25 22:47:19 +0300

    Add support for dark theme in ui kit page

--- src/app/showcase/components/uikit/uikit.component.ts
+++ src/app/showcase/components/uikit/uikit.component.ts
@@ -1,9 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AppConfigService } from '../../service/appconfigservice';
 
 @Component({
     templateUrl: './uikit.component.html',
     styleUrls: ['uikit.component.scss']
 })
-export class UIKitComponent {
+export class UIKitComponent implements OnInit, OnDestroy {
 
+    subscription: Subscription;
+
+    colorScheme: string = 'light';
+
+    constructor(private configService: AppConfigService) {}
+
+    ngOnInit() {
+        this.subscription = this.configService.configUpdate$.subscribe(config => {
+            this.colorScheme = config.dark ? 'dark' : 'light';
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+    }
 }
