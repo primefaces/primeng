b4ecba410e9501337d855d781b0a24802bfa2c58
mertsincan <sincan.mert@gmail.com>
2022-11-07 01:58:21 +0000

    Fixed #11987 - VirtualScroller triggers onLazyLoad event even though there is no scroll event.

--- src/app/components/scroller/scroller.ts
+++ src/app/components/scroller/scroller.ts
@@ -468,7 +468,7 @@ export class Scroller implements OnInit, AfterContentInit, AfterViewChecked, OnD
         }
 
         if (this.initialized) {
-            const isChanged = !isLoadingChanged && (simpleChanges.items || simpleChanges.itemSize || simpleChanges.scrollHeight || simpleChanges.scrollWidth);
+            const isChanged = !isLoadingChanged && (simpleChanges.items?.previousValue?.length !== simpleChanges.items?.currentValue?.length || simpleChanges.itemSize || simpleChanges.scrollHeight || simpleChanges.scrollWidth);
             isChanged && this.init();
         }
     }
