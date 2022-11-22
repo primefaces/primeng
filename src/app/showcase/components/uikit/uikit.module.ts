43a334525741ed3323217a55644a423769451708
gucal <gucalaliriza@outlook.com>
2022-11-22 16:29:50 +0300

    UI Kit pricing update

--- src/app/showcase/components/uikit/uikit.module.ts
+++ src/app/showcase/components/uikit/uikit.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UIKitComponent } from './uikit.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { UIKitService } from '../../service/uikitservice';
 import { UIKitRoutingModule } from './uikit-routing.module';
+import { UIKitComponent } from './uikit.component';
 
 @NgModule({
-    imports: [CommonModule, UIKitRoutingModule],
+    imports: [CommonModule, UIKitRoutingModule, HttpClientModule],
+    providers: [UIKitService],
     declarations: [UIKitComponent]
 })
 export class UIKitModule {}
