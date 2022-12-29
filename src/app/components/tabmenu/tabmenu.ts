e0be9f599c72d80352de4080bbe3c6daf50f62d5
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-12-29 17:23:16 +0300

    Format fix

--- src/app/components/tabmenu/tabmenu.ts
+++ src/app/components/tabmenu/tabmenu.ts
@@ -185,8 +185,7 @@ export class TabMenu implements AfterContentInit, AfterViewInit, AfterViewChecke
         if (item.routerLink) {
             const routerLink = Array.isArray(item.routerLink) ? item.routerLink : [item.routerLink];
 
-            return this.router.isActive(this.router.createUrlTree(routerLink, {relativeTo: this.route}).toString(),
-                item.routerLinkActiveOptions?.exact ?? item.routerLinkActiveOptions ?? false);
+            return this.router.isActive(this.router.createUrlTree(routerLink, { relativeTo: this.route }).toString(), item.routerLinkActiveOptions?.exact ?? item.routerLinkActiveOptions ?? false);
         }
 
         return item === this.activeItem;
