e40e735db499d0953c109f074c531703ef72dee8
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-03-17 18:37:48 +0300

    add title and meta service

--- src/app/showcase/layout/doc/app.doc.component.ts
+++ src/app/showcase/layout/doc/app.doc.component.ts
@@ -1,12 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { Doc } from '../../domain/doc';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-doc',
     templateUrl: './app.doc.component.html'
 })
-export class AppDoc {
+export class AppDoc implements OnInit, OnChanges {
+    @Input() title!: string;
+
     @Input() docs!: Doc[];
 
     @Input() header!: string;
@@ -17,13 +20,23 @@ export class AppDoc {
 
     activeTab!: number;
 
-    constructor(private router: Router) {}
+    constructor(private router: Router, private titleService: Title, private metaService: Meta) {}
 
     ngOnInit() {
         if (this.router.url.includes('#api')) {
             this.activeTab = 1;
         } else {
             this.activeTab = 0;
+        }
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes.title && changes.title.currentValue){
+            this.titleService.setTitle(changes.title.currentValue);
+        }
+
+        if (changes.description && changes.description.currentValue){
+            this.metaService.updateTag({ name: 'description', content: changes.description.currentValue });
         }
     }
 
