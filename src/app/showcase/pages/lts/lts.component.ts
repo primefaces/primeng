6051558ccb8dc785e5e690b38fcda96a0885d7c2
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-03-19 12:53:57 +0300

    add lts doc

--- src/app/showcase/pages/lts/lts.component.ts
+++ src/app/showcase/pages/lts/lts.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
-
+import { ChangelogDoc } from '../../doc/lts/changelogdoc';
+import { ConfigDoc } from '../../doc/lts/configdoc';
+import { DownloadDoc } from '../../doc/lts/downloaddoc';
+import { FaqDoc } from '../../doc/lts/faqdoc';
+import { IntroDoc } from '../../doc/lts/introdoc';
+import { LicenceDoc } from '../../doc/lts/licencedoc';
 @Component({
     templateUrl: './lts.component.html'
 })
@@ -9,4 +14,37 @@ export class LTSComponent {
         this.titleService.setTitle('Long Term Support - PrimeNG');
         this.metaService.updateTag({ name: 'description', content: 'Long Term Support' });
     }
+
+    docs = [
+        {
+            id: 'what-is-lts',
+            label: 'What is LTS?',
+            component: IntroDoc
+        },
+        {
+            id: 'licence-terms',
+            label: 'License Terms',
+            component: LicenceDoc
+        },
+        {
+            id: 'changelog',
+            label: 'Changelog',
+            component: ChangelogDoc
+        },
+        {
+            id: 'download',
+            label: 'Download',
+            component: DownloadDoc
+        },
+        {
+            id: 'configuration',
+            label: 'Configuration',
+            component: ConfigDoc
+        },
+        {
+            id: 'faq',
+            label: 'FAQ',
+            component: FaqDoc
+        }
+    ];
 }
--- src/app/showcase/pages/lts/lts.module.ts
+++ src/app/showcase/pages/lts/lts.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { AccordionModule } from 'primeng/accordion';
-import { AppCodeModule } from 'src/app/showcase/layout/doc/code/app.code.component';
+import { LtsDocModule } from '../../doc/lts/ltsdoc.module';
 import { LTSRoutingModule } from './lts-routing.module';
 import { LTSComponent } from './lts.component';
 
 @NgModule({
-    imports: [CommonModule, LTSRoutingModule, AccordionModule, AppCodeModule],
+    imports: [CommonModule, LTSRoutingModule, LtsDocModule],
     declarations: [LTSComponent]
 })
 export class LTSModule {}
