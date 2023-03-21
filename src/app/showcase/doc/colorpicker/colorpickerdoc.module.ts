bf8d5f134be04e3518bed6d23d8a5cc9c1e033a5
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-03-21 16:19:00 +0300

    add reactive form example

--- src/app/showcase/doc/colorpicker/colorpickerdoc.module.ts
+++ src/app/showcase/doc/colorpicker/colorpickerdoc.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppDocModule } from '../../layout/doc/app.doc.module';
 import { AppCodeModule } from '../../layout/doc/code/app.code.component';
 import { ColorPickerModule } from 'primeng/colorpicker';
@@ -14,10 +14,11 @@ import { PropsDoc } from './propsdoc';
 import { StyleDoc } from './styledoc';
 import { EventsDoc } from './eventsdoc';
 import { AccessibilityDoc } from './accessibilitydoc';
+import { ReactiveFormsDoc } from './reactiveformsdoc';
 
 @NgModule({
-    imports: [CommonModule, RouterModule, AppCodeModule, AppDocModule, FormsModule, ColorPickerModule],
+    imports: [CommonModule, RouterModule, AppCodeModule, AppDocModule, FormsModule, ReactiveFormsModule ,ColorPickerModule],
     exports: [AppDocModule],
-    declarations: [ImportDoc, BasicDoc, InlineDoc, FormatDoc, DisabledDoc, PropsDoc, StyleDoc, EventsDoc, AccessibilityDoc]
+    declarations: [ImportDoc, BasicDoc, InlineDoc, FormatDoc, DisabledDoc, PropsDoc, StyleDoc, EventsDoc, AccessibilityDoc, ReactiveFormsDoc]
 })
 export class ColorPickerDocModule {}
