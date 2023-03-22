c5756803e1803ac5ce0cafd3b20d7584dda2edd6
Cagatay Civici <cagatay.civici@gmail.com>
2023-03-22 19:00:05 +0300

    Created news

--- src/app/showcase/layout/app.module.ts
+++ src/app/showcase/layout/app.module.ts
@@ -23,12 +23,12 @@ import { AppMainComponent } from './app.main.component';
 import { AppConfigModule } from './config/app.config.module';
 import { AppFooterComponent } from './footer/app.footer.component';
 import { AppMenuModule } from './menu/app.menu.module';
-import { AppNewsComponent } from './news/app.news.component';
+import { AppNewsModule } from './news/app.news.module';
 import { AppTopbarModule } from './topbar/app.topbar.module';
 
 @NgModule({
-    declarations: [AppComponent, AppNewsComponent, AppFooterComponent, AppMainComponent],
-    imports: [FormsModule, ReactiveFormsModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, LandingModule, AppConfigModule, AppTopbarModule, AppMenuModule],
+    declarations: [AppComponent, AppFooterComponent, AppMainComponent],
+    imports: [FormsModule, ReactiveFormsModule, AppRoutingModule, AppNewsModule, HttpClientModule, BrowserAnimationsModule, LandingModule, AppConfigModule, AppTopbarModule, AppMenuModule],
     providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }, CarService, CountryService, EventService, NodeService, IconService, CustomerService, PhotoService, AppConfigService, ProductService],
     bootstrap: [AppComponent]
 })
