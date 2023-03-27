3febb29b73861c08e279577c66e2b2d0cb65c382
gucal <gucalaliriza@outlook.com>
2023-03-27 14:50:04 +0300

    Redirect locale docs

--- src/app/showcase/doc/tristatecheckbox/accessibilitydoc.ts
+++ src/app/showcase/doc/tristatecheckbox/accessibilitydoc.ts
@@ -10,7 +10,7 @@ import { Code } from '../../domain/code';
                 <p>
                     TriStateCheckbox component uses an element with <i>checkbox</i> role. Value to describe the component can either be provided with <i>aria-labelledby</i> or <i>aria-label</i> props. Component adds an element with
                     <i>aria-live</i> attribute that is only visible to screen readers to read the value displayed. Values to read are defined with the <i>trueLabel</i>, <i>falseLabel</i> and <i>nullLabel</i> keys of the <i>aria</i> property from the
-                    <a href="/locale">locale</a> API. This is an example of a custom accessibility implementation as there is no one to one mapping between the component design and the WCAG specification.
+                    <a href="/configuration/#locale">locale</a> API. This is an example of a custom accessibility implementation as there is no one to one mapping between the component design and the WCAG specification.
                 </p>
             </app-docsectiontext>
 
