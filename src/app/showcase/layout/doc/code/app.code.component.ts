785a93849a587939d734b3018dac3c6792e388fd
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-04-03 14:25:12 +0300

    code format

--- src/app/showcase/layout/doc/code/app.code.component.ts
+++ src/app/showcase/layout/doc/code/app.code.component.ts
@@ -33,14 +33,8 @@ export class AppCodeComponent {
     lang!: string;
 
     ngAfterViewChecked() {
-        if (
-            typeof window !== undefined && 
-            window['Prism'] && 
-            this.codeElement && 
-            !this.codeElement.nativeElement.classList.contains('prism')
-        ) {
+        if (typeof window !== undefined && window['Prism'] && this.codeElement && !this.codeElement.nativeElement.classList.contains('prism')) {
             window['Prism'].highlightElement(this.codeElement.nativeElement);
-            // Add prism class so that change events do not trigger re-highlight (#12846)
             this.codeElement.nativeElement.classList.add('prism');
         }
     }
