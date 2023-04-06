f7ad7102f0af18319e61129566594d40c5fe5213
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-04-06 14:45:58 +0300

    Fix error

--- src/app/components/scrolltop/scrolltop.ts
+++ src/app/components/scrolltop/scrolltop.ts
@@ -20,9 +20,11 @@ import { ChevronUpIcon } from 'primeng/icon/chevronup';
             [ngStyle]="style"
             type="button"
         >
-            <span *ngIf="!iconTemplate" [class]="icon" [ngClass]="'p-scrolltop-icon'"></span>
-            <ChevronUpIcon [ngIf]="!icon" [ngClass]="'p-scrolltop-icon'" [ngStyle]="{'font-size': '1rem', 'scale': '1.5'}"/>
-            <ng-container [ngIf]="!icon" *ngTemplateOutlet="iconTemplate; context: { styleClass: 'p-scrolltop-icon'}"></ng-container>
+        <ng-container *ngIf="!iconTemplate">
+            <span *ngIf="icon" [class]="icon" [ngClass]="'p-scrolltop-icon'"></span>
+            <ChevronUpIcon *ngIf="!icon" [ngClass]="'p-scrolltop-icon'" [ngStyle]="{'font-size': '1rem', 'scale': '1.5'}"/>
+        </ng-container>
+        <ng-template [ngIf]="!icon" *ngTemplateOutlet="iconTemplate; context: { styleClass: 'p-scrolltop-icon'}"></ng-template>
         </button>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush,
