7d38b41c48876b990c027a3ca167f4d65e3dd3f3
SubhajitD <subhajitdebnath.999@gmail.com>
2023-04-08 12:34:19 +0530

    datepicker PrevButton code indentation updated

--- src/app/components/calendar/calendar.ts
+++ src/app/components/calendar/calendar.ts
@@ -112,7 +112,14 @@ export type CalendarTypeView = 'date' | 'month' | 'year';
                     <div class="p-datepicker-group-container">
                         <div class="p-datepicker-group" *ngFor="let month of months; let i = index">
                             <div class="p-datepicker-header">
-                                <button (keydown)="onContainerButtonKeydown($event)" class="p-datepicker-prev p-link" (click)="onPrevButtonClick($event)" *ngIf="i === 0" type="button" pRipple>
+                                <button
+                                    (keydown)="onContainerButtonKeydown($event)"
+                                    class="p-datepicker-prev p-link"
+                                    (click)="onPrevButtonClick($event)"
+                                    *ngIf="i === 0"
+                                    type="button"
+                                    pRipple
+                                >
                                     <span class="p-datepicker-prev-icon pi pi-chevron-left"></span>
                                 </button>
                                 <div class="p-datepicker-title">
