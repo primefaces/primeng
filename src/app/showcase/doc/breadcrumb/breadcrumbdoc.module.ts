6552d2e09764f7600f0d3be74562aadc3364d23a
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-04-10 12:17:39 +0300

    add templates doc

--- src/app/showcase/doc/breadcrumb/breadcrumbdoc.module.ts
+++ src/app/showcase/doc/breadcrumb/breadcrumbdoc.module.ts
@@ -11,10 +11,11 @@ import { ImportDoc } from './importdoc';
 import { MenuItemDoc } from './menuitemdoc';
 import { PropsDoc } from './propsdoc';
 import { AccessibilityDoc } from './accessibilitydoc';
+import { TemplatesDoc } from './templatesdoc';
 
 @NgModule({
     imports: [CommonModule, AppCodeModule, RouterModule, BreadcrumbModule, AppDocModule],
-    declarations: [BasicDoc, EventsDoc, ImportDoc, MenuItemDoc, PropsDoc, EventsDoc, StyleDoc, AccessibilityDoc],
+    declarations: [BasicDoc, EventsDoc, ImportDoc, MenuItemDoc, PropsDoc, EventsDoc, StyleDoc, AccessibilityDoc, TemplatesDoc],
     exports: [AppDocModule]
 })
 export class BreadcrumbDocModule {}
