97bbb553564b91c53e8b8ddbc00c38de1ba48158
Cagatay Civici <cagatay.civici@gmail.com>
2023-04-19 14:05:46 +0300

    Fixed deployment

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import 'zone.js/node';
 
 import { APP_BASE_HREF } from '@angular/common';
 import { ngExpressEngine } from '@nguniversal/express-engine';
-import * as express from 'express';
+import express from 'express';
 import { existsSync } from 'fs';
 import { join } from 'path';
 
