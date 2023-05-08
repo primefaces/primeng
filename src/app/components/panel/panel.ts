7b4c13ea4b550ab26d582d6e0e19ef4e2b94673a
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-05-08 15:18:32 +0300

    refactor panel

--- src/app/components/panel/panel.ts
+++ src/app/components/panel/panel.ts
@@ -111,45 +111,56 @@ export class Panel implements AfterContentInit, BlockableUI {
     /**
      * Defines if content of panel can be expanded and collapsed.
      * @defaultValue false
+     * @group Props
      */
     @Input() toggleable: boolean | undefined;
     /**
      * Header text of the panel.
+     * @group Props
      */
     @Input() header: string | undefined;
     /**
      * Defines the initial state of panel content, supports one or two-way binding as well.
+     * @group Props
      * @defaultValue false
      */
     @Input() collapsed: boolean | undefined;
     /**
      * Inline style of the component.
+     * @group Props
      */
     @Input() style: { [klass: string]: any } | null | undefined;
     /**
      * Style class of the component.
+     * @group Props
      */
     @Input() styleClass: string | undefined;
     /**
      * Position of the icons.
+     * @group Props
      */
     @Input() iconPos: 'start' | 'end' | 'center' = 'end';
     /**
      * Expand icon of the toggle button.
      * @deprecated since v15.4.2, use `headericons` template instead.
+     * @group Props
      */
     @Input() expandIcon: string | undefined;
     /**
      * Collapse icon of the toggle button.
      * @deprecated since v15.4.2, use `headericons` template instead.
+     * @group Props
      */
     @Input() collapseIcon: string | undefined;
     /**
      * Specifies if header of panel cannot be displayed.
+     * @deprecated since v15.4.2, use `headericons` template instead.
+     * @group Props
      */
     @Input() showHeader: boolean = true;
     /**
      * Specifies the toggler element to toggle the panel content.
+     * @group Props
      */
     @Input() toggler: 'icon' | 'header' = 'icon';
     /**
@@ -157,6 +168,11 @@ export class Panel implements AfterContentInit, BlockableUI {
      * @param {boolean} value - New value.
      * @group Emits
      */
+    /**
+     * Transition options of the animation.
+     * @group Props
+     */
+    @Input() transitionOptions: string = '400ms cubic-bezier(0.86, 0, 0.07, 1)';
     @Output() collapsedChange: EventEmitter<boolean> = new EventEmitter<boolean>();
     /**
      * Callback to invoke before panel toggle.
@@ -170,33 +186,29 @@ export class Panel implements AfterContentInit, BlockableUI {
      * @group Emits
      */
     @Output() onAfterToggle: EventEmitter<PanelToggleEvent> = new EventEmitter<PanelToggleEvent>();
-    /**
-     * Transition options of the animation.
-     */
-    @Input() transitionOptions: string = '400ms cubic-bezier(0.86, 0, 0.07, 1)';
 
-    @ContentChild(Footer) footerFacet: TemplateRef<any> | null;
+    @ContentChild(Footer) footerFacet: TemplateRef<any> | undefined;
 
-    @ContentChildren(PrimeTemplate) templates: QueryList<PrimeTemplate>;
+    @ContentChildren(PrimeTemplate) templates?: QueryList<PrimeTemplate>;
 
-    public iconTemplate: TemplateRef<any>;
+    public iconTemplate: TemplateRef<any> | undefined;
 
-    animating: boolean;
+    animating?: boolean;
 
-    headerTemplate: TemplateRef<any>;
+    headerTemplate: TemplateRef<any> | undefined;
 
-    contentTemplate: TemplateRef<any>;
+    contentTemplate: TemplateRef<any> | undefined;
 
-    footerTemplate: TemplateRef<any>;
+    footerTemplate: TemplateRef<any> | undefined;
 
-    headerIconTemplate: TemplateRef<any>;
+    headerIconTemplate: TemplateRef<any> | undefined;
 
     id: string = `p-panel-${idx++}`;
 
     constructor(private el: ElementRef) {}
 
     ngAfterContentInit() {
-        this.templates.forEach((item) => {
+        (this.templates as QueryList<PrimeTemplate>).forEach((item) => {
             switch (item.getType()) {
                 case 'header':
                     this.headerTemplate = item.template;
