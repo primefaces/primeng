1095fd1ae5fc68b28fb586b6121c1ff1b639fe87
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-05-11 23:36:32 +0300

    ColorPicker | Add types

---
+++ src/app/components/colorpicker/colorpicker.interface.ts
@@ -0,0 +1,14 @@
+/**
+ * Custom change event.
+ * @see {@link ColorPicker.onChange}
+ */
+export interface ColorPickerChangeEvent {
+    /**
+     * Browser event
+     */
+    originalEvent: Event;
+    /**
+     * Selected color value.
+     */
+    value: string | object;
+}
