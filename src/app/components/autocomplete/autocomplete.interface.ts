650d558d8bf671498a294786fb68ac4d992b86d4
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-05-11 15:15:27 +0300

    Autocomplete | Add interface.ts

---
+++ src/app/components/autocomplete/autocomplete.interface.ts
@@ -0,0 +1,84 @@
+import { TemplateRef } from '@angular/core';
+
+/**
+ * Custom complete event.
+ * @see {@link AutoComplete.completeMethod}
+ * @event
+ */
+export interface AutoCompleteCompleteEvent {
+    /**
+     * Browser event.
+     */
+    originalEvent: Event;
+    /**
+     * Selected option value.
+     */
+    query: string;
+}
+/**
+ * Custom complete event.
+ * @see {@link AutoComplete.completeMethod}
+ * @event
+ */
+export interface AutoCompleteDropdownClickEvent {
+    /**
+     * Browser event.
+     */
+    originalEvent: Event;
+    /**
+     * Selected option value.
+     */
+    query: string;
+}
+/**
+ * Defines valid templates in AutoComplete.
+ * @group Templates
+ */
+export interface AutoCompleteTemplates {
+    /**
+     * Custom item template.
+     */
+    item: TemplateRef<any>;
+    /**
+     * Custom group template.
+     * @param {Object} implicit - option group.
+     */
+    group: TemplateRef<any>;
+    /**
+     * Custom selected item template.
+     * @param {Object} implicit - selected item.
+     */
+    selectedItem: TemplateRef<any>;
+    /**
+     * Custom header template.
+     */
+    header: TemplateRef<any>;
+    /**
+     * Custom empty template.
+     */
+    empty: TemplateRef<any>;
+    /**
+     * Custom footer template.
+     */
+    footer: TemplateRef<any>;
+    /**
+     * Custom loader template.
+     */
+    loader: TemplateRef<any>;
+    /**
+     * Custom remove token icon template.
+     */
+    removetokenicon: TemplateRef<any>;
+    /**
+     * Custom loading icon template.
+     */
+    loadingicon: TemplateRef<any>;
+    /**
+     * Custom clear icon template.
+     */
+    clearicon: TemplateRef<any>;
+    /**
+     * Custom dropdown icon template.
+     */
+    dropdownicon: TemplateRef<any>;
+}
