8bb1eb6462c0f19998e2bb5cc5345af22a0beab7
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-05-11 17:11:23 +0300

    Chips | Add types

---
+++ src/app/components/chips/chips.interface.ts
@@ -0,0 +1,52 @@
+import { TemplateRef } from "@angular/core";
+
+/**
+ * Custom add event.
+ * @see {@link Chips.onAdd}
+ * @event
+ */
+export interface ChipsAddEvent {
+    /**
+     * Browser event.
+     */
+    originalEvent: Event;
+    /**
+     * Added/Removed item value.
+     */
+    value: any;
+}
+/**
+ * Custom remove event.
+ * @see {@link Chips.onRemove}
+ * @event
+ */
+export interface ChipsRemoveEvent extends ChipsAddEvent {}
+/**
+ * Custom click event.
+ * @see {@link Chips.onChipClick}
+ * @event
+ */
+export interface ChipsClickEvent extends ChipsAddEvent {}
+/**
+ * Defines valid templates in Chips.
+ * @group Templates
+ */
+export interface ChipsTemplates {
+    /**
+     * Custom item template.
+     */
+    item(scope: {
+        /**
+         * Value of the chip element.
+         */
+        $implicit: any
+    }): TemplateRef<any> | null;
+    /**
+     * Custom remove icon template.
+     */
+    removetokenicon: TemplateRef<any> | null;
+    /**
+     * Custom clear icon template.
+     */
+    clearicon: TemplateRef<any> | null;
+}
