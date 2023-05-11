8e9e798113fec00cba443108426fa9da9a52080b
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-05-12 01:11:49 +0300

    Editor | Add types

---
+++ src/app/components/editor/editor.interface.ts
@@ -0,0 +1,64 @@
+//@ts-ignore
+import { TemplateRef } from '@angular/core';
+import Quill from 'quill';
+
+/**
+ * Custom text change event.
+ * @see {@link Editor.onTextChange}
+ */
+export interface EditorTextChangeEvent {
+    /**
+     * Current value as html.
+     */
+    htmlValue: string;
+    /**
+     * Current value as text.
+     */
+    textValue: string;
+    /**
+     * Representation of the change.
+     */
+    delta: string;
+    /**
+     * Source of change. Will be either 'user' or 'api'.
+     */
+    source: string;
+}
+/**
+ * Custom selection change event.
+ * @see {@link Editor.onSelectionChange}
+ */
+export interface EditorSelectionChangeEvent {
+    /**
+     * Representation of the selection boundaries.
+     */
+    range: string;
+    /**
+     * Representation of the previous selection boundaries.
+     */
+    oldRange: string;
+    /**
+     * Source of change. Will be either 'user' or 'api'.
+     */
+    source: string;
+}
+/**
+ * Custom load event.
+ * @see {@link EditorEmits.onInit}
+ */
+export interface EditorInitEvent {
+    /**
+     * Text editor instance.
+     */
+    editor: Quill;
+}
+/**
+ * Defines valid templates in Editor.
+ * @group Templates
+ */
+export interface EditorTemplates {
+    /**
+     * Custom header template.
+     */
+    header: TemplateRef<any>;
+}
