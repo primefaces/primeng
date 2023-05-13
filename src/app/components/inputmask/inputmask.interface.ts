3192ea2836ce617aef8b8d0b1bda6c9961c46991
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-05-13 16:35:47 +0300

    InputMask | Add types

---
+++ src/app/components/inputmask/inputmask.interface.ts
@@ -0,0 +1,13 @@
+import { TemplateRef } from "@angular/core";
+
+export type Caret = { begin: number; end: number };
+/**
+ * Defines valid templates in InputMask.
+ * @group Templates
+ */
+export interface InputMaskTemplates {
+    /**
+     * Custom clear icon template.
+     */
+    clearicon: TemplateRef<any> | null;
+}
