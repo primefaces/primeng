52e6cced176eddf6a180fef38be225c8fc1275f8
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-05-18 12:45:45 +0300

    TreeTable | add missing import

--- src/app/components/treetable/treetable.interface.ts
+++ src/app/components/treetable/treetable.interface.ts
@@ -1,5 +1,6 @@
 import { TemplateRef } from '@angular/core';
 import { FilterMetadata, LazyLoadMeta, SortMeta, TreeNode } from 'primeng/api';
+import { TreeTable } from './treetable';
 /**
  * Paginator state.
  */
