7923ce4ad7d11b7b76b23865c86f68696100880d
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-05-18 12:26:45 +0300

    Rating | Add missing interface

--- src/app/components/rating/rating.interface.ts
+++ src/app/components/rating/rating.interface.ts
@@ -1,3 +1,5 @@
+import { TemplateRef } from '@angular/core';
+import { Rating } from './rating';
 /**
  * Custom change event.
  * @see {@link Rating.onRate}
@@ -11,4 +13,22 @@ export interface RatingRateEvent {
      * Selected option value
      */
     value: number;
+}
+/**
+ * Defines valid templates in Rating.
+ * @group Templates
+ */
+export interface RatingTemplates {
+    /**
+     * Custom on icon template.
+     */
+    onicon: TemplateRef<any>;
+    /**
+     * Custom off icon template.
+     */
+    officon: TemplateRef<any>;
+    /**
+     * Custom cancel icon template.
+     */
+    cancelicon: TemplateRef<any>;
 }
