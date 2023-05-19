28f8061b10bd16eff1aa81a130bab5b6c0288196
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-05-19 12:04:44 +0300

    move ts-helpers

--- src/app/components/ts-helpers.ts
+++
@@ -1,4 +0,0 @@
-export declare type Booleanish = boolean | 'true' | 'false';
-export declare type Numberish = number | string;
-export declare type Nullable<T = void> = T | null | undefined;
-export declare type VoidListener = VoidFunction | null | undefined;
