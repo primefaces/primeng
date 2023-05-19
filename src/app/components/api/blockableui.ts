4142c806e06b2ba77e76282f548673c83f0221c6
gucal <gucalaliriza@outlook.com>
2023-05-19 12:04:50 +0300

    Add description for api interfaces

--- src/app/components/api/blockableui.ts
+++ src/app/components/api/blockableui.ts
@@ -1,3 +1,10 @@
+/**
+ * Represents a blockable user interface element.
+ */
 export interface BlockableUI {
+    /**
+     * Retrieves the blockable element associated with the UI.
+     * @returns The HTML element that can be blocked.
+     */
     getBlockableElement(): HTMLElement;
 }
--- src/app/components/api/filtermetadata.ts
+++ src/app/components/api/filtermetadata.ts
@@ -1,5 +1,19 @@
+/**
+ * Represents metadata for filtering a data set.
+ */
 export interface FilterMetadata {
+    /**
+     * The value used for filtering.
+     */
     value?: any;
+
+    /**
+     * The match mode for filtering.
+     */
     matchMode?: string;
+
+    /**
+     * The operator for filtering.
+     */
     operator?: string;
 }
