e3810028ab2ddba417e712815fc234492c8d2a5a
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-05-22 10:47:30 +0300

    Fix wrong icon

--- src/app/showcase/doc/panel/toggleabledoc.ts
+++ src/app/showcase/doc/panel/toggleabledoc.ts
@@ -12,9 +12,6 @@ import { Code } from '../../domain/code';
         </app-docsectiontext>
         <div class="card flex justify-content-center">
             <p-panel header="Header" [toggleable]="true">
-                <ng-template pTemplate="headericons">
-                    <i class="pi pi-user"></i>
-                </ng-template>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
