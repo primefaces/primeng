28e1d3ae1f5ed00bb5e7a96cda3d5926a72897fa
gucal <gucalaliriza@outlook.com>
2023-06-02 15:37:15 +0300

    Update type for Speeddial demos

--- src/app/showcase/doc/speeddial/quartercircledoc.ts
+++ src/app/showcase/doc/speeddial/quartercircledoc.ts
@@ -26,7 +26,7 @@ export class QuarterCircleDoc implements OnInit {
 
     @Input() title: string;
 
-    items: MenuItem[];
+    items: MenuItem[] | undefined;
 
     constructor(private messageService: MessageService) {}
 
@@ -91,7 +91,7 @@ import { MenuItem, MessageService } from 'primeng/api';
     providers: [MessageService]
 })
 export class SpeedDialQuarterCircleDemo implements OnInit {
-    items: MenuItem[];
+    items: MenuItem[] | undefined;
 
     constructor(private messageService: MessageService) {}
 
