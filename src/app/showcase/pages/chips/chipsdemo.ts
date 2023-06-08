58936350cece3a4f8c387ae992f262b2ccde4b76
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-06-08 16:33:18 +0300

    Remove static apidocs

--- src/app/showcase/pages/chips/chipsdemo.ts
+++ src/app/showcase/pages/chips/chipsdemo.ts
@@ -5,10 +5,8 @@ import { CommaSeperatorDoc } from '../../doc/chips/commaseperator.doc';
 import { RegexpSeperatorDoc } from '../../doc/chips/regexpseperator.doc';
 import { TemplateDoc } from '../../doc/chips/templatedoc';
 import { StyleDoc } from '../../doc/chips/styledoc';
-import { PropsDoc } from '../../doc/chips/propsdoc';
 import { AccessibilityDoc } from '../../doc/chips/accessibilitydoc';
 import { ReactiveFormsDoc } from '../../doc/chips/reactiveformsdoc';
-import { TemplatesDoc } from '../../doc/chips/templatesdoc';
 
 @Component({
     templateUrl: './chipsdemo.html'
@@ -54,19 +52,6 @@ export class ChipsDemo {
             id: 'accessibility',
             label: 'Accessibility',
             component: AccessibilityDoc
-        }
-    ];
-
-    apiDocs = [
-        {
-            id: 'properties',
-            label: 'Properties',
-            component: PropsDoc
-        },
-        {
-            id: 'templates',
-            label: 'Templates',
-            component: TemplatesDoc
         }
     ];
 }
