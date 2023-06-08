2140b4a28abef52c00a37a2e2b53f4579de350cb
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-06-08 16:48:49 +0300

    Remove static apidocs

--- src/app/showcase/doc/inputnumber/inputnumberdoc.module.ts
+++ src/app/showcase/doc/inputnumber/inputnumberdoc.module.ts
@@ -9,23 +9,19 @@ import { AccessibilityDoc } from './accessibilitydoc';
 import { ButtonsDoc } from './buttonsdoc';
 import { CurrencyDoc } from './currencydoc';
 import { DisabledDoc } from './disableddoc';
-import { EventsDoc } from './eventsdoc';
 import { FloatlabelDoc } from './floatlabeldoc';
 import { ImportDoc } from './importdoc';
 import { InvalidDoc } from './invaliddoc';
 import { LocaleDoc } from './localedoc';
-import { MethodsDoc } from './methodsdoc';
 import { NumeralsDoc } from './numeralsdoc';
 import { PrefixSuffixDoc } from './prefixsuffixdoc';
-import { PropsDoc } from './propsdoc';
 import { ReactiveFormsDoc } from './reactiveformsdoc';
 import { StyleDoc } from './styledoc';
 import { VerticalDoc } from './verticaldoc';
-import { TemplatesDoc } from './templatesdoc';
 
 @NgModule({
     imports: [CommonModule, FormsModule, ReactiveFormsModule, InputNumberModule, RouterModule, AppCodeModule, AppDocModule],
     exports: [AppDocModule],
-    declarations: [ImportDoc, NumeralsDoc, LocaleDoc, CurrencyDoc, PrefixSuffixDoc, ButtonsDoc, VerticalDoc, FloatlabelDoc, InvalidDoc, DisabledDoc, PropsDoc, MethodsDoc, EventsDoc, StyleDoc, AccessibilityDoc, ReactiveFormsDoc, TemplatesDoc]
+    declarations: [ImportDoc, NumeralsDoc, LocaleDoc, CurrencyDoc, PrefixSuffixDoc, ButtonsDoc, VerticalDoc, FloatlabelDoc, InvalidDoc, DisabledDoc, StyleDoc, AccessibilityDoc, ReactiveFormsDoc]
 })
 export class InputNumberDocModule {}
