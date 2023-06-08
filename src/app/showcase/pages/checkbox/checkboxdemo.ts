da52d5b6db8e56ca2339b9042dd987e6900d22e6
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-06-08 16:31:48 +0300

    Remove static apidocs

--- src/app/showcase/pages/checkbox/checkboxdemo.ts
+++ src/app/showcase/pages/checkbox/checkboxdemo.ts
@@ -6,10 +6,8 @@ import { MultipleDoc } from '../../doc/checkbox/multipledoc';
 import { DynamicDoc } from '../../doc/checkbox/dynamicdoc';
 import { DisabledDoc } from '../../doc/checkbox/disableddoc';
 import { StyleDoc } from '../../doc/checkbox/styledoc';
-import { PropsDoc } from '../../doc/checkbox/propsdoc';
 import { AccessibilityDoc } from '../../doc/checkbox/accessibilitydoc';
 import { ReactiveFormsDoc } from '../../doc/checkbox/reactiveformsdoc';
-import { TemplatesDoc } from '../../doc/checkbox/templatesdoc';
 
 @Component({
     templateUrl: './checkboxdemo.html'
@@ -60,19 +58,6 @@ export class CheckboxDemo {
             id: 'accessibility',
             label: 'Accessibility',
             component: AccessibilityDoc
-        }
-    ];
-
-    apiDocs = [
-        {
-            id: 'properties',
-            label: 'Properties',
-            component: PropsDoc
-        },
-        {
-            id: 'templates',
-            label: 'Templates',
-            component: TemplatesDoc
         }
     ];
 }
