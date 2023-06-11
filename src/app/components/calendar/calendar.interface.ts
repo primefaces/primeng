a25307058b30ca693659cdacea0b79ef8cf6007c
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-06-11 23:06:19 +0300

    add descriptions

--- src/app/components/calendar/calendar.interface.ts
+++ src/app/components/calendar/calendar.interface.ts
@@ -68,15 +68,45 @@ export interface CalendarTemplates {
  * @group Interface
  */
 export interface LocaleSettings {
+    /**
+     * Day value.
+     */
     firstDayOfWeek?: number;
+    /**
+     * Day names.
+     */
     dayNames?: string[];
+    /**
+     * Shortened day names.
+     */
     dayNamesShort?: string[];
+    /**
+     * Minimum days names.
+     */
     dayNamesMin?: string[];
+    /**
+     * Month names.
+     */
     monthNames?: string[];
+    /**
+     * Shortened month names.
+     */
     monthNamesShort?: string[];
+    /**
+     * Value of today date string.
+     */
     today?: string;
+    /**
+     * Clear.
+     */
     clear?: string;
+    /**
+     * Date format.
+     */
     dateFormat?: string;
+    /**
+     * Week header.
+     */
     weekHeader?: string;
 }
 /**
@@ -84,9 +114,21 @@ export interface LocaleSettings {
  * @group Interface
  */
 export interface Month {
+    /**
+     * Mont value.
+     */
     month?: number;
+    /**
+     * Year value.
+     */
     year?: number;
+    /**
+     * Array of dates.
+     */
     dates?: Date[];
+    /**
+     * Array of week numbers.
+     */
     weekNumbers?: number[];
 }
 /**
