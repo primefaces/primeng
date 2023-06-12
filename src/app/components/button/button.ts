a48358823bba8635270b84d81cd64edb7b0c7beb
Egor Volvachev <axe.onegaimy@gmail.com>
2023-06-12 08:25:48 +0300

    fix(primeng/p-button): spinner style not working propely
    Fixes #13148.

--- src/app/components/button/button.ts
+++ src/app/components/button/button.ts
@@ -239,7 +239,7 @@ export class ButtonDirective implements AfterViewInit, OnDestroy {
             <ng-container *ngIf="loading">
                 <ng-container *ngIf="!loadingIconTemplate">
                     <span *ngIf="loadingIcon" [class]="'p-button-loading-icon' + icon" [ngClass]="iconClass()"></span>
-                    <SpinnerIcon *ngIf="!loadingIcon" [styleClass]="iconClass() + ' p-button-loading-icon'" [spin]="true" />
+                    <SpinnerIcon *ngIf="!loadingIcon" [styleClass]="spinnerIconClass()" [spin]="true" />
                 </ng-container>
                 <span *ngIf="loadingIconTemplate" class="p-button-loading-icon">
                     <ng-template *ngTemplateOutlet="loadingIconTemplate"></ng-template>
@@ -348,6 +348,12 @@ export class Button implements AfterContentInit {
     iconTemplate: TemplateRef<any> | undefined;
 
     @ContentChildren(PrimeTemplate) templates: QueryList<PrimeTemplate> | undefined;
+
+    spinnerIconClass(): string {
+        return Object.entries(this.iconClass())
+            .filter(([, value]) => !!value)
+            .reduce((acc, [key]) => acc + ` ${key}`, 'p-button-loading-icon');
+    }
 
     iconClass() {
         return {
