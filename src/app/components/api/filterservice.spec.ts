cb58d3976ddfbe69d5a45a457851ffbbace2f45f
Allan Juan <allanjuansil@gmail.com>
2023-06-18 11:07:14 -0300

    Escrever testes de contains

--- src/app/components/api/filterservice.spec.ts
+++ src/app/components/api/filterservice.spec.ts
@@ -24,4 +24,28 @@ describe('FilterService', () => {
       expect(startsWith('Fabrício Andrey', 'Diogo')).toBe(false)
     })
   });
+
+  describe('contains()', () => {
+    const contains = new FilterService().filters.contains
+
+    it('deve retornar true se o filtro for undefined, null ou uma string em branco', () => {
+      expect(contains('Leandro Lo', undefined)).toBe(true);
+      expect(contains('Leandro Lo', null)).toBe(true);
+      expect(contains('Leandro Lo', '')).toBe(true);
+      expect(contains('Leandro Lo', ' ')).toBe(true);
+    })
+
+    it('deve retornar false se o valor for undefined ou null', () => {
+      expect(contains(undefined, 'Jansen Gomes')).toBe(false)
+      expect(contains(undefined, 'Jansen Gomes')).toBe(false)
+    })
+
+    it('deve retornar true se a string de valor contém o filtro', () => {
+      expect(contains('Leandro Lo', 'Lo')).toBe(true)
+    })
+
+    it('deve retornar false se a string de valor não contém o filtro', () => {
+      expect(contains('Leandro Lo', 'Leo')).toBe(false)
+    })
+  })
 });
