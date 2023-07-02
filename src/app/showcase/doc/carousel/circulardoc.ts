8d0585a7dc46ffbff9e8252a8f7be7f4a412aff3
Diego <diegofranchina@gmail.com>
2023-07-02 19:10:18 +0200

    Fixed carousel docs examples adding changeDetectorRef

--- src/app/showcase/doc/carousel/circulardoc.ts
+++ src/app/showcase/doc/carousel/circulardoc.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { Code } from '../../domain/code';
 import { Product } from '../../domain/product';
 import { ProductService } from '../../service/productservice';
@@ -41,11 +41,12 @@ export class CircularDoc implements OnInit {
 
     responsiveOptions: any[] | undefined;
 
-    constructor(private productService: ProductService) {}
+    constructor(private productService: ProductService, private cdr: ChangeDetectorRef) { }
 
     ngOnInit() {
         this.productService.getProductsSmall().then((products) => {
             this.products = products;
+            this.cdr.detectChanges();
         });
 
         this.responsiveOptions = [
