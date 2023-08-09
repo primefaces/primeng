79fb240f91961c1f291d2bdffecb3c13bd813659
Diego <diegofranchina@gmail.com>
2023-08-09 19:07:33 +0200

    Added 1024 base unit conversion

--- src/app/components/fileupload/fileupload.ts
+++ src/app/components/fileupload/fileupload.ts
@@ -828,7 +828,7 @@ export class FileUpload implements AfterViewInit, AfterContentInit, OnInit, OnDe
         if (bytes == 0) {
             return '0 B';
         }
-        let k = 1000,
+        let k = 1024,
             dm = 3,
             sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
             i = Math.floor(Math.log(bytes) / Math.log(k));
