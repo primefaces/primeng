568719e7ab2b0ea26be039d2f2cc7df3d06de7db
Justin White <kyjus25@gmail.com>
2023-08-18 13:06:00 -0500

    changeDetector not being used

--- src/app/components/tooltip/tooltip.ts
+++ src/app/components/tooltip/tooltip.ts
@@ -163,7 +163,7 @@ export class Tooltip implements AfterViewInit, OnDestroy {
 
     resizeListener: any;
 
-    constructor(@Inject(PLATFORM_ID) private platformId: any, public el: ElementRef, public zone: NgZone, public config: PrimeNGConfig, private renderer: Renderer2, private changeDetector: ChangeDetectorRef) {}
+    constructor(@Inject(PLATFORM_ID) private platformId: any, public el: ElementRef, public zone: NgZone, public config: PrimeNGConfig, private renderer: Renderer2, private viewContainer: ViewContainerRef) {}
 
     ngAfterViewInit() {
         if (isPlatformBrowser(this.platformId)) {
