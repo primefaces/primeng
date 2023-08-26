cddfde2da8b248ce6f45a30447f8a263da95bd8d
jase <804836+jase88@users.noreply.github.com>
2023-08-27 01:41:12 +0200

    Fixed #12722 - Calendar: Error if using datatype string with selectionMode="range" and keepInvalid=true (#13524)
    * fix: error where value is assumed to be an array, but is a string because keepInvalid property is set
    
    * fix: start and end value being strings instead of date instances
    
    * fix: start and end value being strings instead of date instances
    
    * fix: error where value is assumed to be an array, but is a string because keepInvalid property is set

--- src/app/components/calendar/calendar.ts
+++ src/app/components/calendar/calendar.ts
@@ -1551,7 +1551,7 @@ export class Calendar implements OnInit, OnDestroy, ControlValueAccessor {
                 this.onModelChange(this.formatDateTime(this.value));
             } else {
                 let stringArrValue = null;
-                if (this.value) {
+                if (Array.isArray(this.value)) {
                     stringArrValue = this.value.map((date: Date) => this.formatDateTime(date));
                 }
                 this.onModelChange(stringArrValue);
@@ -1678,7 +1678,7 @@ export class Calendar implements OnInit, OnDestroy, ControlValueAccessor {
 
     isDateBetween(start: Date, end: Date, dateMeta: any) {
         let between: boolean = false;
-        if (start && end) {
+        if (ObjectUtils.isDate(start) && ObjectUtils.isDate(end)) {
             let date: Date = new Date(dateMeta.year, dateMeta.month, dateMeta.day);
             return start.getTime() <= date.getTime() && end.getTime() >= date.getTime();
         }
