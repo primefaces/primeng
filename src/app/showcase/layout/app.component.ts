adf7075ca8fbd0406fef37d89368eb2cad5b47be
gucal <gucalaliriza@outlook.com>
2023-09-07 14:31:18 +0300

    Add gtag manager

--- src/app/showcase/layout/app.component.ts
+++ src/app/showcase/layout/app.component.ts
@@ -1,15 +1,26 @@
 import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { Component, Inject, OnDestroy, OnInit, PLATFORM_ID, Renderer2 } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { AppConfigService } from '../service/appconfigservice';
 import Announcement from '../data/news.json';
+import { AppConfigService } from '../service/appconfigservice';
+
+declare let gtag: Function;
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit, OnDestroy {
-    constructor(@Inject(DOCUMENT) private document: Document, @Inject(PLATFORM_ID) private platformId: any, private renderer: Renderer2, private configService: AppConfigService) {}
+    constructor(@Inject(DOCUMENT) private document: Document, @Inject(PLATFORM_ID) private platformId: any, private renderer: Renderer2, private configService: AppConfigService, private router: Router) {
+        this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                gtag('event', 'page_view', {
+                    page_path: event.urlAfterRedirects
+                });
+            }
+        });
+    }
 
     public subscription: Subscription;
 
