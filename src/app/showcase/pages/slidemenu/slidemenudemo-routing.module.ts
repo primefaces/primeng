45f3cda1cadaec2444436027856cec3e89f636a6
gucal <gucalaliriza@outlook.com>
2023-10-09 14:00:34 +0300

    Fix #13830 SlideMenu: Deprecate from PrimeNG

--- src/app/showcase/pages/slidemenu/slidemenudemo-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { SlideMenuDemo } from './slidemenudemo';
-
-@NgModule({
-    imports: [RouterModule.forChild([{ path: '', component: SlideMenuDemo }])],
-    exports: [RouterModule]
-})
-export class SlideMenuDemoRoutingModule {}
