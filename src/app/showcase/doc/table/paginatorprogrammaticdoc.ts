54d3d0f75400a162638714dfbf4ca90ab6757e5b
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-10-12 09:25:55 +0300

    Demo cosmetics

--- src/app/showcase/doc/table/paginatorprogrammaticdoc.ts
+++ src/app/showcase/doc/table/paginatorprogrammaticdoc.ts
@@ -11,7 +11,7 @@ import { CustomerService } from '../../service/customerservice';
             <p _ngcontent-ylv-c94="">Paginator can also be controlled via model using a binding to the <i>first</i> property where changes trigger a pagination.</p>
         </app-docsectiontext>
         <div class="card">
-            <div class="mb-3">
+            <div class="mb-3 flex gap-1">
                 <p-button type="button" icon="pi pi-chevron-left" (click)="prev()" [disabled]="isFirstPage()" styleClass="p-button-text"></p-button>
                 <p-button type="button" icon="pi pi-refresh" (click)="reset()" styleClass="p-button-text"></p-button>
                 <p-button type="button" icon="pi pi-chevron-right" (click)="next()" [disabled]="isLastPage()" styleClass="p-button-text"></p-button>
