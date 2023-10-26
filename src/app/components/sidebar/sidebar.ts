f69e615de552f54b671fa4a42759548126fc26bd
maruthumj <maruthupandian72@gmail.com>
2023-10-26 10:08:39 +0530

    onHide event emitter change

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -12,10 +12,12 @@ import {
     Inject,
     Input,
     NgModule,
+    OnChanges,
     OnDestroy,
     Output,
     QueryList,
     Renderer2,
+    SimpleChanges,
     TemplateRef,
     ViewEncapsulation
 } from '@angular/core';
@@ -95,7 +97,7 @@ const hideAnimation = animation([animate('{{transition}}', style({ transform: '{
         class: 'p-element'
     }
 })
-export class Sidebar implements AfterViewInit, AfterContentInit, OnDestroy {
+export class Sidebar implements AfterViewInit, AfterContentInit, OnDestroy, OnChanges {
     /**
      *  Target element to attach the dialog, valid values are "body" or a local ng-template variable of another element (note: use binding with brackets for template variables, e.g. [appendTo]="mydiv" for a div element having #mydiv as variable name).
      * @group Props
@@ -255,6 +257,17 @@ export class Sidebar implements AfterViewInit, AfterContentInit, OnDestroy {
     ngAfterViewInit() {
         this.initialized = true;
     }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if(changes.visible){
+            if(!changes.visible.firstChange){
+                if(changes.visible.currentValue === false){
+                    this.hide();
+                }
+            }
+        }
+    }
+    
 
     ngAfterContentInit() {
         this.templates?.forEach((item) => {
