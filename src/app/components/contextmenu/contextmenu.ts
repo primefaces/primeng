d89afc8eec660abcd509659d147c1e93b46e2071
maruthumj <maruthupandian72@gmail.com>
2023-10-26 10:38:28 +0530

    appended elements getting removed after the ngondestroy

--- src/app/components/contextmenu/contextmenu.ts
+++ src/app/components/contextmenu/contextmenu.ts
@@ -1148,9 +1148,17 @@ export class ContextMenu implements OnInit, AfterContentInit, OnDestroy {
         }
     }
 
+    removeAppendedElements(){
+         if (this.appendTo) {
+            if (this.appendTo === 'body') this.renderer.removeChild(this.document.body, this.containerViewChild.nativeElement);
+            else DomHandler.removeChild(this.containerViewChild.nativeElement, this.appendTo);
+        }
+    }
+
     ngOnDestroy() {
         this.unbindGlobalListeners();
         this.unbindTriggerEventListener();
+        this.removeAppendedElements();
     }
 }
 
