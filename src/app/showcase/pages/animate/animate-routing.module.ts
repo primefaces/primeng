cda6a6ea2f3d29912ebba808dc4afa2f76990b5f
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-10-27 17:07:22 +0300

    Remove demo page of pAnimate

--- src/app/showcase/pages/animate/animate-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { AnimateDemo } from './animatedemo';
-
-@NgModule({
-    imports: [RouterModule.forChild([{ path: '', component: AnimateDemo }])],
-    exports: [RouterModule]
-})
-export class AnimateDemoRoutingModule {}
