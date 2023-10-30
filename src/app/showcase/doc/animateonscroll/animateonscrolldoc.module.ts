c8255bec44776dbea377b7214c390dd5d51cbc93
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-10-30 17:05:02 +0300

    Add animateonscroll doc

---
+++ src/app/showcase/doc/animateonscroll/animateonscrolldoc.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppDocModule } from '../../layout/doc/app.doc.module';
+import { AppCodeModule } from '../../layout/doc/code/app.code.component';
+import { ImportDoc } from './importdoc';
+import { BasicDoc } from './basicdoc';
+import { AccessibilityDoc } from './accessibilitydoc';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+
+@NgModule({
+    imports: [CommonModule, RouterModule, AppCodeModule, AppDocModule, AnimateOnScrollModule],
+    declarations: [ImportDoc, BasicDoc, AccessibilityDoc],
+    exports: [AppDocModule]
+})
+export class AnimateOnScrollDocModule {}
