14c18937e3426d32e4bb9aed6075a66079684731
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-10-31 22:47:18 +0300

    Rating | Update accessibility doc

--- src/app/showcase/doc/rating/accessibilitydoc.ts
+++ src/app/showcase/doc/rating/accessibilitydoc.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'accessibility-doc',
-    template: ` <app-developmentsection>
+    template: `
         <div>
             <app-docsectiontext [title]="title" [id]="id">
                 <h3>Screen Reader</h3>
@@ -57,8 +57,7 @@ import { Component, Input } from '@angular/core';
                     </tbody>
                 </table>
             </div>
-        </div>
-    </app-developmentsection>`
+        </div>`
 })
 export class AccessibilityDoc {
     @Input() id: string;
