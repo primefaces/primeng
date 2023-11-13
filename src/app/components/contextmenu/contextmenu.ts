2c9050a2d6d48e2f32a7e8604c1df854c1c036ba
Hugo Graf <hg@hugograf.com>
2023-11-13 20:00:43 +0100

    Fix when nested context menu in nested context is not shown

--- src/app/components/contextmenu/contextmenu.ts
+++ src/app/components/contextmenu/contextmenu.ts
@@ -1151,10 +1151,12 @@ export class ContextMenu implements OnInit, AfterContentInit, OnDestroy {
     removeAppendedElements() {
         if (this.appendTo) {
            if (this.appendTo === 'body') {
-               if(this.containerViewChild.nativeElement)    
+               if(this.containerViewChild) {    
                    this.renderer.removeChild(this.document.body, this.containerViewChild.nativeElement);
-           }   
-           else DomHandler.removeChild(this.containerViewChild.nativeElement, this.appendTo);
+               }
+           } else {
+               DomHandler.removeChild(this.containerViewChild.nativeElement, this.appendTo);
+           }
         }
     }
 
