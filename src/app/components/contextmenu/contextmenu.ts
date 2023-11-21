f7f13481d6485deea6b13539edc09e5b54697601
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-11-21 16:21:07 +0300

    Code format

--- src/app/components/contextmenu/contextmenu.ts
+++ src/app/components/contextmenu/contextmenu.ts
@@ -1151,8 +1151,8 @@ export class ContextMenu implements OnInit, AfterContentInit, OnDestroy {
     removeAppendedElements() {
         if (this.appendTo) {
             if (this.appendTo === 'body') {
-                if(this.containerViewChild) {    
-                   this.renderer.removeChild(this.document.body, this.containerViewChild.nativeElement);
+                if (this.containerViewChild) {
+                    this.renderer.removeChild(this.document.body, this.containerViewChild.nativeElement);
                 }
             } else {
                 DomHandler.removeChild(this.containerViewChild.nativeElement, this.appendTo);
--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -12,7 +12,6 @@ import {
     Inject,
     Input,
     NgModule,
-    OnChanges,
     OnDestroy,
     Output,
     QueryList,
