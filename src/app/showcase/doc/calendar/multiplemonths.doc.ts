e07a076c0b508ee0c6847a5b6d66b7ae965ccfa5
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-11-22 10:12:32 +0300

    calendar doc update completed

--- src/app/showcase/doc/calendar/multiplemonths.doc.ts
+++ src/app/showcase/doc/calendar/multiplemonths.doc.ts
@@ -1,22 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Code } from '../../domain/code';
 
 @Component({
     selector: 'calendar-multiplemonths-demo',
-    template: ` <section class="py-4">
-        <app-docsectiontext [title]="title" [id]="id">
+    template: ` 
+        <app-docsectiontext>
             <p>Number of months to display is configured with the <i>numberOfMonths</i> property.</p>
         </app-docsectiontext>
         <div class="card flex justify-content-center">
             <p-calendar [(ngModel)]="date" [numberOfMonths]="3"></p-calendar>
         </div>
         <app-code [code]="code" selector="calendar-multiplemonths-demo"></app-code>
-    </section>`
+    `
 })
 export class MultipleMonthDoc {
-    @Input() id: string;
-
-    @Input() title: string;
 
     date: Date[] | undefined;
 
