3c6126b9546c7f45a02235a8787de331473aa314
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-11-22 13:19:06 +0300

    slider doc update completed

--- src/app/showcase/doc/slider/inputdoc.ts
+++ src/app/showcase/doc/slider/inputdoc.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Code } from '../../domain/code';
 
 @Component({
     selector: 'input-doc',
-    template: ` <section class="py-4">
-        <app-docsectiontext [title]="title" [id]="id">
+    template: ` 
+        <app-docsectiontext>
             <p>Slider is connected to an input field using two-way binding.</p>
         </app-docsectiontext>
         <div class="card flex justify-content-center">
@@ -14,12 +14,9 @@ import { Code } from '../../domain/code';
             </div>
         </div>
         <app-code [code]="code" selector="slider-input-demo"></app-code>
-    </section>`
+    `
 })
 export class InputDoc {
-    @Input() id: string;
-
-    @Input() title: string;
 
     value: number = 50;
 
--- src/app/showcase/doc/slider/verticaldoc.ts
+++ src/app/showcase/doc/slider/verticaldoc.ts
@@ -1,22 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Code } from '../../domain/code';
 
 @Component({
     selector: 'vertical-doc',
-    template: ` <section class="py-4">
-        <app-docsectiontext [title]="title" [id]="id">
+    template: `
+        <app-docsectiontext>
             <p>Default layout of slider is <i>horizontal</i>, use <i>orientation</i> property for the alternative <i>vertical</i> mode.</p>
         </app-docsectiontext>
         <div class="card flex justify-content-center">
             <p-slider [(ngModel)]="value" orientation="vertical"></p-slider>
         </div>
         <app-code [code]="code" selector="slider-vertical-demo"></app-code>
-    </section>`
+    `
 })
 export class VerticalDoc {
-    @Input() id: string;
-
-    @Input() title: string;
 
     value!: number;
 
