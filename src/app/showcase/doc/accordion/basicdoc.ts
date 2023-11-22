03fbef6c89d176f3856bf0dfe4a20eec86e9a309
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-11-22 15:59:07 +0300

    accordion doc update completed

--- src/app/showcase/doc/accordion/basicdoc.ts
+++ src/app/showcase/doc/accordion/basicdoc.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Code } from '../../domain/code';
 
 @Component({
     selector: 'basic-doc',
-    template: ` <section class="py-4">
-        <app-docsectiontext [title]="title" [id]="id">
+    template: `
+        <app-docsectiontext>
             <p>Accordion consists of one or more <i>AccordionTab</i> elements which are collapsed by default. Tab to expand initially can be defined with the <i>activeIndex</i> property.</p>
         </app-docsectiontext>
         <div class="card flex justify-content-center">
@@ -30,12 +30,9 @@ import { Code } from '../../domain/code';
             </p-accordion>
         </div>
         <app-code [code]="code" selector="accordion-basic-demo"></app-code>
-    </section>`
+    `
 })
 export class BasicDoc {
-    @Input() id: string;
-
-    @Input() title: string;
 
     code: Code = {
         basic: `
