21d1e330ce515dfa7041172228be81ad98c52f85
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-11-22 11:56:21 +0300

    inputswitch doc update completed

--- src/app/showcase/doc/inputswitch/accessibilitydoc.ts
+++ src/app/showcase/doc/inputswitch/accessibilitydoc.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Code } from '../../domain/code';
 
 @Component({
     selector: 'accessibility-doc',
     template: ` <div>
-        <app-docsectiontext [title]="title" [id]="id">
+        <app-docsectiontext>
             <h3>Screen Reader</h3>
             <p>
                 InputSwitch component uses a hidden native checkbox element with <i>switch</i> role internally that is only visible to screen readers. Value to describe the component can either be provided via <i>label</i> tag combined with
@@ -42,9 +42,6 @@ import { Code } from '../../domain/code';
     </div>`
 })
 export class AccessibilityDoc {
-    @Input() id: string;
-
-    @Input() title: string;
 
     code: Code = {
         basic: `<label for="switch1">Remember Me</label>
