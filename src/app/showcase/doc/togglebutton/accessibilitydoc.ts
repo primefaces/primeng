d8b12702e76043e6b143602ed4874fb1fd885079
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-11-22 13:41:44 +0300

    togglebutton doc update completed

--- src/app/showcase/doc/togglebutton/accessibilitydoc.ts
+++ src/app/showcase/doc/togglebutton/accessibilitydoc.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Code } from '../../domain/code';
 
 @Component({
     selector: 'accessibility-doc',
     template: ` <div>
-        <app-docsectiontext [title]="title" [id]="id">
+        <app-docsectiontext>
             <h3>Screen Reader</h3>
             <p>
                 ToggleButton component uses an element with <i>button</i> role and updates <i>aria-pressed</i> state for screen readers. Value to describe the component can be defined with <i>ariaLabelledBy</i> or <i>ariaLabel</i> props, it is highly
@@ -43,9 +43,6 @@ import { Code } from '../../domain/code';
     </div>`
 })
 export class AccessibilityDoc {
-    @Input() id: string;
-
-    @Input() title: string;
 
     code: Code = {
         basic: `<span id="rememberme">Remember Me</span>
