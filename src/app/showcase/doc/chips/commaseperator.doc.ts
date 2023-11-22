22f4711ebdcba1f64de4c02081fb016765640835
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-11-22 10:40:23 +0300

    chips doc update completed

--- src/app/showcase/doc/chips/commaseperator.doc.ts
+++ src/app/showcase/doc/chips/commaseperator.doc.ts
@@ -1,22 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Code } from '../../domain/code';
 
 @Component({
     selector: 'chips-comma-seperator-demo',
-    template: ` <section class="py-4">
-        <app-docsectiontext [title]="title" [id]="id">
+    template: `
+        <app-docsectiontext>
             <p>A new chip is added when <i>enter</i> key is pressed, <i>separator</i> property allows definining an additional key. Currently only valid value is <i>,</i> to create a new item when comma key is pressed.</p>
         </app-docsectiontext>
         <div class="card p-fluid">
             <p-chips [(ngModel)]="values" separator="," placeholder="Hint: a, b, c"></p-chips>
         </div>
         <app-code [code]="code" selector="chips-comma-seperator-demo"></app-code>
-    </section>`
+    `
 })
 export class CommaSeperatorDoc {
-    @Input() id: string;
-
-    @Input() title: string;
 
     values: string[] | undefined;
 
