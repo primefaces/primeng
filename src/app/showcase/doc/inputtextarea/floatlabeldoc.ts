0971c98b56e19ad9939f8f18901ebfb2cc4dceac
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-11-22 12:04:58 +0300

    inputtextarea doc update completed

--- src/app/showcase/doc/inputtextarea/floatlabeldoc.ts
+++ src/app/showcase/doc/inputtextarea/floatlabeldoc.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Code } from '../../domain/code';
 
 @Component({
     selector: 'floatlabel-doc',
-    template: ` <section class="py-4">
-        <app-docsectiontext [title]="title" [id]="id">
+    template: `
+        <app-docsectiontext>
             <p>A floating label appears on top of the input field when focused.</p>
         </app-docsectiontext>
         <div class="card flex justify-content-center">
@@ -14,12 +14,9 @@ import { Code } from '../../domain/code';
             </span>
         </div>
         <app-code [code]="code" selector="input-textarea-floatlabel-demo"></app-code>
-    </section>`
+    `
 })
 export class FloatlabelDoc {
-    @Input() id: string;
-
-    @Input() title: string;
 
     code: Code = {
         basic: `
