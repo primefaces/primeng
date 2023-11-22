041526c6c5eb5c127a774cd2062f45109d36837f
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-11-22 12:37:32 +0300

    keyfilter doc update completed

--- src/app/showcase/doc/keyfilter/accessibilitydoc.ts
+++ src/app/showcase/doc/keyfilter/accessibilitydoc.ts
@@ -1,15 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'accessibility-doc',
     template: ` <div>
-        <app-docsectiontext [title]="title" [id]="id">
+        <app-docsectiontext>
             <p>Refer to <a href="/inputtext">InputText</a> for accessibility as KeyFilter is a built-in add-on of the InputText.</p>
         </app-docsectiontext>
     </div>`
 })
 export class AccessibilityDoc {
-    @Input() id: string;
-
-    @Input() title: string;
+ 
 }
