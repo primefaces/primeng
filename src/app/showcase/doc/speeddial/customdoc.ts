61b786d316de8a315e65c0f98877a9c8c4d1af49
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-11-22 14:03:31 +0300

    speed dial doc update completed

--- src/app/showcase/doc/speeddial/customdoc.ts
+++ src/app/showcase/doc/speeddial/customdoc.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuItem, MessageService } from 'primeng/api';
 import { Code } from '../../domain/code';
 
 @Component({
     selector: 'custom-doc',
-    template: ` <section class="py-4">
-        <app-docsectiontext [title]="title" [id]="id">
+    template: ` 
+        <app-docsectiontext>
             <p>SpeedDial sample with an outlined button, custom icons and <i>transitionDelay</i>.</p>
         </app-docsectiontext>
         <div class="card">
@@ -15,13 +15,10 @@ import { Code } from '../../domain/code';
             </div>
         </div>
         <app-code [code]="code" selector="speed-dial-custom-demo"></app-code>
-    </section>`,
+    `,
     providers: [MessageService]
 })
 export class CustomDoc implements OnInit {
-    @Input() id: string;
-
-    @Input() title: string;
 
     items: MenuItem[] | undefined;
 
