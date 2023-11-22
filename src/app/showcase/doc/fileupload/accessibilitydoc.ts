cecb6347092e47eb3dd56f733c8d20246993456e
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-11-22 23:55:00 +0300

    fileupload doc update completed

--- src/app/showcase/doc/fileupload/accessibilitydoc.ts
+++ src/app/showcase/doc/fileupload/accessibilitydoc.ts
@@ -1,9 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'accessibility-doc',
     template: `
-        <app-docsectiontext [title]="title" [id]="id">
+        <app-docsectiontext>
             <h3>Screen Reader</h3>
             <p>FileUpload uses a hidden native <i>input</i> element with <i>type="file"</i> for screen readers.</p>
             <h3>Keyboard Support</h3>
@@ -12,7 +12,5 @@ import { Component, Input } from '@angular/core';
     `
 })
 export class AccessibilityDoc {
-    @Input() id: string;
 
-    @Input() title: string;
 }
