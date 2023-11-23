7e9fbf09e959f7bd415d545c3ba34aa30da8e97d
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-11-23 13:12:25 +0300

    progressbar doc update completed

--- src/app/showcase/doc/progressbar/importdoc.ts
+++ src/app/showcase/doc/progressbar/importdoc.ts
@@ -1,21 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Code } from '../../domain/code';
 
 @Component({
     selector: 'import-doc',
-    template: ` <section class="py-4">
-        <app-docsectiontext [title]="title" [id]="id"> </app-docsectiontext>
+    template: ` 
         <app-code [code]="code" [hideToggleCode]="true"></app-code>
-    </section>`
+    `
 })
 export class ImportDoc {
-    @Input() id: string;
-
-    @Input() title: string;
 
     code: Code = {
         typescript: `import { ProgressBarModule } from 'primeng/progressbar';
 // For dynamic progressbar demo
 import { ToastModule } from 'primeng/toast';`
     };
+
 }
