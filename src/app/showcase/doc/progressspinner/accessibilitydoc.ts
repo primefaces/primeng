cdd735645ea76f89c6eca68af2b5cd092a0d4834
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-11-23 13:15:37 +0300

    progress spinner doc update completed

--- src/app/showcase/doc/progressspinner/accessibilitydoc.ts
+++ src/app/showcase/doc/progressspinner/accessibilitydoc.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Code } from '../../domain/code';
 
 @Component({
     selector: 'accessibility-doc',
     template: ` <div>
-        <app-docsectiontext [title]="title" [id]="id">
+        <app-docsectiontext>
             <h3>Screen Reader</h3>
             <p>ProgressSpinner components uses <i>progressbar</i> role. Value to describe the component can be defined using <i>aria-labelledby</i> and <i>aria-label</i> props.</p>
         </app-docsectiontext>
@@ -16,11 +16,9 @@ import { Code } from '../../domain/code';
     </div>`
 })
 export class AccessibilityDoc {
-    @Input() id: string;
-
-    @Input() title: string;
 
     code: Code = {
         html: `<p-progressSpinner aria-label="Loading"></p-progressSpinner>`
     };
+
 }
--- src/app/showcase/doc/progressspinner/templatedoc.ts
+++ src/app/showcase/doc/progressspinner/templatedoc.ts
@@ -3,15 +3,15 @@ import { Code } from '../../domain/code';
 
 @Component({
     selector: 'template-doc',
-    template: ` <section class="py-4">
-        <app-docsectiontext [title]="title" [id]="id">
+    template: `
+        <app-docsectiontext>
             <p>ProgressSpinner can be customized with styling property like <i>styleClass</i>, <i>strokeWidth</i> and <i>fill</i>.</p>
         </app-docsectiontext>
         <div class="card">
             <p-progressSpinner styleClass="w-4rem h-4rem" strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s"></p-progressSpinner>
         </div>
         <app-code [code]="code" selector="progress-spinner-template-demo"></app-code>
-    </section>`
+    `
 })
 export class TemplateDoc {
     @Input() id: string;
