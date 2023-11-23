513b9a5e2654111a58e734b5dc6c4084a17b4024
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-11-23 12:51:15 +0300

    avatar doc update completed

--- src/app/showcase/doc/avatar/templatedoc.ts
+++ src/app/showcase/doc/avatar/templatedoc.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Code } from '../../domain/code';
 
 @Component({
     selector: 'avatar-template-demo',
-    template: ` <section class="py-4">
-        <app-docsectiontext [title]="title" [id]="id">
+    template: `
+        <app-docsectiontext>
             <p>Content can easily be customized with the dynamic content instead of using the built-in modes.</p>
         </app-docsectiontext>
         <div class="card flex justify-content-center">
@@ -13,12 +13,9 @@ import { Code } from '../../domain/code';
             </p-avatar>
         </div>
         <app-code [code]="code" selector="avatar-template-demo"></app-code>
-    </section>`
+    `
 })
 export class TemplateDoc {
-    @Input() id: string;
-
-    @Input() title: string;
 
     code: Code = {
         basic: `
