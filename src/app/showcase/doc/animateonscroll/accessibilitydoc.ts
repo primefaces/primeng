0e36576e3ee9b07927eb14dbf1320630d9c75df8
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-11-23 13:48:37 +0300

    animateonscroll doc update completed

--- src/app/showcase/doc/animateonscroll/accessibilitydoc.ts
+++ src/app/showcase/doc/animateonscroll/accessibilitydoc.ts
@@ -1,9 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'accessibility-doc',
     template: ` <div>
-        <app-docsectiontext [title]="title" [id]="id">
+        <app-docsectiontext>
             <h3>Screen Reader</h3>
             <p>AnimateOnScroll does not require any roles and attributes.</p>
             <h3>Keyboard Support</h3>
@@ -12,7 +12,5 @@ import { Component, Input } from '@angular/core';
     </div>`
 })
 export class AccessibilityDoc {
-    @Input() id: string;
 
-    @Input() title: string;
 }
