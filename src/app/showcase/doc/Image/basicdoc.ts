e7619e9bd9454532e95917201b9ad445d259caf7
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-11-23 12:01:37 +0300

    image doc update completed

--- src/app/showcase/doc/Image/basicdoc.ts
+++ src/app/showcase/doc/Image/basicdoc.ts
@@ -1,22 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Code } from '../../domain/code';
 
 @Component({
     selector: 'basic-doc',
-    template: ` <section class="py-4">
-        <app-docsectiontext [title]="title" [id]="id">
+    template: `
+        <app-docsectiontext>
             <p>Image is used as the native <i>img</i> element and supports all properties that the native element has. For multiple image, see <a [routerLink]="['/galleria']">Galleria.</a></p>
         </app-docsectiontext>
         <div class="card flex justify-content-center">
             <p-image src="https://primefaces.org/cdn/primeng/images/galleria/galleria1.jpg" alt="Image" width="250"></p-image>
         </div>
         <app-code [code]="code" selector="image-basic-demo"></app-code>
-    </section>`
+    `
 })
 export class BasicDoc {
-    @Input() id: string;
-
-    @Input() title: string;
 
     code: Code = {
         basic: `
