e95500c829390b689a803647d89dff5508d2e457
Cagatay Civici <cagatay.civici@gmail.com>
2023-11-28 18:06:20 +0300

    Cleanup

--- src/app/showcase/layout/topbar/app.topbar.component.ts
+++ src/app/showcase/layout/topbar/app.topbar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, DOCUMENT, isPlatformBrowser } from '@angular/common';
-import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Inject, Input, OnDestroy, OnInit, Output, PLATFORM_ID, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Inject, Input, OnDestroy, OnInit, Output, PLATFORM_ID, Renderer2 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import docsearch from '@docsearch/js';
