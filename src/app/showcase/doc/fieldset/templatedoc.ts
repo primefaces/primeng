462125b06bc805b123d21508a22af7b74b563841
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-12-07 13:34:30 +0300

    panel template doc updated

--- src/app/showcase/doc/fieldset/templatedoc.ts
+++ src/app/showcase/doc/fieldset/templatedoc.ts
@@ -5,17 +5,20 @@ import { Code } from '../../domain/code';
     selector: 'fieldset-template-demo',
     template: `
         <app-docsectiontext>
-            <p>Legend section can also be defined with custom content instead of primitive values.</p>
+            <p>Header section can also be defined with custom content instead of primitive values.</p>
         </app-docsectiontext>
         <div class="card">
             <p-fieldset>
                 <ng-template pTemplate="header">
-                    <div class="flex align-items-center text-primary">
-                        <span class="pi pi-user mr-2"></span>
-                        <span class="font-bold text-lg">User Details</span>
+                    <div class="flex align-items-center gap-2 px-2">
+                        <p-avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" shape="circle" />
+                        <span class="font-bold">Amy Elsner</span>
                     </div>
                 </ng-template>
-                Content
+                <p class="m-0">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
             </p-fieldset>
         </div>
         <app-code [code]="code" selector="fieldset-template-demo"></app-code>
@@ -25,25 +28,30 @@ export class TemplateDoc {
     code: Code = {
         basic: `<p-fieldset>
     <ng-template pTemplate="header">
-        <div class="flex align-items-center text-primary">
-            <span class="pi pi-user mr-2"></span>
-            <span class="font-bold text-lg">User Details</span>
+        <div class="flex align-items-center gap-2 px-2">
+            <p-avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" shape="circle" />
+            <span class="font-bold">Amy Elsner</span>
         </div>
     </ng-template>
-    Content
+    <p class="m-0">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
 </p-fieldset>`,
 
-        html: `
-<div class="card">
-    <p-fieldset>
-        <ng-template pTemplate="header">
-            <div class="flex align-items-center text-primary">
-                <span class="pi pi-user mr-2"></span>
-                <span class="font-bold text-lg">User Details</span>
-            </div>
-        </ng-template>
-        Content
-    </p-fieldset>
+        html: `<div class="card">
+<p-fieldset>
+<ng-template pTemplate="header">
+    <div class="flex align-items-center gap-2 px-2">
+        <p-avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" shape="circle" />
+        <span class="font-bold">Amy Elsner</span>
+    </div>
+</ng-template>
+<p class="m-0">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+</p-fieldset>
 </div>`,
 
         typescript: `
