0418b94c1730a27cdecd4f1633c30b497a59d6f4
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-12-07 11:38:33 +0300

    toolbar basic doc update completed

--- src/app/showcase/doc/toolbar/toolbardoc.module.ts
+++ src/app/showcase/doc/toolbar/toolbardoc.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { SplitButtonModule } from 'primeng/splitbutton';
+import { InputTextModule } from 'primeng/inputtext';
 import { ToolbarModule } from 'primeng/toolbar';
 import { AppDocModule } from '../../layout/doc/app.doc.module';
 import { AppCodeModule } from '../../layout/doc/app.code.component';
@@ -13,7 +14,7 @@ import { StyleDoc } from './styledoc';
 import { TemplateDoc } from './templatedoc';
 
 @NgModule({
-    imports: [CommonModule, AppCodeModule, AppDocModule, ToolbarModule, RouterModule, ButtonModule, SplitButtonModule],
+    imports: [CommonModule, AppCodeModule, AppDocModule, ToolbarModule, RouterModule, ButtonModule, SplitButtonModule, InputTextModule],
     exports: [AppDocModule],
     declarations: [ImportDoc, BasicDoc, TemplateDoc, StyleDoc, AccessibilityDoc]
 })
