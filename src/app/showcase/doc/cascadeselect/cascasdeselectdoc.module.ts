b95238996989fa21e07dbdd00368c3ac7fd6e7a9
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-12-08 16:51:23 +0300

    cascadeselect floatlabel and invalid doc added

--- src/app/showcase/doc/cascadeselect/cascasdeselectdoc.module.ts
+++ src/app/showcase/doc/cascadeselect/cascasdeselectdoc.module.ts
@@ -8,6 +8,8 @@ import { AppCodeModule } from '../../layout/doc/app.code.component';
 import { AccessibilityDoc } from './accessibilitydoc';
 import { BasicDoc } from './basicdoc';
 import { ImportDoc } from './importdoc';
+import { InvalidDoc } from './invaliddoc';
+import { FloatLabelDoc } from './floatlabeldoc';
 import { ReactiveFormsDoc } from './reactiveformsdoc';
 import { StyleDoc } from './styledoc';
 import { TemplateDoc } from './templatedoc';
@@ -15,6 +17,6 @@ import { TemplateDoc } from './templatedoc';
 @NgModule({
     imports: [CommonModule, RouterModule, CascadeSelectModule, FormsModule, AppCodeModule, AppDocModule, ReactiveFormsModule],
     exports: [AppDocModule],
-    declarations: [ImportDoc, BasicDoc, TemplateDoc, StyleDoc, AccessibilityDoc, ReactiveFormsDoc]
+    declarations: [ImportDoc, BasicDoc, TemplateDoc, InvalidDoc, FloatLabelDoc, StyleDoc, AccessibilityDoc, ReactiveFormsDoc]
 })
 export class CascadeSelectDocModule {}
