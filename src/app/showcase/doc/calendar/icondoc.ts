24bb2aa991ca2da491c98bd696e4f3d3bd76460e
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-12-11 16:15:30 +0300

    Calendar | Refactor icon demo, add inputIconTemplate

--- src/app/showcase/doc/calendar/icondoc.ts
+++ src/app/showcase/doc/calendar/icondoc.ts
@@ -7,32 +7,78 @@ import { Code } from '../../domain/code';
         <app-docsectiontext>
             <p>An additional icon is displayed next to the input field when <i>showIcon</i> is present.</p>
         </app-docsectiontext>
-        <div class="card flex justify-content-center">
-            <p-calendar [(ngModel)]="date" [showIcon]="true"></p-calendar>
+        <div class="card flex flex-wrap gap-3 p-fluid">
+            <div class="flex-auto">
+                <label for="buttondisplay" class="font-bold block mb-2"> Button Display </label>
+                <p-calendar [(ngModel)]="date1" [showIcon]="true" inputId="buttondisplay"></p-calendar>
+            </div>
+
+            <div class="flex-auto">
+                <label for="icondisplay" class="font-bold block mb-2"> Icon Display </label>
+                <p-calendar [(ngModel)]="date2" [iconDisplay]="'input'" [showIcon]="true" inputId="icondisplay"></p-calendar>
+            </div>
+
+            <div class="flex-auto">
+                <label for="templatedisplay" class="font-bold block mb-2"> Template Display </label>
+                <p-calendar [(ngModel)]="date3" [iconDisplay]="'input'" [showIcon]="true" inputId="templatedisplay">
+                    <ng-template pTemplate="inputicon" let-clickCallBack="clickCallBack">
+                        <i class="pi pi-user" (click)="clickCallBack($event)"></i>
+                    </ng-template>
+                </p-calendar>
+            </div>
         </div>
         <app-code [code]="code" selector="calendar-icon-demo"></app-code>
     `
 })
 export class IconDoc {
-    date: Date | undefined;
+    date1: Date | undefined;
+
+    date2: Date | undefined;
+
+    date3: Date | undefined;
 
     code: Code = {
-        basic: `<p-calendar [(ngModel)]="date" [showIcon]="true"></p-calendar>`,
+        basic: `<p-calendar [(ngModel)]="date1" [showIcon]="true"></p-calendar>
+<p-calendar [(ngModel)]="date2" [iconDisplay]="'input'" [showIcon]="true"></p-calendar>
+<p-calendar [(ngModel)]="date3" [iconDisplay]="'input'" [showIcon]="true" inputId="templatedisplay">
+    <ng-template pTemplate="inputicon" let-clickCallBack="clickCallBack">
+        <i class="pi pi-user" (click)="clickCallBack($event)"></i>
+    </ng-template>
+</p-calendar>`,
 
-        html: `
-<div class="card flex justify-content-center">
-    <p-calendar [(ngModel)]="date" [showIcon]="true"></p-calendar>
+        html: `<div class="card flex flex-wrap gap-3 p-fluid">
+    <div class="flex-auto">
+        <label for="buttondisplay" class="font-bold block mb-2"> Button Display </label>
+        <p-calendar [(ngModel)]="date1" [showIcon]="true" inputId="buttondisplay"></p-calendar>
+    </div>
+
+    <div class="flex-auto">
+        <label for="icondisplay" class="font-bold block mb-2"> Icon Display </label>
+        <p-calendar [(ngModel)]="date2" [iconDisplay]="'input'" [showIcon]="true" inputId="icondisplay"></p-calendar>
+    </div>
+
+    <div class="flex-auto">
+        <label for="templatedisplay" class="font-bold block mb-2"> Template Display </label>
+        <p-calendar [(ngModel)]="date3" [iconDisplay]="'input'" [showIcon]="true" inputId="templatedisplay">
+            <ng-template pTemplate="inputicon" let-clickCallBack="clickCallBack">
+                <i class="pi pi-user" (click)="clickCallBack($event)"></i>
+            </ng-template>
+        </p-calendar>
+    </div>
 </div>`,
 
-        typescript: `
-import { Component } from '@angular/core';
+        typescript: `import { Component } from '@angular/core';
 
 @Component({
     selector: 'calendar-icon-demo',
     templateUrl: './calendar-icon-demo.html'
 })
 export class CalendarIconDemo {
-    date: Date | undefined;
+    date1: Date | undefined;
+
+    date2: Date | undefined;
+
+    date3: Date | undefined;
 }`
     };
 }
