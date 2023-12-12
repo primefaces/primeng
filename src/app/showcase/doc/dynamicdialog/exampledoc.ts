602062f0c5ac35695570d0e121024aa70be15fd5
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2023-12-12 23:03:34 +0300

    infodemo update

--- src/app/showcase/doc/dynamicdialog/exampledoc.ts
+++ src/app/showcase/doc/dynamicdialog/exampledoc.ts
@@ -228,15 +228,37 @@ import { MessageService } from 'primeng/api';
     providers: [DialogService, MessageService],
     template: \`<div>
     <p>
-        There are <strong></strong> products in total in this list.
+        There are <strong>{{ totalProducts }}</strong> products in total in this list.
     </p>
     <div class="flex justify-content-end">
-        <p-button type="button" label="Close" ></p-button>
+        <p-button type="button" label="Close" (click)="close()"></p-button>
     </div>
 </div>\`
 })
 export class InfoDemo {
-   
+    totalProducts: number = 0;
+
+    instance: DynamicDialogComponent | undefined;
+
+    constructor(public ref: DynamicDialogRef, private dialogService: DialogService) {
+        this.instance = this.dialogService.getInstance(this.ref);
+    }
+
+    ngOnInit() {
+        if (this.instance && this.instance.data) {
+            this.totalProducts = this.instance.data['totalProducts'];
+        }
+    }
+
+    close() {
+        this.ref.close();
+    }
+
+    ngOnDestroy() {
+        if (this.ref) {
+            this.ref.close();
+        }
+    }
 }`
         }
         
