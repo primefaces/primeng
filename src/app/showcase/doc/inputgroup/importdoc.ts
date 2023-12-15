459365c4c08ae35fffc638783f988b690443972e
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-12-15 14:25:41 +0300

    Fix typo

--- src/app/showcase/doc/inputgroup/importdoc.ts
+++ src/app/showcase/doc/inputgroup/importdoc.ts
@@ -7,7 +7,7 @@ import { Code } from '../../domain/code';
 })
 export class ImportDoc {
     code: Code = {
-        typescript: `import { InputGroup } from 'primeng/inputgroup';
+        typescript: `import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddon } from 'primeng/inputgroupaddon';`
     };
 }
