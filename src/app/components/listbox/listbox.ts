50ffef108851084db9c57eea648062bccd24edfa
Anatolii <anatolii.dmytruk@idealscorp.com>
2024-01-24 23:17:52 +0200

    fix: https://github.com/primefaces/primeng/issues/14459 listbox empty message for filter

--- src/app/components/listbox/listbox.ts
+++ src/app/components/listbox/listbox.ts
@@ -1411,7 +1411,7 @@ export class Listbox implements AfterContentInit, OnInit, ControlValueAccessor, 
     }
 
     isEmpty() {
-        return !this._options() || (this._options() && this._options().length === 0);
+        return !this._options()?.length || !this.visibleOptions()?.length;
     }
 
     hasFilter() {
