34b5e81f37c8cddaefa2d109d06160c2438e233f
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2024-02-20 13:23:47 +0300

    Fix typo

--- src/app/showcase/pages/floatlabel/floatlabeldemo.module.ts
+++ src/app/showcase/pages/floatlabel/floatlabeldemo.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { FocusTrapDocModule } from '../../doc/focustrap/focustrapdoc.module';
 import { FloatLabelDemo } from './floatlabeldemo';
 import { FloatLabelDemoRoutingModule } from './floatlabeldemo-routing.module';
+import { FloatLabelDocModule } from '../../doc/floatlabel/floatlabeldoc.module';
 
 @NgModule({
-    imports: [CommonModule, FloatLabelDemoRoutingModule, FormsModule, FocusTrapDocModule],
+    imports: [CommonModule, FloatLabelDemoRoutingModule, FormsModule, FloatLabelDocModule],
     declarations: [FloatLabelDemo]
 })
 export class FloatLabelDemoModule {}
