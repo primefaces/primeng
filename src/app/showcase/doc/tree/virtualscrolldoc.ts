cd04621688fbb4d30e6cb553fc90a16922491fea
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-02-26 12:58:15 +0300

    stackblitz update

--- src/app/showcase/doc/tree/virtualscrolldoc.ts
+++ src/app/showcase/doc/tree/virtualscrolldoc.ts
@@ -46,14 +46,15 @@ export class VirtualScrollDoc implements OnInit {
 </div>`,
 
         typescript: `
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { TreeNode } from 'primeng/api';
+import { NodeService } from '../../service/nodeservice';
 
 @Component({
-    selector: 'tree-template-demo',
-    templateUrl: './tree-template-demo.html'
+    selector: 'tree-virtual-scroll-demo',
+    templateUrl: './tree-virtual-scroll-demo.html'
 })
-export class TreeTemplateDemo implements OnInit {
+export class TreeVirtualScrollDemo implements OnInit {
     loading: boolean = false;
 
     files!: TreeNode[];
@@ -75,6 +76,36 @@ export class TreeTemplateDemo implements OnInit {
         return duplicatedData;
     }
 
-}`
+}`,
+service: ['NodeService'],
+
+data: `
+/* NodeService */
+{
+key: '0',
+label: 'Documents',
+data: 'Documents Folder',
+icon: 'pi pi-fw pi-inbox',
+children: [
+{
+    key: '0-0',
+    label: 'Work',
+    data: 'Work Folder',
+    icon: 'pi pi-fw pi-cog',
+    children: [
+        { key: '0-0-0', label: 'Expenses.doc', icon: 'pi pi-fw pi-file', data: 'Expenses Document' },
+        { key: '0-0-1', label: 'Resume.doc', icon: 'pi pi-fw pi-file', data: 'Resume Document' }
+    ]
+},
+{
+    key: '0-1',
+    label: 'Home',
+    data: 'Home Folder',
+    icon: 'pi pi-fw pi-home',
+    children: [{ key: '0-1-0', label: 'Invoices.txt', icon: 'pi pi-fw pi-file', data: 'Invoices for this month' }]
+}
+]
+},
+...`
     };
 }
