9da9d4df06fc94f1c88b551c32e9700edae17ff0
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-03-11 13:47:00 +0300

    sampledoc && templatedoc update

--- src/app/showcase/doc/inputotp/sampledoc.ts
+++ src/app/showcase/doc/inputotp/sampledoc.ts
@@ -112,7 +112,46 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'input-otp-sample-demo',
-    templateUrl: './input-otp-sample-demo.html'
+    templateUrl: './input-otp-sample-demo.html',
+    styles: [
+        \`
+        .custom-otp-input {
+            width: 48px;
+            height: 48px;
+            font-size: 24px;
+            appearance: none;
+            text-align: center;
+            transition: all 0.2s;
+            border-radius: 0;
+            border: 1px solid var(--surface-400);
+            background: transparent;
+            outline-offset: -2px;
+            outline-color: transparent;
+            border-right: 0 none;
+            transition: outline-color 0.3s;
+            color: var(--text-color);
+        }
+
+        .custom-otp-input:focus {
+            outline: 2px solid var(--primary-color);
+        }
+
+        .custom-otp-input:first-child,
+        .custom-otp-input:nth-child(5) {
+            border-top-left-radius: 12px;
+            border-bottom-left-radius: 12px;
+        }
+
+        .custom-otp-input:nth-child(3),
+        .custom-otp-input:last-child {
+            border-top-right-radius: 12px;
+            border-bottom-right-radius: 12px;
+            border-right-width: 1px;
+            border-right-style: solid;
+            border-color: var(--surface-400);
+        }
+        \`
+    ],
 })
 export class InputOtpSampleDemo {
     value: any;
--- src/app/showcase/doc/inputotp/templatedoc.ts
+++ src/app/showcase/doc/inputotp/templatedoc.ts
@@ -43,22 +43,52 @@ export class TemplateDoc {
     value: any;
 
     code: Code = {
-        basic: `<p-inputSwitch [(ngModel)]="checked"></p-inputSwitch>`,
+        basic: `<p-inputOtp [(ngModel)]="value">
+        <ng-template pTemplate="input" let-events="events" let-attrs="attrs">
+            <input pInputText class="custom-otp-input" (input)="events.input($event)" type="text" [attr.value]="attrs.value" [maxLength]="1" />
+        </ng-template>
+</p-inputOtp>`,
 
-        html: ` 
-<div class="card flex justify-content-center">
-    <p-inputSwitch [(ngModel)]="checked"></p-inputSwitch>
+        html: `<div class="card flex justify-content-center">
+<p-inputOtp [(ngModel)]="value">
+    <ng-template pTemplate="input" let-events="events" let-attrs="attrs">
+        <input pInputText class="custom-otp-input" (input)="events.input($event)" type="text" [attr.value]="attrs.value" [maxLength]="1" />
+    </ng-template>
+</p-inputOtp>
 </div>`,
 
         typescript: `
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'input-switch-basic-demo',
-    templateUrl: './input-switch-basic-demo.html'
+    selector: 'input-otp-template-demo',
+    templateUrl: './input-otp-template-demo.html',
+    styles: [
+        \`
+        .custom-otp-input {
+            width: 45px;
+            font-size: 36px;
+            border: 0 none;
+            appearance: none;
+            text-align: center;
+            transition: all 0.2s;
+            background: transparent;
+            border-bottom: 2px solid var(--surface-500);
+            border-radius: 0px;
+            margin: 0 0.2rem;
+        }
+
+        .custom-otp-input:focus {
+            outline: 0 none;
+            box-shadow: none;
+            border-bottom-color: var(--primary-color);
+        } 
+        \`
+    ],
 })
-export class InputSwitchBasicDemo {
-    checked: boolean = false;
+
+export class InputOtpTemplateDemo {
+    value: any;
 }`
     };
 }
