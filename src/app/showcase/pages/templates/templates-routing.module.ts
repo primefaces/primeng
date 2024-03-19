2ff932be1cb66afc3f323e9780162907fd671a32
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2024-03-19 11:04:42 +0300

    Refactor

--- src/app/showcase/pages/templates/templates-routing.module.ts
+++ src/app/showcase/pages/templates/templates-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TemplatesComponent } from './templates.component';
-import { LearnMoreComponent } from './learnmore/learnmore.component';
 
 @NgModule({
-    imports: [RouterModule.forChild([{ path: '', pathMatch: 'full', component: TemplatesComponent },  { path: ':id', component: LearnMoreComponent}])],
+    imports: [
+        RouterModule.forChild([
+            { path: '', pathMatch: 'full', component: TemplatesComponent },
+            { path: ':id', loadChildren: () => import('./learnmore/learnmore.module').then((m) => m.LearnMoreModule) }
+        ])
+    ],
     exports: [RouterModule]
 })
 export class TemplatesRoutingModule {}
