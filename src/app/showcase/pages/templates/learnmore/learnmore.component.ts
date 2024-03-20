94cde9136823ca3e78a71dd789fd5a07103ea192
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-03-20 16:32:21 +0300

    redirect added

--- src/app/showcase/pages/templates/learnmore/learnmore.component.ts
+++ src/app/showcase/pages/templates/learnmore/learnmore.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import * as templateData from './templatedata.json';
 @Component({
     selector: 'templates',
     templateUrl: './learnmore.component.html',
-    styleUrl:"./learnmore.scss"
+    styleUrl: './learnmore.scss'
 })
 export class LearnMoreComponent implements OnInit {
-
     id: string;
 
-    selectedTemplate
+    selectedTemplate;
 
-    constructor(private route: ActivatedRoute) {}
+    constructor(private route: ActivatedRoute, private router: Router) {}
 
-    ngOnInit(){
+    ngOnInit() {
         this.id = this.route.snapshot.paramMap.get('id');
         this.selectedTemplate = templateData.templates.find((item: any) => item.name === this.id)?.data;
+        if (!this.selectedTemplate) {
+            this.router.navigateByUrl('/not-found');
+        }
     }
- 
 }
