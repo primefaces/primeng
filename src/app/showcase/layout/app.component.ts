28242834199313a509ee16d804647b527d2eddc9
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2024-03-21 10:42:05 +0300

    Remove unused code

--- src/app/showcase/layout/app.component.ts
+++ src/app/showcase/layout/app.component.ts
@@ -38,12 +38,6 @@ export class AppComponent implements OnInit {
             }
 
             this.bindRouteEvents();
-            this.configService.config.update((val) => ({
-                ...val,
-                theme: 'lara-dark-blue',
-                darkMode: true,
-                ripple: true
-            }));
         });
     }
     ngOnInit(): void {
