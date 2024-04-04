aaf9dd90d2890eaa093efad394e0a46d26c08d02
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-04-04 16:13:18 +0300

    updated editor examples

--- src/app/showcase/doc/editor/basicdoc.ts
+++ src/app/showcase/doc/editor/basicdoc.ts
@@ -8,7 +8,7 @@ import { Code } from '@domain/code';
             <p>Editor provides a default toolbar with common options, to customize it define your elements inside the header element. Refer to <a href="http://quilljs.com/docs/modules/toolbar/">Quill documentation</a> for available controls.</p>
         </app-docsectiontext>
         <div class="card">
-            <p-editor [(ngModel)]="text" [style]="{ height: '320px' }"></p-editor>
+            <p-editor [(ngModel)]="text" [style]="{ height: '320px' }"/>
         </div>
         <app-code [code]="code" selector="editor-basic-demo"></app-code>
     `
@@ -17,19 +17,21 @@ export class BasicDoc {
     text: string | undefined;
 
     code: Code = {
-        basic: `<p-editor [(ngModel)]="text" [style]="{ height: '320px' }"></p-editor>`,
+        basic: `<p-editor [(ngModel)]="text" [style]="{ height: '320px' }"/>`,
 
-        html: `
-<div class="card">
-    <p-editor [(ngModel)]="text" [style]="{ height: '320px' }"></p-editor>
+        html: `<div class="card">
+    <p-editor [(ngModel)]="text" [style]="{ height: '320px' }"/>
 </div>`,
 
-        typescript: `
-import { Component } from '@angular/core';
+        typescript: `import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { EditorModule } from 'primeng/editor';
 
 @Component({
     selector: 'editor-basic-demo',
-    templateUrl: './editor-basic-demo.html'
+    templateUrl: './editor-basic-demo.html',
+    standalone: true,
+    imports: [FormsModule, EditorModule]
 })
 export class EditorBasicDemo {
     text: string | undefined;
