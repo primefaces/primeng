6c51497d429734b7da9a16b17037dbecd51b5826
jacver <92895942+jacver@users.noreply.github.com>
2024-04-11 11:23:42 -0400

    conditional close
    conditional close and docs update

--- src/app/showcase/doc/messages/servicedoc.ts
+++ src/app/showcase/doc/messages/servicedoc.ts
@@ -32,8 +32,8 @@ export class ServiceDoc {
 
     addMultiple() {
         this.messageService.addAll([
-            { severity: 'success', summary: 'Service Message', detail: 'Via MessageService' },
-            { severity: 'info', summary: 'Info Message', detail: 'Via MessageService' }
+            { severity: 'success', summary: 'Service Message', detail: 'Via MessageService', closable: true },
+            { severity: 'info', summary: 'Info Message', detail: 'Via MessageService', closable: false }
         ]);
     }
 
@@ -75,8 +75,8 @@ export class MessagesServiceDemo {
 
     addMultiple() {
         this.messageService.addAll([
-            {severity:'success', summary:'Service Message', detail:'Via MessageService'},
-            {severity:'info', summary:'Info Message', detail:'Via MessageService'}
+            { severity: 'success', summary: 'Service Message', detail: 'Via MessageService', closable: true },
+            { severity: 'info', summary: 'Info Message', detail: 'Via MessageService', closable: false }
         ]);
     }
     
