90a532913071dbe5fe5feda41ede04ff2658543f
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-04-19 12:11:04 +0300

    updated scrollpanel examples

--- src/app/showcase/doc/scrollpanel/basicdoc.ts
+++ src/app/showcase/doc/scrollpanel/basicdoc.ts
@@ -30,16 +30,13 @@ export class BasicDoc {
     code: Code = {
         basic: `<p-scrollPanel [style]="{ width: '100%', height: '150px' }">
     <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Lorem ipsum dolor sit amet...
     </p>
     <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
-        ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium...
     </p>
     <p class="m-0">
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui
-        officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+        At vero eos et accusamus et iusto odio dignissimos...
     </p>
 </p-scrollPanel>`,
 
