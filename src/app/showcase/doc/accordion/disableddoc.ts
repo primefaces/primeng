f94ee243ea2e6a5ce3bb81c34b3b4c8b038112c0
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-04-19 12:09:36 +0300

    updated accordion examples

--- src/app/showcase/doc/accordion/disableddoc.ts
+++ src/app/showcase/doc/accordion/disableddoc.ts
@@ -27,12 +27,7 @@ import { Code } from '@domain/code';
                         qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
                     </p>
                 </p-accordionTab>
-                <p-accordionTab header="Header IV" [disabled]="true">
-                    <p>
-                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
-                        qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
-                    </p>
-                </p-accordionTab>
+                <p-accordionTab header="Header IV" [disabled]="true" />
             </p-accordion>
         </div>
         <app-code [code]="code" selector="accordion-disabled-demo"></app-code>
@@ -43,32 +38,23 @@ export class DisabledDoc {
         basic: `<p-accordion [activeIndex]="0">
     <p-accordionTab header="Header I">
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet...
         </p>
     </p-accordionTab>
     <p-accordionTab header="Header II">
         <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
-            ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium...
         </p>
     </p-accordionTab>
     <p-accordionTab header="Header III">
         <p>
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
-            qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+            At vero eos et accusamus et iusto odio dignissimos...
         </p>
     </p-accordionTab>
-    <p-accordionTab header="Header IV" [disabled]="true">
-        <p>
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
-            qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
-        </p>
-    </p-accordionTab>
+    <p-accordionTab header="Header IV" [disabled]="true" />
 </p-accordion>`,
 
-        html: `
-<div class="card flex justify-content-center">
+        html: `<div class="card flex justify-content-center">
     <p-accordion [activeIndex]="0">
         <p-accordionTab header="Header I">
             <p>
@@ -88,12 +74,7 @@ export class DisabledDoc {
                 qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
             </p>
         </p-accordionTab>
-        <p-accordionTab header="Header IV" [disabled]="true">
-            <p>
-                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
-                qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
-            </p>
-        </p-accordionTab>        
+        <p-accordionTab header="Header IV" [disabled]="true" />  
     </p-accordion>
 </div>`,
 
--- src/app/showcase/doc/accordion/templatedoc.ts
+++ src/app/showcase/doc/accordion/templatedoc.ts
@@ -59,40 +59,49 @@ export class TemplateDoc {
         <p-accordionTab>
             <ng-template pTemplate="header">
                 <span class="flex align-items-center gap-2 w-full">
-                    <p-avatar image="https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png" shape="circle" />
-                    <span class="font-bold white-space-nowrap">Amy Elsner</span>
+                    <p-avatar 
+                        image="https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png" 
+                        shape="circle" />
+                    <span class="font-bold white-space-nowrap">
+                        Amy Elsner
+                    </span>
                     <p-badge value="3" class="ml-auto mr-2" />
                 </span>
             </ng-template>
             <p class="m-0">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet...
             </p>
         </p-accordionTab>
         <p-accordionTab>
             <ng-template pTemplate="header">
                 <span class="flex align-items-center gap-2 w-full">
-                    <p-avatar image="https://primefaces.org/cdn/primeng/images/demo/avatar/onyamalimba.png" shape="circle" />
-                    <span class="font-bold white-space-nowrap">Onyama Limba</span>
+                    <p-avatar 
+                        image="https://primefaces.org/cdn/primeng/images/demo/avatar/onyamalimba.png" 
+                        shape="circle" />
+                    <span class="font-bold white-space-nowrap">
+                        Onyama Limba
+                    </span>
                     <p-badge value="4" class="ml-auto mr-2" />
                 </span>
             </ng-template>
             <p class="m-0">
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
-                ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium...
             </p>
         </p-accordionTab>
         <p-accordionTab>
             <ng-template pTemplate="header">
                 <span class="flex align-items-center gap-2 w-full">
-                    <p-avatar image="https://primefaces.org/cdn/primeng/images/demo/avatar/ionibowcher.png" shape="circle" />
-                    <span class="font-bold white-space-nowrap">Ioni Bowcher</span>
+                    <p-avatar 
+                        image="https://primefaces.org/cdn/primeng/images/demo/avatar/ionibowcher.png" 
+                        shape="circle" />
+                    <span class="font-bold white-space-nowrap">
+                        Ioni Bowcher
+                    </span>
                     <p-badge value="2" class="ml-auto mr-2" />
                 </span>
             </ng-template>
             <p class="m-0">
-                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
-                qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+                At vero eos et accusamus et iusto odio dignissimos...
             </p>
         </p-accordionTab>
 </p-accordion>`,
@@ -102,8 +111,12 @@ export class TemplateDoc {
     <p-accordionTab>
         <ng-template pTemplate="header">
             <span class="flex align-items-center gap-2 w-full">
-                <p-avatar image="https://primefaces.org/cdn/primeng/images/avatar/amyelsner.png" shape="circle" />
-                <span class="font-bold white-space-nowrap">Amy Elsner</span>
+                <p-avatar 
+                    image="https://primefaces.org/cdn/primeng/images/avatar/amyelsner.png" 
+                    shape="circle" />
+                <span class="font-bold white-space-nowrap">
+                    Amy Elsner
+                </span>
                 <p-badge value="3" class="ml-auto mr-2" />
             </span>
         </ng-template>
@@ -115,8 +128,12 @@ export class TemplateDoc {
     <p-accordionTab>
         <ng-template pTemplate="header">
             <span class="flex align-items-center gap-2 w-full">
-                <p-avatar image="https://primefaces.org/cdn/primeng/images/avatar/onyamalimba.png" shape="circle" />
-                <span class="font-bold white-space-nowrap">Onyama Limba</span>
+                <p-avatar 
+                    image="https://primefaces.org/cdn/primeng/images/avatar/onyamalimba.png" 
+                    shape="circle" />
+                <span class="font-bold white-space-nowrap">
+                    Onyama Limba
+                </span>
                 <p-badge value="4" class="ml-auto mr-2" />
             </span>
         </ng-template>
@@ -128,8 +145,12 @@ export class TemplateDoc {
 <p-accordionTab>
     <ng-template pTemplate="header">
         <span class="flex align-items-center gap-2 w-full">
-            <p-avatar image="https://primefaces.org/cdn/primeng/images/avatar/ionibowcher.png" shape="circle" />
-            <span class="font-bold white-space-nowrap">Ioni Bowcher</span>
+            <p-avatar 
+                image="https://primefaces.org/cdn/primeng/images/avatar/ionibowcher.png" 
+                shape="circle" />
+            <span class="font-bold white-space-nowrap">
+                Ioni Bowcher
+            </span>
             <p-badge value="2" class="ml-auto mr-2" />
         </span>
     </ng-template>
