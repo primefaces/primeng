3723a11852d1167faf8723ae6b148c775594f850
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-04-19 10:36:25 +0300

    updated card examples

--- src/app/showcase/doc/card/basicdoc.ts
+++ src/app/showcase/doc/card/basicdoc.ts
@@ -8,7 +8,7 @@ import { Code } from '@domain/code';
             <p>A simple Card is created with a <i>header</i> property along with the content as children.</p>
         </app-docsectiontext>
         <div class="card flex justify-content-center">
-            <p-card header="Title">
+            <p-card header="Simple Card">
                 <p class="m-0">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
                     quas!
@@ -20,16 +20,15 @@ import { Code } from '@domain/code';
 })
 export class BasicDoc {
     code: Code = {
-        basic: `<p-card header="Title">
+        basic: `<p-card header="Simple Card">
     <p class="m-0">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
         quas!
     </p>
 </p-card>`,
 
-        html: `
-<div class="card flex justify-content-center">
-    <p-card header="Title">
+        html: `<div class="card flex justify-content-center">
+    <p-card header="Simple Card">
         <p class="m-0">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
             quas!
@@ -37,12 +36,14 @@ export class BasicDoc {
     </p-card>
 </div>`,
 
-        typescript: `
-import { Component } from '@angular/core';
+        typescript: `import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
 
 @Component({
     selector: 'card-basic-demo',
-    templateUrl: './card-basic-demo.html'
+    templateUrl: './card-basic-demo.html',
+    standalone: true,
+    imports: [CardModule]
 })
 export class CardBasicDemo {}`
     };
