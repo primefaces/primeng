55da69987becc440149ac002a4478bc856ba7669
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-04-25 13:34:19 +0300

    updated fileupload examples

--- src/app/showcase/doc/fileupload/basicdoc.ts
+++ src/app/showcase/doc/fileupload/basicdoc.ts
@@ -14,8 +14,8 @@ interface UploadEvent {
             <p>FileUpload basic <i>mode</i> provides a simpler UI as an alternative to default advanced mode.</p>
         </app-docsectiontext>
         <div class="card flex justify-content-center">
-            <p-toast></p-toast>
-            <p-fileUpload mode="basic" chooseLabel="Choose" name="demo[]" url="https://www.primefaces.org/cdn/api/upload.php" accept="image/*" maxFileSize="1000000" (onUpload)="onUpload($event)"></p-fileUpload>
+            <p-toast />
+            <p-fileUpload mode="basic" chooseLabel="Choose" chooseIcon="pi pi-upload" name="demo[]" url="https://www.primefaces.org/cdn/api/upload.php" accept="image/*" maxFileSize="1000000" (onUpload)="onUpload($event)" />
         </div>
         <app-code [code]="code" selector="file-upload-basic-demo"></app-code>
     `,
@@ -29,15 +29,31 @@ export class BasicDoc {
     }
 
     code: Code = {
-        basic: `<p-fileUpload mode="basic" chooseLabel="Choose" name="demo[]" url="https://www.primefaces.org/cdn/api/upload.php" accept="image/*" maxFileSize="1000000" (onUpload)="onUpload($event)"></p-fileUpload>`,
-        html: `
-<div class="card flex justify-content-center">
-    <p-toast></p-toast>
-    <p-fileUpload mode="basic" chooseLabel="Choose" name="demo[]" url="https://www.primefaces.org/cdn/api/upload.php" accept="image/*" maxFileSize="1000000" (onUpload)="onUpload($event)"></p-fileUpload>
+        basic: `<p-fileUpload 
+    mode="basic" 
+    chooseLabel="Choose" 
+    chooseIcon="pi pi-upload"
+    name="demo[]" 
+    url="https://www.primefaces.org/cdn/api/upload.php" 
+    accept="image/*" 
+    maxFileSize="1000000" 
+    (onUpload)="onUpload($event)" />`,
+        html: `<div class="card flex justify-content-center">
+    <p-toast />
+    <p-fileUpload 
+        mode="basic" 
+        chooseLabel="Choose" 
+        chooseIcon="pi pi-upload"
+        name="demo[]" 
+        url="https://www.primefaces.org/cdn/api/upload.php" 
+        accept="image/*" 
+        maxFileSize="1000000" 
+        (onUpload)="onUpload($event)" />
 </div>`,
-        typescript: `
-import { Component } from '@angular/core';
+        typescript: `import { Component } from '@angular/core';
 import { MessageService } from 'primeng/api';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ToastModule } from 'primeng/toast';
 
 interface UploadEvent {
     originalEvent: Event;
@@ -47,6 +63,8 @@ interface UploadEvent {
 @Component({
     selector: 'file-upload-basic-demo',
     templateUrl: './file-upload-basic-demo.html',
+    standalone: true,
+    imports: [FileUploadModule, ToastModule],
     providers: [MessageService]
 })
 export class FileUploadBasicDemo {
