7e6b636b4bf1a712272c44c827643a481d068845
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-05-02 18:07:51 +0300

    updated badge examples

--- src/app/showcase/doc/badge/basicdoc.ts
+++ src/app/showcase/doc/badge/basicdoc.ts
@@ -8,24 +8,25 @@ import { Code } from '@domain/code';
             <p>Content of the badge is specified using the <i>value</i> property.</p>
         </app-docsectiontext>
         <div class="card flex justify-content-center">
-            <p-badge [value]="2"></p-badge>
+            <p-badge [value]="2" />
         </div>
         <app-code [code]="code" selector="badge-basic-demo"></app-code>
     `
 })
 export class BasicDoc {
     code: Code = {
-        basic: `<p-badge [value]="2"></p-badge>`,
-        html: `
-<div class="card flex justify-content-center">
-    <p-badge [value]="2"></p-badge>
+        basic: `<p-badge [value]="2" />`,
+        html: `<div class="card flex justify-content-center">
+    <p-badge [value]="2" />
 </div>`,
-        typescript: `
-import { Component } from '@angular/core';
+        typescript: `import { Component } from '@angular/core';
+import { BadgeModule } from 'primeng/badge';
 
 @Component({
     selector: 'badge-basic-demo',
-    templateUrl: './badge-basic-demo.html'
+    templateUrl: './badge-basic-demo.html',
+    standalone: true,
+    imports: [BadgeModule]
 })
 export class BadgeBasicDemo {}`
     };
