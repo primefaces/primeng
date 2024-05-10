d10cb096a1345d3228881cc3437d43c57ed4c010
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2024-05-10 13:21:25 +0300

    Fixed #15482 - Rework and update focustrap

--- src/app/components/focustrap/focustrap.ts
+++ src/app/components/focustrap/focustrap.ts
@@ -1,7 +1,7 @@
 import { DomHandler } from 'primeng/dom';
+import { CommonModule, DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { Directive, ElementRef, Input, NgModule, inject, booleanAttribute, PLATFORM_ID } from '@angular/core';
 
-import { CommonModule } from '@angular/common';
-import { Directive, ElementRef, HostListener, Input, NgModule, inject, booleanAttribute } from '@angular/core';
 /**
  * Focus Trap keeps focus within a certain DOM element while tabbing.
  * @group Components
@@ -19,20 +19,68 @@ export class FocusTrap {
      */
     @Input({ transform: booleanAttribute }) pFocusTrapDisabled: boolean = false;
 
+    platformId = inject(PLATFORM_ID);
+
     host: ElementRef = inject(ElementRef);
 
-    @HostListener('keydown.tab', ['$event'])
-    @HostListener('keydown.shift.tab', ['$event'])
-    onkeydown(e: KeyboardEvent) {
-        if (this.pFocusTrapDisabled !== true) {
-            e.preventDefault();
-            const focusableElement = DomHandler.getNextFocusableElement(this.host.nativeElement, e.shiftKey);
-            if (focusableElement) {
-                focusableElement.focus();
-                focusableElement.select?.();
-            }
+    document: Document = inject(DOCUMENT);
+
+    firstHiddenFocusableElement!: HTMLElement;
+
+    lastHiddenFocusableElement!: HTMLElement;
+
+    ngOnInit() {
+        if (isPlatformBrowser(this.platformId) && !this.pFocusTrapDisabled) {
+            this.createHiddenFocusableElements();
         }
     }
+
+    getComputedSelector(selector) {
+        return `:not(.p-hidden-focusable):not([data-p-hidden-focusable="true"])${selector ?? ''}`;
+    }
+
+    createHiddenFocusableElements() {
+        const tabindex = '0';
+
+        const createFocusableElement = (onFocus) => {
+            return DomHandler.createElement('span', {
+                class: 'p-hidden-accessible p-hidden-focusable',
+                tabindex,
+                role: 'presentation',
+                'aria-hidden': true,
+                'data-p-hidden-accessible': true,
+                'data-p-hidden-focusable': true,
+                onFocus: onFocus?.bind(this)
+            });
+        };
+
+        this.firstHiddenFocusableElement = createFocusableElement(this.onFirstHiddenElementFocus);
+        this.lastHiddenFocusableElement = createFocusableElement(this.onLastHiddenElementFocus);
+
+        this.firstHiddenFocusableElement.setAttribute('data-pc-section', 'firstfocusableelement');
+        this.lastHiddenFocusableElement.setAttribute('data-pc-section', 'lastfocusableelement');
+
+        this.host.nativeElement.prepend(this.firstHiddenFocusableElement);
+        this.host.nativeElement.append(this.lastHiddenFocusableElement);
+    }
+
+    onFirstHiddenElementFocus(event) {
+        const { currentTarget, relatedTarget } = event;
+        const focusableElement =
+            relatedTarget === this.lastHiddenFocusableElement || !this.host.nativeElement?.contains(relatedTarget) ? DomHandler.getFirstFocusableElement(currentTarget.parentElement, ':not(.p-hidden-focusable)') : this.lastHiddenFocusableElement;
+
+        DomHandler.focus(focusableElement);
+    }
+
+    onLastHiddenElementFocus(event) {
+        const { currentTarget, relatedTarget } = event;
+        const focusableElement =
+            relatedTarget === this.firstHiddenFocusableElement || !this.host.nativeElement?.contains(relatedTarget) ? DomHandler.getLastFocusableElement(currentTarget.parentElement, ':not(.p-hidden-focusable)') : this.firstHiddenFocusableElement;
+
+        DomHandler.focus(focusableElement);
+    }
+
+    ngOnDestroy() {}
 }
 
 @NgModule({
