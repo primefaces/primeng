84edff46473f59d7e732049907753c8c05f25d5d
Diego Franchina <diegofranchina@gmail.com>
2024-05-20 12:10:41 +0200

    Added import in toolbar

--- src/app/showcase/doc/toolbar/importdoc.ts
+++ src/app/showcase/doc/toolbar/importdoc.ts
@@ -7,6 +7,7 @@ import { Code } from '@domain/code';
 })
 export class ImportDoc {
     code: Code = {
-        typescript: `import { ToolbarModule } from 'primeng/toolbar';`
+        typescript: `import { ToolbarModule } from 'primeng/toolbar';
+import { SharedModule } from 'primeng/api'; // Required if you will use Template`
     };
 }
