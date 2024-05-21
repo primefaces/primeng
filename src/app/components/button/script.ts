6285493b1965f8ea992a262f8a09c79ec9002fa4
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2024-05-21 10:35:24 +0300

    Add script and style

---
+++ src/app/components/button/script.ts
@@ -0,0 +1,20 @@
+const theme = require('./style.ts');
+//@ts-ignore
+const fs = require('fs');
+
+const variables = {
+    'button.primary.color': 'red',
+    'button.primary.background': 'blue'
+};
+
+const getKey = (value) => {
+    return variables[value];
+};
+
+const _theme = theme.theme;
+const k = _theme({ dt: (key) => `${getKey(key)}` });
+
+fs.writeFile('src/app/components/button/button.css', k, (err) => {
+    if (err) throw err;
+    console.log('File has been saved');
+});
