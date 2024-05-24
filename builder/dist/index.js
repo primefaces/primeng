a78a39fc6f17c3884d4c20306214f6cb8d307c68
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2024-05-24 10:55:31 +0300

    Improve script

--- builder/dist/index.js
+++ builder/dist/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const architect_1 = require("@angular-devkit/architect");
 const core_1 = require("@angular-devkit/core");
 const fs_1 = require("fs");
+const _path = require("path");
 exports.default = (0, architect_1.createBuilder)(styleBuilder);
 const components = ['button'];
 const variables = {
@@ -16,16 +17,24 @@ async function styleBuilder(options, ctx) {
     // builder logic
     ctx.logger.info('Building started');
     try {
-        const path = `${(0, core_1.getSystemPath)((0, core_1.normalize)(ctx.workspaceRoot))}/`;
-        for (let component in components) {
-            const _c = components[component];
-            const { theme } = await Promise.resolve(`${`${path}/src/app/components/${_c}/style.ts`}`).then(s => require(s));
-            (0, fs_1.writeFile)(`${path}/src/app/components/${_c}/${_c}.css`, theme({ dt: (key) => `${getKey(key)}` }), (err) => {
-                if (err)
-                    throw err;
-                ctx.logger.info('File has been saved');
-            });
-        }
+        const componentsPath = `${(0, core_1.getSystemPath)((0, core_1.normalize)(ctx.workspaceRoot))}/src/app/components`;
+        const components = (0, fs_1.readdirSync)(componentsPath, { withFileTypes: true })
+            .filter((dirent) => dirent.isDirectory())
+            .map((dirent) => dirent.name)
+            .filter((folder) => {
+            const _f = (0, fs_1.readdirSync)(_path.join(componentsPath, folder));
+            return _f.some((file) => _path.extname(file) === '.css');
+        });
+        console.log(components);
+        // const path = `${getSystemPath(normalize(ctx.workspaceRoot))}/`;
+        // for (let component in components) {
+        //     const _c = components[component];
+        //     const { theme } = await import(`${path}/src/app/components/${_c}/style.ts`);
+        //     writeFile(`${path}/src/app/components/${_c}/${_c}.css`, theme({ dt: (key) => `${getKey(key)}` }), (err) => {
+        //         if (err) throw err;
+        //         ctx.logger.info('File has been saved');
+        //     });
+        // }
     }
     catch (err) { }
     return { success: true };
--- builder/src/index.ts
+++ builder/src/index.ts
@@ -1,6 +1,7 @@
 import { BuilderContext, BuilderOutput, createBuilder } from '@angular-devkit/architect';
 import { JsonObject, getSystemPath, normalize } from '@angular-devkit/core';
-import { writeFile } from 'fs';
+import { writeFile, readdirSync } from 'fs';
+import * as _path from 'path';
 
 interface Options extends JsonObject {
     source: string;
@@ -24,15 +25,22 @@ async function styleBuilder(options: Options, ctx: BuilderContext): Promise<Buil
     // builder logic
     ctx.logger.info('Building started');
     try {
-        const path = `${getSystemPath(normalize(ctx.workspaceRoot))}/`;
+        const componentsPath = `${getSystemPath(normalize(ctx.workspaceRoot))}/src/app/components`;
+        const components = readdirSync(componentsPath, { withFileTypes: true })
+            .filter((dirent) => dirent.isDirectory())
+            .map((dirent) => dirent.name)
+            .filter((folder) => {
+                const _f = readdirSync(_path.join(componentsPath, folder));
+                return _f.some((file) => _path.extname(file) === '.css');
+            });
 
         for (let component in components) {
             const _c = components[component];
-            const { theme } = await import(`${path}/src/app/components/${_c}/style.ts`);
-            writeFile(`${path}/src/app/components/${_c}/${_c}.css`, theme({ dt: (key) => `${getKey(key)}` }), (err) => {
-                if (err) throw err;
-                ctx.logger.info('File has been saved');
-            });
+            const { theme } = await import(`${componentsPath}/${_c}/style.ts`);
+            theme &&
+                writeFile(`${componentsPath}/${_c}/${_c}.css`, theme({ dt: (key) => `${getKey(key)}` }), (err) => {
+                    if (err) throw err;
+                });
         }
     } catch (err) {}
     return { success: true };
