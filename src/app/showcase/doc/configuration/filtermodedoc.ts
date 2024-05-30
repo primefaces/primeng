47d61a501d961ff7ea2827ae08bc95ed9bb5bd9b
Khayo Vannucci <khayovannucci@gmail.com>
2024-05-30 17:44:45 -0300

    Update filtermodedoc.ts
    add import to code exemple

--- src/app/showcase/doc/configuration/filtermodedoc.ts
+++ src/app/showcase/doc/configuration/filtermodedoc.ts
@@ -12,7 +12,7 @@ import { Code } from '@domain/code';
 })
 export class FilterModeDoc {
     code: Code = {
-        typescript: `import { PrimeNGConfig } from 'primeng/api';
+        typescript: `import { FilterMatchMode, PrimeNGConfig } from 'primeng/api';
 
 @Component({
     selector: 'app-root',
