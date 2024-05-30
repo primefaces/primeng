0009d57c4864f7105a42c542bd81230961b1548e
timon riemslagh <tri@myharmoney.eu>
2024-05-30 13:59:30 +0200

    fix(splitbutton): 15710 fix aria expanded issue

--- src/app/components/splitbutton/splitbutton.ts
+++ src/app/components/splitbutton/splitbutton.ts
@@ -89,6 +89,8 @@ type SplitButtonIconPosition = 'left' | 'right';
                 [appendTo]="appendTo"
                 [showTransitionOptions]="showTransitionOptions"
                 [hideTransitionOptions]="hideTransitionOptions"
+                (onHide)="onHide()"
+                (onShow)="onShow()"
             ></p-tieredMenu>
         </div>
     `,
@@ -225,6 +227,16 @@ export class SplitButton {
      */
     @Output() onClick: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
     /**
+     * Callback to invoke when overlay menu is hidden.
+     * @group Emits
+     */
+    @Output() onMenuHide: EventEmitter<any> = new EventEmitter<any>();
+    /**
+     * Callback to invoke when overlay menu is shown.
+     * @group Emits
+     */
+    @Output() onMenuShow: EventEmitter<any> = new EventEmitter<any>();
+    /**
      * Callback to invoke when dropdown button is clicked.
      * @param {MouseEvent} event - Mouse event.
      * @group Emits
@@ -331,7 +343,6 @@ export class SplitButton {
     onDropdownButtonClick(event?: MouseEvent) {
         this.onDropdownClick.emit(event);
         this.menu?.toggle({ currentTarget: this.containerViewChild?.nativeElement, relativeAlign: this.appendTo == null });
-        this.isExpanded.set(this.menu.visible);
     }
 
     onDropdownButtonKeydown(event: KeyboardEvent) {
@@ -339,6 +350,16 @@ export class SplitButton {
             this.onDropdownButtonClick();
             event.preventDefault();
         }
+    }
+
+    onHide() {
+        this.isExpanded.set(false);
+        this.onMenuHide.emit();
+    }
+
+    onShow() {
+        this.isExpanded.set(true);
+        this.onMenuShow.emit();
     }
 }
 
