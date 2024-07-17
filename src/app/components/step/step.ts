de14fed2d5833d1e970e2624dc59ba1bc7678053
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-07-17 17:54:47 +0300

    add stepclick

--- src/app/components/step/step.ts
+++ src/app/components/step/step.ts
@@ -5,24 +5,22 @@ import { Nullable } from 'primeng/ts-helpers';
 import { UniqueComponentId } from 'primeng/utils';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { StepperSeparator } from '../stepper/stepperseparator';
+import { Stepper } from 'primeng/stepper';
 
 @Component({
     selector: 'p-step',
     standalone: true,
     template: `
         <div class="p-step">
-            <!-- <button [id]="id" class="p-step-header" role="tab" type="button" [tabindex]="disabled ? -1 : undefined" [attr.aria-controls]="ariaControls" [disabled]="disabled" (click)="onStepClick()"> -->
-            <button class="p-step-header">
+            <button [id]="id" class="p-step-header" role="tab" type="button" [tabindex]="disabled ? -1 : undefined" [attr.aria-controls]="ariaControls" [disabled]="disabled" (click)="onStepClick()">
                 <span class="p-step-number">{{ activeValue }}</span>
                 <span class="p-step-title">
                     <ng-content></ng-content>
                 </span>
             </button>
-            <!-- </button>  -->
             <p-stepperSeparator></p-stepperSeparator>
         </div>
     `,
-    changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None,
     styleUrls: ['./step.css'],
     host: {
@@ -32,10 +30,35 @@ import { StepperSeparator } from '../stepper/stepperseparator';
 })
 export class Step {
     @Input() value: string | number | undefined;
-    
+
     @Input({ transform: booleanAttribute }) disabled: boolean = false;
 
-    get activeValue(){
-        return 2
+    isSeparatorVisible = false
+
+    constructor(private stepper: Stepper) {}
+
+    get active() {
+        return this.stepper.isStepActive(this.activeValue);
+    }
+
+    get ariaControls() {
+        return `${this.stepper?.id}_steppanel_${this.activeValue}`;
+    }
+    
+    get activeValue() {
+        // return !!this.$pcStepItem ? this.$pcStepItem?.value : this.value;
+        return this.value;
+    }
+
+    get id() {
+        return `${this.stepper.id}_step_${this.activeValue}`;
+    }
+
+    isStepDisabled() {
+        return this.stepper.isStepDisabled();
+    }
+
+    onStepClick() {
+        this.stepper.updateValue(this.activeValue);
     }
 }
