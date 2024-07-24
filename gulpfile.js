48cdbd4c16503d31f65e605ec66547192082a3c1
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2024-07-24 19:05:43 +0300

    Remove unused css

--- gulpfile.js
+++ gulpfile.js
@@ -6,24 +6,6 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     flatten = require('gulp-flatten');
 
-gulp.task('build-css', function () {
-    return gulp.src(['src/app/components/common/common.css', 'src/app/components/**/*.css']).pipe(concat('primeng.css')).pipe(gulp.dest('dist/resources'));
-});
-
-gulp.task('build-css-prod', function () {
-    return gulp
-        .src(['src/app/components/common/common.css', 'src/app/components/ripple/ripple.css', 'src/app/components/tooltip/tooltip.css'])
-        .pipe(concat('primeng.css'))
-        .pipe(gulp.dest('dist/resources'))
-        .pipe(uglifycss({ uglyComments: true }))
-        .pipe(rename('primeng.min.css'))
-        .pipe(gulp.dest('dist/resources'));
-});
-
-gulp.task('copy-component-css', function () {
-    return gulp.src(['src/app/components/**/*.css', 'src/app/components/**/images/*.png', 'src/app/components/**/images/*.gif']).pipe(gulp.dest('dist/resources/components'));
-});
-
 gulp.task('images', function () {
     return gulp.src(['src/app/components/**/images/*.png', 'src/app/components/**/images/*.gif']).pipe(flatten()).pipe(gulp.dest('dist/resources/images'));
 });
