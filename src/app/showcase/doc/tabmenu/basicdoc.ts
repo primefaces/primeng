96fc148488e5edfb49e542b88d0cd7d35c4798ca
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-07-24 15:19:35 +0300

    revert demo changes

--- src/app/showcase/doc/tabmenu/basicdoc.ts
+++ src/app/showcase/doc/tabmenu/basicdoc.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { Code } from '@domain/code';
 
@@ -9,7 +9,7 @@ import { Code } from '@domain/code';
             <p>TabMenu requires a collection of menuitems as its model.</p>
         </app-docsectiontext>
         <div class="card">
-        <p-tabMenu [scrollable]="true" [model]="items" [activeItem]="activeItem"></p-tabMenu>
+            <p-tabMenu [model]="items" />
         </div>
         <app-code [code]="code" selector="tab-menu-basic-demo"></app-code>
     `
@@ -17,21 +17,13 @@ import { Code } from '@domain/code';
 export class BasicDoc implements OnInit {
     items: MenuItem[] | undefined;
 
-    activeItem: MenuItem | undefined;
-
-    constructor(private cd:ChangeDetectorRef){
-        
-    }
-
     ngOnInit() {
-      this.items = Array.from({ length: 50 }, (_, i) => ({
-        label: `Tab ${i + 1}`,
-      }));
-  
-      setTimeout((_) => {
-        this.activeItem = this.items[40];
-        this.cd.markForCheck()
-      }, 2000);
+        this.items = [
+            { label: 'Dashboard', icon: 'pi pi-home' },
+            { label: 'Transactions', icon: 'pi pi-chart-line' },
+            { label: 'Products', icon: 'pi pi-list' },
+            { label: 'Messages', icon: 'pi pi-inbox' }
+        ];
     }
 
     code: Code = {
--- src/app/showcase/pages/tabmenu/tabmenudemo.ts
+++ src/app/showcase/pages/tabmenu/tabmenudemo.ts
@@ -13,12 +13,45 @@ import { RouterDoc } from '@doc/tabmenu/routerdoc';
 })
 export class TabMenuDemo {
     docs = [
-     
+        {
+            id: 'import',
+            label: 'Import',
+            component: ImportDoc
+        },
         {
             id: 'basic',
             label: 'Basic',
             component: BasicDoc
         },
-     
+        {
+            id: 'controlled',
+            label: 'Controlled',
+            component: ControlledDoc
+        },
+        {
+            id: 'template',
+            label: 'Template',
+            component: TemplateDoc
+        },
+        {
+            id: 'command',
+            label: 'Command',
+            component: CommandDoc
+        },
+        {
+            id: 'router',
+            label: 'Router',
+            component: RouterDoc
+        },
+        {
+            id: 'style',
+            label: 'Style',
+            component: StyleDoc
+        },
+        {
+            id: 'accessibility',
+            label: 'Accessibility',
+            component: AccessibilityDoc
+        }
     ];
 }
