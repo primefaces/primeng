fe04715b94f5296bfd21850e388d4edbe9cb9bb2
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-08-14 14:06:32 +0300

    fix typo

--- src/app/showcase/pages/drawer/drawerdemo.module.ts
+++ src/app/showcase/pages/drawer/drawerdemo.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { DrawerDocModule } from '@doc/drawer/sidebardoc.module';
+import { DrawerDocModule } from '@doc/drawer/drawerdoc.module';
 import { DrawerDemo } from './drawerdemo';
 import { DrawerDemoRoutingModule } from './drawerdemo-routing.module';
 
