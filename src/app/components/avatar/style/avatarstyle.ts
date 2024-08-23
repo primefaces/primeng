3ee2f6e6df02338f19f43019fc14ce05503630c9
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2024-08-23 17:31:06 +0300

    code format

--- src/app/components/avatar/style/avatarstyle.ts
+++ src/app/components/avatar/style/avatarstyle.ts
@@ -75,11 +75,11 @@ const classes = {
             'p-avatar-image': props.image != null,
             'p-avatar-circle': props.shape === 'circle',
             'p-avatar-lg': props.size === 'large',
-            'p-avatar-xl': props.size === 'xlarge'
-        }
+            'p-avatar-xl': props.size === 'xlarge',
+        },
     ],
     label: 'p-avatar-label',
-    icon: 'p-avatar-icon'
+    icon: 'p-avatar-icon',
 };
 
 @Injectable()
