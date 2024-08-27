3e7025525499f0e52b682fabc8afad74171975dd
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-08-27 12:53:30 +0300

    update inputnumber docs

--- src/app/showcase/doc/inputnumber/localedoc.ts
+++ src/app/showcase/doc/inputnumber/localedoc.ts
@@ -7,7 +7,7 @@ import { Code } from '@domain/code';
         <app-docsectiontext>
             <p>Localization information such as grouping and decimal symbols are defined with the <i>locale</i> property which defaults to the user locale.</p>
         </app-docsectiontext>
-        <div class="card flex flex-wrap gap-4 p-fluid">
+        <p-fluid class="card flex flex-wrap gap-4">
             <div class="flex-auto">
                 <label class="block font-bold mb-2" for="locale-user">User Locale</label>
                 <p-inputNumber [(ngModel)]="value1" inputId="locale-user" [minFractionDigits]="2" />
@@ -24,7 +24,7 @@ import { Code } from '@domain/code';
                 <label class="block font-bold mb-2" for="locale-indian">Indian Locale</label>
                 <p-inputNumber [(ngModel)]="value4" inputId="locale-indian" mode="decimal" locale="en-IN" [minFractionDigits]="2" />
             </div>
-        </div>
+        </p-fluid>
         <app-code [code]="code" selector="input-number-locale-demo"></app-code>
     `
 })
@@ -64,7 +64,7 @@ export class LocaleDoc {
     locale="en-IN" 
     [minFractionDigits]="2" />`,
 
-        html: `<div class="card flex flex-wrap gap-4 p-fluid">
+        html: `<p-fluid class="card flex flex-wrap gap-4">
     <div class="flex-auto">
         <label class="block font-bold mb-2" for="locale-user">
             User Locale
@@ -107,17 +107,18 @@ export class LocaleDoc {
             locale="en-IN" 
             [minFractionDigits]="2" /> 
     </div>
-</div>`,
+</p-fluid>`,
 
         typescript: `import { Component } from '@angular/core';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { FormsModule } from '@angular/forms';
+import { FluidModule } from 'primeng/fluid';
 
 @Component({
     selector: 'input-number-locale-demo',
     templateUrl: './input-number-locale-demo.html',
     standalone: true,
-    imports: [FormsModule, InputNumberModule]
+    imports: [FormsModule, InputNumberModule, FluidModule]
 })
 export class InputNumberLocaleDemo {
     value1: number = 151351;
