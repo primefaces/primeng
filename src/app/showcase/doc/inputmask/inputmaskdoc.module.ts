340e0ffeb6d7e840ecba2773b7ad879c2631cdee
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-08-27 12:41:10 +0300

    update inputmask | maskdoc

--- src/app/showcase/doc/inputmask/inputmaskdoc.module.ts
+++ src/app/showcase/doc/inputmask/inputmaskdoc.module.ts
@@ -18,10 +18,34 @@ import { SlotCharDoc } from './slotchardoc';
 import { StyleDoc } from './styledoc';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { FilledDoc } from './filleddoc';
+import { FluidModule } from 'primeng/fluid';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, InputMaskModule, RouterModule, AppCodeModule, AppDocModule, FloatLabelModule],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        InputMaskModule,
+        RouterModule,
+        AppCodeModule,
+        AppDocModule,
+        FloatLabelModule,
+        FluidModule,
+    ],
     exports: [AppDocModule],
-    declarations: [ImportDoc, BasicDoc, MaskDoc, SlotCharDoc, OptionalDoc, FloatlabelDoc, DisabledDoc, InvalidDoc, StyleDoc, AccessibilityDoc, ReactiveFormsDoc, FilledDoc]
+    declarations: [
+        ImportDoc,
+        BasicDoc,
+        MaskDoc,
+        SlotCharDoc,
+        OptionalDoc,
+        FloatlabelDoc,
+        DisabledDoc,
+        InvalidDoc,
+        StyleDoc,
+        AccessibilityDoc,
+        ReactiveFormsDoc,
+        FilledDoc,
+    ],
 })
 export class InputMaskDocModule {}
