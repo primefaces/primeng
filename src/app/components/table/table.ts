eec7d04bc4ca2329be25010718465c38ac955d51
Marcio Tasca Junior <marcio.junior@triersistemas.com.br>
2024-08-28 13:24:41 -0300

    fix editable column disabled temporarily

--- src/app/components/table/table.ts
+++ src/app/components/table/table.ts
@@ -4191,7 +4191,9 @@ export class EditableColumn implements OnChanges, AfterViewInit, OnDestroy {
     ) {}
 
     public ngOnChanges(changes: SimpleChanges): void {
-        if (this.el.nativeElement && !changes.data?.firstChange) {
+        if (changes.pEditableColumnDisabled?.currentValue === true && DomHandler.hasClass(this.el.nativeElement, 'p-cell-editing')) {
+            this.closeEditingCell(false, new Event('cancel'))
+        } else if (changes.data?.firstChange === false && this.el.nativeElement) {
             this.dt.updateEditingCell(this.el.nativeElement, this.data, this.field, <number>this.rowIndex);
         }
     }
