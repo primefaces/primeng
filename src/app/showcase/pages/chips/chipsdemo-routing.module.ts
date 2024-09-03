447a1064ae7e88cbc3e6751eff1bfa7716fd4cf2
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2024-09-03 13:06:51 +0300

    Remove Chips component, fixes #16091

--- src/app/showcase/pages/chips/chipsdemo-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { ChipsDemo } from './chipsdemo';
-
-@NgModule({
-    imports: [RouterModule.forChild([{ path: '', component: ChipsDemo }])],
-    exports: [RouterModule]
-})
-export class ChipsDemoRoutingModule {}
