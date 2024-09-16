daa57faf0da82b267dded985612372bc9fe16f1c
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-09-16 09:49:53 +0300

    update confirmpopup headless doc

--- src/app/showcase/doc/confirmpopup/headlessdoc.ts
+++ src/app/showcase/doc/confirmpopup/headlessdoc.ts
@@ -1,7 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { Code } from '@domain/code';
-import { ConfirmPopup } from 'primeng/confirmpopup';
 @Component({
     selector: 'confirm-popup-headless-demo',
     template: `
@@ -10,13 +9,13 @@ import { ConfirmPopup } from 'primeng/confirmpopup';
         </app-docsectiontext>
         <div class="card flex justify-center">
             <p-toast />
-            <p-confirmpopup #confirmPopupRef>
+            <p-confirmpopup #cp>
                 <ng-template pTemplate="headless" let-message>
                     <div class="rounded p-4">
                         <span>{{ message.message }}</span>
                         <div class="flex items-center gap-2 mt-4">
-                            <p-button (onClick)="accept()" label="Save" size="small" />
-                            <p-button (onClick)="reject()" label="Cancel" [text]="true" size="small" severity="secondary" />
+                            <p-button (onClick)="cp.accept()" label="Save" size="small" />
+                            <p-button (onClick)="cp.reject()" label="Cancel" [text]="true" size="small" severity="secondary" />
                         </div>
                     </div>
                 </ng-template>
@@ -33,16 +32,6 @@ export class HeadlessDoc {
         private messageService: MessageService,
     ) {}
 
-    @ViewChild(ConfirmPopup) confirmPopup!: ConfirmPopup;
-
-    accept() {
-        this.confirmPopup.accept();
-    }
-
-    reject() {
-        this.confirmPopup.reject();
-    }
-
     confirm(event: Event) {
         this.confirmationService.confirm({
             target: event.target as EventTarget,
@@ -58,13 +47,13 @@ export class HeadlessDoc {
 
     code: Code = {
         basic: `<p-toast />
-    <p-confirmpopup #confirmPopupRef>
+    <p-confirmpopup #cp>
         <ng-template pTemplate="headless" let-message>
             <div class="rounded p-4">
                 <span>{{ message.message }}</span>
                 <div class="flex items-center gap-2 mt-4">
-                    <p-button (onClick)="accept()" label="Save" size="small" />
-                    <p-button (onClick)="reject()" label="Cancel" [text]="true" size="small" severity="secondary" />
+                    <p-button (onClick)="cp.accept()" label="Save" size="small" />
+                    <p-button (onClick)="cp.reject()" label="Cancel" [text]="true" size="small" severity="secondary" />
                 </div>
             </div>
         </ng-template>
@@ -73,13 +62,13 @@ export class HeadlessDoc {
 
         html: `<div class="card flex justify-center gap-2">
     <p-toast />
-    <p-confirmpopup #confirmPopupRef>
+    <p-confirmpopup #cp>
         <ng-template pTemplate="headless" let-message>
             <div class="rounded p-4">
                 <span>{{ message.message }}</span>
                 <div class="flex items-center gap-2 mt-4">
-                    <p-button (onClick)="accept()" label="Save" size="small" />
-                    <p-button (onClick)="reject()" label="Cancel" [text]="true" size="small" severity="secondary" />
+                    <p-button (onClick)="cp.accept()" label="Save" size="small" />
+                    <p-button (onClick)="cp.reject()" label="Cancel" [text]="true" size="small" severity="secondary" />
                 </div>
             </div>
         </ng-template>
@@ -87,9 +76,9 @@ export class HeadlessDoc {
 <p-button (onClick)="confirm($event)" label="Save" />
 </div>`,
 
-        typescript: `import { Component, ViewChild } from '@angular/core';
+        typescript: `import { Component } from '@angular/core';
 import { ConfirmationService, MessageService } from 'primeng/api';
-import { ConfirmPopup, ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
 import { ToastModule } from 'primeng/toast';
 import { ButtonModule } from 'primeng/button';
 
@@ -102,16 +91,6 @@ import { ButtonModule } from 'primeng/button';
 })
 export class ConfirmPopupHeadlessDemo {
    constructor(private confirmationService: ConfirmationService, private messageService: MessageService) {}
-
-    @ViewChild(ConfirmPopup) confirmPopup!: ConfirmPopup;
-
-    accept() {
-        this.confirmPopup.accept();
-    }
-
-    reject() {
-        this.confirmPopup.reject();
-    }
 
     confirm(event: Event) {
         this.confirmationService.confirm({
