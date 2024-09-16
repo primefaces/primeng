49318c8fbd85f93819b5985617c9628ec6957876
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-09-16 17:01:20 +0300

    add template separator

--- src/app/showcase/pages/templates/freya/freyaseparator.ts
+++ src/app/showcase/pages/templates/freya/freyaseparator.ts
@@ -1,20 +1,20 @@
 import { Component, inject } from '@angular/core';
+import { TemplateSeparatorModule } from '@layout/templates/templateseparator';
 import { AppConfigService } from '@service/appconfigservice';
 
 @Component({
     standalone: true,
     selector: 'freya-separator',
+    imports: [TemplateSeparatorModule],
     template: `
-        <div class="template-separator">
-            <span class="template-separator-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="49" viewBox="0 0 48 49" fill="none">
-                    <path
-                        d="M14 12H21.1333V16.5881L34.2105 14V20L21.1333 22.5881V24.5881L34.2105 22V28L21.1333 30.5881V36H14V12Z"
-                        [attr.fill]="fillColor"
-                    />
-                </svg>
-            </span>
-        </div>
+        <template-separator>
+            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="49" viewBox="0 0 48 49" fill="none">
+                <path
+                    d="M14 12H21.1333V16.5881L34.2105 14V20L21.1333 22.5881V24.5881L34.2105 22V28L21.1333 30.5881V36H14V12Z"
+                    [attr.fill]="fillColor"
+                />
+            </svg>
+        </template-separator>
     `,
 })
 export class FreyaSeparator {
