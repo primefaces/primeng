f9f53cc6f5461a3b73690adbe415f73f2f709148
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-09-26 15:02:09 +0300

    update checkbox disableddoc

--- src/app/showcase/doc/checkbox/disableddoc.ts
+++ src/app/showcase/doc/checkbox/disableddoc.ts
@@ -8,8 +8,8 @@ import { Code } from '@domain/code';
             <p>When <i>disabled</i> is present, the element cannot be edited and focused.</p>
         </app-docsectiontext>
         <div class="card flex justify-center gap-2">
-            <p-checkbox [disabled]="true" [(ngModel)]="checked1" />
-            <p-checkbox [disabled]="true" [(ngModel)]="checked2" />
+            <p-checkbox [(ngModel)]="checked1" [binary]="true" [disabled]="true" />
+            <p-checkbox [(ngModel)]="checked2" [binary]="true" [disabled]="true" />
         </div>
         <app-code [code]="code" selector="checkbox-disabled-demo"></app-code>
     `,
@@ -20,12 +20,12 @@ export class DisabledDoc {
     checked2: boolean = true;
 
     code: Code = {
-        basic: `<p-checkbox [disabled]="true" [(ngModel)]="checked1" />
-<p-checkbox [disabled]="true" [(ngModel)]="checked2" />`,
+        basic: `<p-checkbox [(ngModel)]="checked1" [binary]="true" [disabled]="true" />
+<p-checkbox [(ngModel)]="checked2" [binary]="true" [disabled]="true" />`,
 
         html: `<div class="card flex justify-center gap-2">
-    <p-checkbox [disabled]="true" [(ngModel)]="checked1" />
-    <p-checkbox [disabled]="true" [(ngModel)]="checked2" />
+    <p-checkbox [(ngModel)]="checked1" [binary]="true" [disabled]="true" />
+    <p-checkbox [(ngModel)]="checked2" [binary]="true" [disabled]="true" />
 </div>`,
 
         typescript: `import { Component } from '@angular/core';
