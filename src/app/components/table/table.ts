7e62dc1e751dd73f48ccdab05f94d251f1025ac4
Kevin MORIER <igore-land@hotmail.fr>
2024-09-27 20:09:23 +0200

    Resolve error with attribute Filters of p-table

--- src/app/components/table/table.ts
+++ src/app/components/table/table.ts
@@ -5610,7 +5610,7 @@ export class ColumnFilter implements AfterContentInit {
     }
 
     isRowMatchModeSelected(matchMode: string) {
-        return (<FilterMetadata>this.dt.filters[<string>this.field]).matchMode === matchMode;
+        return (<FilterMetadata>this.dt.filters[<string>this.field])?.matchMode === matchMode;
     }
 
     addConstraint() {
@@ -5895,7 +5895,7 @@ export class ColumnFilter implements AfterContentInit {
                 *ngTemplateOutlet="
                     filterTemplate;
                     context: {
-                        $implicit: filterConstraint.value,
+                        $implicit: filterConstraint?.value,
                         filterCallback: filterCallback,
                         type: type,
                         field: field,
