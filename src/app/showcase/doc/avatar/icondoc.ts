89ffdca6a625b392ac79d93b6a7d04b25140f74b
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-10-03 17:21:19 +0300

    update avatar docs

--- src/app/showcase/doc/avatar/icondoc.ts
+++ src/app/showcase/doc/avatar/icondoc.ts
@@ -7,33 +7,34 @@ import { Code } from '@domain/code';
         <app-docsectiontext>
             <p>A font icon is displayed as an Avatar with the <i>icon</i> property.</p>
         </app-docsectiontext>
-        <div class="grid grid-cols-12 gap-4 card grid-nogutter">
-            <div class="col-span-12 md:col-span-4">
-                <h5>Icon</h5>
-                <p-avatar icon="pi pi-user" styleClass="mr-2" size="xlarge" />
-                <p-avatar icon="pi pi-user" styleClass="mr-2" size="large" [style]="{ 'background-color': '#ece9fc', color: '#2a1261' }" />
-                <p-avatar icon="pi pi-user" styleClass="mr-2" [style]="{ 'background-color': '#dee9fc', color: '#1a2551' }" />
-            </div>
-            <div class="col-span-12 md:col-span-4">
-                <h5>Icon - Circle</h5>
-                <p-avatar icon="pi pi-user" styleClass="mr-2" size="xlarge" shape="circle" />
-                <p-avatar
-                    icon="pi pi-user"
-                    styleClass="mr-2"
-                    size="large"
-                    [style]="{ 'background-color': '#ece9fc', color: '#2a1261' }"
-                    shape="circle"
-                />
-                <p-avatar
-                    icon="pi pi-user"
-                    styleClass="mr-2"
-                    [style]="{ 'background-color': '#dee9fc', color: '#1a2551' }"
-                    shape="circle"
-                />
-            </div>
-            <div class="col-span-12 md:col-span-4">
-                <h5>Badge</h5>
-                <p-avatar icon="pi pi-user" pBadge value="4" size="xlarge" badgeSize="large" />
+        <div class="card">
+            <div class="flex flex-wrap gap-8">
+                <div class="flex-auto">
+                    <h5>Icon</h5>
+                    <p-avatar icon="pi pi-user" class="mr-2" size="xlarge" />
+                    <p-avatar icon="pi pi-user" class="mr-2" size="large" style="background-color: #ece9fc; color: #2a1261" />
+                    <p-avatar icon="pi pi-user" style="background-color: #dee9fc; color: #1a2551" />
+                </div>
+
+                <div class="flex-auto">
+                    <h5>Circle</h5>
+                    <p-avatar icon="pi pi-user" class="mr-2" size="xlarge" shape="circle" />
+                    <p-avatar
+                        icon="pi pi-user"
+                        class="mr-2"
+                        size="large"
+                        style="background-color: #ece9fc; color: #2a1261"
+                        shape="circle"
+                    />
+                    <p-avatar icon="pi pi-user" style="background-color: #dee9fc; color: #1a2551" shape="circle" />
+                </div>
+
+                <div class="flex-auto">
+                    <h5>Badge</h5>
+                    <p-overlay-badge value="4" severity="danger" class="inline-flex">
+                        <p-avatar icon="pi pi-user" size="xlarge" />
+                    </p-overlay-badge>
+                </div>
             </div>
         </div>
         <app-code [code]="code" selector="avatar-icon-demo"></app-code>
@@ -41,95 +42,56 @@ import { Code } from '@domain/code';
 })
 export class IconDoc {
     code: Code = {
-        basic: `<p-avatar 
-    icon="pi pi-user" 
-    styleClass="mr-2" size="xlarge" />
-<p-avatar 
-    icon="pi pi-user" 
-    styleClass="mr-2"
-    size="large" 
-    [style]="{ 'background-color': '#ece9fc', color: '#2a1261' }" />
-<p-avatar 
-    icon="pi pi-user" 
-    styleClass="mr-2" 
-    [style]="{ 'background-color': '#dee9fc', color: '#1a2551' }" />
+        basic: `<p-avatar icon="pi pi-user" class="mr-2" size="xlarge" />
+<p-avatar icon="pi pi-user" class="mr-2" size="large" style="background-color: #ece9fc; color: #2a1261" />
+<p-avatar icon="pi pi-user" style="background-color: #dee9fc; color: #1a2551" />
+<p-avatar icon="pi pi-user" class="mr-2" size="xlarge" shape="circle" />
+<p-avatar icon="pi pi-user" class="mr-2" size="large" style="background-color: #ece9fc; color: #2a1261" shape="circle" />
+<p-avatar icon="pi pi-user" style="background-color: #dee9fc; color: #1a2551" shape="circle" />
 
-<p-avatar 
-    icon="pi pi-user" 
-    styleClass="mr-2" 
-    size="xlarge" 
-    shape="circle" />
-<p-avatar 
-    icon="pi pi-user" 
-    styleClass="mr-2" 
-    size="large" 
-    [style]="{ 'background-color': '#ece9fc', color: '#2a1261' }" 
-    shape="circle" />
-<p-avatar 
-    icon="pi pi-user" 
-    styleClass="mr-2" 
-    [style]="{ 'background-color': '#dee9fc', color: '#1a2551' }" 
-    shape="circle" />
-        
-<p-avatar 
-    icon="pi pi-user" 
-    pBadge 
-    value="4" 
-    size="xlarge"
-    badgeSize="large" />`,
-        html: `<div class="col-span-12 md:col-span-4">
-        <h5>Icon</h5>
-        <p-avatar 
-            icon="pi pi-user" 
-            styleClass="mr-2" 
-            size="xlarge" />
-        <p-avatar 
-            icon="pi pi-user" 
-            styleClass="mr-2" 
-            size="large" 
-            [style]="{ 'background-color': '#ece9fc', color: '#2a1261' }" />
-        <p-avatar 
-            icon="pi pi-user" 
-            styleClass="mr-2" 
-            [style]="{ 'background-color': '#dee9fc', color: '#1a2551' }" />
+<p-overlay-badge value="4" severity="danger" class="inline-flex">
+    <p-avatar icon="pi pi-user" size="xlarge" />
+</p-overlay-badge>`,
+
+        html: `<div class="card">
+    <div class="flex flex-wrap gap-8">
+        <div class="flex-auto">
+            <h5>Icon</h5>
+            <p-avatar icon="pi pi-user" class="mr-2" size="xlarge" />
+            <p-avatar icon="pi pi-user" class="mr-2" size="large" style="background-color: #ece9fc; color: #2a1261" />
+            <p-avatar icon="pi pi-user" style="background-color: #dee9fc; color: #1a2551" />
+        </div>
+
+        <div class="flex-auto">
+            <h5>Circle</h5>
+            <p-avatar icon="pi pi-user" class="mr-2" size="xlarge" shape="circle" />
+            <p-avatar
+                icon="pi pi-user"
+                class="mr-2"
+                size="large"
+                style="background-color: #ece9fc; color: #2a1261"
+                shape="circle"
+            />
+            <p-avatar icon="pi pi-user" style="background-color: #dee9fc; color: #1a2551" shape="circle" />
+        </div>
+
+        <div class="flex-auto">
+            <h5>Badge</h5>
+            <p-overlay-badge value="4" severity="danger" class="inline-flex">
+                <p-avatar icon="pi pi-user" size="xlarge" />
+            </p-overlay-badge>
+        </div>
     </div>
-    <div class="col-span-12 md:col-span-4">
-        <h5>Icon - Circle</h5>
-        <p-avatar 
-            icon="pi pi-user" 
-            styleClass="mr-2" 
-            size="xlarge" 
-            shape="circle" />
-        <p-avatar 
-            icon="pi pi-user" 
-            styleClass="mr-2"
-            size="large" 
-            [style]="{ 'background-color': '#ece9fc', color: '#2a1261' }"
-            shape="circle" />
-        <p-avatar 
-            icon="pi pi-user" 
-            styleClass="mr-2" 
-            [style]="{ 'background-color': '#dee9fc', color: '#1a2551' }" 
-            shape="circle" />
-    </div>
-    <div class="col-span-12 md:col-span-4">
-        <h5>Badge</h5>
-        <p-avatar 
-            icon="pi pi-user" 
-            pBadge 
-            value="4" 
-            size="xlarge"
-            badgeSize="large" />
-    </div>`,
+</div>`,
         typescript: `import { Component } from '@angular/core';
 import { AvatarModule } from 'primeng/avatar';
-import { BadgeModule } from 'primeng/badge';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
         
 @Component({
     selector: 'avatar-icon-demo',
     templateUrl: './avatar-icon-demo.html',
     standalone: true,
-    imports: [AvatarModule, BadgeModule]
+    imports: [AvatarModule, OverlayBadgeModule]
 })
 export class AvatarIconDemo {}`,
     };
