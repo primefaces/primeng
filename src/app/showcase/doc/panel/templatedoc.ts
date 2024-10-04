38cddba580b28145a8bd636c05f1e23d63bbfe09
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-10-04 12:55:26 +0300

    update panel templatedoc

--- src/app/showcase/doc/panel/templatedoc.ts
+++ src/app/showcase/doc/panel/templatedoc.ts
@@ -11,21 +11,21 @@ import { Code } from '@domain/code';
             <p-panel [toggleable]="true">
                 <ng-template pTemplate="header">
                     <div class="flex items-center gap-2">
-                        <p-avatar image="https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png" size="large" shape="circle" />
+                        <p-avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" shape="circle" />
                         <span class="font-bold">Amy Elsner</span>
                     </div>
                 </ng-template>
                 <ng-template pTemplate="footer">
                     <div class="flex flex-wrap items-center justify-between gap-4">
                         <div class="flex items-center gap-2">
-                            <p-button icon="pi pi-user" [rounded]="true" [text]="true" />
-                            <p-button icon="pi pi-bookmark" severity="secondary" [rounded]="true" [text]="true" />
+                            <p-button icon="pi pi-user" rounded text></p-button>
+                            <p-button icon="pi pi-bookmark" severity="secondary" rounded text></p-button>
                         </div>
-                        <span class="p-text-secondary">Updated 2 hours ago</span>
+                        <span class="text-surface-500 dark:text-surface-400">Updated 2 hours ago</span>
                     </div>
                 </ng-template>
                 <ng-template pTemplate="icons">
-                    <p-button icon="pi pi-cog" severity="secondary" rounded text (click)="menu.toggle($event)" />
+                    <p-button icon="pi pi-cog" severity="secondary" rounded text (click)="toggle()" />
                     <p-menu #menu id="config_menu" [model]="items" [popup]="true" />
                 </ng-template>
                 <p class="m-0">
@@ -66,67 +66,58 @@ export class TemplateDoc implements OnInit {
         basic: `<p-panel [toggleable]="true">
     <ng-template pTemplate="header">
         <div class="flex items-center gap-2">
-            <p-avatar 
-                image="https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png" 
-                size="large" 
-                shape="circle" />
-            <span class="font-bold">
-                Amy Elsner
-            </span>
+            <p-avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" shape="circle" />
+            <span class="font-bold">Amy Elsner</span>
         </div>
     </ng-template>
     <ng-template pTemplate="footer">
         <div class="flex flex-wrap items-center justify-between gap-4">
             <div class="flex items-center gap-2">
-               <p-button icon="pi pi-user" [rounded]="true" [text]="true" />
-                <p-button icon="pi pi-bookmark" severity="secondary" [rounded]="true" [text]="true" />
+                <p-button icon="pi pi-user" rounded text></p-button>
+                <p-button icon="pi pi-bookmark" severity="secondary" rounded text></p-button>
             </div>
-            <span class="p-text-secondary">
-                Updated 2 hours ago
-            </span>
+            <span class="text-surface-500 dark:text-surface-400">Updated 2 hours ago</span>
         </div>
     </ng-template>
     <ng-template pTemplate="icons">
-        <p-button icon="pi pi-cog" severity="secondary" rounded text (click)="menu.toggle($event)"/>
+        <p-button icon="pi pi-cog" severity="secondary" rounded text (click)="toggle()" />
         <p-menu #menu id="config_menu" [model]="items" [popup]="true" />
     </ng-template>
     <p class="m-0">
-        Lorem ipsum dolor sit amet...
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
 </p-panel>`,
 
         html: `<div class="card">
     <p-panel [toggleable]="true">
-    <ng-template pTemplate="header">
-        <div class="flex items-center gap-2">
-            <p-avatar 
-                image="https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png" 
-                size="large" 
-                shape="circle" />
-            <span class="font-bold">
-                Amy Elsner
-            </span>
-        </div>
-    </ng-template>
-    <ng-template pTemplate="footer">
-        <div class="flex flex-wrap items-center justify-between gap-4">
+        <ng-template pTemplate="header">
             <div class="flex items-center gap-2">
-                <p-button icon="pi pi-user" [rounded]="true" [text]="true" />
-                <p-button icon="pi pi-bookmark" severity="secondary" [rounded]="true" [text]="true" />
+                <p-avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" shape="circle" />
+                <span class="font-bold">Amy Elsner</span>
             </div>
-            <span class="p-text-secondary">
-                Updated 2 hours ago
-            </span>
-        </div>
-    </ng-template>
-    <ng-template pTemplate="icons">
-        <p-button icon="pi pi-cog" severity="secondary" rounded text (click)="menu.toggle($event)"/>
-        <p-menu #menu id="config_menu" [model]="items" [popup]="true" />
-    </ng-template>
-    <p class="m-0">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
+        </ng-template>
+        <ng-template pTemplate="footer">
+            <div class="flex flex-wrap items-center justify-between gap-4">
+                <div class="flex items-center gap-2">
+                    <p-button icon="pi pi-user" rounded text></p-button>
+                    <p-button icon="pi pi-bookmark" severity="secondary" rounded text></p-button>
+                </div>
+                <span class="text-surface-500 dark:text-surface-400">Updated 2 hours ago</span>
+            </div>
+        </ng-template>
+        <ng-template pTemplate="icons">
+            <p-button icon="pi pi-cog" severity="secondary" rounded text (click)="toggle()" />
+            <p-menu #menu id="config_menu" [model]="items" [popup]="true" />
+        </ng-template>
+        <p class="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
     </p-panel>
 </div>`,
 
@@ -134,12 +125,13 @@ export class TemplateDoc implements OnInit {
 import { PanelModule } from 'primeng/panel';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
     selector: 'panel-template-demo',
     templateUrl: './panel-template-demo.html',
     standalone: true,
-    imports: [PanelModule, AvatarModule, ButtonModule]
+    imports: [PanelModule, AvatarModule, ButtonModule, MenuModule]
 })
 export class PanelTemplateDemo implements OnInit {
     items: { label?: string; icon?: string; separator?: boolean }[] = [];
