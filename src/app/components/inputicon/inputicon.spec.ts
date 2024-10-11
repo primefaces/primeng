b7d9654d0065245ac9b090a5332c3827187060d5
Mohamed Ben Makhlouf <benmakhlouf@softcatalyst.com>
2024-10-11 23:00:34 +0200

    convert to input signal for input icon and add test cases

---
+++ src/app/components/inputicon/inputicon.spec.ts
@@ -0,0 +1,40 @@
+import { ComponentRef } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { InputIcon, InputIconModule } from './inputicon';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+fdescribe('InputIcon', () => {
+    let inputIcon: InputIcon;
+    let fixture: ComponentFixture<InputIcon>;
+    let inputIconRef: ComponentRef<InputIcon>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [NoopAnimationsModule, InputIconModule],
+        });
+
+        fixture = TestBed.createComponent(InputIcon);
+        inputIcon = fixture.componentInstance;
+        inputIconRef = fixture.componentRef;
+    });
+
+    it('should create the component', () => {
+        expect(inputIcon).toBeTruthy();
+    });
+
+    it('should always have the p-inputicon class by default', () => {
+        fixture.detectChanges();
+
+        expect(fixture.nativeElement.classList).toContain('p-inputicon');
+    });
+
+    it('should apply custom classes from styleClass input', () => {
+        inputIconRef.setInput('styleClass', 'custom-class');
+        fixture.detectChanges();
+
+        const inputIconHostElement = fixture.nativeElement;
+        expect(inputIconHostElement.classList).toContain('custom-class');
+        expect(inputIconHostElement.classList).toContain('p-inputicon');
+    });
+
+});
--- src/app/components/inputicon/inputicon.ts
+++ src/app/components/inputicon/inputicon.ts
@@ -1,5 +1,4 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, Input, NgModule, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, NgModule, ViewEncapsulation } from '@angular/core';
 import { SharedModule } from 'primeng/api';
 import { BaseComponent } from 'primeng/basecomponent';
 import { InputIconStyle } from './style/inputiconstyle';
@@ -15,7 +14,7 @@ import { InputIconStyle } from './style/inputiconstyle';
     changeDetection: ChangeDetectionStrategy.OnPush,
     providers: [InputIconStyle],
     host:{
-        '[class]': 'styleClass',
+        '[class]': 'styleClass()',
         '[class.p-inputicon]': 'true',
     }
 })
@@ -24,13 +23,13 @@ export class InputIcon extends BaseComponent {
      * Style class of the element.
      * @group Props
      */
-    @Input() styleClass: string | undefined;
+    styleClass = input<string>();
 
     _componentStyle = inject(InputIconStyle);
 }
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [],
     exports: [InputIcon, SharedModule],
     declarations: [InputIcon],
 })
