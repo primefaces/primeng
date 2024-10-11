f6ea5e5703f07be70da340ceb131bac9edd1ccd7
Simon Hagemann <s.hagemann@hs-solutions.de>
2024-10-11 13:44:09 +0200

    dropdown: added option to disable showing the panel on key down (#16462)

--- src/app/components/dropdown/dropdown.ts
+++ src/app/components/dropdown/dropdown.ts
@@ -649,6 +649,11 @@ export class Dropdown implements OnInit, AfterViewInit, AfterContentInit, AfterV
      */
     @Input({ transform: booleanAttribute }) autofocusFilter: boolean = true;
     /**
+     * Determines if the panel will be shown when the input is focused and receives a character key down event.
+     * @group Props
+     */
+    @Input({ transform: booleanAttribute }) autoShowPanelOnPrintableCharacterKeyDown: boolean = true;
+    /**
      * When present, it specifies that the component should be disabled.
      * @group Props
      */
@@ -1551,7 +1556,7 @@ export class Dropdown implements OnInit, AfterViewInit, AfterContentInit, AfterV
 
             default:
                 if (!event.metaKey && ObjectUtils.isPrintableCharacter(event.key)) {
-                    !this.overlayVisible && this.show();
+                    !this.overlayVisible && this.autoShowPanelOnPrintableCharacterKeyDown && this.show();
                     !this.editable && this.searchOptions(event, event.key);
                 }
 
