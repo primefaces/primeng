ec76ff3c33ed8c922f1720c22f8b24e457b2ca0b
Mohamed Ben Makhlouf <benmakhlouf@softcatalyst.com>
2024-10-14 18:48:41 +0200

    convert to input signal for iconfield

--- src/app/components/iconfield/iconfield.ts
+++ src/app/components/iconfield/iconfield.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, Input, NgModule, ViewEncapsulation } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, inject, input, NgModule, ViewEncapsulation } from '@angular/core';
 import { SharedModule } from 'primeng/api';
 import { BaseComponent } from 'primeng/basecomponent';
 import { IconFieldStyle } from './style/iconfieldstyle';
@@ -10,7 +10,7 @@ import { IconFieldStyle } from './style/iconfieldstyle';
  */
 @Component({
     selector: 'p-iconfield, p-iconField',
-    template: ` <div class="p-iconfield" [ngClass]="containerClass" [class]="styleClass"><ng-content></ng-content></div>`,
+    template: ` <div class="p-iconfield" [ngClass]="containerClass()" [class]="styleClass()"><ng-content></ng-content></div>`,
     providers: [IconFieldStyle],
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,24 +20,30 @@ export class IconField extends BaseComponent {
      * Position of the icon.
      * @group Props
      */
-    @Input() iconPosition: 'right' | 'left' | undefined;
+    iconPosition = input<'right' | 'left'>();
     /**
      * Style class of the component.
      * @group Props
      */
-    @Input() styleClass: string;
+    styleClass = input<string>();
+
+    /**
+     * Computes the container class based on the icon position.
+     * @returns An object with the container class.
+     * @group Methods
+     */
+    containerClass = computed(() => {
+        return {
+            'p-iconfield-left': this.iconPosition() === 'left',
+            'p-iconfield-right': this.iconPosition() === 'right',
+        };
+    });
 
     _componentStyle = inject(IconFieldStyle);
-    get containerClass() {
-        return {
-            'p-iconfield-left': this.iconPosition === 'left',
-            'p-iconfield-right': this.iconPosition === 'right',
-        };
-    }
 }
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [NgClass],
     exports: [IconField, SharedModule],
     declarations: [IconField],
 })
