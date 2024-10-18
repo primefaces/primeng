4a54e809c6c7510d6a900d7c0ede2347c4971868
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2024-10-18 14:53:36 +0300

    Add new material theme, fixes #16583

--- src/app/showcase/layout/configurator/app.configurator.component.ts
+++ src/app/showcase/layout/configurator/app.configurator.component.ts
@@ -12,10 +12,12 @@ import { $t, updatePreset, updateSurfacePalette } from 'primeng/themes';
 import { Aura } from 'primeng/themes/aura';
 import { Lara } from 'primeng/themes/lara';
 import { Nora } from 'primeng/themes/nora';
+import { Material } from 'primeng/themes/material';
 import { ToggleSwitchModule } from 'primeng/toggleswitch';
 
 const presets = {
     Aura,
+    Material,
     Lara,
     Nora,
 };
@@ -66,6 +68,7 @@ const presets = {
                     [(ngModel)]="selectedPreset"
                     (ngModelChange)="onPresetChange($event)"
                     [allowEmpty]="false"
+                    styleClass="inline-flex"
                 />
             </div>
             <div class="config-panel-settings">
