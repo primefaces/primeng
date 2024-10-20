d33ee8bc2c10ab7e65f1d01734f06bbc0779213d
Mohamed Ben Makhlouf <benmakhlouf@softcatalyst.com>
2024-10-20 12:29:21 +0200

    convert to modern input

--- src/app/components/togglebutton/togglebutton.ts
+++ src/app/components/togglebutton/togglebutton.ts
@@ -6,9 +6,10 @@ import {
     ContentChildren,
     EventEmitter,
     forwardRef,
-    HostBinding,
+    computed,
     inject,
-    Input,
+    input,
+    model,
     NgModule,
     numberAttribute,
     Output,
@@ -40,26 +41,27 @@ export const TOGGLEBUTTON_VALUE_ACCESSOR: any = {
             pRipple
             type="button"
             [ngClass]="cx('root')"
-            [class]="styleClass"
-            [tabindex]="tabindex"
-            [disabled]="disabled"
+            [class]="styleClass()"
+            [tabindex]="tabindex()"
+            [disabled]="disabled()"
             (click)="toggle($event)"
-            [attr.aria-labelledby]="ariaLabelledBy"
+            [attr.aria-label]="ariaLabel()"
+            [attr.aria-labelledby]="ariaLabelledBy()"
             [attr.aria-pressed]="checked"
             [attr.data-p-checked]="active"
-            [attr.data-p-disabled]="disabled"
+            [attr.data-p-disabled]="disabled()"
         >
             <span [ngClass]="cx('content')">
                 <ng-container *ngTemplateOutlet="contentTemplate; context: { $implicit: checked }"></ng-container>
                 @if (!contentTemplate) {
                     @if (!iconTemplate) {
-                        @if (onIcon || offIcon) {
+                        @if (onIcon() || offIcon()) {
                             <span
-                                [class]="checked ? this.onIcon : this.offIcon"
+                                [class]="checked ? onIcon() : offIcon()"
                                 [ngClass]="{
                                     'p-togglebutton-icon': true,
-                                    'p-togglebutton-icon-left': iconPos === 'left',
-                                    'p-togglebutton-icon-right': iconPos === 'right',
+                                    'p-togglebutton-icon-left': iconPos() === 'left',
+                                    'p-togglebutton-icon-right': iconPos() === 'right',
                                 }"
                                 [attr.data-pc-section]="'icon'"
                             ></span>
@@ -67,15 +69,16 @@ export const TOGGLEBUTTON_VALUE_ACCESSOR: any = {
                     } @else {
                         <ng-container *ngTemplateOutlet="iconTemplate; context: { $implicit: checked }"></ng-container>
                     }
-                    @if (onLabel || offLabel) {
+                    @if (onLabel() || offLabel()) {
                         <span [ngClass]="cx('label')" [attr.data-pc-section]="'label'">
-                            {{ checked ? (hasOnLabel ? onLabel : '') : hasOffLabel ? offLabel : '' }}
+                            {{ checked ? (hasOnLabel() ? onLabel() : '') : hasOffLabel() ? offLabel() : '' }}
                         </span>
                     }
                 }
             </span>
         </button>
     `,
+    host: { '[class]': 'styleClass()' },
     standalone: true,
     imports: [Ripple, AutoFocus, SharedModule, CommonModule],
     providers: [TOGGLEBUTTON_VALUE_ACCESSOR, ToggleButtonStyle],
@@ -86,75 +89,72 @@ export class ToggleButton extends BaseComponent implements ControlValueAccessor 
      * Label for the on state.
      * @group Props
      */
-    @Input() onLabel: string = 'Yes';
+    onLabel = input<string>('Yes');
     /**
      * Label for the off state.
      * @group Props
      */
-    @Input() offLabel: string = 'No';
+    offLabel = input<string>('No');
     /**
      * Icon for the on state.
      * @group Props
      */
-    @Input() onIcon: string | undefined;
+    onIcon = input<string>();
     /**
      * Icon for the off state.
      * @group Props
      */
-    @Input() offIcon: string | undefined;
+    offIcon = input<string>();
     /**
      * Defines a string that labels the input for accessibility.
      * @group Props
      */
-    @Input() ariaLabel: string | undefined;
+    ariaLabel = input<string>();
     /**
      * Establishes relationships between the component and label(s) where its value should be one or more element IDs.
      * @group Props
      */
-    @Input() ariaLabelledBy: string | undefined;
+    ariaLabelledBy = input<string>();
     /**
      * When present, it specifies that the element should be disabled.
      * @group Props
      */
-    @Input({ transform: booleanAttribute }) disabled: boolean | undefined;
+    disabled = model<boolean>();
     /**
      * Inline style of the element.
      * @group Props
      */
-    @Input() style: any;
+    style = input<{ [klass: string]: any } | null>();
     /**
      * Style class of the element.
      * @group Props
      */
-    @Input() styleClass: string | undefined;
-    @HostBinding('class') get hostClass() {
-        return this.styleClass || '';
-    }
+    styleClass = input<string>();
     /**
      * Identifier of the focus input to match a label defined for the component.
      * @group Props
      */
-    @Input() inputId: string | undefined;
+    inputId = input<string>();
     /**
      * Index of the element in tabbing order.
      * @group Props
      */
-    @Input({ transform: numberAttribute }) tabindex: number | undefined = 0;
+    tabindex = input<number, any>(0, { transform: numberAttribute });
     /**
      * Position of the icon.
      * @group Props
      */
-    @Input() iconPos: 'left' | 'right' = 'left';
+    iconPos = input<'left' | 'right'>('left');
     /**
      * When present, it specifies that the component should automatically get focus on load.
      * @group Props
      */
-    @Input({ transform: booleanAttribute }) autofocus: boolean | undefined;
+    autofocus = input<boolean, any>(undefined, { transform: booleanAttribute });
     /**
      * Whether selection can not be cleared.
      * @group Props
      */
-    @Input() allowEmpty: boolean | undefined;
+    allowEmpty = input<boolean, any>(undefined, { transform: booleanAttribute });
     /**
      * Callback to invoke on value change.
      * @param {ToggleButtonChangeEvent} event - Custom change event.
@@ -193,7 +193,7 @@ export class ToggleButton extends BaseComponent implements ControlValueAccessor 
     }
 
     toggle(event: Event) {
-        if (!this.disabled && !(this.allowEmpty === false && this.checked)) {
+        if (!this.disabled() && !(this.allowEmpty() === false && this.checked)) {
             this.checked = !this.checked;
             this.onModelChange(this.checked);
             this.onModelTouched();
@@ -237,17 +237,13 @@ export class ToggleButton extends BaseComponent implements ControlValueAccessor 
     }
 
     setDisabledState(val: boolean): void {
-        this.disabled = val;
+        this.disabled.set(val);
         this.cd.markForCheck();
     }
 
-    get hasOnLabel(): boolean {
-        return (this.onLabel && this.onLabel.length > 0) as boolean;
-    }
+    hasOnLabel = computed<boolean>(() => this.onLabel() && this.onLabel().length > 0);
 
-    get hasOffLabel(): boolean {
-        return (this.onLabel && this.onLabel.length > 0) as boolean;
-    }
+    hasOffLabel = computed<boolean>(() => this.offLabel() && this.offLabel().length > 0);
 
     get active() {
         return this.checked === true;
