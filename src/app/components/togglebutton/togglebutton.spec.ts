ae81ec7d13ae52bcee19e5f6df6670b7427dc730
Mohamed Ben Makhlouf <benmakhlouf@softcatalyst.com>
2024-10-20 12:54:18 +0200

    correct existent tests

--- src/app/components/togglebutton/togglebutton.spec.ts
+++ src/app/components/togglebutton/togglebutton.spec.ts
@@ -1,3 +1,4 @@
+import { ComponentRef } from '@angular/core';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { ToggleButton } from './togglebutton';
@@ -6,39 +7,37 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 describe('ToggleButton', () => {
     let toggleButton: ToggleButton;
     let fixture: ComponentFixture<ToggleButton>;
+    let toggleButtonRef: ComponentRef<ToggleButton>;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [NoopAnimationsModule],
-            declarations: [ToggleButton],
+            imports: [NoopAnimationsModule, ToggleButton],
         });
 
         fixture = TestBed.createComponent(ToggleButton);
         toggleButton = fixture.componentInstance;
+        toggleButtonRef = fixture.componentRef;
+        fixture.detectChanges();
     });
 
     it('should display the onLabel and offLabel', () => {
-        toggleButton.onLabel = 'YES';
-        toggleButton.offLabel = 'NO';
-        fixture.detectChanges();
-
         const clickEl = fixture.nativeElement.querySelector('.p-togglebutton');
         clickEl.click();
         fixture.detectChanges();
 
-        const labelEl = fixture.debugElement.query(By.css('.p-button-label'));
-        expect(labelEl.nativeElement.textContent).toBe('YES');
+        const labelEl = fixture.debugElement.query(By.css('.p-togglebutton-label'));
+        expect(labelEl.nativeElement.textContent.trim()).toBe('Yes');
 
         clickEl.click();
         fixture.detectChanges();
 
-        expect(labelEl.nativeElement.textContent).toBe('NO');
+        expect(labelEl.nativeElement.textContent.trim()).toBe('No');
     });
 
     it('Should display as checked when value is true by default', () => {
-        toggleButton.checked = true;
+        toggleButton.checked.set(true);
         fixture.detectChanges();
 
-        expect(toggleButton.checked).toBe(true);
+        expect(toggleButton.active()).toBe(true);
     });
 });
