f7974ce34d645ce66758584247c17c1cc85ee6b2
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2024-10-28 14:59:23 +0300

    Remove duplicates

--- src/app/components/themes/lara/multiselect/index.ts
+++ src/app/components/themes/lara/multiselect/index.ts
@@ -21,37 +21,37 @@ export default {
             style: '{form.field.focus.ring.style}',
             color: '{form.field.focus.ring.color}',
             offset: '{form.field.focus.ring.offset}',
-            shadow: '{form.field.focus.ring.shadow}'
+            shadow: '{form.field.focus.ring.shadow}',
         },
         transitionDuration: '{form.field.transition.duration}',
         sm: {
             fontSize: '{form.field.sm.font.size}',
             paddingX: '{form.field.sm.padding.x}',
-            paddingY: '{form.field.sm.padding.y}'
+            paddingY: '{form.field.sm.padding.y}',
         },
         lg: {
             fontSize: '{form.field.lg.font.size}',
             paddingX: '{form.field.lg.padding.x}',
-            paddingY: '{form.field.lg.padding.y}'
-        }
+            paddingY: '{form.field.lg.padding.y}',
+        },
     },
     dropdown: {
         width: '2.5rem',
-        color: '{form.field.icon.color}'
+        color: '{form.field.icon.color}',
     },
     overlay: {
         background: '{overlay.select.background}',
         borderColor: '{overlay.select.border.color}',
         borderRadius: '{overlay.select.border.radius}',
         color: '{overlay.select.color}',
-        shadow: '{overlay.select.shadow}'
+        shadow: '{overlay.select.shadow}',
     },
     list: {
         padding: '{list.padding}',
         gap: '{list.gap}',
         header: {
-            padding: '{list.header.padding}'
-        }
+            padding: '{list.header.padding}',
+        },
     },
     option: {
         focusBackground: '{list.option.focus.background}',
@@ -63,20 +63,18 @@ export default {
         selectedFocusColor: '{list.option.selected.focus.color}',
         padding: '{list.option.padding}',
         borderRadius: '{list.option.border.radius}',
-        gap: '0.5rem'
+        gap: '0.5rem',
     },
     optionGroup: {
         background: '{list.option.group.background}',
         color: '{list.option.group.color}',
-        background: '{list.option.group.background}',
-        color: '{list.option.group.color}',
         fontWeight: '{list.option.group.font.weight}',
-        padding: '{list.option.group.padding}'
+        padding: '{list.option.group.padding}',
     },
     chip: {
-        borderRadius: '{border.radius.sm}'
+        borderRadius: '{border.radius.sm}',
     },
     emptyMessage: {
-        padding: '{list.option.padding}'
-    }
+        padding: '{list.option.padding}',
+    },
 };
