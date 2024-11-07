36b9b2efd953f1f76cb254b7be6fb68f752fdc26
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-11-07 15:12:22 +0300

    update Accordion | selectors

--- src/app/components/accordion/accordion.ts
+++ src/app/components/accordion/accordion.ts
@@ -76,7 +76,7 @@ export interface AccordionToggleIconTemplateContext {
  * @group Components
  */
 @Component({
-    selector: 'p-accordion-panel',
+    selector: 'p-accordion-panel, p-accordionpanel',
     imports: [CommonModule],
     standalone: true,
     template: `<ng-content />`,
@@ -122,7 +122,7 @@ export class AccordionPanel extends BaseComponent {
  * @group Components
  */
 @Component({
-    selector: 'p-accordion-header',
+    selector: 'p-accordion-header, p-accordionheader',
     imports: [CommonModule, ChevronDownIcon, ChevronUpIcon, Ripple],
     standalone: true,
     template: `
@@ -310,7 +310,7 @@ export class AccordionHeader extends BaseComponent {
 }
 
 @Component({
-    selector: 'p-accordion-content',
+    selector: 'p-accordion-content, p-accordioncontent',
     imports: [CommonModule],
     standalone: true,
     template: ` <div class="p-accordioncontent-content">
@@ -368,7 +368,7 @@ export class AccordionContent extends BaseComponent {
  * @group Components
  */
 @Component({
-    selector: 'p-accordionTab, p-accordion-tab',
+    selector: 'p-accordionTab, p-accordion-tab, p-accordiontab',
     standalone: true,
     imports: [CommonModule, ChevronDownIcon, ChevronUpIcon],
     template: `
