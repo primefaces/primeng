21d66333acd144e1849d6a89372e6015e9a5b02b
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2024-11-08 23:44:26 +0300

    Refactor

--- packages/primeng/src/api/provideprimengconfig.ts
+++ packages/primeng/src/api/provideprimengconfig.ts
@@ -1,10 +1,10 @@
 import { EnvironmentProviders, InjectionToken, makeEnvironmentProviders } from '@angular/core';
 
-export type PrimeNgFeature = { theme?: { preset: any }; ripple?: boolean; inputStyle?: 'outlined' | 'filled'; csp?: { nonce: string | undefined } };
+export type PrimeNgConfigProps = { theme?: { preset: any }; ripple?: boolean; inputStyle?: 'outlined' | 'filled'; csp?: { nonce: string | undefined } };
 
-export const PRIME_NG_CONFIG = new InjectionToken<PrimeNgFeature>('PRIME_NG_CONFIG');
+export const PRIME_NG_CONFIG = new InjectionToken<PrimeNgConfigProps>('PRIME_NG_CONFIG');
 
-export function providePrimeNgConfig(...features: PrimeNgFeature[]): EnvironmentProviders {
+export function providePrimeNgConfig(...features: PrimeNgConfigProps[]): EnvironmentProviders {
     const providers = features.map((feature) => ({
         provide: PRIME_NG_CONFIG,
         useValue: feature
