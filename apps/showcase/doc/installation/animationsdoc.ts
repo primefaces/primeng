80d1d4da91c7ab3a57bd4433cf73a030e7e54f62
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-11-08 18:11:48 +0300

    fix typo

--- apps/showcase/doc/installation/animationsdoc.ts
+++ apps/showcase/doc/installation/animationsdoc.ts
@@ -33,9 +33,9 @@ export const appConfig: ApplicationConfig = {
 
     code2: Code = {
         typescript: `// main.ts
-import {bootstrapApplication} from '@angular/platform-browser';
-import {appConfig} from './app/app.config';
-import {AppComponent} from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
 `
