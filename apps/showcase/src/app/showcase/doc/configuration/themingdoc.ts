75c7657e59dcd6a9005c3eaeaab8f271b5dd0c28
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2024-11-12 12:13:43 +0300

    Update documentation

--- apps/showcase/src/app/showcase/doc/configuration/themingdoc.ts
+++ apps/showcase/src/app/showcase/doc/configuration/themingdoc.ts
@@ -8,6 +8,10 @@ import { Code } from '@domain/code';
             <p>PrimeNG provides 4 predefined themes out of the box; Aura, Material, Lara and Nora. Default theme is Aura with emerald as the primary color. See the <a routerLink="/theming">theming</a> documentation for details.</p>
         </app-docsectiontext>
         <app-code [code]="code" [hideToggleCode]="true"></app-code>
+        <app-docsectiontext>
+            <p>Other option is the provide PrimeNG config in the <i>ApplicationConfig</i> as an environment provider.</p>
+        </app-docsectiontext>
+        <app-code [code]="code2" [hideToggleCode]="true"></app-code>
     `
 })
 export class ThemingDoc {
@@ -23,5 +27,22 @@ export class AppComponent {
         this.primeng.theme.set({ preset: Aura });
     }
 }`
+    };
+
+    code2: Code = {
+        typescript: `import { ApplicationConfig } from '@angular/core';
+import { providePrimeNgConfig } from 'primeng/api';
+import { bootstrapApplication } from '@angular/platform-browser';
+import Noir from '../themes/app-theme';
+
+const appConfig: ApplicationConfig = {
+    providers: [
+        // other environment providers...
+        providePrimeNgConfig({ theme: Noir, ripple: true, inputStyle: 'outlined' }),
+    ]
+};
+
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+`
     };
 }
