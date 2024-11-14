56d29515aa58af7bcbb668e1dd27ef976da207bb
Mert Sincan <sincan.mert@gmail.com>
2024-11-14 00:43:21 +0000

    Fixed #16739 - Theme types can be optional in Config

--- packages/primeng/src/api/provideprimengconfig.ts
+++ packages/primeng/src/api/provideprimengconfig.ts
@@ -1,6 +1,6 @@
 import { EnvironmentProviders, InjectionToken, makeEnvironmentProviders } from '@angular/core';
 
-export type ThemeType = { preset: any; options: any } | 'none' | undefined;
+export type ThemeType = { preset?: any; options?: any } | 'none' | undefined;
 
 export type PrimeNGConfigType = { theme?: ThemeType; ripple?: boolean; inputStyle?: 'outlined' | 'filled'; csp?: { nonce: string | undefined } };
 
