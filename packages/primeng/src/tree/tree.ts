73ba3eeed070c7bcabde30dbf6fcb13ffc49d077
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-11-15 13:14:11 +0300

    Fixed #15886 | Tree hierarchy

--- packages/primeng/src/tree/tree.ts
+++ packages/primeng/src/tree/tree.ts
@@ -144,7 +144,7 @@ import {
                         </span>
                     </span>
                 </div>
-                <ul class="p-tree-node-children" style="display: none;" *ngIf="!tree.virtualScroll && node.children && node.expanded" [style.display]="node.expanded ? 'block' : 'none'" role="tree">
+                <ul class="p-tree-node-children" style="display: none;" *ngIf="!tree.virtualScroll && node.children && node.expanded" [style.display]="node.expanded ? 'block' : 'none'" role="group">
                     <p-treeNode
                         *ngFor="let childNode of node.children; let firstChild = first; let lastChild = last; let index = index; trackBy: tree.trackBy"
                         [node]="childNode"
