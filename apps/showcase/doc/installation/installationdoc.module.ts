6e26e48037c2dd684e1f3105576598e1c9e58bbb
Cagatay Civici <cagatay.civici@gmail.com>
2024-11-15 12:27:17 +0300

    New getting started docs

--- apps/showcase/doc/installation/installationdoc.module.ts
+++ apps/showcase/doc/installation/installationdoc.module.ts
@@ -5,17 +5,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-import { AnimationsDoc } from './animationsdoc';
 import { DownloadDoc } from './downloaddoc';
 import { ExamplesDoc } from './examplesdoc';
 import { NextStepsDoc } from './nextstepsdoc';
+import { ProviderDoc } from './providerdoc';
 import { ThemeDoc } from './themedoc';
 import { VerifyDoc } from './verifydoc';
 import { VideosDoc } from './videos/videosdoc';
 
 @NgModule({
     imports: [CommonModule, RouterModule, AppCodeModule, FormsModule, AppDocModule, ButtonModule],
-    declarations: [DownloadDoc, ThemeDoc, VerifyDoc, NextStepsDoc, AnimationsDoc, ExamplesDoc, VideosDoc],
+    declarations: [DownloadDoc, ThemeDoc, ProviderDoc, VerifyDoc, NextStepsDoc, ExamplesDoc, VideosDoc],
     exports: [AppDocModule]
 })
 export class InstallationDocModule {}
