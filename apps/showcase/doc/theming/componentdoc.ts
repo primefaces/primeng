7890aee1963235694b0aa4305f3ade6fed066689
Mert Sincan <sincan.mert@gmail.com>
2024-11-18 14:04:03 +0000

    Update componentdoc.ts

--- apps/showcase/doc/theming/componentdoc.ts
+++ apps/showcase/doc/theming/componentdoc.ts
@@ -5,14 +5,12 @@ import { Component } from '@angular/core';
     template: `
         <app-docsectiontext>
             <p>
-                The design tokens of a specific component is defined at <i>components</i> layer. Overriding components tokens is not the
-                recommended approach if you are building your own style, building your own preset should be preferred instead. This
-                configuration is global and applies to all card components, in case you need to customize a particular component on a page
-                locally, view the Scoped CSS section for an example.
+                The design tokens of a specific component is defined at <i>components</i> layer. Overriding components tokens is not the recommended approach if you are building your own style, building your own preset should be preferred instead.
+                This configuration is global and applies to all card components, in case you need to customize a particular component on a page locally, view the Scoped CSS section for an example.
             </p>
         </app-docsectiontext>
         <app-code [code]="code" selector="component-demo" [hideToggleCode]="true"></app-code>
-    `,
+    `
 })
 export class ComponentDoc {
     code = {
@@ -41,6 +39,6 @@ export class ComponentDoc {
             }
         }
     }
-});`,
+});`
     };
 }
