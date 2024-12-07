274bbd8d1fbc9fd6323da38ba7779927af852b50
Luca Peruzzo <peruzzoluca98@gmail.com>
2024-12-07 13:43:30 +0100

    feat(fluid): update to angular 19

--- packages/primeng/src/fluid/fluid.ts
+++ packages/primeng/src/fluid/fluid.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, NgModule, ViewEncapsulation } from '@angular/core';
 import { BaseComponent } from 'primeng/basecomponent';
 import { FluidStyle } from './style/fluidstyle';
@@ -10,8 +9,7 @@ import { FluidStyle } from './style/fluidstyle';
 @Component({
     selector: 'p-fluid',
     template: ` <ng-content></ng-content> `,
-    standalone: true,
-    imports: [CommonModule],
+    imports: [],
     changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None,
     providers: [FluidStyle],
