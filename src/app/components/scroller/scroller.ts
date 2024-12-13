primefaces/primeng

commit 5ce92d20cfea29a774cd41d008364c2a9bb38996

Fix: Ensure p-scroller updates height after clearing filters (#16954) (#16979)
* Create npm-grunt.yml

Do ewentualnej modyfikacji w przyszłości

* create an index.html file

* Update index.html

* Update index.html

* Fix: Improve dynamic styling for avatar component

* Remove unnecessary files

* Fix dropdown virtual scroll filter bug

* Restore changes for previous issue

* Restore changes for previous issu

---------

Co-authored-by: Mateusz Adamczyk <163399256+mateuszstudentzero@users.noreply.github.com>
Co-authored-by: mateuszstudentzero <71056@student.pb.edu.pl>

src/app/components/scroller/scroller.ts
@@ -554,6 +554,12 @@ export class Scroller implements OnInit, AfterContentInit, AfterViewChecked, OnD
             }
         }
 
+        if (simpleChanges.items) {
+            this.setSpacerSize();
+            this.setSize();
+            this.cd.detectChanges();
+        }
+
         if (this.initialized) {
             const isChanged = !isLoadingChanged && (simpleChanges.items?.previousValue?.length !== simpleChanges.items?.currentValue?.length || simpleChanges.itemSize || simpleChanges.scrollHeight || simpleChanges.scrollWidth);
 

commit d6fdcd4c5c8bcf7d63ae8ceb6ab4fa072106baed

fixed resize update regression from #16979

src/app/components/scroller/scroller.ts
@@ -554,12 +554,6 @@ export class Scroller implements OnInit, AfterContentInit, AfterViewChecked, OnD
             }
         }
 
-        if (simpleChanges.items) {
-            this.setSpacerSize();
-            this.setSize();
-            this.cd.detectChanges();
-        }
-
         if (this.initialized) {
             const isChanged = !isLoadingChanged && (simpleChanges.items?.previousValue?.length !== simpleChanges.items?.currentValue?.length || simpleChanges.itemSize || simpleChanges.scrollHeight || simpleChanges.scrollWidth);
 
@@ -834,9 +828,7 @@ export class Scroller implements OnInit, AfterContentInit, AfterViewChecked, OnD
                     contentHeight !== this.defaultContentHeight && ((<ElementRef>this.elementViewChild).nativeElement.style.height = '');
 
                     const [width, height] = [DomHandler.getWidth((<ElementRef>this.elementViewChild).nativeElement), DomHandler.getHeight((<ElementRef>this.elementViewChild).nativeElement)];
-                    (this.both || this.horizontal) && ((<ElementRef>this.elementViewChild).nativeElement.style.width = width < <number>this.defaultWidth ? width + 'px' : this._scrollWidth || this.defaultWidth + 'px');
-                    (this.both || this.vertical) && ((<ElementRef>this.elementViewChild).nativeElement.style.height = height < <number>this.defaultHeight ? height + 'px' : this._scrollHeight || this.defaultHeight + 'px');
-
+                    
                     this.contentEl.style.minHeight = this.contentEl.style.minWidth = '';
                     this.contentEl.style.position = '';
                     (<ElementRef>this.elementViewChild).nativeElement.style.contain = '';
@@ -845,6 +837,9 @@ export class Scroller implements OnInit, AfterContentInit, AfterViewChecked, OnD
                     this.defaultHeight = height;
                     this.defaultContentWidth = contentWidth;
                     this.defaultContentHeight = contentHeight;
+
+                    (this.both || this.horizontal) && ((<ElementRef>this.elementViewChild).nativeElement.style.width = width < <number>this.defaultWidth ? width + 'px' : this._scrollWidth || this.defaultWidth + 'px');
+                    (this.both || this.vertical) && ((<ElementRef>this.elementViewChild).nativeElement.style.height = height < <number>this.defaultHeight ? height + 'px' : this._scrollHeight || this.defaultHeight + 'px');
                 }
             });
         }