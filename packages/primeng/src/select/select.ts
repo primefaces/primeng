58e0c502b8b7f4a1983b6cd02ee26416bf517ddc
mark7p <gen.paniterce@gmail.com>
2024-12-18 23:29:56 +0800

    fix searchOptions auto scroll

--- packages/primeng/src/select/select.ts
+++ packages/primeng/src/select/select.ts
@@ -2023,7 +2023,9 @@ export class Select extends BaseComponent implements OnInit, AfterViewInit, Afte
         }
 
         if (optionIndex !== -1) {
-            this.changeFocusedOptionIndex(event, optionIndex);
+            setTimeout(() => {
+                this.changeFocusedOptionIndex(event, optionIndex);
+            });
         }
 
         if (this.searchTimeout) {
