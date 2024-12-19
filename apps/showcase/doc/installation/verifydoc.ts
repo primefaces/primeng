6ad8ae8996290872554426da1a4987fcdc27dabb
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-12-19 14:12:15 +0300

    update verifydoc

--- apps/showcase/doc/installation/verifydoc.ts
+++ apps/showcase/doc/installation/verifydoc.ts
@@ -12,7 +12,7 @@ import { Component } from '@angular/core';
             </p>
         </app-docsectiontext>
         <div class="card flex justify-center">
-            <p-button label="Check"></p-button>
+            <p-button label="Check" />
         </div>
         <app-code [code]="code1" [hideStackBlitz]="true" [hideCodeSandbox]="true" [hideToggleCode]="true" class="block mb-4"></app-code>
         <app-code [code]="code2" [hideStackBlitz]="true" [hideCodeSandbox]="true" [hideToggleCode]="true"></app-code>
@@ -20,12 +20,21 @@ import { Component } from '@angular/core';
 })
 export class VerifyDoc {
     code1: Code = {
-        typescript: `import { ButtonModule } from 'primeng/button';`
+        typescript: `import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+    selector: 'button-demo',
+    templateUrl: './button-demo.html',
+    standalone: true,
+    imports: [ButtonModule]
+})
+export class ButtonDemo {}`
     };
 
     code2: Code = {
         html: `<div class="card flex justify-center">
-    <p-button label="Check"></p-button>
+    <p-button label="Check" />
 </div>`
     };
 }
