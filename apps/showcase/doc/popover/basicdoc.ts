f835728f2b8325b1c61cd0fd8376e71df0aabcaa
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-12-24 10:38:44 +0300

    update popover | basicdoc

--- apps/showcase/doc/popover/basicdoc.ts
+++ apps/showcase/doc/popover/basicdoc.ts
@@ -61,44 +61,44 @@ export class BasicDoc {
 
     code: Code = {
         basic: `<p-button (click)="op.toggle($event)" icon="pi pi-share-alt" label="Share" />
-    <p-popover #op>
-        <div class="flex flex-col gap-4 w-[25rem]">
-            <div>
-                <span class="font-medium text-surface-900 dark:text-surface-0 block mb-2">Share this document</span>
+<p-popover #op>
+    <div class="flex flex-col gap-4 w-[25rem]">
+        <div>
+            <span class="font-medium text-surface-900 dark:text-surface-0 block mb-2">Share this document</span>
+            <p-inputgroup>
+                <input pInputText value="https://primeng.org/12323ff26t2g243g423g234gg52hy25XADXAG3" readonly class="w-[25rem]" />
+                <p-inputgroup-addon>
+                    <i class="pi pi-copy"></i>
+                </p-inputgroup-addon>
+            </p-inputgroup>
+        </div>
+        <div>
+            <span class="font-medium text-surface-900 dark:text-surface-0 block mb-2">Invite Member</span>
+            <div class="flex">
                 <p-inputgroup>
-                    <input pInputText value="https://primeng.org/12323ff26t2g243g423g234gg52hy25XADXAG3" readonly class="w-[25rem]" />
-                    <p-inputgroup-addon>
-                        <i class="pi pi-copy"></i>
-                    </p-inputgroup-addon>
+                    <input pInputText disabled />
+                    <button pButton label="Invite" icon="pi pi-users"></button>
                 </p-inputgroup>
             </div>
-            <div>
-                <span class="font-medium text-surface-900 dark:text-surface-0 block mb-2">Invite Member</span>
-                <div class="flex">
-                    <p-inputgroup>
-                        <input pInputText disabled />
-                        <button pButton label="Invite" icon="pi pi-users"></button>
-                    </p-inputgroup>
-                </div>
-            </div>
-            <div>
-                <span class="font-medium text-surface-900 dark:text-surface-0 block mb-2">Team Members</span>
-                <ul class="list-none p-0 m-0 flex flex-col gap-4">
-                    <li *ngFor="let member of members" class="flex items-center gap-2">
-                        <img [src]="'https://primefaces.org/cdn/primeng/images/demo/avatar/' + member.image" style="width: 32px" />
-                        <div>
-                            <span class="font-medium">{{ member.name }}</span>
-                            <div class="text-sm text-muted-color">{{ member.email }}</div>
-                        </div>
-                        <div class="flex items-center gap-2 text-muted-color ml-auto text-sm">
-                            <span>{{ member.role }}</span>
-                            <i class="pi pi-angle-down"></i>
-                        </div>
-                    </li>
-                </ul>
-            </div>
         </div>
-    </p-popover>`,
+        <div>
+            <span class="font-medium text-surface-900 dark:text-surface-0 block mb-2">Team Members</span>
+            <ul class="list-none p-0 m-0 flex flex-col gap-4">
+                <li *ngFor="let member of members" class="flex items-center gap-2">
+                    <img [src]="'https://primefaces.org/cdn/primeng/images/demo/avatar/' + member.image" style="width: 32px" />
+                    <div>
+                        <span class="font-medium">{{ member.name }}</span>
+                        <div class="text-sm text-muted-color">{{ member.email }}</div>
+                    </div>
+                    <div class="flex items-center gap-2 text-muted-color ml-auto text-sm">
+                        <span>{{ member.role }}</span>
+                        <i class="pi pi-angle-down"></i>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</p-popover>`,
 
         html: `<div class="card flex justify-center">
     <p-button (click)="op.toggle($event)" icon="pi pi-share-alt" label="Share" />
