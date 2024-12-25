fa46272eb8068c38f93101b8574e75970fa73ce5
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-12-25 14:30:03 +0300

    update dialog docs

--- apps/showcase/doc/dialog/responsivedoc.ts
+++ apps/showcase/doc/dialog/responsivedoc.ts
@@ -32,33 +32,19 @@ export class ResponsiveDoc {
 
     code: Code = {
         basic: `<p-button (click)="showDialog()" label="Show" />
-<p-dialog
-    header="Header"
-    [(visible)]="visible"
-    [modal]="true"
-    [breakpoints]="{ '1199px': '75vw', '575px': '90vw' }"
-    [style]="{ width: '50vw' }"
-    [draggable]="false"
-    [resizable]="false">
-        <p>
-            Lorem ipsum dolor sit amet...
-        </p>
+<p-dialog header="Header" [(visible)]="visible" [modal]="true" [breakpoints]="{ '1199px': '75vw', '575px': '90vw' }" [style]="{ width: '50vw' }" [draggable]="false" [resizable]="false">
+    <p>
+        Lorem ipsum dolor sit amet...
+    </p>
 </p-dialog>`,
 
         html: `<div class="card flex justify-center">
     <p-button (click)="showDialog()" label="Show" />
-    <p-dialog
-        header="Header"
-        [(visible)]="visible"
-        [modal]="true"
-        [breakpoints]="{ '1199px': '75vw', '575px': '90vw' }"
-        [style]="{ width: '50vw' }"
-        [draggable]="false"
-        [resizable]="false">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+    <p-dialog header="Header" [(visible)]="visible" [modal]="true" [breakpoints]="{ '1199px': '75vw', '575px': '90vw' }" [style]="{ width: '50vw' }" [draggable]="false" [resizable]="false">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
     </p-dialog>
 </div>`,
 
