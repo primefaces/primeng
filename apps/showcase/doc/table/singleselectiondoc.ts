247057112f2093f90d229eae0862ae1cffc982c1
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2024-12-25 11:37:17 +0300

    update table | singleselectiondoc

--- apps/showcase/doc/table/singleselectiondoc.ts
+++ apps/showcase/doc/table/singleselectiondoc.ts
@@ -66,29 +66,24 @@ export class SingleSelectionDoc {
 
     code: Code = {
         basic: `<p-toggleswitch [(ngModel)]="metaKey" inputId="input-metakey" />
-    <p-table
-        [value]="products"
-        selectionMode="single"
-        [(selection)]="selectedProduct"
-        [metaKeySelection]="metaKey" dataKey="id"
-        [tableStyle]="{ 'min-width': '50rem' }">
-            <ng-template #header>
-                <tr>
-                    <th>Code</th>
-                    <th>Name</th>
-                    <th>Category</th>
-                    <th>Quantity</th>
-                </tr>
-            </ng-template>
-            <ng-template #body let-product>
-                <tr [pSelectableRow]="product">
-                    <td>{{ product.code }}</td>
-                    <td>{{ product.name }}</td>
-                    <td>{{ product.category }}</td>
-                    <td>{{ product.quantity }}</td>
-                </tr>
-            </ng-template>
-    </p-table>`,
+<p-table [value]="products" selectionMode="single" [(selection)]="selectedProduct" [metaKeySelection]="metaKey" dataKey="id" [tableStyle]="{ 'min-width': '50rem' }">
+    <ng-template #header>
+        <tr>
+            <th>Code</th>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Quantity</th>
+        </tr>
+    </ng-template>
+    <ng-template #body let-product>
+        <tr [pSelectableRow]="product">
+            <td>{{ product.code }}</td>
+            <td>{{ product.name }}</td>
+            <td>{{ product.category }}</td>
+            <td>{{ product.quantity }}</td>
+        </tr>
+    </ng-template>
+</p-table>`,
 
         html: `<div class="card">
     <div class="flex justify-center items-center mb-6 gap-2">
