413ac5ba6ad0cf38127dc9ee302405f23021c6d7
Egor Boaghi <e.boaghi@developmentaid.org>
2025-01-05 13:16:05 +0200

    Add in button directive possibility to store the icon in case it was already created, allowing icon restoration after loading is complete.

--- packages/primeng/src/button/button.ts
+++ packages/primeng/src/button/button.ts
@@ -358,6 +358,11 @@ export class ButtonDirective extends BaseComponent implements AfterViewInit, OnD
 
     createIcon() {
         const created = findSingle(this.htmlElement, '.p-button-icon');
+        // store icon to allow its restoration after loading is finished.
+        if (!this.icon && created) {
+            const iconClasses: string[] = created.className.split(' ');
+            this.icon = iconClasses.filter((className: string) => !className.startsWith('p-')).join(' ');
+        }
         if (!created && (this.icon || this.loading)) {
             let iconElement = this.document.createElement('span');
             iconElement.className = 'p-button-icon';
