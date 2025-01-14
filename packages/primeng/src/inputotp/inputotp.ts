badd514326c27e27d06d5ab1408879e266d9f760
Balazs Meszegeto <mesz86@hotmail.com>
2025-01-14 21:09:02 +0100

    Remove onKeyDown event relying on KeyboardEvent.code -- Fixes #17392 issue

--- packages/primeng/src/inputotp/inputotp.ts
+++ packages/primeng/src/inputotp/inputotp.ts
@@ -378,7 +378,7 @@ export class InputOtp extends BaseComponent implements AfterContentInit {
                 break;
 
             default:
-                if ((this.integerOnly && !((event.code.startsWith('Digit') || event.code.startsWith('Numpad')) && Number(event.key) >= 0 && Number(event.key) <= 9)) || (this.tokens.join('').length >= this.length && event.code !== 'Delete')) {
+                if ((this.integerOnly && !(Number(event.key) >= 0 && Number(event.key) <= 9)) || (this.tokens.join('').length >= this.length && event.code !== 'Delete')) {
                     event.preventDefault();
                 }
 
