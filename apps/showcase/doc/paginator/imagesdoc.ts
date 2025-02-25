281fc8ce60936bfb062006d378287e12fdfc10eb
Wendelstein7 <23213464+Wendelstein7@users.noreply.github.com>
2025-02-25 09:56:43 +0100

    Update imagesdoc.ts

--- apps/showcase/doc/paginator/imagesdoc.ts
+++ apps/showcase/doc/paginator/imagesdoc.ts
@@ -22,8 +22,8 @@ export class ImagesDoc {
     rows: number = 10;
 
     onPageChange(event: PaginatorState) {
-        this.first = event.first;
-        this.rows = event.rows;
+        this.first = event.first ?? 0;
+        this.rows = event.rows ?? 10;
     }
 
     code: Code = {
@@ -50,8 +50,8 @@ export class PaginatorImagesDemo {
     rows: number = 10;
 
     onPageChange(event: PaginatorState) {
-        this.first = event.first;
-        this.rows = event.rows;
+        this.first = event.first ?? 0;
+        this.rows = event.rows ?? 10;
     }
 }`
     };
