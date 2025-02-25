b1ea92df8d56aab558c42ad83d5eb5efe094af9b
Wendelstein7 <23213464+Wendelstein7@users.noreply.github.com>
2025-02-25 09:56:06 +0100

    Update basicdoc.ts

--- apps/showcase/doc/paginator/basicdoc.ts
+++ apps/showcase/doc/paginator/basicdoc.ts
@@ -24,8 +24,8 @@ export class BasicDoc {
     rows: number = 10;
 
     onPageChange(event: PaginatorState) {
-        this.first = event.first;
-        this.rows = event.rows;
+        this.first = event.first ?? 0;
+        this.rows = event.rows ?? 10;
     }
 
     code: Code = {
@@ -50,8 +50,8 @@ export class PaginatorBasicDemo {
     rows: number = 10;
 
     onPageChange(event: PaginatorState) {
-        this.first = event.first;
-        this.rows = event.rows;
+        this.first = event.first ?? 0;
+        this.rows = event.rows ?? 10;
     }
 }`
     };
