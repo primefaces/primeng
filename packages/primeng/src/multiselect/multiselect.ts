0abfafd29dcbaf1f26231e75cf05bb51786f3610
Mike Shevchenko <mikleshef@inveritasoft.com>
2025-03-12 17:16:25 +0200

    fix: multiselect #17390

--- packages/primeng/src/multiselect/multiselect.ts
+++ packages/primeng/src/multiselect/multiselect.ts
@@ -101,7 +101,7 @@ export const MULTISELECT_VALUE_ACCESSOR: any = {
         >
             <p-checkbox [ngModel]="selected" [binary]="true" [tabindex]="-1" [variant]="variant" [ariaLabel]="label">
                 <ng-container *ngIf="itemCheckboxIconTemplate">
-                    <ng-template #icon let-klass="class">
+                    <ng-template #checkboxicon let-klass="class">
                         <ng-template *ngTemplateOutlet="itemCheckboxIconTemplate; context: { checked: selected, class: klass }"></ng-template>
                     </ng-template>
                 </ng-container>
