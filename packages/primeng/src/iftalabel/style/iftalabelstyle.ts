8da498cf70b3066e59d0f9c8ed553cace44a99ac
Ledson Silva <ledson.dev@gmail.com>
2025-03-13 10:50:33 -0300

    Fix: Inftalabel for fields datepicker, number, autocomplete, mask, password and iconfield.

--- packages/primeng/src/iftalabel/style/iftalabelstyle.ts
+++ packages/primeng/src/iftalabel/style/iftalabelstyle.ts
@@ -27,7 +27,13 @@ const theme = ({ dt }) => `
 .p-iftalabel .p-multiselect-label-container,
 .p-iftalabel .p-autocomplete-input-multiple,
 .p-iftalabel .p-cascadeselect-label,
-.p-iftalabel .p-treeselect-label {
+.p-iftalabel .p-treeselect-label,
+.p-iftalabel .p-datepicker-input,
+.p-iftalabel .p-inputmask,
+.p-iftalabel .p-autocomplete .p-inputtext,
+.p-iftalabel .p-inputnumber .p-inputtext,
+.p-iftalabel .p-password .p-inputtext,
+.p-iftalabel > .p-iconfield .p-inputtext {
     padding-top: ${dt('iftalabel.input.padding.top')};
 }
 
@@ -42,7 +48,7 @@ const theme = ({ dt }) => `
     color: ${dt('iftalabel.focus.color')};
 }
 
-.p-iftalabel > .p-inputicon {
+.p-iftalabel > .p-iconfield .p-inputicon {
     top: ${dt('iftalabel.input.padding.top')};
     transform: translateY(25%);
     margin-top: 0;
