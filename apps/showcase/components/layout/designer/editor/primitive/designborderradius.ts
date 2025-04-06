2a8953fd42000c8ccc18ca32d5bde591f94974d0
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2025-04-06 12:33:24 +0300

    refactor: refactor border-radius section

--- apps/showcase/components/layout/designer/editor/primitive/designborderradius.ts
+++ apps/showcase/components/layout/designer/editor/primitive/designborderradius.ts
@@ -12,22 +12,22 @@ import { DesignTokenField } from '@/components/layout/designer/editor/designtoke
     template: ` <p-fieldset legend="Rounded" [toggleable]="true">
         <section class="grid grid-cols-4 gap-2">
             <div class="flex flex-col gap-1">
-                <design-token-field [(modelValue)]="designerService.designer().theme.preset?.primitive.borderRadius.none" label="None" />
+                <design-token-field [(modelValue)]="borderRadiusNone" label="None" />
             </div>
             <div class="flex flex-col gap-1">
-                <design-token-field [(modelValue)]="designerService.designer().theme.preset?.primitive.borderRadius.xs" label="Extra Small" />
+                <design-token-field [(modelValue)]="borderRadiusXs" label="Extra Small" />
             </div>
             <div class="flex flex-col gap-1">
-                <design-token-field [(modelValue)]="designerService.designer().theme.preset?.primitive.borderRadius.sm" label="Small" />
+                <design-token-field [(modelValue)]="borderRadiusSm" label="Small" />
             </div>
             <div class="flex flex-col gap-1">
-                <design-token-field [(modelValue)]="designerService.designer().theme.preset?.primitive.borderRadius.md" label="Medium" />
+                <design-token-field [(modelValue)]="borderRadiusMd" label="Medium" />
             </div>
             <div class="flex flex-col gap-1">
-                <design-token-field [(modelValue)]="designerService.designer().theme.preset?.primitive.borderRadius.lg" label="Large" />
+                <design-token-field [(modelValue)]="borderRadiusLg" label="Large" />
             </div>
             <div class="flex flex-col gap-1">
-                <design-token-field [(modelValue)]="designerService.designer().theme.preset?.primitive.borderRadius.xl" label="Extra Large" />
+                <design-token-field [(modelValue)]="borderRadiusXl" label="Extra Large" />
             </div>
         </section>
     </p-fieldset>`,
@@ -35,4 +35,64 @@ import { DesignTokenField } from '@/components/layout/designer/editor/designtoke
 })
 export class DesignBorderRadius {
     designerService = inject(DesignerService);
+
+    get borderRadiusNone() {
+        return this.designerService.designer().theme?.preset?.primitive.borderRadius.none;
+    }
+    set borderRadiusNone(value: any) {
+        this.designerService.designer.update((prev) => ({
+            ...prev,
+            theme: { ...prev.theme, preset: { ...prev.theme.preset, primitive: { ...prev.theme.preset.primitive, borderRadius: { ...prev.theme.preset.primitive.borderRadius, none: value } } } }
+        }));
+    }
+
+    get borderRadiusXs() {
+        return this.designerService.designer().theme?.preset?.primitive.borderRadius.xs;
+    }
+    set borderRadiusXs(value: any) {
+        this.designerService.designer.update((prev) => ({
+            ...prev,
+            theme: { ...prev.theme, preset: { ...prev.theme.preset, primitive: { ...prev.theme.preset.primitive, borderRadius: { ...prev.theme.preset.primitive.borderRadius, xs: value } } } }
+        }));
+    }
+
+    get borderRadiusSm() {
+        return this.designerService.designer().theme?.preset?.primitive.borderRadius.sm;
+    }
+    set borderRadiusSm(value: any) {
+        this.designerService.designer.update((prev) => ({
+            ...prev,
+            theme: { ...prev.theme, preset: { ...prev.theme.preset, primitive: { ...prev.theme.preset.primitive, borderRadius: { ...prev.theme.preset.primitive.borderRadius, sm: value } } } }
+        }));
+    }
+
+    get borderRadiusMd() {
+        return this.designerService.designer().theme?.preset?.primitive.borderRadius.md;
+    }
+    set borderRadiusMd(value: any) {
+        this.designerService.designer.update((prev) => ({
+            ...prev,
+            theme: { ...prev.theme, preset: { ...prev.theme.preset, primitive: { ...prev.theme.preset.primitive, borderRadius: { ...prev.theme.preset.primitive.borderRadius, md: value } } } }
+        }));
+    }
+
+    get borderRadiusLg() {
+        return this.designerService.designer().theme?.preset?.primitive.borderRadius.lg;
+    }
+    set borderRadiusLg(value: any) {
+        this.designerService.designer.update((prev) => ({
+            ...prev,
+            theme: { ...prev.theme, preset: { ...prev.theme.preset, primitive: { ...prev.theme.preset.primitive, borderRadius: { ...prev.theme.preset.primitive.borderRadius, lg: value } } } }
+        }));
+    }
+
+    get borderRadiusXl() {
+        return this.designerService.designer().theme?.preset?.primitive.borderRadius.xl;
+    }
+    set borderRadiusXl(value: any) {
+        this.designerService.designer.update((prev) => ({
+            ...prev,
+            theme: { ...prev.theme, preset: { ...prev.theme.preset, primitive: { ...prev.theme.preset.primitive, borderRadius: { ...prev.theme.preset.primitive.borderRadius, xl: value } } } }
+        }));
+    }
 }
