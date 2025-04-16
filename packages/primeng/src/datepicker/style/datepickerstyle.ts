62eaae271519ff5b5f56a694ded5f4cf25c927d8
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-04-16 14:57:11 +0300

    fix: fix build error

--- packages/primeng/src/datepicker/style/datepickerstyle.ts
+++ packages/primeng/src/datepicker/style/datepickerstyle.ts
@@ -477,10 +477,10 @@ const classes = {
         if (instance.isRangeSelection() && instance.isSelected(date) && date.selectable) {
             const startDate = instance.value[0];
             const endDate = instance.value[1];
-            
+
             const isStart = startDate && date.year === startDate.getFullYear() && date.month === startDate.getMonth() && date.day === startDate.getDate();
             const isEnd = endDate && date.year === endDate.getFullYear() && date.month === endDate.getMonth() && date.day === endDate.getDate();
-            
+
             selectedDayClass = isStart || isEnd ? 'p-datepicker-day-selected' : 'p-datepicker-day-selected-range';
         }
 
