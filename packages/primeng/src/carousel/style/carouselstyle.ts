72b5619dff6a0afe2bc989576feab043862f58fe
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-05-05 16:29:46 +0300

    fix: carousel - add missing styleclass

--- packages/primeng/src/carousel/style/carouselstyle.ts
+++ packages/primeng/src/carousel/style/carouselstyle.ts
@@ -6,6 +6,7 @@ import { BaseStyle } from 'primeng/base';
 const classes = {
     root: ({ instance }) => [
         'p-carousel p-component',
+        instance.styleClass,
         {
             'p-carousel-vertical': instance.isVertical(),
             'p-carousel-horizontal': !instance.isVertical()
