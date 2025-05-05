65ce8d30d701d57f10c93f3fc5fc542375e2be09
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-05-05 14:11:10 +0300

    fix: #18227 - badge |  migrate to primeuix

--- packages/primeng/src/badge/style/badgestyle.ts
+++ packages/primeng/src/badge/style/badgestyle.ts
@@ -1,85 +1,11 @@
+// @ts-nocheck
 import { Injectable } from '@angular/core';
 import { isEmpty, isNotEmpty } from '@primeuix/utils';
+import { style } from '@primeuix/styles/badge';
 import { BaseStyle } from 'primeng/base';
 
 const theme = ({ dt }) => `
-.p-badge {
-    display: inline-flex;
-    border-radius: ${dt('badge.border.radius')};
-    justify-content: center;
-    padding: ${dt('badge.padding')};
-    background: ${dt('badge.primary.background')};
-    color: ${dt('badge.primary.color')};
-    font-size: ${dt('badge.font.size')};
-    font-weight: ${dt('badge.font.weight')};
-    min-width: ${dt('badge.min.width')};
-    height: ${dt('badge.height')};
-    line-height: ${dt('badge.height')};
-}
-
-.p-badge-dot {
-    width: ${dt('badge.dot.size')};
-    min-width: ${dt('badge.dot.size')};
-    height: ${dt('badge.dot.size')};
-    border-radius: 50%;
-    padding: 0;
-}
-
-.p-badge-circle {
-    padding: 0;
-    border-radius: 50%;
-}
-
-.p-badge-secondary {
-    background: ${dt('badge.secondary.background')};
-    color: ${dt('badge.secondary.color')};
-}
-
-.p-badge-success {
-    background: ${dt('badge.success.background')};
-    color: ${dt('badge.success.color')};
-}
-
-.p-badge-info {
-    background: ${dt('badge.info.background')};
-    color: ${dt('badge.info.color')};
-}
-
-.p-badge-warn {
-    background: ${dt('badge.warn.background')};
-    color: ${dt('badge.warn.color')};
-}
-
-.p-badge-danger {
-    background: ${dt('badge.danger.background')};
-    color: ${dt('badge.danger.color')};
-}
-
-.p-badge-contrast {
-    background: ${dt('badge.contrast.background')};
-    color: ${dt('badge.contrast.color')};
-}
-
-.p-badge-sm {
-    font-size: ${dt('badge.sm.font.size')};
-    min-width: ${dt('badge.sm.min.width')};
-    height: ${dt('badge.sm.height')};
-    line-height: ${dt('badge.sm.height')};
-}
-
-.p-badge-lg {
-    font-size: ${dt('badge.lg.font.size')};
-    min-width: ${dt('badge.lg.min.width')};
-    height: ${dt('badge.lg.height')};
-    line-height: ${dt('badge.lg.height')};
-}
-
-.p-badge-xl {
-    font-size: ${dt('badge.xl.font.size')};
-    min-width: ${dt('badge.xl.min.width')};
-    height: ${dt('badge.xl.height')};
-    line-height: ${dt('badge.xl.height')};
-}
+${style({ dt })}
 
 /* For PrimeNG (directive)*/
 
@@ -98,20 +24,21 @@ const theme = ({ dt }) => `
 `;
 
 const classes = {
-    root: ({ props, instance }) => [
+    root: ({ instance }) => [
         'p-badge p-component',
+        instance.styleClass,
         {
-            'p-badge-circle': isNotEmpty(props.value) && String(props.value).length === 1,
-            'p-badge-dot': isEmpty(props.value) && !instance.$slots.default,
-            'p-badge-sm': props.size === 'small',
-            'p-badge-lg': props.size === 'large',
-            'p-badge-xl': props.size === 'xlarge',
-            'p-badge-info': props.severity === 'info',
-            'p-badge-success': props.severity === 'success',
-            'p-badge-warn': props.severity === 'warn',
-            'p-badge-danger': props.severity === 'danger',
-            'p-badge-secondary': props.severity === 'secondary',
-            'p-badge-contrast': props.severity === 'contrast'
+            'p-badge-circle': isNotEmpty(instance.value()) && String(instance.value()).length === 1,
+            'p-badge-dot': isEmpty(instance.value()),
+            'p-badge-sm': instance.size() === 'small' || instance.badgeSize() === 'small',
+            'p-badge-lg': instance.size() === 'large' || instance.badgeSize() === 'large',
+            'p-badge-xl': instance.size() === 'xlarge' || instance.badgeSize() === 'xlarge',
+            'p-badge-info': instance.severity() === 'info',
+            'p-badge-success': instance.severity() === 'success',
+            'p-badge-warn': instance.severity() === 'warn',
+            'p-badge-danger': instance.severity() === 'danger',
+            'p-badge-secondary': instance.severity() === 'secondary',
+            'p-badge-contrast': instance.severity() === 'contrast'
         }
     ]
 };
