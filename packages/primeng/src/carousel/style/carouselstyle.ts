3b5ef6c4c4534eaa3f70c567438409c20d210789
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-05-05 16:22:12 +0300

    fix: #18231 - Carousel - migrate to primeuix

--- packages/primeng/src/carousel/style/carouselstyle.ts
+++ packages/primeng/src/carousel/style/carouselstyle.ts
@@ -1,107 +1,7 @@
+// @ts-nocheck
 import { Injectable } from '@angular/core';
+import { style } from '@primeuix/styles/carousel';
 import { BaseStyle } from 'primeng/base';
-
-const theme = ({ dt }) => `
-.p-carousel {
-    display: flex;
-    flex-direction: column;
-}
-
-.p-carousel-content-container {
-    display: flex;
-    flex-direction: column;
-    overflow: auto;
-}
-
-.p-carousel-content {
-    display: flex;
-    flex-direction: row;
-    gap: ${dt('carousel.content.gap')};
-}
-
-.p-carousel-content:dir(rtl) {
-    flex-direction: row-reverse;
-}
-
-.p-carousel-viewport {
-    overflow: hidden;
-    width: 100%;
-}
-
-.p-carousel-item-list {
-    display: flex;
-    flex-direction: row;
-}
-
-.p-carousel-item-list:dir(rtl) {
-    flex-direction: row-reverse;
-}
-
-.p-carousel-prev-button,
-.p-carousel-next-button {
-    align-self: center;
-    flex-shrink: 0;
-}
-
-.p-carousel-indicator-list {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    flex-wrap: wrap;
-    padding: ${dt('carousel.indicator.list.padding')};
-    gap: ${dt('carousel.indicator.list.gap')};
-    margin: 0;
-    list-style: none;
-}
-
-.p-carousel-indicator-button {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: ${dt('carousel.indicator.background')};
-    width: ${dt('carousel.indicator.width')};
-    height: ${dt('carousel.indicator.height')};
-    border: 0 none;
-    transition: background ${dt('carousel.transition.duration')}, color ${dt('carousel.transition.duration')}, outline-color ${dt('carousel.transition.duration')}, box-shadow ${dt('carousel.transition.duration')};
-    outline-color: transparent;
-    border-radius: ${dt('carousel.indicator.border.radius')};
-    padding: 0;
-    margin: 0;
-    user-select: none;
-    cursor: pointer;
-}
-
-.p-carousel-indicator-button:focus-visible {
-    box-shadow: ${dt('carousel.indicator.focus.ring.shadow')};
-    outline: ${dt('carousel.indicator.focus.ring.width')} ${dt('carousel.indicator.focus.ring.style')} ${dt('carousel.indicator.focus.ring.color')};
-    outline-offset: ${dt('carousel.indicator.focus.ring.offset')};
-}
-
-.p-carousel-indicator-button:hover {
-    background: ${dt('carousel.indicator.hover.background')};
-}
-
-.p-carousel-indicator-active .p-carousel-indicator-button {
-    background: ${dt('carousel.indicator.active.background')};
-}
-
-.p-carousel-vertical .p-carousel-content {
-    flex-direction: column;
-}
-
-.p-carousel-vertical .p-carousel-item-list {
-    flex-direction: column;
-    height: 100%;
-}
-
-.p-items-hidden .p-carousel-item {
-    visibility: hidden;
-}
-
-.p-items-hidden .p-carousel-item.p-carousel-item-active {
-    visibility: visible;
-}
-`;
 
 const classes = {
     root: ({ instance }) => [
@@ -159,7 +59,7 @@ const classes = {
 export class CarouselStyle extends BaseStyle {
     name = 'carousel';
 
-    theme = theme;
+    theme = style;
 
     classes = classes;
 }
