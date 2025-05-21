eab2f485dff18c50deac4596e74e8caaa1a2457e
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2025-05-21 14:53:00 +0300

    fix: fixes #18372, migrate tooltip to primeuix

--- packages/primeng/src/tooltip/style/tooltipstyle.ts
+++ packages/primeng/src/tooltip/style/tooltipstyle.ts
@@ -1,75 +1,35 @@
 import { Injectable } from '@angular/core';
 import { BaseStyle } from 'primeng/base';
+import { style } from '@primeuix/styles/tooltip';
+import { css } from '@primeuix/styled';
 
-const theme = ({ dt }) => `
-.p-tooltip {
-    position: absolute;
-    display: none;
-    max-width: ${dt('tooltip.max.width')};
-}
+const theme = css`
+    ${style}
+    /* For PrimeNG */
 
-.p-tooltip-right,
-.p-tooltip-left {
-    padding: 0 ${dt('tooltip.gutter')};
-}
+    .p-tooltip-arrow {
+        scale: 2;
+    }
 
-.p-tooltip-top,
-.p-tooltip-bottom {
-    padding: ${dt('tooltip.gutter')} 0;
-}
+    .p-tooltip-right .p-tooltip-arrow {
+        top: 50%;
+        left: 0;
+    }
 
-.p-tooltip-text {
-    white-space: pre-line;
-    word-break: break-word;
-    background: ${dt('tooltip.background')};
-    color: ${dt('tooltip.color')};
-    padding: ${dt('tooltip.padding')};
-    box-shadow: ${dt('tooltip.shadow')};
-    border-radius: ${dt('tooltip.border.radius')};
-}
+    .p-tooltip-left .p-tooltip-arrow {
+        top: 50%;
+        right: 0;
+    }
 
-.p-tooltip-arrow {
-    position: absolute;
-    width: 0;
-    height: 0;
-    border-color: transparent;
-    border-style: solid;
-    scale: 2;
-}
+    .p-tooltip-top .p-tooltip-arrow {
+        bottom: 0;
+        left: 50%;
+    }
 
-.p-tooltip-right .p-tooltip-arrow {
-    top: 50%;
-    left: 0;
-    margin-top: calc(-1 * ${dt('tooltip.gutter')});
-    border-width: ${dt('tooltip.gutter')} ${dt('tooltip.gutter')} ${dt('tooltip.gutter')} 0;
-    border-right-color: ${dt('tooltip.background')};
-}
-
-.p-tooltip-left .p-tooltip-arrow {
-    top: 50%;
-    right: 0;
-    margin-top: calc(-1 * ${dt('tooltip.gutter')});
-    border-width: ${dt('tooltip.gutter')} 0 ${dt('tooltip.gutter')} ${dt('tooltip.gutter')};
-    border-left-color: ${dt('tooltip.background')};
-}
-
-.p-tooltip-top .p-tooltip-arrow {
-    bottom: 0;
-    left: 50%;
-    margin-left: calc(-1 * ${dt('tooltip.gutter')});
-    border-width: ${dt('tooltip.gutter')} ${dt('tooltip.gutter')} 0 ${dt('tooltip.gutter')};
-    border-top-color: ${dt('tooltip.background')};
-    border-bottom-color: ${dt('tooltip.background')};
-}
-
-.p-tooltip-bottom .p-tooltip-arrow {
-    top: 0;
-    left: 50%;
-    margin-left: calc(-1 * ${dt('tooltip.gutter')});
-    border-width: 0 ${dt('tooltip.gutter')} ${dt('tooltip.gutter')} ${dt('tooltip.gutter')};
-    border-top-color: ${dt('tooltip.background')};
-    border-bottom-color: ${dt('tooltip.background')};
-}
+    .p-tooltip-bottom .p-tooltip-arrow {
+        top: 0;
+        left: 50%;
+    }
 `;
 
 const classes = {
