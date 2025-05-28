39652639dd8f3fa268aab5199aa9db23167f9a4e
gianluca-moro <74053868+gianluca-moro@users.noreply.github.com>
2025-05-28 13:35:48 +0200

    fix(16570): autocompete: do not clear whole input on backspace

--- packages/primeng/src/autocomplete/autocomplete.ts
+++ packages/primeng/src/autocomplete/autocomplete.ts
@@ -1510,10 +1510,6 @@ export class AutoComplete extends BaseComponent implements AfterViewChecked, Aft
 
             event.stopPropagation(); // To prevent onBackspaceKeyOnMultiple method
         }
-
-        if (!this.multiple && this.showClear && this.findSelectedOptionIndex() != -1) {
-            this.clear();
-        }
     }
 
     onArrowLeftKeyOnMultiple(event) {
