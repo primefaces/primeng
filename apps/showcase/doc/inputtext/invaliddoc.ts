daa7498406200fe5aa44af00d44689b6977c621a
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-06-02 10:21:20 +0300

    docs: update inputtext invaliddoc

--- apps/showcase/doc/inputtext/invaliddoc.ts
+++ apps/showcase/doc/inputtext/invaliddoc.ts
@@ -9,19 +9,24 @@ import { Component } from '@angular/core';
             <p>Invalid state is displayed using the <i>invalid</i> prop to indicate a failed validation. You can use this style when integrating with form validation libraries.</p>
         </app-docsectiontext>
         <div class="card flex flex-wrap justify-center gap-4">
-            <input pInputText [(ngModel)]="value" name="value" placeholder="Name" [invalid]="true" required />
+            <input pInputText [(ngModel)]="value1" [invalid]="!value1" placeholder="Name" />
+            <input pInputText [(ngModel)]="value2" [invalid]="!value2" variant="filled" placeholder="Name" />
         </div>
         <app-code [code]="code" selector="input-text-invalid-demo"></app-code>
     `
 })
 export class InvalidDoc {
-    value: string | undefined;
+    value1: string | undefined;
+
+    value2: string | undefined;
 
     code: Code = {
-        basic: `<input pInputText [(ngModel)]="value" name="value" placeholder="Name" [invalid]="true" required />`,
+        basic: `<input pInputText [(ngModel)]="value1" [invalid]="!value1" placeholder="Name" />
+<input pInputText [(ngModel)]="value2" [invalid]="!value2" variant="filled" placeholder="Name" />`,
 
         html: `<div class="card flex flex-wrap justify-center gap-4">
-    <input pInputText [(ngModel)]="value" name="value" placeholder="Name" [invalid]="true" required />
+    <input pInputText [(ngModel)]="value1" [invalid]="!value1" placeholder="Name" />
+    <input pInputText [(ngModel)]="value2" [invalid]="!value2" variant="filled" placeholder="Name" />
 </div>`,
 
         typescript: `import { Component } from '@angular/core';
