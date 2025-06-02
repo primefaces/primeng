9ad8e230f5dcc8cbad81d2bea109019cc87ee1fc
jase88 <804836+jase88@users.noreply.github.com>
2025-06-02 15:33:59 +0200

    fix(inpututils): remove transformToBoolean function and update exports

--- packages/primeng/src/utils/inpututils.ts
+++ packages/primeng/src/utils/inpututils.ts
@@ -1,7 +1,3 @@
-export const transformToBoolean = (value: any): boolean => {
-    return !!value;
-};
-
 export const transformToNumber = (value: string | number): number => {
     return typeof value === 'string' ? parseFloat(value) : value;
 };
--- packages/primeng/src/utils/public_api.ts
+++ packages/primeng/src/utils/public_api.ts
@@ -1,6 +1,6 @@
 import { ObjectUtils } from './objectutils';
 import { UniqueComponentId } from './uniquecomponentid';
 import ZIndexUtils from './zindexutils';
-import { transformToBoolean, transformToNumber } from './inpututils';
+import { transformToNumber } from './inpututils';
 
-export { ZIndexUtils, UniqueComponentId, ObjectUtils, transformToNumber, transformToBoolean };
+export { ZIndexUtils, UniqueComponentId, ObjectUtils, transformToNumber };
