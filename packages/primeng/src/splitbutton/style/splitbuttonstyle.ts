4aa042114e796336e8deff390df50569b06501d8
Cagatay Civici <cagatay.civici@gmail.com>
2025-06-23 15:50:06 +0300

    fix: fix splitbutton styles as it is derived from primeuix

--- packages/primeng/src/splitbutton/style/splitbuttonstyle.ts
+++ packages/primeng/src/splitbutton/style/splitbuttonstyle.ts
@@ -1,43 +1,6 @@
 import { Injectable } from '@angular/core';
-import { css, dt } from '@primeuix/styled';
 import { style } from '@primeuix/styles/splitbutton';
 import { BaseStyle } from 'primeng/base';
-
-const theme = css`
-    ${style}
-
-    /* For PrimeNG */
-    .p-splitbutton-button.p-button {
-        border-start-end-radius: 0;
-        border-end-end-radius: 0;
-        border-right: 0 none;
-    }
-
-    .p-splitbutton-button.p-button:focus-visible,
-    .p-splitbutton-dropdown.p-button:focus-visible {
-        z-index: 1;
-    }
-
-    .p-splitbutton-button.p-button:not(:disabled):hover,
-    .p-splitbutton-button.p-button:not(:disabled):active {
-        border-right: 0 none;
-    }
-
-    .p-splitbutton-dropdown.p-button {
-        border-start-start-radius: 0;
-        border-end-start-radius: 0;
-    }
-
-    .p-splitbutton-rounded .p-splitbutton-dropdown {
-        border-start-end-radius: ${dt('splitbutton.rounded.border.radius')};
-        border-end-end-radius: ${dt('splitbutton.rounded.border.radius')};
-    }
-
-    .p-splitbutton-rounded > .p-splitbutton-button {
-        border-start-start-radius: ${dt('splitbutton.rounded.border.radius')};
-        border-end-start-radius: ${dt('splitbutton.rounded.border.radius')};
-    }
-`;
 
 const classes = {
     root: ({ instance }) => [
@@ -58,7 +21,7 @@ const classes = {
 export class SplitButtonStyle extends BaseStyle {
     name = 'splitbutton';
 
-    theme = theme;
+    theme = style;
 
     classes = classes;
 }
