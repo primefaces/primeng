52a860e921a9a389df4cb05e1d8b53a0f312839b
Cagatay Civici <cagatay.civici@gmail.com>
2025-06-28 19:20:40 +0300

    chore: update angular link

--- apps/showcase/doc/splitbutton/templatedoc.ts
+++ apps/showcase/doc/splitbutton/templatedoc.ts
@@ -41,7 +41,7 @@ export class TemplateDoc {
                     this.delete();
                 }
             },
-            { label: 'Angular Website', url: 'http://angular.io' },
+            { label: 'Angular.dev', url: 'https://angular.dev' },
             { separator: true },
             { label: 'Upload', routerLink: ['/fileupload'] }
         ];
@@ -101,7 +101,7 @@ export class SplitButtonTemplateDemo {
                     this.delete();
                 }
             },
-            { label: 'Angular Website', url: 'http://angular.io' },
+            { label: 'Angular.dev', url: 'https://angular.dev' },
             { separator: true },
             { label: 'Upload', routerLink: ['/fileupload'] }
         ];
