620dce4bcfb5a72e3821ed07eb3bcb4b64e15b96
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-06-30 12:58:02 +0300

    fix: blockui blockable element issue

--- packages/primeng/src/blockui/style/blockuistyle.ts
+++ packages/primeng/src/blockui/style/blockuistyle.ts
@@ -1,23 +1,6 @@
 import { Injectable } from '@angular/core';
+import { style } from '@primeuix/styles/blockui';
 import { BaseStyle } from 'primeng/base';
-
-const theme = ({ dt }) => `
-.p-blockui {
-    position: relative;
-}
-
-.p-blockui-mask {
-    border-radius: ${dt('blockui.border.radius')};
-}
-
-.p-blockui-mask.p-overlay-mask {
-    position: absolute;
-}
-
-.p-blockui-mask-document.p-overlay-mask {
-    position: fixed;
-}
-`;
 
 const classes = {
     root: ({ instance }) => [
@@ -32,7 +15,7 @@ const classes = {
 export class BlockUiStyle extends BaseStyle {
     name = 'blockui';
 
-    theme = theme;
+    theme = style;
 
     classes = classes;
 }
