06d055f1eb04c72d4633f4184828fc87b8258256
Cagatay Civici <cagatay.civici@gmail.com>
2025-07-02 14:26:51 +0300

    chore: update playground

--- apps/showcase/pages/playground/index.ts
+++ apps/showcase/pages/playground/index.ts
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
                 <p>Experience PrimeNG right now with the interactive environment.</p>
             </div>
             <section class="py-6">
-                <iframe class="w-full h-full" style="border: 1px solid rgba(0, 0, 0, 0.1); border-radius: 2px; min-height: 800px" allowfullscreen src="https://stackblitz.com/edit/stackblitz-starters-qnu7pwn3?embed=1&file=src%2Fmain.ts"></iframe>
+                <iframe class="w-full h-full" style="border: 1px solid rgba(0, 0, 0, 0.1); border-radius: 2px; min-height: 800px" allowfullscreen src="https://stackblitz.com/edit/stackblitz-starters-pqtgozez?embed=1&file=src%2Fmain.ts"></iframe>
             </section>
         </div>
     </div>`
