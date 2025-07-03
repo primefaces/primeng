39f2f6f031bdef5e0098115425637d1e6e545e65
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-07-03 14:45:16 +0300

    docs: update router demos

--- apps/showcase/doc/breadcrumb/routerdoc.ts
+++ apps/showcase/doc/breadcrumb/routerdoc.ts
@@ -1,5 +1,5 @@
 import { Code } from '@/domain/code';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
 @Component({
@@ -10,67 +10,21 @@ import { MenuItem } from 'primeng/api';
             <p>Items with navigation are defined with templating to be able to use a routerLink directive, an external link or programmatic navigation.</p>
         </app-docsectiontext>
         <div class="card flex justify-center">
-            <p-breadcrumb class="max-w-full" [model]="items">
-                <ng-template #item let-item>
-                    <ng-container *ngIf="item.route; else elseBlock">
-                        <a [routerLink]="item.route" class="p-breadcrumb-item-link">
-                            <span [ngClass]="[item.icon ? item.icon : '', 'text-color']"></span>
-                            <span class="text-primary font-semibold">{{ item.label }}</span>
-                        </a>
-                    </ng-container>
-                    <ng-template #elseBlock>
-                        <a [href]="item.url">
-                            <span class="text-color">{{ item.label }}</span>
-                        </a>
-                    </ng-template>
-                </ng-template>
-            </p-breadcrumb>
+            <p-breadcrumb [home]="home" [model]="items" />
         </div>
         <app-code [code]="code" selector="breadcrumb-router-demo"></app-code>
     `
 })
-export class RouterDoc implements OnInit {
-    items: MenuItem[] | undefined;
+export class RouterDoc {
+    items: MenuItem[] = [{ label: 'Components' }, { label: 'Form' }, { label: 'InputText', routerLink: '/inputtext' }];
 
-    home: MenuItem | undefined;
-
-    ngOnInit() {
-        this.items = [{ icon: 'pi pi-home', route: '/installation' }, { label: 'Components' }, { label: 'Form' }, { label: 'InputText', route: '/inputtext' }];
-    }
+    home: MenuItem = { icon: 'pi pi-home', routerLink: '/' };
 
     code: Code = {
-        basic: `<p-breadcrumb class="max-w-full" [model]="items">
-    <ng-template #item let-item>
-        <ng-container *ngIf="item.route; else elseBlock">
-            <a [routerLink]="item.route" class="p-breadcrumb-item-link">
-                <span [ngClass]="[item.icon ? item.icon : '', 'text-color']"></span>
-                <span class="text-primary font-semibold">{{ item.label }}</span>
-            </a>
-        </ng-container>
-        <ng-template #elseBlock>
-            <a [href]="item.url">
-                <span class="text-color">{{ item.label }}</span>
-            </a>
-        </ng-template>
-    </ng-template>
-</p-breadcrumb>`,
+        basic: `<p-breadcrumb [home]="home" [model]="items" />`,
 
         html: `<div class="card flex justify-center">
-    <p-breadcrumb class="max-w-full" [model]="items">
-        <ng-template #item let-item>
-            <ng-container *ngIf="item.route; else elseBlock">
-                <a [routerLink]="item.route" class="p-breadcrumb-item-link">
-                    <span [ngClass]="[item.icon ? item.icon : '', 'text-color']"></span>
-                    <span class="text-primary font-semibold">{{ item.label }}</span>
-                </a>
-            </ng-container>
-            <ng-template #elseBlock>
-                <a [href]="item.url">
-                    <span class="text-color">{{ item.label }}</span>
-                </a>
-            </ng-template>
-        </ng-template>
-    </p-breadcrumb>
+    <p-breadcrumb [home]="home" [model]="items" />
 </div>`,
 
         typescript: `import { Component, OnInit } from '@angular/core';
@@ -84,7 +38,7 @@ import { RouterModule } from '@angular/router';
     standalone: true,
     imports: [Breadcrumb, RouterModule]
 })
-export class BreadcrumbRouterDemo implements OnInit {
+export class BreadcrumbRouterDemo {
     items: MenuItem[] | undefined;
 
     home: MenuItem | undefined;
