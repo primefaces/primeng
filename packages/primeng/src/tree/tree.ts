fdefdb1ad00d07a24fb96186ac45a8a9e417b8f7
Tuomola Taneli (Ruokavirasto) <taneli.tuomola@ruokavirasto.fi>
2025-07-28 15:53:00 +0300

    fix(#17463): add togglerAriaLabel input

--- packages/primeng/src/tree/tree.ts
+++ packages/primeng/src/tree/tree.ts
@@ -100,7 +100,7 @@ import {
                     (dragstart)="onDragStart($event)"
                     (dragend)="onDragStop($event)"
                 >
-                    <button type="button" [attr.data-pc-section]="'toggler'" [class]="cx('nodeToggleButton')" (click)="toggle($event)" pRipple tabindex="-1">
+                    <button type="button" [attr.aria-label]="togglerAriaLabel" [attr.data-pc-section]="'toggler'" [class]="cx('nodeToggleButton')" (click)="toggle($event)" pRipple tabindex="-1">
                         <ng-container *ngIf="!tree.togglerIconTemplate && !tree._togglerIconTemplate">
                             <ng-container *ngIf="!node.loading">
                                 <svg data-p-icon="chevron-right" *ngIf="!node.expanded" [class]="cx('nodeToggleIcon')" />
@@ -162,6 +162,7 @@ import {
                         [itemSize]="itemSize"
                         [level]="level + 1"
                         [loadingMode]="loadingMode"
+                        [togglerAriaLabel]="togglerAriaLabel"
                     ></p-treeNode>
                 </ul>
             </li>
@@ -204,6 +205,8 @@ export class UITreeNode extends BaseComponent implements OnInit {
     @Input({ transform: numberAttribute }) itemSize: number | undefined;
 
     @Input() loadingMode: string;
+
+    @Input() togglerAriaLabel: string;
 
     tree: Tree = inject(forwardRef(() => Tree));
 
@@ -773,6 +776,7 @@ export class UITreeNode extends BaseComponent implements OnInit {
                             [itemSize]="scrollerOptions.itemSize"
                             [indentation]="indentation"
                             [loadingMode]="loadingMode"
+                            [togglerAriaLabel]="togglerAriaLabel"
                         ></p-treeNode>
                     </ul>
                 </ng-template>
@@ -793,6 +797,7 @@ export class UITreeNode extends BaseComponent implements OnInit {
                             [index]="index"
                             [level]="0"
                             [loadingMode]="loadingMode"
+                            [togglerAriaLabel]="togglerAriaLabel"
                         ></p-treeNode>
                     </ul>
                 </div>
