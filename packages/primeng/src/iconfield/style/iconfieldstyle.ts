347c711ce254ca91ecb5062e53f6939eb606587a
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-08-26 14:31:43 +0300

    fix: #18804

--- packages/primeng/src/iconfield/style/iconfieldstyle.ts
+++ packages/primeng/src/iconfield/style/iconfieldstyle.ts
@@ -2,6 +2,20 @@ import { Injectable } from '@angular/core';
 import { style } from '@primeuix/styles/iconfield';
 import { BaseStyle } from 'primeng/base';
 
+const theme = /*css*/ `
+    ${style}
+
+    /* For PrimeNG */
+    .p-iconfield p-inputmask:not(:first-child) .p-inputtext {
+        padding-inline-start: calc((dt('form.field.padding.x') * 2) + dt('icon.size'));
+    }
+
+    .p-iconfield p-inputmask:not(:last-child) .p-inputtext {
+        padding-inline-end: calc((dt('form.field.padding.x') * 2) + dt('icon.size'));
+    }
+  
+`;
+
 const classes = {
     root: ({ instance }) => [
         'p-iconfield',
@@ -16,7 +30,7 @@ const classes = {
 export class IconFieldStyle extends BaseStyle {
     name = 'iconfield';
 
-    theme = style;
+    theme = theme;
 
     classes = classes;
 }
