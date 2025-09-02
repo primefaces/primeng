255f0cc14851a8acfca75b01ce760cfd94420075
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-09-02 15:53:08 +0300

    chore: cascadeselect docs |  migrate to standalone component

--- apps/showcase/doc/cascadeselect/sizesdoc.ts
+++ apps/showcase/doc/cascadeselect/sizesdoc.ts
@@ -1,9 +1,14 @@
 import { Code } from '@/domain/code';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CascadeSelectModule } from 'primeng/cascadeselect';
+import { AppCode } from '@/components/doc/app.code';
+import { AppDocSectionText } from '@/components/doc/app.docsectiontext';
 
 @Component({
     selector: 'cascade-select-sizes-demo',
-    standalone: false,
+    standalone: true,
+    imports: [FormsModule, CascadeSelectModule, AppCode, AppDocSectionText],
     template: `
         <app-docsectiontext>
             <p>CascadeSelect provides <i>small</i> and <i>large</i> sizes as alternatives to the base.</p>
