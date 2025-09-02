8c0425f5bb62fe6ebaec8d44a644b095cc1fd0a4
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-09-02 14:40:51 +0300

    chore: autofocus docs | migrate to standalone component

--- apps/showcase/pages/autofocus/index.ts
+++ apps/showcase/pages/autofocus/index.ts
@@ -1,11 +1,13 @@
-import { AutoFocusDocModule } from '@/doc/autofocus/autofocusdoc.module';
 import { BasicDoc } from '@/doc/autofocus/basicdoc';
 import { ImportDoc } from '@/doc/autofocus/importdoc';
+import { AppDoc } from '@/components/doc/app.doc';
+import { AppDocService } from '@/components/doc/app.doc.service';
 import { Component } from '@angular/core';
 
 @Component({
     standalone: true,
-    imports: [AutoFocusDocModule],
+    imports: [AppDoc],
+    providers: [AppDocService],
     template: ` <app-doc docTitle="Angular AutoFocus Directive" header="AutoFocus" description="AutoFocus manages focus on focusable element on load." [docs]="docs" [apiDocs]="['AutoFocus']"></app-doc>`
 })
 export class AutoFocusDemo {
