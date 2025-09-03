3e08cf9b1699539a6996c7460b1f1d9b2b132c67
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-09-03 13:44:53 +0300

    chore: focustrap docs |  migrate to standalone component

--- apps/showcase/pages/focustrap/index.ts
+++ apps/showcase/pages/focustrap/index.ts
@@ -1,12 +1,14 @@
 import { BasicDoc } from '@/doc/focustrap/basicdoc';
-import { FocusTrapDocModule } from '@/doc/focustrap/focustrapdoc.module';
 import { ImportDoc } from '@/doc/focustrap/importdoc';
 import { Component } from '@angular/core';
+import { AppDoc } from '@/components/doc/app.doc';
+import { AppDocService } from '@/components/doc/app.doc.service';
 
 @Component({
     template: ` <app-doc docTitle="Angular Focus Trap Component" header="Focus Trap" description="Focus Trap keeps focus within a certain DOM element while tabbing." [docs]="docs" [apiDocs]="['FocusTrap']"></app-doc> `,
     standalone: true,
-    imports: [FocusTrapDocModule]
+    imports: [AppDoc],
+    providers: [AppDocService]
 })
 export class FocusTrapDemo {
     docs = [
