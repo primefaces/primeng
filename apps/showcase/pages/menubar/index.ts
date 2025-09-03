7c91abf182165b24c4e0c778a98c8d116f7471b3
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-09-03 16:23:05 +0300

    chore: menubar docs |  migrate to standalone component

--- apps/showcase/pages/menubar/index.ts
+++ apps/showcase/pages/menubar/index.ts
@@ -2,15 +2,17 @@ import { AccessibilityDoc } from '@/doc/menubar/accessibilitydoc';
 import { BasicDoc } from '@/doc/menubar/basicdoc';
 import { CommandDoc } from '@/doc/menubar/commanddoc';
 import { ImportDoc } from '@/doc/menubar/importdoc';
-import { MenubarDocModule } from '@/doc/menubar/menubardoc.module';
 import { RouterDoc } from '@/doc/menubar/routerdoc';
 import { TemplateDoc } from '@/doc/menubar/templatedoc';
 import { Component } from '@angular/core';
+import { AppDoc } from '@/components/doc/app.doc';
+import { AppDocService } from '@/components/doc/app.doc.service';
 
 @Component({
     template: `<app-doc docTitle="Angular Menubar Component" header="Menubar" description="Menubar is a horizontal menu component." [docs]="docs" [apiDocs]="['Menubar', 'MenuItem']" themeDocs="menubar"></app-doc> `,
     standalone: true,
-    imports: [MenubarDocModule]
+    imports: [AppDoc],
+    providers: [AppDocService]
 })
 export class MenubarDemo {
     docs = [
@@ -39,7 +41,6 @@ export class MenubarDemo {
             label: 'Router',
             component: RouterDoc
         },
-
         {
             id: 'accessibility',
             label: 'Accessibility',
