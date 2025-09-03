d5407073b441b70e82c24e9ebe23832bac595f6a
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-09-03 15:17:07 +0300

    chore: imagecompare docs |  migrate to standalone component

--- apps/showcase/pages/imagecompare/index.ts
+++ apps/showcase/pages/imagecompare/index.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { ImportDoc } from '../../doc/imagecompare/importdoc';
-import { ImageCompareDocModule } from '../../doc/imagecompare/imagecomparedoc.module';
 import { BasicDoc } from '../../doc/imagecompare/basicdoc';
 import { AccessibilityDoc } from '../../doc/imagecompare/accessibilitydoc';
 import { ResponsiveDoc } from '@/doc/imagecompare/responsivedoc';
+import { AppDoc } from '@/components/doc/app.doc';
+import { AppDocService } from '@/components/doc/app.doc.service';
 
 @Component({
     template: `<app-doc docTitle="Angular ImageCompare Component" header="ImageCompare" description="Compare two images side by side with a slider." [docs]="docs" [apiDocs]="['ImageCompare']" themeDocs="imagecompare"></app-doc> `,
     standalone: true,
-    imports: [ImageCompareDocModule]
+    imports: [AppDoc],
+    providers: [AppDocService]
 })
 export class ImageCompareDemo {
     docs = [
