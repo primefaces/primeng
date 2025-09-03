411d0a9414b34d0c8acc69a5ec1d09a696d27e03
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-09-03 15:36:02 +0300

    chore: inputmask docs |  migrate to standalone component

--- apps/showcase/pages/inputmask/index.ts
+++ apps/showcase/pages/inputmask/index.ts
@@ -5,7 +5,6 @@ import { FilledDoc } from '@/doc/inputmask/filleddoc';
 import { FloatlabelDoc } from '@/doc/inputmask/floatlabeldoc';
 import { IftaLabelDoc } from '@/doc/inputmask/iftalabeldoc';
 import { ImportDoc } from '@/doc/inputmask/importdoc';
-import { InputMaskDocModule } from '@/doc/inputmask/inputmaskdoc.module';
 import { InvalidDoc } from '@/doc/inputmask/invaliddoc';
 import { MaskDoc } from '@/doc/inputmask/maskdoc';
 import { OptionalDoc } from '@/doc/inputmask/optionaldoc';
@@ -14,6 +13,8 @@ import { SizesDoc } from '@/doc/inputmask/sizesdoc';
 import { SlotCharDoc } from '@/doc/inputmask/slotchardoc';
 import { TemplateDrivenFormsDoc } from '@/doc/inputmask/templatedrivenformsdoc';
 import { Component } from '@angular/core';
+import { AppDoc } from '@/components/doc/app.doc';
+import { AppDocService } from '@/components/doc/app.doc.service';
 
 @Component({
     template: `<app-doc
@@ -25,7 +26,8 @@ import { Component } from '@angular/core';
         themeDocs="inputmask"
     ></app-doc> `,
     standalone: true,
-    imports: [InputMaskDocModule]
+    imports: [AppDoc],
+    providers: [AppDocService]
 })
 export class InputMaskDemo {
     docs = [
