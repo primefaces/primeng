e2fee857918c2ad68a5920483376ddfa32225b54
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-09-03 12:53:24 +0300

    chore: dock docs |  migrate to standalone component

--- apps/showcase/pages/dock/index.ts
+++ apps/showcase/pages/dock/index.ts
@@ -1,14 +1,16 @@
 import { AccessibilityDoc } from '@/doc/dock/accessibilitydoc';
 import { AdvancedDoc } from '@/doc/dock/advanceddoc';
 import { BasicDoc } from '@/doc/dock/basicdoc';
-import { DockDocModule } from '@/doc/dock/dockdoc.module';
 import { ImportDoc } from '@/doc/dock/importdoc';
 import { Component } from '@angular/core';
+import { AppDoc } from '@/components/doc/app.doc';
+import { AppDocService } from '@/components/doc/app.doc.service';
 
 @Component({
     standalone: true,
-    imports: [DockDocModule],
-    template: ` <app-doc docTitle="Angular Dock Component" header="Dock" description="Dock is a navigation component consisting of menuitems." [docs]="docs" [apiDocs]="['Dock', 'MenuItem']" themeDocs="dock"></app-doc> `
+    imports: [AppDoc],
+    template: ` <app-doc docTitle="Angular Dock Component" header="Dock" description="Dock is a navigation component consisting of menuitems." [docs]="docs" [apiDocs]="['Dock', 'MenuItem']" themeDocs="dock"></app-doc> `,
+    providers: [AppDocService]
 })
 export class DockDemo {
     docs = [
@@ -27,7 +29,6 @@ export class DockDemo {
             label: 'Advanced',
             component: AdvancedDoc
         },
-
         {
             id: 'accessibility',
             label: 'Accessibility',
