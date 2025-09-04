239ea737fa926357f55ec426a77304d6444eb53e
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-09-04 16:01:16 +0300

    chore: terminal docs |  migrate to standalone component

--- apps/showcase/pages/terminal/index.ts
+++ apps/showcase/pages/terminal/index.ts
@@ -1,13 +1,15 @@
 import { AccessibilityDoc } from '@/doc/terminal/accessibilitydoc';
 import { BasicDoc } from '@/doc/terminal/basicdoc';
 import { ImportDoc } from '@/doc/terminal/importdoc';
-import { TerminalDocModule } from '@/doc/terminal/terminaldoc.module';
 import { Component } from '@angular/core';
+import { AppDoc } from '@/components/doc/app.doc';
+import { AppDocService } from '@/components/doc/app.doc.service';
 
 @Component({
     template: `<app-doc docTitle="Angular Terminal Component" header="Terminal" description='Terminal is a text based user interface. Enter "date" to display the current date.' [docs]="docs" [apiDocs]="['Terminal']" themeDocs="terminal"></app-doc> `,
     standalone: true,
-    imports: [TerminalDocModule]
+    imports: [AppDoc],
+    providers: [AppDocService]
 })
 export class TerminalDemo {
     docs = [
