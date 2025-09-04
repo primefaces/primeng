ce78c3f6fc1c4bff4f90027e7920231274603237
Kyle Andres <KAndres@jhacorp.com>
2025-09-04 09:12:32 -0500

    Stops characters from being shifted to the left on InputMask

--- packages/primeng/src/inputmask/inputmask.ts
+++ packages/primeng/src/inputmask/inputmask.ts
@@ -658,6 +658,12 @@ export class InputMask extends BaseInput implements OnInit, AfterContentInit {
                 this.buffer[i] = this.getPlaceholder(i);
                 while (pos++ < test.length) {
                     c = test.charAt(pos - 1);
+                    if (c === this.getPlaceholder(i)) {
+                        lastMatch = i;
+                        if (!this.keepBuffer) {
+                            i = this.len; // Empty slot. Stop processing.
+                        }
+                    }
                     if (this.tests[i].test(c)) {
                         if (!this.keepBuffer) {
                             this.buffer[i] = c;
