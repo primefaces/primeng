fc5f4013df4e82d12ac09bea2deed867c57eca2a
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-09-04 09:03:42 +0300

    chore: ripple docs |  migrate to standalone component

--- apps/showcase/pages/ripple/index.ts
+++ apps/showcase/pages/ripple/index.ts
@@ -2,14 +2,16 @@ import { AccessibilityDoc } from '@/doc/ripple/accessibilitydoc';
 import { CustomDoc } from '@/doc/ripple/customdoc';
 import { DefaultDoc } from '@/doc/ripple/defaultdoc';
 import { ImportDoc } from '@/doc/ripple/importdoc';
-import { RippleDocModule } from '@/doc/ripple/rippledoc.module';
 import { Component } from '@angular/core';
+import { AppDoc } from '@/components/doc/app.doc';
+import { AppDocService } from '@/components/doc/app.doc.service';
 
 @Component({
     template: `<app-doc docTitle="Angular Ripple Component" header="Ripple" description="Ripple directive adds ripple effect to the host element." [docs]="docs" themeDocs="ripple"></app-doc>`,
     standalone: true,
-    imports: [RippleDocModule],
-    styleUrl: './rippledemo.scss'
+    imports: [AppDoc],
+    styleUrl: './rippledemo.scss',
+    providers: [AppDocService]
 })
 export class RippleDemo {
     docs = [
@@ -28,7 +30,6 @@ export class RippleDemo {
             label: 'Custom',
             component: CustomDoc
         },
-
         {
             id: 'accessibility',
             label: 'Accessibility',
