d8a396a11229085abee1596058c919f001dea6c6
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-09-04 10:10:39 +0300

    chore: skeleton docs |  migrate to standalone component

--- apps/showcase/pages/skeleton/index.ts
+++ apps/showcase/pages/skeleton/index.ts
@@ -4,14 +4,16 @@ import { DataTableDoc } from '@/doc/skeleton/datatabledoc';
 import { ImportDoc } from '@/doc/skeleton/importdoc';
 import { ListDoc } from '@/doc/skeleton/listdoc';
 import { ShapesDoc } from '@/doc/skeleton/shapesdoc';
-import { SkeletonDocModule } from '@/doc/skeleton/skeletondoc.module';
+import { AppDoc } from '@/components/doc/app.doc';
 import { Component } from '@angular/core';
+import { AppDocService } from '@/components/doc/app.doc.service';
 
 @Component({
     template: `<app-doc docTitle="Angular Skeleton Component" header="Skeleton" description="Skeleton is a placeholder to display instead of the actual content." [docs]="docs" [apiDocs]="['Skeleton']" themeDocs="skeleton"></app-doc>`,
     standalone: true,
-    imports: [SkeletonDocModule],
-    styleUrl: './skeletondemo.scss'
+    imports: [AppDoc],
+    styleUrl: './skeletondemo.scss',
+    providers: [AppDocService]
 })
 export class SkeletonDemo {
     docs = [
