1672244a38960ff114c3373fea6ec60c3b392014
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-09-05 09:51:00 +0300

    chore: treeselect docs |  migrate to standalone component

--- apps/showcase/pages/treeselect/index.ts
+++ apps/showcase/pages/treeselect/index.ts
@@ -14,9 +14,10 @@ import { ReactiveFormsDoc } from '@/doc/treeselect/reactiveformsdoc';
 import { SizesDoc } from '@/doc/treeselect/sizesdoc';
 import { TemplateDoc } from '@/doc/treeselect/templatedoc';
 import { TemplateDrivenFormsDoc } from '@/doc/treeselect/templatedrivenformsdoc';
-import { TreeSelectDocModule } from '@/doc/treeselect/treeselectdoc.module';
 import { VirtualScrollDoc } from '@/doc/treeselect/virtualscrolldoc';
 import { Component } from '@angular/core';
+import { AppDoc } from '@/components/doc/app.doc';
+import { AppDocService } from '@/components/doc/app.doc.service';
 
 @Component({
     template: `<app-doc
@@ -28,7 +29,8 @@ import { Component } from '@angular/core';
         themeDocs="treeselect"
     ></app-doc>`,
     standalone: true,
-    imports: [TreeSelectDocModule]
+    imports: [AppDoc],
+    providers: [AppDocService]
 })
 export class TreeSelectDemo {
     docs = [
