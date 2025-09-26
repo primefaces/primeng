bd4b77605084ea261715ddf1954b17418eba7034
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2025-09-26 12:46:52 +0300

    fix: refactor typo

--- apps/showcase/pages/pclass/index.ts
+++ apps/showcase/pages/pclass/index.ts
@@ -4,13 +4,7 @@ import { ImportDoc } from '@/doc/pclass/importdoc';
 import { Component } from '@angular/core';
 
 @Component({
-    template: `<app-doc
-        docTitle="Angular ClassNames Directive"
-        header="pClass"
-        description="ClassNames provides extended class binding functionality that is not possible with the native Angular directives."
-        [docs]="docs"
-        [apiDocs]="['PClass']"
-    ></app-doc>`,
+    template: `<app-doc docTitle="Angular Class Directive" header="pClass" description="pClass provides extended class binding functionality that is not possible with the native Angular directives." [docs]="docs" [apiDocs]="['PClass']"></app-doc>`,
     standalone: true,
     imports: [AppDoc]
 })
