af268c19bf8b2b8ad3e7d1638cfcf44614813ee4
Mert Sincan <sincan.mert@gmail.com>
2025-10-02 12:31:03 +0100

    refactor: lifecycle hooks and event naming
    Introduces a custom Lifecycle interface to replace Angular lifecycle types in BaseComponent, providing overridable methods for subclasses.

--- packages/primeng/src/basecomponent/basecomponent.types.ts
+++ packages/primeng/src/basecomponent/basecomponent.types.ts
@@ -1,5 +1,46 @@
-import type { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, DoCheck, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import type { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
-export type NgLifecycle = OnInit & OnChanges & DoCheck & AfterContentInit & AfterContentChecked & AfterViewInit & AfterViewChecked & OnDestroy;
+export interface Lifecycle {
+    /**
+     * Simulates Angular's ngOnInit hook.
+     * @see {@link OnInit#ngOnInit}
+     */
+    onInit(): void;
+    /**
+     * Simulates Angular's ngOnChanges hook.
+     * @see {@link OnChanges#ngOnChanges}
+     */
+    onChanges(changes: SimpleChanges): void;
+    /**
+     * Simulates Angular's ngDoCheck hook.
+     * @see {@link DoCheck#ngDoCheck}
+     */
+    onDoCheck(): void;
+    /**
+     * Simulates Angular's ngOnDestroy hook.
+     * @see {@link OnDestroy#ngOnDestroy}
+     */
+    onDestroy(): void;
+    /**
+     * Simulates Angular's ngAfterContentInit hook.
+     * @see {@link AfterContentInit#ngAfterContentInit}
+     */
+    onAfterContentInit(): void;
+    /**
+     * Simulates Angular's ngAfterContentChecked hook.
+     * @see {@link AfterContentChecked#ngAfterContentChecked}
+     */
+    onAfterContentChecked(): void;
+    /**
+     * Simulates Angular's ngAfterViewInit hook.
+     * @see {@link AfterViewInit#ngAfterViewInit}
+     */
+    onAfterViewInit(): void;
+    /**
+     * Simulates Angular's ngAfterViewChecked hook.
+     * @see {@link AfterViewChecked#ngAfterViewChecked}
+     */
+    onAfterViewChecked(): void;
+}
 
-export type LifecycleHooks = 'onInit' | 'onChanges' | 'doCheck' | 'onDestroy' | 'onAfterContentInit' | 'onAfterContentChecked' | 'onAfterViewInit' | 'onAfterViewChecked';
+export type LifecycleHooks = 'onBeforeInit' | keyof Lifecycle;
