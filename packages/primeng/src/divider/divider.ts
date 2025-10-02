bde0edc0ccf30fc4ef456e7da413c7dcef94560b
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-10-02 13:11:41 +0300

    chore: add divider pt

--- packages/primeng/src/divider/divider.ts
+++ packages/primeng/src/divider/divider.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, Input, NgModule, ViewEncapsulation } from '@angular/core';
+import { AfterViewChecked, ChangeDetectionStrategy, Component, inject, InjectionToken, Input, NgModule, ViewEncapsulation } from '@angular/core';
 import { SharedModule } from 'primeng/api';
-import { BaseComponent } from 'primeng/basecomponent';
+import { BaseComponent, PARENT_INSTANCE } from 'primeng/basecomponent';
+import { Bind } from 'primeng/pbind';
 import { DividerStyle } from './style/dividerstyle';
+
+const DIVIDER_INSTANCE = new InjectionToken<Divider>('DIVIDER_INSTANCE');
 
 /**
  * Divider is used to separate contents.
@@ -11,9 +14,9 @@ import { DividerStyle } from './style/dividerstyle';
 @Component({
     selector: 'p-divider',
     standalone: true,
-    imports: [CommonModule, SharedModule],
+    imports: [CommonModule, SharedModule, Bind],
     template: `
-        <div [class]="cx('content')">
+        <div [pBind]="ptm('content')" [class]="cx('content')">
             <ng-content></ng-content>
         </div>
     `,
@@ -21,14 +24,21 @@ import { DividerStyle } from './style/dividerstyle';
     encapsulation: ViewEncapsulation.None,
     host: {
         '[attr.aria-orientation]': 'layout',
-        'data-pc-name': 'divider',
         role: 'separator',
         '[class]': "cn(cx('root'), styleClass)",
         '[style]': "sx('root')"
     },
-    providers: [DividerStyle]
+    providers: [DividerStyle, { provide: DIVIDER_INSTANCE, useExisting: Divider }, { provide: PARENT_INSTANCE, useExisting: Divider }],
+    hostDirectives: [Bind]
 })
-export class Divider extends BaseComponent {
+export class Divider extends BaseComponent implements AfterViewChecked {
+    $pcDivider: Divider | undefined = inject(DIVIDER_INSTANCE, { optional: true, skipSelf: true }) ?? undefined;
+
+    bindDirectiveInstance = inject(Bind, { self: true });
+
+    ngAfterViewChecked(): void {
+        this.bindDirectiveInstance.setAttrs(this.ptms(['host', 'root']));
+    }
     /**
      * Style class of the component.
      * @deprecated since v20.0.0, use `class` instead.
@@ -55,7 +65,7 @@ export class Divider extends BaseComponent {
 }
 
 @NgModule({
-    imports: [Divider],
-    exports: [Divider]
+    imports: [Divider, Bind],
+    exports: [Divider, Bind]
 })
 export class DividerModule {}
