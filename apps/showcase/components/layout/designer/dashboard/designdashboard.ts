7bd21b845eccd7bc086561b92bce2832d1636d4f
Cagatay Civici <cagatay.civici@gmail.com>
2025-10-05 12:14:29 +0300

    chore: enable designer

--- apps/showcase/components/layout/designer/dashboard/designdashboard.ts
+++ apps/showcase/components/layout/designer/dashboard/designdashboard.ts
@@ -18,12 +18,8 @@ import { ToastModule } from 'primeng/toast';
                 <img src="https://primefaces.org/cdn/designer/{{ isDarkTheme() ? 'editor-intro-dark.png' : 'editor-intro.png' }}" />
             </a>
         </div>
-        <div class="text-lg font-semibold mb-2">System Maintenance</div>
-        <p>We're currently performing scheduled maintenance on our system to launch our new PrimeUI Store application that hosts the Theme Designer APIs.</p>
-        <p>We apologize for any inconvenience and expect to be back online by 05 Oct 2025, 18:00 UTC.</p>
-        <p>Thank you for your patience. We're excited to introduce you to our improved platform!</p>
 
-        <!-- <div class="text-lg font-semibold mb-2">Authenticate</div>
+        <div class="text-lg font-semibold mb-2">Authenticate</div>
         <div *ngIf="!verified()">
             <span class="block leading-6 mb-4"
                 >Theme Designer is the ultimate tool to customize and design your own themes featuring a visual editor, figma to theme code, cloud storage, and migration assistant. <a routerLink="/designer" class="doc-link">Discover</a> more about
@@ -92,7 +88,7 @@ import { ToastModule } from 'primeng/toast';
                     <i class="pi pi-ellipsis-h !text-xs"></i>
                 </button>
             </div>
-        </div>-->
+        </div>
         <p-menu #menu [model]="themeOptions" [popup]="true" appendTo="body" (onShow)="onMenuShow()" (onHide)="onMenuHide()" />`,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
