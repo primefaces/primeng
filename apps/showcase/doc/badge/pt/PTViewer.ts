9176d61f5a14f455816a6be71b82e7e3ae820db1
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-10-08 15:46:30 +0300

    refactor: badge pt viewer

--- apps/showcase/doc/badge/pt/PTViewer.ts
+++ apps/showcase/doc/badge/pt/PTViewer.ts
@@ -2,19 +2,19 @@ import { AppDocPtViewer, getPTOptions } from '@/components/doc/app.docptviewer';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { BadgeModule } from 'primeng/badge';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
 
 @Component({
     selector: 'badge-pt-viewer',
     standalone: true,
-    imports: [CommonModule, AppDocPtViewer, BadgeModule],
+    imports: [CommonModule, AppDocPtViewer, BadgeModule, OverlayBadgeModule],
     template: `
         <app-docptviewer [docs]="docs">
             <div class="flex flex-wrap gap-8">
                 <p-badge value="2"></p-badge>
-                <p-badge value="8" severity="success"></p-badge>
-                <p-badge value="4" severity="info"></p-badge>
-                <p-badge value="12" severity="warn"></p-badge>
-                <p-badge value="3" severity="danger"></p-badge>
+                <p-overlaybadge value="3">
+                    <i class="pi pi-bell" style="font-size: 2rem"></i>
+                </p-overlaybadge>
             </div>
         </app-docptviewer>
     `
@@ -24,6 +24,10 @@ export class PTViewer {
         {
             data: getPTOptions('Badge'),
             key: 'Badge'
+        },
+        {
+            data: getPTOptions('OverlayBadge'),
+            key: 'OverlayBadge'
         }
     ];
 }
