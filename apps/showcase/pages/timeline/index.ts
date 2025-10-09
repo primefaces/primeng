f91d6a765029cc9fef3901c64bfe273b69b78769
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-10-09 09:55:22 +0300

    feat: add timeline pt

--- apps/showcase/pages/timeline/index.ts
+++ apps/showcase/pages/timeline/index.ts
@@ -5,16 +5,19 @@ import { HorizontalDoc } from '@/doc/timeline/horizontaldoc';
 import { ImportDoc } from '@/doc/timeline/importdoc';
 import { OppositeDoc } from '@/doc/timeline/oppositedoc';
 import { TemplateDoc } from '@/doc/timeline/templatedoc';
+import { PTComponent } from '@/doc/timeline/pt/PTComponent';
 import { Component } from '@angular/core';
 import { AppDoc } from '@/components/doc/app.doc';
 
 @Component({
-    template: `<app-doc docTitle="Angular Timeline Component" header="Timeline" description="Timeline visualizes a series of chained events." [docs]="docs" [apiDocs]="['Timeline']" themeDocs="timeline"></app-doc>`,
+    template: `<app-doc docTitle="Angular Timeline Component" header="Timeline" description="Timeline visualizes a series of chained events." [docs]="docs" [apiDocs]="['Timeline']" [ptDocs]="ptComponent" themeDocs="timeline"></app-doc>`,
     standalone: true,
     imports: [AppDoc],
     styleUrl: './timelinedemo.scss'
 })
 export class TimelineDemo {
+    ptComponent = PTComponent;
+
     docs = [
         {
             id: 'import',
