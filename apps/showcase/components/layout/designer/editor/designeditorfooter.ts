27d2314f843742f6ad692692aca4fd1ee047c68c
Cagatay Civici <cagatay.civici@gmail.com>
2025-10-14 11:20:52 +0300

    chore: update designer to support ci themes

--- apps/showcase/components/layout/designer/editor/designeditorfooter.ts
+++ apps/showcase/components/layout/designer/editor/designeditorfooter.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { DesignerService } from '@/service/designerservice';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
 @Component({
     standalone: true,
@@ -8,7 +8,7 @@ import { CommonModule } from '@angular/common';
     imports: [CommonModule],
     template: `<div class="flex justify-end gap-2">
         <button type="button" (click)="download()" class="btn-design-outlined">Download</button>
-        <button type="button" (click)="apply()" class="btn-design">Apply</button>
+        <button type="button" (click)="apply()" class="btn-design" [disabled]="designerService.isThemeViewOnly()">Apply</button>
     </div>`,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
--- apps/showcase/service/designerservice.ts
+++ apps/showcase/service/designerservice.ts
@@ -10,6 +10,7 @@ export interface Theme {
     name: string;
     preset: any;
     config: any;
+    origin: string;
 }
 
 export interface Designer {
@@ -44,7 +45,8 @@ export class DesignerService {
             key: null,
             name: null,
             preset: null,
-            config: null
+            config: null,
+            origin: null
         },
         acTokens: [],
         themes: []
@@ -73,6 +75,8 @@ export class DesignerService {
     newPreset = signal<any>(null);
 
     figmaData = signal<any>(null);
+
+    isThemeViewOnly = computed(() => this.designer().theme.origin !== 'web');
 
     openDashboard() {
         this.designer.update((prev) => ({ ...prev, activeView: 'dashboard' }));
@@ -163,7 +167,8 @@ export class DesignerService {
                             key: null,
                             name: null,
                             preset: null,
-                            config: null
+                            config: null,
+                            origin: null
                         },
                         acTokens: [],
                         themes: []
@@ -196,7 +201,7 @@ export class DesignerService {
     }
 
     async saveTheme(theme: any) {
-        if (this.designer().verified) {
+        if (this.designer().verified && theme.origin === 'web') {
             const url = `${this.baseUrl}/theme/update`;
 
             const body = {
@@ -377,7 +382,7 @@ export class DesignerService {
     }
 
     async renameTheme(theme: any) {
-        if (theme.t_name && theme.t_name.trim().length > 0) {
+        if (theme.t_name && theme.t_name.trim().length > 0 && theme.t_origin === 'web') {
             const url = `${this.baseUrl}/theme/rename/${theme.t_key}`;
             const options = {
                 withCredentials: true,
@@ -441,7 +446,8 @@ export class DesignerService {
                 config: {
                     font_size: '14px',
                     font_family: 'Inter var'
-                }
+                },
+                origin: 'web'
             }
         }));
         this.missingTokens.set([]);
@@ -475,11 +481,11 @@ export class DesignerService {
                     if (res.error) {
                         this.messageService.add({ key: 'designer', severity: 'error', summary: 'An error occurred', detail: res.error.message, life: 3000 });
                     } else {
-                        const data = res.data;
-                        if (data.lostAndFound?.length) {
+                        this.loadThemeEditor(res.data.t_key, res.data.t_preset);
+
+                        if (res.data.lostAndFound?.length) {
                             this.messageService.add({ key: 'designer', severity: 'warn', summary: 'Warning', detail: 'There are missing tokens. An update is recommended using the "Migration Assistant" in the settings section.', life: 3000 });
                         }
-                        this.designer.update((prev) => ({ ...prev, activeTab: 0, activeView: 'dashboard' }));
                     }
                 },
                 error: (err: any) => {
@@ -498,7 +504,13 @@ export class DesignerService {
         this.designer.update((prev) => ({
             ...prev,
             active: true,
-            theme: { key: data.t_key, name: data.t_name, preset: typeof data.t_preset === 'string' ? JSON.parse(data.t_preset) : data.t_preset, config: typeof data.t_config === 'string' ? JSON.parse(data.t_config) : data.t_config }
+            theme: {
+                key: data.t_key,
+                name: data.t_name,
+                preset: typeof data.t_preset === 'string' ? JSON.parse(data.t_preset) : data.t_preset,
+                config: typeof data.t_config === 'string' ? JSON.parse(data.t_config) : data.t_config,
+                origin: data.t_origin
+            }
         }));
 
         usePreset(this.designer().theme.preset);
