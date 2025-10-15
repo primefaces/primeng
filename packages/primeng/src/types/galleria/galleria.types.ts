53c240e7ebb9c92cb6a08b43f9b5b66b084e7a12
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-10-15 14:04:30 +0300

    refactor: pt types

--- packages/primeng/src/types/galleria/galleria.types.ts
+++ packages/primeng/src/types/galleria/galleria.types.ts
@@ -1,101 +1,12 @@
 import type { PassThrough, PassThroughOption } from 'primeng/api';
 import { TemplateRef } from '@angular/core';
-
-/**
- * Responsive options of the component.
- * @group Interface
- */
-export interface GalleriaResponsiveOptions {
-    /**
-     * Breakpoint for responsive mode. Exp; @media screen and (max-width: ${breakpoint}) {...}
-     */
-    breakpoint: string;
-    /**
-     * The number of visible items on breakpoint.
-     */
-    numVisible: number;
-}
-
-/**
- * Defines valid templates in Galleria.
- * @group Templates
- */
-export interface GalleriaTemplates {
-    /**
-     * Custom template of header.
-     */
-    header(): TemplateRef<any>;
-    /**
-     * Custom template of footer.
-     */
-    footer(): TemplateRef<any>;
-    /**
-     * Custom template of indicator.
-     * @param {Object} context - indicator data.
-     */
-    indicator(context: {
-        /**
-         * Index of the item.
-         */
-        $implicit: any;
-    }): TemplateRef<{ $implicit: any }>;
-    /**
-     * Custom template of closeicon.
-     */
-    closeicon(): TemplateRef<any>;
-    /**
-     * Custom template of itemnexticon.
-     */
-    itemnexticon(): TemplateRef<any>;
-    /**
-     * Custom template of itempreviousicon.
-     */
-    itempreviousicon(): TemplateRef<any>;
-    /**
-     * Custom template of previousthumbnailicon.
-     */
-    previousthumbnailicon(): TemplateRef<any>;
-    /**
-     * Custom template of nextthumbnailicon.
-     */
-    nextthumbnailicon(): TemplateRef<any>;
-    /**
-     * Custom template of caption.
-     * @param {Object} context - item data.
-     */
-    caption(context: {
-        /**
-         * Item instance.
-         */
-        $implicit: any;
-    }): TemplateRef<{ $implicit: any }>;
-    /**
-     * Custom template of thumbnail.
-     * @param {Object} context - item data.
-     */
-    thumbnail(context: {
-        /**
-         * Item instance.
-         */
-        $implicit: any;
-    }): TemplateRef<{ $implicit: any }>;
-    /**
-     * Custom template of item.
-     * @param {Object} context - item data.
-     */
-    item(context: {
-        /**
-         * Item instance.
-         */
-        $implicit: any;
-    }): TemplateRef<{ $implicit: any }>;
-}
+import type { Galleria } from 'primeng/galleria';
 
 /**
  * Custom pass-through(pt) options.
  * @template I Type of instance.
  *
- * @see {@link GalleriaProps.pt}
+ * @see {@link Galleria.pt}
  * @group Interface
  */
 export interface GalleriaPassThroughOptions<I = unknown> {
@@ -220,3 +131,93 @@ export interface GalleriaPassThroughOptions<I = unknown> {
  * @template I Type of instance.
  */
 export type GalleriaPassThrough<I = unknown> = PassThrough<I, GalleriaPassThroughOptions<I>>;
+
+/**
+ * Responsive options of the component.
+ * @group Interface
+ */
+export interface GalleriaResponsiveOptions {
+    /**
+     * Breakpoint for responsive mode. Exp; @media screen and (max-width: ${breakpoint}) {...}
+     */
+    breakpoint: string;
+    /**
+     * The number of visible items on breakpoint.
+     */
+    numVisible: number;
+}
+
+/**
+ * Defines valid templates in Galleria.
+ * @group Templates
+ */
+export interface GalleriaTemplates {
+    /**
+     * Custom template of header.
+     */
+    header(): TemplateRef<any>;
+    /**
+     * Custom template of footer.
+     */
+    footer(): TemplateRef<any>;
+    /**
+     * Custom template of indicator.
+     * @param {Object} context - indicator data.
+     */
+    indicator(context: {
+        /**
+         * Index of the item.
+         */
+        $implicit: any;
+    }): TemplateRef<{ $implicit: any }>;
+    /**
+     * Custom template of closeicon.
+     */
+    closeicon(): TemplateRef<any>;
+    /**
+     * Custom template of itemnexticon.
+     */
+    itemnexticon(): TemplateRef<any>;
+    /**
+     * Custom template of itempreviousicon.
+     */
+    itempreviousicon(): TemplateRef<any>;
+    /**
+     * Custom template of previousthumbnailicon.
+     */
+    previousthumbnailicon(): TemplateRef<any>;
+    /**
+     * Custom template of nextthumbnailicon.
+     */
+    nextthumbnailicon(): TemplateRef<any>;
+    /**
+     * Custom template of caption.
+     * @param {Object} context - item data.
+     */
+    caption(context: {
+        /**
+         * Item instance.
+         */
+        $implicit: any;
+    }): TemplateRef<{ $implicit: any }>;
+    /**
+     * Custom template of thumbnail.
+     * @param {Object} context - item data.
+     */
+    thumbnail(context: {
+        /**
+         * Item instance.
+         */
+        $implicit: any;
+    }): TemplateRef<{ $implicit: any }>;
+    /**
+     * Custom template of item.
+     * @param {Object} context - item data.
+     */
+    item(context: {
+        /**
+         * Item instance.
+         */
+        $implicit: any;
+    }): TemplateRef<{ $implicit: any }>;
+}
