b2104d7d38f4d80dd525c442a8687883e1a47fec
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2025-10-21 14:13:57 +0300

    docs: add new documentation route

--- apps/showcase/pages/guides/routes.ts
+++ apps/showcase/pages/guides/routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { AccessibilityDemoComponent } from './accessibility/accessibilitydemo';
+import { PassThroughDemoComponent } from './passthrough/passthroughdemo';
 import { PrimeFlexDemoComponent } from './primeflex/primeflexdemo';
 import { RTLDemoComponent } from './rtl/rtldemo';
 import { TemplateUpdateDemoComponent } from './templateupdate/templateupdatedemo.component';
-import { PassThroughDemoComponent } from './passthrough/passthroughdemo';
 
 export default [
     { path: '', redirectTo: 'accessibility', pathMatch: 'full' },
@@ -11,5 +11,6 @@ export default [
     { path: 'passthrough', component: PassThroughDemoComponent },
     { path: 'templateupdate', component: TemplateUpdateDemoComponent },
     { path: 'primeflex', component: PrimeFlexDemoComponent },
-    { path: 'rtl', component: RTLDemoComponent }
+    { path: 'rtl', component: RTLDemoComponent },
+    { path: 'passthrough', component: PassThroughDemoComponent }
 ] satisfies Routes;
