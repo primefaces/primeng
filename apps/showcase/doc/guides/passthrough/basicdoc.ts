12a049430467415004c0070317abe21a34a4dcaa
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2025-10-21 13:58:10 +0300

    chore: update pt docs

--- apps/showcase/doc/guides/passthrough/basicdoc.ts
+++ apps/showcase/doc/guides/passthrough/basicdoc.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
-import { AppDocSectionText } from '@/components/doc/app.docsectiontext';
-import { PanelModule } from 'primeng/panel';
 import { AppCode } from '@/components/doc/app.code';
+import { AppDocSectionText } from '@/components/doc/app.docsectiontext';
 import { Code } from '@/domain/code';
+import { Component } from '@angular/core';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
     selector: 'basic-doc',
--- apps/showcase/pages/guides/passthrough/passthroughdemo.ts
+++ apps/showcase/pages/guides/passthrough/passthroughdemo.ts
@@ -1,15 +1,18 @@
 import { AppDoc } from '@/components/doc/app.doc';
 import { BasicDoc } from '@/doc/guides/passthrough/basicdoc';
+import { CustomCssDoc } from '@/doc/guides/passthrough/customcssdoc';
 import { GlobalDoc } from '@/doc/guides/passthrough/globaldoc';
 import { IntroductionDoc } from '@/doc/guides/passthrough/introductiondoc';
+import { LifeCycleDoc } from '@/doc/guides/passthrough/lifecycledoc';
 import { PcPrefixDoc } from '@/doc/guides/passthrough/pcprefixdoc';
+import { UsePassThroughDoc } from '@/doc/guides/passthrough/usepassthroughdoc';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'passthrough',
     standalone: true,
     imports: [AppDoc],
-    template: ` <app-doc docTitle="Pass Through - PrimeNG" header="Pass Through" description="The Pass Through attributes is an API to access the internal DOM Structure of the components." [docs]="docs"></app-doc> `
+    template: ` <app-doc docTitle="PrimeNG - Pass Through" header="Pass Through" description="The Pass Through attributes is an API to access the internal DOM Structure of the components." [docs]="docs"></app-doc> `
 })
 export class PassThroughDemoComponent {
     docs = [
@@ -29,9 +32,24 @@ export class PassThroughDemoComponent {
             component: PcPrefixDoc
         },
         {
+            id: 'life-cycle',
+            label: 'Lifecycle',
+            component: LifeCycleDoc
+        },
+        {
             id: 'global',
             label: 'Global',
             component: GlobalDoc
+        },
+        {
+            id: 'custom-css',
+            label: 'Custom CSS',
+            component: CustomCssDoc
+        },
+        {
+            id: 'use-pt',
+            label: 'UsePassThrough',
+            component: UsePassThroughDoc
         }
     ];
 }
