4b43df676fdd5bbced1fbfa8a127463109ed885b
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-11-03 16:35:18 +0300

    refactor: #19036 - skeleton

--- packages/primeng/src/skeleton/skeleton.ts
+++ packages/primeng/src/skeleton/skeleton.ts
@@ -23,7 +23,8 @@ const SKELETON_INSTANCE = new InjectionToken<Skeleton>('SKELETON_INSTANCE');
     host: {
         '[attr.aria-hidden]': 'true',
         '[class]': "cn(cx('root'), styleClass)",
-        '[style]': 'containerStyle'
+        '[style]': 'containerStyle',
+        '[attr.data-p]': 'dataP'
     },
     hostDirectives: [Bind]
 })
@@ -82,6 +83,12 @@ export class Skeleton extends BaseComponent<SkeletonPassThrough> {
         else style = { ...inlineStyles, width: this.width, height: this.height, borderRadius: this.borderRadius };
 
         return style;
+    }
+
+    get dataP() {
+        return this.cn({
+            [this.shape]: this.shape
+        });
     }
 }
 
