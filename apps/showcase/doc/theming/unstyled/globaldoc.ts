71f841035688d0c4f5230d15a2a97e4279a8a36a
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-11-07 11:39:59 +0300

    chore: update globaldoc

--- apps/showcase/doc/theming/unstyled/globaldoc.ts
+++ apps/showcase/doc/theming/unstyled/globaldoc.ts
@@ -30,13 +30,7 @@ export const appConfig: ApplicationConfig = {
                 button: {
                     root: 'bg-teal-500 hover:bg-teal-700 active:bg-teal-900 cursor-pointer py-2 px-4 rounded-full border-0 flex gap-2',
                     label: 'text-white font-bold text-lg',
-                    icon: 'text-white text-xl'
-                },
-                panel: {
-                    header: 'bg-primary text-primary-contrast border-primary',
-                    content: 'border-primary text-lg text-primary-700',
-                    title: 'bg-primary text-primary-contrast text-xl',
-                    toggler: 'bg-primary text-primary-contrast hover:text-primary hover:bg-primary-contrast'
+                    icon: 'text-white !text-xl'
                 }
             }
         })
