d0e1916b00e5e68c4336a62f6f77c7a398ba65b2
Cagatay Civici <cagatay.civici@gmail.com>
2025-12-02 10:20:37 +0300

    chore: removed provideAnimationsAsync

--- apps/showcase/doc/theming/styled/themedoc.ts
+++ apps/showcase/doc/theming/styled/themedoc.ts
@@ -17,13 +17,11 @@ import { Component } from '@angular/core';
 export class ThemeDoc {
     code: Code = {
         typescript: `import { ApplicationConfig } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideAnimationsAsync(),
         providePrimeNG({
             theme: {
                 preset: Aura
