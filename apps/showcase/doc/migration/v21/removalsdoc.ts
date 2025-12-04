600b859f1d6d3086eadbf9c8f4db0163cbbc7492
Cagatay Civici <cagatay.civici@gmail.com>
2025-12-04 15:48:06 +0300

    chore: fixed link

--- apps/showcase/doc/migration/v21/removalsdoc.ts
+++ apps/showcase/doc/migration/v21/removalsdoc.ts
@@ -9,7 +9,7 @@ import { TagModule } from 'primeng/tag';
     imports: [TagModule, AppDocSectionText, RouterModule],
     template: `
         <app-docsectiontext>
-            <p>This version contains no API removals. For a list of APIs scheduled for removal in v22, refer to the <a routerLink="/theming/unstyled" class="text-primary font-medium hover:underline">v20 deprecations</a> section.</p>
+            <p>This version contains no API removals. For a list of APIs scheduled for removal in v22, refer to the <a routerLink="/migration/v20" class="text-primary font-medium hover:underline">v20 deprecations</a> section.</p>
         </app-docsectiontext>
     `
 })
