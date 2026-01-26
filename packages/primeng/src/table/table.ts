b3a010137f05b116e82e754879a930b37f79ddb1
S1m0zK <givemesoftwarepls@gmail.com>
2026-01-26 20:03:34 +0100

    fix for undefined overwriting default value in p-table for pagination

--- packages/primeng/src/table/table.ts
+++ packages/primeng/src/table/table.ts
@@ -2971,12 +2971,12 @@ export class Table<RowData = any> extends BaseComponent<TablePassThrough> implem
             let state: TableState = JSON.parse(stateString, reviver);
 
             if (this.paginator) {
-                if (this.first) {
+                if (this.first !== undefined && state.first) {
                     this.first = state.first;
                     this.firstChange.emit(this.first);
                 }
 
-                if (this.rows) {
+                if (this.rows !== undefined && state.rows) {
                     this.rows = state.rows;
                     this.rowsChange.emit(this.rows);
                 }
