ae23431871fbaf7258a2595d1bc229dbad1e9798
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2026-02-20 12:37:43 +0300

    refactor: extract Accordion sub-components into separate files

--- packages/primeng/src/accordion/public_api.ts
+++ packages/primeng/src/accordion/public_api.ts
@@ -3,5 +3,4 @@ export * from './accordion';
 export * from './accordion-content';
 export * from './accordion-header';
 export * from './accordion-panel';
-export * from './accordion-token';
 export * from './style/accordionstyle';
